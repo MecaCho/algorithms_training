d6fd21a86d5d4dcd9f89f0b43957d75330c93e54
MecaCho <1099833328@qq.com>
2020-02-28 17:52:07 +0800

    Update leetcode-008-myAtoi.py

--- algorithms/leetcode-008-myAtoi.py
+++ algorithms/leetcode-008-myAtoi.py
@@ -1,1 +1,8 @@
-leetcode-008-myAtoi
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        num = int(*re.findall("^[\-\+]?\d+", str.strip(" ")))
+        return min(2**31 -1, max(-2**31, num))
