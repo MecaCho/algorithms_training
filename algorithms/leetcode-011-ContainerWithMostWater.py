a96da05fcdc9f7f10b0cd2c90e413f309de807b5
MecaCho <1099833328@qq.com>
2020-03-01 00:10:49 +0800

    Update leetcode-011-ContainerWithMostWater.py

--- algorithms/leetcode-011-ContainerWithMostWater.py
+++ algorithms/leetcode-011-ContainerWithMostWater.py
@@ -1,1 +1,40 @@
-leetcode-011-ContainerWithMostWater
+```
+11. Container With Most Water
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example:
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+ 
+
+11. 盛最多水的容器
+给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+示例:
+输入: [1,8,6,2,5,4,8,3,7]
+输出: 49
+```
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        i = 0
+        j = len(height) -1
+        max_area = 0
+        while i < j:
+            max_area = max(max_area, min(height[i], height[j]) * (j - i))
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return max_area
+     
