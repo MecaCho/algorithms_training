cf9366866e959ff990f04384787d9a5b0b3f2ca7
MecaCho <1099833328@qq.com>
2020-03-05 22:49:50 +0800

    Update leetcode-015-3Sum.py

--- algorithms/leetcode-015-3Sum.py
+++ algorithms/leetcode-015-3Sum.py
@@ -35,9 +35,40 @@ A solution set is:
 
 ```
 class Solution(object):
+    def twoSum(self, nums, target):
+        tg_map = {}
+        res = []
+        for num in nums:
+            if num in tg_map:
+                res.append([num, target - num])
+            else:
+                tg_map[target - num] = num
+        return res
+
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        length = len(nums)
+        res = []
+        res_dict = {}
+        if length < 3:
+            return []
+        if len(set(nums)) == 1:
+            if nums[0] == 0:
+                return [[0,0,0]]
+            return []
+        for i in range(length):
+            j = i + 1
+            target = 0 - nums[i]
+            tg_list = self.twoSum(nums[j:], target)
+            for tg in tg_list:
+                new_ = sorted([nums[i], tg[0], tg[1]])
+                key = "".join([str(x) for x in new_])
+                res_dict[key] = new_
+        for k, value in res_dict.items():
+            res.append(value)
+
+        return res
 ```
