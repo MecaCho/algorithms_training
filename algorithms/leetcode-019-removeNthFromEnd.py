88c53b2468b94cf6a5026e95b5b4d778b30eafc4
MecaCho <1099833328@qq.com>
2020-03-06 23:03:27 +0800

    Update leetcode-019-removeNthFromEnd.py

--- algorithms/leetcode-019-removeNthFromEnd.py
+++ algorithms/leetcode-019-removeNthFromEnd.py
@@ -61,3 +61,34 @@ class Solution(object):
             first = first.next
         last.next = last.next.next
         return head
+    
+    
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        res = ListNode(0)
+        res.next = head
+        first = res
+        last = res
+        for i in range(n + 1):
+            first = first.next
+
+        while first:
+            last = last.next
+            first = first.next
+
+        last.next = last.next.next
+
+        return res.next
+'''
