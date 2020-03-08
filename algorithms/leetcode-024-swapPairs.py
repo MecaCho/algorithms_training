929db7b7cbf623dcb3fb1329e23a1abdef4840ea
MecaCho <1099833328@qq.com>
2020-03-08 20:27:54 +0800

    Create leetcode-024-swapPairs.py

---
+++ algorithms/leetcode-024-swapPairs.py
@@ -0,0 +1,52 @@
+'''
+24. Swap Nodes in Pairs
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+24. 两两交换链表中的节点
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        first = ListNode(0)
+        first.next = head
+        res = first
+        second = head
+        cur = head
+        while cur and cur.next:
+            cur = cur.next.next
+
+            first.next = second.next
+            first.next.next = second
+            second.next = cur
+
+            first = first.next.next
+            second = second.next
+
+        return res.next
