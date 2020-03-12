991777e730019a8b7e6697154cbdd4c543a717f3
MecaCho <1099833328@qq.com>
2020-03-12 10:36:35 +0800

    Create leetcode-1071-gcdOfStrings.py

---
+++ algorithms/leetcode-1071-gcdOfStrings.py
@@ -0,0 +1,76 @@
+‘’‘
+1071. Greatest Common Divisor of Strings
+For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T concatenated with itself 1 or more times)
+
+Return the largest string X such that X divides str1 and X divides str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+
+Note:
+
+1 <= str1.length <= 1000
+1 <= str2.length <= 1000
+str1[i] and str2[i] are English uppercase letters.
+
+1071. 字符串的最大公因子
+对于字符串 S 和 T，只有在 S = T + ... + T（T 与自身连接 1 次或多次）时，我们才认定 “T 能除尽 S”。
+
+返回最长字符串 X，要求满足 X 能除尽 str1 且 X 能除尽 str2。
+
+ 
+
+示例 1：
+
+输入：str1 = "ABCABC", str2 = "ABC"
+输出："ABC"
+示例 2：
+
+输入：str1 = "ABABAB", str2 = "ABAB"
+输出："AB"
+示例 3：
+
+输入：str1 = "LEET", str2 = "CODE"
+输出：""
+ 
+
+提示：
+
+1 <= str1.length <= 1000
+1 <= str2.length <= 1000
+str1[i] 和 str2[i] 为大写英文字母
+’‘’
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        len1 = len(str1)
+        len2 = len(str2)
+        def gcd(m, n):
+            if n == 0:
+                return m
+            else:
+                return gcd(n, m % n)
+        gcd_num = gcd(len2, len1)
+
+        if str1 + str2 == str2 + str1:
+            return str1[:gcd_num]
+        else:
+            return ""
