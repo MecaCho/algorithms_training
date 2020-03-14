8ff0587d02cc28c98877d3c6a13abedd19eb6565
MecaCho <1099833328@qq.com>
2020-03-14 15:10:51 +0800

    add leetcode minDepth of tree;

---
+++ algorithms/leetcode-111-minDepth.py
@@ -0,0 +1,63 @@
+'''
+
+111. Minimum Depth of Binary Tree
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.
+
+111. 二叉树的最小深度
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最小深度  2.
+'''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def deep_trave(root):
+            if not root:
+                return 0
+            if not root.left:
+                return deep_trave(root.right) + 1
+            if not root.right:
+                return deep_trave(root.left) + 1
+            return min(deep_trave(root.left), deep_trave(root.right)) + 1
+        return deep_trave(root)
