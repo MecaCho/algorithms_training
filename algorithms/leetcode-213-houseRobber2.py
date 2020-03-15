137b892bf7d1ab1e32219ee89ce9180bf9a81f8d
MecaCho <1099833328@qq.com>
2020-03-15 22:23:14 +0800

    add leetcode house rob 2;

---
+++ algorithms/leetcode-213-houseRobber2.py
@@ -0,0 +1,61 @@
+'''
+
+213. 打家劫舍 II
+你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+
+示例 1:
+
+输入: [2,3,2]
+输出: 3
+解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+示例 2:
+
+输入: [1,2,3,1]
+输出: 4
+解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+     
+213. House Robber II
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+             because they are adjacent houses.
+Example 2:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+'''
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        def new_rob(nums):
+            if not nums:
+                return 0
+            dp = [[0, nums[0]]]
+            for i in range(1, len(nums)):
+                dp.append([0, 0])
+                dp[i][0] = max(dp[i-1])
+                dp[i][1] = max(dp[i-1][0] + nums[i], dp[i-1][1])
+            return max(dp[-1])
+        length = len(nums)
+        if length == 0:
+            return 0
+        elif length == 1:
+            return nums[0]
+        return max(new_rob(nums[1:]), new_rob(nums[:length-1]))
