82fefb42018ccb736be1cf067bee4b4498e32cda
MecaCho <1099833328@qq.com>
2020-03-18 01:41:54 +0800

    add leetcode Pascal's Triangle II;

---
+++ algorithms/leetcode-119-PascalTriangleII.py
@@ -0,0 +1,47 @@
+'''
+
+119. 杨辉三角 II
+给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 3
+输出: [1,3,3,1]
+进阶：
+
+你可以优化你的算法到 O(k) 空间复杂度吗？
+
+
+119. Pascal's Triangle II
+Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+'''
+
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        if rowIndex == 0:
+            return [1]
+        else:
+            val_list = [0] + self.getRow(rowIndex - 1) + [0]
+            return [val_list[i] + val_list[i+1] for i in range(len(val_list) - 1)]
