3ce8921c40eec096375ce9d4e3b54ea47cc5230c
MecaCho <1099833328@qq.com>
2020-03-19 11:24:46 +0800

    add leetcode CheckPermutation;

---
+++ algorithms/leetcode-I0102-CheckPermutationLCCI.py
@@ -0,0 +1,45 @@
+'''
+
+面试题 01.02. 判定是否互为字符重排
+给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+
+示例 1：
+
+输入: s1 = "abc", s2 = "bca"
+输出: true 
+示例 2：
+
+输入: s1 = "abc", s2 = "bad"
+输出: false
+说明：
+
+0 <= len(s1) <= 100
+0 <= len(s2) <= 100
+
+面试题 01.02. Check Permutation LCCI
+Given two strings,write a method to decide if one is a permutation of the other.
+
+Example 1:
+
+Input: s1 = "abc", s2 = "bca"
+Output: true
+Example 2:
+
+Input: s1 = "abc", s2 = "bad"
+Output: false
+Note:
+
+0 <= len(s1) <= 100
+0 <= len(s2) <= 100
+'''
+
+
+class Solution(object):
+    def CheckPermutation(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        return set(s1) == set(s2)
+        # return set(list(s1)) == set(list(s2))
