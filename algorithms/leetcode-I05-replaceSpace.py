335f2cba44efe76cc7804e1fb8fe227780432e33
MecaCho <1099833328@qq.com>
2020-03-19 11:03:41 +0800

    add leetcode replaceSpace;

---
+++ algorithms/leetcode-I05-replaceSpace.py
@@ -0,0 +1,26 @@
+'''
+面试题05. 替换空格
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+ 
+
+示例 1：
+
+输入：s = "We are happy."
+输出："We%20are%20happy."
+ 
+
+限制：
+
+0 <= s 的长度 <= 10000
+'''
+
+
+
+class Solution(object):
+    def replaceSpace(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.replace(" ", "%20")
--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -10,17 +10,17 @@ def qrt(n):
         count += 1
         mid = float((l+r)/2)
         abs_ = abs(float(mid * mid - n))
-        print l, r, abs_
+        print(l, r, abs_)
         if abs_ < 0.0001:
             return mid
         elif mid * mid > n:
             r = mid
         elif mid * mid < n:
             l = mid
-    print "count :{}".format(count)
+    print("count :{}".format(count))
 
 
 if __name__ == '__main__':
-    print qrt(100)
+    print(qrt(100))
     # print qrt(98)
 
