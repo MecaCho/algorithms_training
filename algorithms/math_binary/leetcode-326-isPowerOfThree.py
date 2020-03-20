7fab1f2f64e180a1add80362b1ca83b8ec9bd309
MecaCho <1099833328@qq.com>
2020-03-21 00:28:47 +0800

    add leetcode isPowerOfThree;

---
+++ algorithms/math_binary/leetcode-326-isPowerOfThree.py
@@ -0,0 +1,59 @@
+'''
+326. Power of Three
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+Follow up:
+Could you do it without using any loop / recursion?
+
+326. 3的幂
+给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+
+示例 1:
+
+输入: 27
+输出: true
+示例 2:
+
+输入: 0
+输出: false
+示例 3:
+
+输入: 9
+输出: true
+示例 4:
+
+输入: 45
+输出: false
+进阶：
+你能不使用循环或者递归来完成本题吗？
+'''
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        while n:
+            if n == 1:
+                return True
+            if n % 3 != 0:
+                return False
+            n /= 3
+        return False
