f7facc7bf3085bb24dd505e33949965de4640338
MecaCho <1099833328@qq.com>
2020-03-21 01:32:48 +0800

    add leetcode 454 fourSumCount;;

---
+++ algorithms/hash/leetcode-454-fourSumCount.go
@@ -0,0 +1,61 @@
+package hash
+//454. 四数相加 II
+//给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+//
+//为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
+//
+//例如:
+//
+//输入:
+//A = [ 1, 2]
+//B = [-2,-1]
+//C = [-1, 2]
+//D = [ 0, 2]
+//
+//输出:
+//2
+//
+//解释:
+//两个元组如下:
+//1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+//2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
+//454. 4Sum II
+//Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+//
+//To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+//
+//Example:
+//
+//Input:
+//A = [ 1, 2]
+//B = [-2,-1]
+//C = [-1, 2]
+//D = [ 0, 2]
+//
+//Output:
+//2
+//
+//Explanation:
+//The two tuples are:
+//1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+//2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
+
+func fourSumCount(A []int, B []int, C []int, D []int) int {
+    hashMap := make(map[int]int, len(A) * len(B))
+    for _,a := range A{
+        for _,b := range B{
+            hashMap[a+b] ++
+        }
+    }
+    count := 0
+    for _,c := range C{
+        for _,d := range D{
+            if num,ok := hashMap[-(c+d)];ok{
+                count += num
+            }
+        }
+    }
+    return count
+}
