c33b4fd9e029921cc3ce4c2803cee45b18125eec
MecaCho <1099833328@qq.com>
2020-03-25 21:10:27 +0800

    add leetcode quick_sort;

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -1,4 +1,40 @@
 
+import random
+import time
+
+
+def log_time(func):
+    def wrap(*args):
+        s_time = time.time()
+        ret = func(*args)
+        print time.time() - s_time
+        return ret
+    return wrap
+
+
+quick_sort_lambda = lambda arr: arr if not arr else quick_sort_lambda([key for key in arr if key < arr[0]]) + [arr[0]] + quick_sort_lambda([key for key in arr if key > arr[0]])
+
+
+def quick_sort(arr):
+    if arr:
+        left_arr, right_arr, mid_arr = [], [], []
+        for key in arr:
+            if key < arr[0]:
+                left_arr.append(key)
+            elif key > arr[0]:
+                right_arr.append(key)
+            else:
+                mid_arr.append(key)
+        # left_arr = [key for key in arr if key < arr[0]]
+        # right_arr = [key for key in arr if key > arr[0]]
+        return quick_sort(left_arr) + mid_arr + quick_sort(right_arr)
+    return arr
+
+
+@log_time
+def log_quick_sort(arr):
+    return quick_sort(arr)
+
 
 class Solution(object):
     def quick_sort(self, nums):
@@ -26,3 +62,11 @@ class Solution(object):
 if __name__ == '__main__':
         demo = Solution()
         print(demo.quick_sort([1,3,4,10,20, 3,9]))
+
+        a = b = c = []
+        c = ["abc"]
+        print(a, b, c)
+        samples = [random.randint(1, 100) for i in range(100)]
+        print(log_quick_sort(samples))
+        print(quick_sort_lambda(samples))
+        print(quick_sort([1, 6, 10, 9, 7, 100, 100]))
