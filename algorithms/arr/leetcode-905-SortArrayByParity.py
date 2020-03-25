7c359896edc418b2e0dc6329d8b805ed805ca1c9
MecaCho <1099833328@qq.com>
2020-03-26 02:45:47 +0800

    add leetcode Sort Array By Parity;

---
+++ algorithms/arr/leetcode-905-SortArrayByParity.py
@@ -0,0 +1,57 @@
+'''
+905. 按奇偶排序数组
+给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
+
+你可以返回满足此条件的任何数组作为答案。
+
+ 
+
+示例：
+
+输入：[3,1,2,4]
+输出：[2,4,3,1]
+输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
+ 
+
+提示：
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+
+905. Sort Array By Parity
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ 
+
+Note:
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+'''
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        i, j = 0, len(A) -1
+        while i < j:
+            if A[i] % 2 == 1 and A[j] % 2 == 0:
+                A[i], A[j] = A[j], A[i]
+                i += 1
+                j -= 1
+            elif A[i] % 2 == 0:
+                i += 1
+            elif A[j] % 2 == 1:
+                j -= 1
+        return A
