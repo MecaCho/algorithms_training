f7b3cd86a9547223a1b1c2af583641fcb01208c1
MecaCho <1099833328@qq.com>
2020-03-25 20:55:17 +0800

    add leetcode mergeTwoLists;

---
+++ algorithms/linked_list/leetcode-I25-mergeTwoLists.py
@@ -0,0 +1,39 @@
+'''
+面试题25. 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+限制：
+
+0 <= 链表长度 <= 1000
+
+注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        elif l1.val > l2.val:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
