63304d915be9c078068d4cb2a82b159ff4662a6a
MecaCho <1099833328@qq.com>
2020-03-25 20:27:21 +0800

    add leetcode Valid Palindrome;

---
+++ algorithms/hash/leetcode-387-FirstUniqueCharacterInAString.py
@@ -0,0 +1,49 @@
+'''
+387. 字符串中的第一个唯一字符
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+案例:
+
+s = "leetcode"
+返回 0.
+
+s = "loveleetcode",
+返回 2.
+ 
+
+注意事项：您可以假定该字符串只包含小写字母。
+
+387. First Unique Character in a String
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+
+'''
+
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        key_dict = {}
+
+        for key in s:
+            if key in key_dict:
+                key_dict[key] += 1
+            else:
+                key_dict[key] = 1
+
+        for i in range(len(s)):
+            if key_dict[s[i]] == 1:
+                return i
+        return -1
---
+++ algorithms/string/leetcode-125-ValidPalindrome.py
@@ -0,0 +1,63 @@
+
+
+'''
+125. 验证回文串
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+示例 2:
+
+输入: "race a car"
+输出: false
+
+125. Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+
+'''
+
+
+import re
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i, j = 0, len(s)-1
+        while i <= j:
+            if re.match('[a-zA-Z0-9]', s[i]) and re.match('[a-zA-Z0-9]', s[j]):
+                if s[i].lower() != s[j].lower():
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            elif not re.match('[a-zA-Z0-9]', s[i]):
+                i += 1
+                continue
+            elif not re.match('[a-zA-Z0-9]', s[j]):
+                j -= 1
+                continue
+
+        return True
+
+    def is_palindrome(self, s):
+        s = "".join(filter(lambda x: x.isalnum(), s)).lower()
+        return s == s[::-1]
