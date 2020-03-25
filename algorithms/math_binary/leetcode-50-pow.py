e2ddbe9f4b2c2fa864e0d61612ae5baf7bf31f91
MecaCho <1099833328@qq.com>
2020-03-25 21:07:17 +0800

    add leetcode Pow(x, n);

---
+++ algorithms/math_binary/leetcode-50-pow.py
@@ -0,0 +1,65 @@
+'''
+50. Pow(x, n)
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+
+50. Pow(x, n)
+实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+示例 1:
+
+输入: 2.00000, 10
+输出: 1024.00000
+示例 2:
+
+输入: 2.10000, 3
+输出: 9.26100
+示例 3:
+
+输入: 2.00000, -2
+输出: 0.25000
+解释: 2-2 = 1/22 = 1/4 = 0.25
+说明:
+
+-100.0 < x < 100.0
+n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+'''
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+
+        exp = abs(n)
+        ret = self.myPow(x, exp >> 1)
+        ret *= ret
+        if exp & 0x1 == 1:
+            ret = x * ret
+
+        if n > 0:
+            return ret
+        return 1.0000000000 / ret
