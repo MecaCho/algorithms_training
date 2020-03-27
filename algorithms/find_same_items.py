aa655c09c6b3ddcce98692881981c89cf792489b
MecaCho <1099833328@qq.com>
2020-03-27 12:11:43 +0800

    add leetcode hasGroupsSizeX;

--- algorithms/find_same_items.py
+++ algorithms/find_same_items.py
@@ -16,8 +16,18 @@ def find_same_items(list1, list2):
 
 
 if __name__ == '__main__':
-    print find_same_items([1, 2, 3, 4, 9], [3, 4, 5, 9, 10])
-    print find_same_items([], [3, 4, 5, 9, 10])
-    print find_same_items([10], [3, 4, 5, 9, 10])
+    import math
+    # math.gcd()
+    import collections
+
+    deck = [1,2,3,4,1]
+    dict_num = collections.Counter(deck)
+    print(dict_num.elements())
+    print(dict_num.values())
+    print(dict_num, dict_num.values())
+    # math.gcd([10,5,10])
+    # print(find_same_items([1, 2, 3, 4, 9], [3, 4, 5, 9, 10]))
+    # print(find_same_items([], [3, 4, 5, 9, 10]))
+    # print(find_same_items([10], [3, 4, 5, 9, 10]))
 
 
