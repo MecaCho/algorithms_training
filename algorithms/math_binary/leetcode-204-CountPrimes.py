2d23c65157f1b8489f413d458d24a591d9fe9b01
MecaCho <1099833328@qq.com>
2020-03-28 02:04:01 +0800

    add leetcode Count Primes;

---
+++ algorithms/math_binary/leetcode-204-CountPrimes.py
@@ -0,0 +1,41 @@
+'''
+204. Count Primes
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+204. 计数质数
+统计所有小于非负整数 n 的质数的数量。
+
+示例:
+
+输入: 10
+输出: 4
+解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+'''
+
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        is_prime_dict = [1] * n
+        is_prime_dict[:2] = [0 ] *2
+        def is_prime(num):
+            for i in range(2, int(math.sqrt(num) ) +1):
+                if num % i == 0:
+                    return False
+            return True
+
+
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if is_prime(i):
+                is_prime_dict[ i *2:n:i] = [0] * (( n -1 - 2* i) / i + 1)
+
+        return sum(is_prime_dict)
--- algorithms/trie/leetcode-820-ShortEncodingOfWords.py
+++ algorithms/trie/leetcode-820-ShortEncodingOfWords.py
@@ -61,3 +61,56 @@ class Solution(object):
                 if word[i:] in new_words:
                     new_words.remove(word[i:])
         return sum(len(word) + 1 for word in new_words)
+
+
+'''
+方法二：字典树
+思路
+
+如方法一所说，目标就是保留所有不是其他单词后缀的单词。
+
+算法
+
+去找到是否不同的单词具有相同的后缀，我们可以将其反序之后插入字典树中。例如，我们有 "time" 和 "me"，可以将 "emit" 和 "em" 插入字典树中。
+
+
+
+然后，字典树的叶子节点（没有孩子的节点）就代表没有后缀的单词，统计叶子节点代表的单词长度加一的和即为我们要的答案。
+
+JavaPython3C++
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        words = list(set(words)) #remove duplicates
+        #Trie is a nested dictionary with nodes created
+        # when fetched entries are missing
+        Trie = lambda: collections.defaultdict(Trie)
+        trie = Trie()
+
+        #reduce(..., S, trie) is trie[S[0]][S[1]][S[2]][...][S[S.length - 1]]
+        nodes = [reduce(dict.__getitem__, word[::-1], trie)
+                 for word in words]
+
+        #Add word to the answer if it's node has no neighbors
+        return sum(len(word) + 1
+                   for i, word in enumerate(words)
+                   if len(nodes[i]) == 0)
+复杂度分析
+
+时间复杂度：O(\sum w_i)O(∑w 
+i
+​	
+ )，其中 w_iw 
+i
+​	
+  是 words[i] 的长度。对于每个单词中的每个字母，只需要进行常数次操作。
+
+空间复杂度：O(S*\sum w_i)O(S∗∑w 
+i
+​	
+ )，字典树的空间开销，其中 SS 为字符集大小。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/short-encoding-of-words/solution/dan-ci-de-ya-suo-bian-ma-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
