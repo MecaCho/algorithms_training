af3363aafbb3376af111e3c7ea8331c991499a6b
MecaCho <1099833328@qq.com>
2020-03-28 01:03:56 +0800

    add leetcode Short Encoding of Words;

---
+++ algorithms/trie/leetcode-820-ShortEncodingOfWords.py
@@ -0,0 +1,63 @@
+
+'''
+
+820. 单词的压缩编码
+给定一个单词列表，我们将这个列表编码成一个索引字符串 S 与一个索引列表 A。
+
+例如，如果这个列表是 ["time", "me", "bell"]，我们就可以将其表示为 S = "time#bell#" 和 indexes = [0, 2, 5]。
+
+对于每一个索引，我们可以通过从字符串 S 中索引的位置开始读取字符串，直到 "#" 结束，来恢复我们之前的单词列表。
+
+那么成功对给定单词列表进行编码的最小字符串长度是多少呢？
+
+ 
+
+示例：
+
+输入: words = ["time", "me", "bell"]
+输出: 10
+说明: S = "time#bell#" ， indexes = [0, 2, 5] 。
+ 
+
+提示：
+
+1 <= words.length <= 2000
+1 <= words[i].length <= 7
+每个单词都是小写字母 。
+
+820. Short Encoding of Words
+Given a list of words, we may encode it by writing a reference string S and a list of indexes A.
+
+For example, if the list of words is ["time", "me", "bell"], we can write it as S = "time#bell#" and indexes = [0, 2, 5].
+
+Then for each index, we will recover the word by reading from the reference string from that index until we reach a "#" character.
+
+What is the length of the shortest reference string S possible that encodes the given words?
+
+Example:
+
+Input: words = ["time", "me", "bell"]
+Output: 10
+Explanation: S = "time#bell#" and indexes = [0, 2, 5].
+ 
+
+Note:
+
+1 <= words.length <= 2000.
+1 <= words[i].length <= 7.
+Each word has only lowercase letters.
+'''
+
+
+class Solution(object):
+    def minimumLengthEncoding(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        new_words = list(set(words))
+        for word in new_words:
+            for i in range(1, len(word)):
+                if word[i:] in new_words:
+                    new_words.remove(word[i:])
+        return sum(len(word) + 1 for word in new_words)
