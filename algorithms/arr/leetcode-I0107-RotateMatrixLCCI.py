5372da6542d2d0a821a4991c2076abf6785a2df1
MecaCho <1099833328@qq.com>
2020-04-07 00:15:49 +0800

    add leetcode Rotate Matrix;

---
+++ algorithms/arr/leetcode-I0107-RotateMatrixLCCI.py
@@ -0,0 +1,90 @@
+
+'''
+面试题 01.07. 旋转矩阵
+给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+
+不占用额外内存空间能否做到？
+
+ 
+
+示例 1:
+
+给定 matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+示例 2:
+
+给定 matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+原地旋转输入矩阵，使其变为:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+
+面试题 01.07. Rotate Matrix LCCI
+Given an image represented by an N x N matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+ 
+
+Example 1:
+
+Given matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+Rotate the matrix in place. It becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+Rotate the matrix in place. It becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+'''
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        res = map(list, zip(*matrix[::-1]))
+        # print(res)
+        matrix[:] = res
--- algorithms/arr/leetcodeI1704-MissingNumberLCCI.py
+++ algorithms/arr/leetcodeI1704-MissingNumberLCCI.py
@@ -47,3 +47,20 @@ class Solution(object):
         """
         length = len(nums) + 1
         return (length + 0) * (length - 1) / 2 - sum(nums)
+
+
+
+# 利用异或的特性，res = res ^ x ^ x。对同一个值异或两次，那么结果等于它本身，所以我们对res从0-nums.length进行异或，同时对nums数组中的值进行异或，出现重复的会消失，所以最后res的值是只出现一次的数字，也就是nums数组中缺失的那个数字。
+class Solution1(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # length = len(nums) + 1
+        # return (length + 0) * (length - 1) / 2 - sum(nums)
+        res = len(nums)
+        for i in range(len(nums)):
+            res ^= i
+            res ^= nums[i]
+        return res
