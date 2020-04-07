a869f07e008acbfb42cb990e1a2c1c6aa266df0a
MecaCho <1099833328@qq.com>
2020-04-08 01:22:05 +0800

    add leetcode singleNumber;

---
+++ jianzhi/leetcode-I56-singleNumber.py
@@ -0,0 +1,42 @@
+
+'''
+面试题56 - II. 数组中数字出现的次数 II
+在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+
+ 
+
+示例 1：
+
+输入：nums = [3,4,3,3]
+输出：4
+示例 2：
+
+输入：nums = [9,1,7,9,7,9,7]
+输出：1
+ 
+
+限制：
+
+1 <= nums.length <= 10000
+1 <= nums[i] < 2^31
+'''
+
+
+
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hash_map = {}
+        for num in nums:
+            if num in hash_map:
+                hash_map[num] += 1
+            else:
+                hash_map[num] = 1
+        for k, v in hash_map.items():
+            if v == 1:
+                return k
