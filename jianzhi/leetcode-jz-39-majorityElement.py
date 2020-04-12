5cc1aaa9a2724238fbb75910e83af482afd2cdf4
MecaCho <1099833328@qq.com>
2020-04-12 20:51:54 +0800

    add leetcode jz majorityElement;

--- jianzhi/leetcode-jz-39-majorityElement.py
+++ jianzhi/leetcode-jz-39-majorityElement.py
@@ -1,44 +1,42 @@
 '''
+面试题39. 数组中出现次数超过一半的数字
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
 
-169. Majority Element
-Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
-You may assume that the array is non-empty and the majority element always exist in the array.
-
-Example 1:
-
-Input: [3,2,3]
-Output: 3
-Example 2:
-
-Input: [2,2,1,1,1,2,2]
-Output: 2
-
-169. 多数元素
-给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
 
 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
 
+
+
 示例 1:
 
-输入: [3,2,3]
-输出: 3
-示例 2:
-
-输入: [2,2,1,1,1,2,2]
+输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
 输出: 2
+
+
+限制：
+
+1 <= 数组长度 <= 50000
+
+
+
+注意：本题与主站 169 题相同：https://leetcode-cn.com/problems/majority-element/
 '''
 
-'''排序法‘’‘
+
+
+
+
+
 class Solution(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        return sorted(nums)[len(nums) / 2]
+        return sorted(nums)[len(nums)//2] if nums else 0
 
-’‘’选举法'''
+
 class Solution1(object):
     def majorityElement(self, nums):
         """
@@ -56,6 +54,7 @@ class Solution1(object):
                 count -= 1
         return res
 
+
 class Solution1_(object):
     def majorityElement(self, nums):
         """
@@ -72,4 +71,3 @@ class Solution1_(object):
         for num in nums:
             if num == x: count += 1
         return x if count > len(nums) // 2 else 0 # 当无众数时返回 0
-
