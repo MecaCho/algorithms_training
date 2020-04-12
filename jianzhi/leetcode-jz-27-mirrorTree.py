cec86eca48000555611da2b6795afd86a9b4b0cc
MecaCho <1099833328@qq.com>
2020-04-12 19:49:24 +0800

    add leetcode jianzhi mirrorTree, the same with leetcode 226;

---
+++ jianzhi/leetcode-jz-27-mirrorTree.py
@@ -0,0 +1,58 @@
+'''
+面试题27. 二叉树的镜像
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+
+
+示例 1：
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+
+限制：
+
+0 <= 节点个数 <= 1000
+
+注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/
+'''
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def mirrorTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        left_ = self.mirrorTree(root.right)
+        right_ = self.mirrorTree(root.left)
+
+        root.left = left_
+        root.right = right_
+        return root
