e5a0fb1b10999e96368869c8040f1b5a5e68c20a
MecaCho <1099833328@qq.com>
2020-04-14 01:34:47 +0800

    add leetcode jz isSymmetric;

---
+++ jianzhi/leetcode-jz-28-isSymmetric.py
@@ -0,0 +1,63 @@
+
+'''
+面试题28. 对称的二叉树
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+
+
+限制：
+
+0 <= 节点个数 <= 1000
+
+注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+'''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def is_mirror(root_left, root_right):
+            if not root_left and not root_right:
+                return True
+            if not root_right or not root_left:
+                return False
+            if root_left.val != root_right.val:
+                return False
+            return is_mirror(root_left.right, root_right.left) and is_mirror(root_left.left, root_right.right)
+        return is_mirror(root, root)
