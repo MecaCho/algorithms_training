2e313e95597d1235529c983836d49f54a7b60be3
MecaCho <1099833328@qq.com>
2020-04-16 00:26:46 +0800

    add leetcode Merge Intervals;

---
+++ algorithms/sort/leetcode-56-MergeIntervals.py
@@ -0,0 +1,50 @@
+'''
+
+56. 合并区间
+给出一个区间的集合，请合并所有重叠的区间。
+
+示例 1:
+
+输入: [[1,3],[2,6],[8,10],[15,18]]
+输出: [[1,6],[8,10],[15,18]]
+解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2:
+
+输入: [[1,4],[4,5]]
+输出: [[1,5]]
+解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+56. Merge Intervals
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+'''
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals = sorted(intervals)
+        res = []
+        for interval in intervals:
+            if not res or interval[0] > res[-1][1]:
+                res.append(interval)
+                continue
+            if not interval or interval[1] < res[-1][1]:
+                continue
+            if interval[1] > res[-1][1]:
+                res[-1][1] = interval[1]
+        return res
