0f2bb89cc065e0f2198bcde19caadaeb643a805e
MecaCho <1099833328@qq.com>
2020-04-17 00:45:58 +0800

    add leetcode Number of Digit One;

---
+++ algorithms/math_binary/leetcode-233-countDigitOne.py
@@ -0,0 +1,41 @@
+
+'''
+
+233. 数字 1 的个数
+给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+
+示例:
+
+输入: 13
+输出: 6
+解释: 数字 1 出现在以下数字中: 1, 10, 11, 12, 13 。
+
+233. Number of Digit One
+Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+
+Example:
+
+Input: 13
+Output: 6
+Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+'''
+
+
+
+class Solution(object):
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        countr = 0
+        i = 1
+        while i <= n:
+
+            divider = i * 10
+            countr += (n / divider) * i + min(max((n % divider) - i + 1, 0), i)
+            i *= 10
+
+        return countr
+
+
