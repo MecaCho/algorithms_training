84914b0ad380f1e900c2e40196cb372fa8151a34
MecaCho <1099833328@qq.com>
2020-04-24 16:33:22 +0800

    add leetcode Generalized Abbreviation solution;

--- algorithms/backtracking/leetcode-320-GeneralizedAbbreviation.py
+++ algorithms/backtracking/leetcode-320-GeneralizedAbbreviation.py
@@ -26,6 +26,24 @@ Output:
 '''
 
 
+class Solution0(object):
+    def generateAbbreviations(self, word):
+        """
+        :type word: str
+        :rtype: List[str]
+        """
+        self.vals = []
+        def backtrack(i, new_word=""):
+            if i == len(word):
+                self.vals.append(new_word)
+            else:
+                for j in range(i, len(word)):
+                    num = str(j-i) if j-i > 0 else ""
+                    backtrack(j+1, new_word + num + word[j])
+                backtrack(len(word), new_word+str(len(word)-i))
+
+        backtrack(0, "")
+        return self.vals
 
 
 class Solution(object):
@@ -78,3 +96,31 @@ class Solution(object):
         #         new_word[j:j+i] = num_s
         #         res.append("".join(new_word))
         # return res
+
+
+class Solution1(object):
+    def generateAbbreviations(self, word):
+        """
+        :type word: str
+        :rtype: List[str]
+        """
+
+        self.vals = []
+
+        def backtrack(i, new_word="", pre_num=0):
+            if i == len(word):
+                self.vals.append(new_word)
+            else:
+                if not new_word:
+                    backtrack(i + 1, new_word + "1", 1)
+                else:
+                    if pre_num != 0:
+                        new_words = list(new_word)
+                        len_ = len(str(pre_num))
+                        new_words[len(new_words) - len_:] = str(pre_num + 1)
+                        print(new_words)
+                        backtrack(i + 1, "".join(new_words), pre_num + 1)
+                    else:
+                        backtrack(i + 1, new_word + "1", 1)
+
+                backtrack(i + 1, new_word + word[i], 0)
