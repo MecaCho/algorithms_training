17ef4488a6b32d5570b42cd7cdc00b2fc199d2ae
MecaCho <1099833328@qq.com>
2020-04-25 23:15:41 +0800

    add leetcode 245. Shortest Word Distance III;;

---
+++ algorithms/hash/leetcode-245-ShortestWordDistanceIII.py
@@ -0,0 +1,60 @@
+'''
+245. 最短单词距离 III
+给定一个单词列表和两个单词 word1 和 word2，返回列表中这两个单词之间的最短距离。
+
+word1 和 word2 是有可能相同的，并且它们将分别表示为列表中两个独立的单词。
+
+示例:
+假设 words = ["practice", "makes", "perfect", "coding", "makes"].
+
+输入: word1 = “makes”, word2 = “coding”
+输出: 1
+输入: word1 = "makes", word2 = "makes"
+输出: 3
+注意:
+你可以假设 word1 和 word2 都在列表里。
+
+245. Shortest Word Distance III
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+word1 and word2 may be the same and they represent two individual words in the list.
+
+Example:
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Input: word1 = “makes”, word2 = “coding”
+Output: 1
+Input: word1 = "makes", word2 = "makes"
+Output: 3
+Note:
+You may assume word1 and word2 are both in the list.
+'''
+
+
+class Solution(object):
+    def shortestWordDistance(self, words, word1, word2):
+        """
+        :type words: List[str]
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        import collections
+        hash_map = collections.defaultdict(list)
+        for i in range(len(words)):
+            hash_map[words[i]].append(i)
+        if word1 == word2:
+            indexs = hash_map[word1]
+            res = float("inf")
+            for i in range(1, len(indexs)):
+                res = min(indexs[i] - indexs[i-1], res)
+            return res
+        word1_list = hash_map[word1]
+        word2_list = hash_map[word2]
+        # print(word1_list, word2_list, hash_map)
+        res = float("inf")
+        for i in word1_list:
+            for j in word2_list:
+                res = min(abs(i-j), res)
+        return res
+
