d664016eb7c1a7ace0257916d3090ff4bf3d403c
MecaCho <1099833328@qq.com>
2020-05-01 00:29:07 +0800

    add leetcode 21. Merge Two Sorted Lists solution;

--- algorithms/linked_list/leetcode-I25-mergeTwoLists.py
+++ algorithms/linked_list/leetcode-I25-mergeTwoLists.py
@@ -14,6 +14,7 @@
 '''
 
 
+# 递归
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -37,3 +38,36 @@ class Solution(object):
         elif l1.val > l2.val:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
+
+
+# 迭代
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution1(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        pre = ListNode(0)
+        cur = pre
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        cur.next = l1 if l1 else l2
+        return pre.next
