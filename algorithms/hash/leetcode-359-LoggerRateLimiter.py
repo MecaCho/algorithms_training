51bd9298d713ea021a3663990ed92e73ee45a8e1
MecaCho <1099833328@qq.com>
2020-05-01 15:03:09 +0800

    add leetcode 359. Logger Rate Limiter;

---
+++ algorithms/hash/leetcode-359-LoggerRateLimiter.py
@@ -0,0 +1,93 @@
+'''
+359. 日志速率限制器
+请你设计一个日志系统，可以流式接收日志以及它的时间戳。
+
+该日志会被打印出来，需要满足一个条件：当且仅当日志内容 在过去的 10 秒钟内没有被打印过。
+
+给你一条日志的内容和它的时间戳（粒度为秒级），如果这条日志在给定的时间戳应该被打印出来，则返回 true，否则请返回 false。
+
+要注意的是，可能会有多条日志在同一时间被系统接收。
+
+示例：
+
+Logger logger = new Logger();
+
+// 日志内容 "foo" 在时刻 1 到达系统
+logger.shouldPrintMessage(1, "foo"); returns true;
+
+// 日志内容 "bar" 在时刻 2 到达系统
+logger.shouldPrintMessage(2,"bar"); returns true;
+
+// 日志内容 "foo" 在时刻 3 到达系统
+logger.shouldPrintMessage(3,"foo"); returns false;
+
+// 日志内容 "bar" 在时刻 8 到达系统
+logger.shouldPrintMessage(8,"bar"); returns false;
+
+// 日志内容 "foo" 在时刻 10 到达系统
+logger.shouldPrintMessage(10,"foo"); returns false;
+
+// 日志内容 "foo" 在时刻 11 到达系统
+logger.shouldPrintMessage(11,"foo"); returns true;
+
+359. Logger Rate Limiter
+Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only if it is not printed in the last 10 seconds.
+
+Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given timestamp, otherwise returns false.
+
+It is possible that several messages arrive roughly at the same time.
+
+Example:
+
+Logger logger = new Logger();
+
+// logging string "foo" at timestamp 1
+logger.shouldPrintMessage(1, "foo"); returns true;
+
+// logging string "bar" at timestamp 2
+logger.shouldPrintMessage(2,"bar"); returns true;
+
+// logging string "foo" at timestamp 3
+logger.shouldPrintMessage(3,"foo"); returns false;
+
+// logging string "bar" at timestamp 8
+logger.shouldPrintMessage(8,"bar"); returns false;
+
+// logging string "foo" at timestamp 10
+logger.shouldPrintMessage(10,"foo"); returns false;
+
+// logging string "foo" at timestamp 11
+logger.shouldPrintMessage(11,"foo"); returns true;
+'''
+
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hash_map = {}
+
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+        res = False
+        if message not in self.hash_map or (timestamp - self.hash_map[message]) >= 10:
+            self.hash_map[message] = timestamp
+            res = True
+        return res
+
+
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
