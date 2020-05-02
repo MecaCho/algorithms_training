3628c76b90942b5715134eb16cf8577ca6e9fe15
MecaCho <1099833328@qq.com>
2020-05-02 21:21:55 +0800

    add leetcode 734. Sentence Similarity;

---
+++ algorithms/hash/leetcode-734-SentenceSimilarity.py
@@ -0,0 +1,66 @@
+'''
+734. 句子相似性
+给定两个句子 words1, words2 （每个用字符串数组表示），和一个相似单词对的列表 pairs ，判断是否两个句子是相似的。
+
+例如，当相似单词对是 pairs = [["great", "fine"], ["acting","drama"], ["skills","talent"]]的时候，"great acting skills" 和 "fine drama talent" 是相似的。
+
+注意相似关系是不具有传递性的。例如，如果 "great" 和 "fine" 是相似的，"fine" 和 "good" 是相似的，但是 "great" 和 "good" 未必是相似的。
+
+但是，相似关系是具有对称性的。例如，"great" 和 "fine" 是相似的相当于 "fine" 和 "great" 是相似的。
+
+而且，一个单词总是与其自身相似。例如，句子 words1 = ["great"], words2 = ["great"], pairs = [] 是相似的，尽管没有输入特定的相似单词对。
+
+最后，句子只会在具有相同单词个数的前提下才会相似。所以一个句子 words1 = ["great"] 永远不可能和句子 words2 = ["doubleplus","good"] 相似。
+
+
+
+注：
+
+words1 and words2 的长度不会超过 1000。
+pairs 的长度不会超过 2000。
+每个pairs[i] 的长度为 2。
+每个 words[i] 和 pairs[i][j] 的长度范围为 [1, 20]。
+
+734. Sentence Similarity
+Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.
+
+For example, "great acting skills" and "fine drama talent" are similar, if the similar word pairs are pairs = [["great", "fine"], ["acting","drama"], ["skills","talent"]].
+
+Note that the similarity relation is not transitive. For example, if "great" and "fine" are similar, and "fine" and "good" are similar, "great" and "good" are not necessarily similar.
+
+However, similarity is symmetric. For example, "great" and "fine" being similar is the same as "fine" and "great" being similar.
+
+Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
+
+Finally, sentences can only be similar if they have the same number of words. So a sentence like words1 = ["great"] can never be similar to words2 = ["doubleplus","good"].
+
+Note:
+
+The length of words1 and words2 will not exceed 1000.
+The length of pairs will not exceed 2000.
+The length of each pairs[i] will be 2.
+The length of each words[i] and pairs[i][j] will be in the range [1, 20].
+'''
+
+
+
+
+
+class Solution(object):
+    def areSentencesSimilar(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2):
+            return False
+        import collections
+        hash_map = collections.defaultdict(list)
+        for i in range(len(pairs)):
+            hash_map[pairs[i][0]].append(pairs[i][1])
+        for i in range(len(words1)):
+            if words1[i] != words2[i] and (not hash_map.get(words1[i]) or words2[i] not in hash_map.get(words1[i])) and (not hash_map.get(words2[i])  or words1[i] not in hash_map.get(words2[i])):
+                return False
+        return True
