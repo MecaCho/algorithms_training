fb6e2ddea431eed1d8151a43980ad761426a8a36
MecaCho <1099833328@qq.com>
2020-05-04 23:35:12 +0800

    add leetcode weekly contest 187 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit;

---
+++ contest/weekly-contest-187/leetcode-1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.py
@@ -0,0 +1,153 @@
+
+
+
+'''
+1438. 绝对差不超过限制的最长连续子数组
+给你一个整数数组 nums ，和一个表示限制的整数 limit，请你返回最长连续子数组的长度，该子数组中的任意两个元素之间的绝对差必须小于或者等于 limit 。
+
+如果不存在满足条件的子数组，则返回 0 。
+
+
+
+示例 1：
+
+输入：nums = [8,2,4,7], limit = 4
+输出：2
+解释：所有子数组如下：
+[8] 最大绝对差 |8-8| = 0 <= 4.
+[8,2] 最大绝对差 |8-2| = 6 > 4.
+[8,2,4] 最大绝对差 |8-2| = 6 > 4.
+[8,2,4,7] 最大绝对差 |8-2| = 6 > 4.
+[2] 最大绝对差 |2-2| = 0 <= 4.
+[2,4] 最大绝对差 |2-4| = 2 <= 4.
+[2,4,7] 最大绝对差 |2-7| = 5 > 4.
+[4] 最大绝对差 |4-4| = 0 <= 4.
+[4,7] 最大绝对差 |4-7| = 3 <= 4.
+[7] 最大绝对差 |7-7| = 0 <= 4.
+因此，满足题意的最长子数组的长度为 2 。
+示例 2：
+
+输入：nums = [10,1,2,4,7,2], limit = 5
+输出：4
+解释：满足题意的最长子数组是 [2,4,7,2]，其最大绝对差 |2-7| = 5 <= 5 。
+示例 3：
+
+输入：nums = [4,2,2,2,4,4,2,2], limit = 0
+输出：3
+
+
+提示：
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+0 <= limit <= 10^9
+
+
+1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+Given an array of integers nums and an integer limit, return the size of the longest continuous subarray such that the absolute difference between any two elements is less than or equal to limit.
+
+In case there is no subarray satisfying the given condition return 0.
+
+
+
+Example 1:
+
+Input: nums = [8,2,4,7], limit = 4
+Output: 2
+Explanation: All subarrays are:
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4.
+Therefore, the size of the longest subarray is 2.
+Example 2:
+
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+Example 3:
+
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+0 <= limit <= 10^9
+'''
+
+
+class Solution(object):
+    def longestSubarray(self, nums, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        mins = []
+        maxs = []
+        cur = []
+        max_length = 0
+        # min_val = nums[0]
+        # max_val = nums[0]
+        for i in range(len(nums)):
+            num = nums[i]
+            cur.append(num)
+
+            # if num < min_val:
+            #     min_val = num
+            # if num >= max_val:
+            #     max_val = num
+
+            if not mins or num <= mins[-1]:
+                mins.append(num)
+            if not maxs or num >= maxs[-1]:
+                maxs.append(num)
+
+            min_val = mins[-1]
+            max_val = maxs[-1]
+
+            # print(min_val, mins, max_val, maxs, cur, len(cur))
+            if max_val - min_val > limit:
+                pop_val = cur.pop(0)
+                if pop_val == mins[0]:
+                    mins.pop(0)
+                    if not mins:
+                        mins.append(cur[0])
+                        i = 1
+                        while i < len(cur):
+                            if cur[i] <= mins[-1]:
+                                mins.append(cur[i])
+                            i += 1
+                if pop_val == maxs[0]:
+                    maxs.pop(0)
+                    if not maxs:
+                        maxs.append(cur[0])
+                        i = 1
+                        while i < len(cur):
+                            if cur[i] >= maxs[-1]:
+                                maxs.append(cur[i])
+                            i += 1
+            else:
+                max_length = max(max_length, len(cur))
+                # print(max_length, len(cur), max(cur), min(cur), cur)
+        # print(len(nums), max(nums), min(nums))
+        return max_length
+
+# tips
+
+'''
+Use a sliding window approach keeping the maximum and minimum value using a data structure like a multiset from STL in C++.
+'''
+
+'''
+More specifically, use the two pointer technique, moving the right pointer as far as possible to the right until the subarray is not valid (maxValue - minValue > limit), then moving the left pointer until the subarray is valid again (maxValue - minValue <= limit). Keep repeating this process.
+'''
--- contest/weekly-contest-187/leetcode-1439-FindTSmallestSumOfAWithSortedRows.py
+++ contest/weekly-contest-187/leetcode-1439-FindTSmallestSumOfAWithSortedRows.py
@@ -139,3 +139,89 @@ class Solution(object):
         # #         steps.append((i+1)*(j+1))
         # print(steps)
         # return
+
+# tips
+
+'''
+Save all visited sums and corresponding indexes in a priority queue. Then, once you pop the smallest sum so far, you can quickly identify the next m candidates for smallest sum by incrementing each row index by 1.
+'''
+
+
+'''
+暴力解法
+解题思路
+将“求前m行的第k个最小数组和”按动态规划的思路划分成子问题：已知前m-1行的最小数组和的列表，求前m行的第k个最小数组和。
+例如，mat = [[1,10,10],[1,4,5],[2,3,6]]，k = 7。已知前两行的最小数组和last_row = [2,5,6,11,11,14,14,15,15]。我们截取last_row的前k个元素（因为后面的元素不再有竞争力），last_row = [2,5,6,11,11,14,14]。然后遍历第三行的每个元素，与last_row的每个元素求和，排序+截取后得到new_row = [4, 5, 7, 8, 8, 8, 9]。返回第k个元素即可。
+复杂度分析
+时间复杂度：O(m*max(nk, klog(k))O(m∗max(nk,klog(k))。遍历每行，时间复杂度为O(m)O(m)。对于每一行，遍历每一列的元素并求和为O(nk)O(nk)，对该行求出的数组和排序的复杂度为O(klog(k))O(klog(k))。
+空间复杂度：O(k)O(k)。每行都要存储一个长度不超过k的"数组和"列表。
+代码
+class Solution:
+    def kthSmallest(self, mat: List[List[int]], k: int) -> int:
+        last_row = [0]
+        for i in range(len(mat)):
+            new_row = [] 
+            for j in range(len(mat[0])):
+                for p in last_row:
+                    new_row.append(p + mat[i][j])
+            new_row.sort()
+            if len(new_row) > k:
+                last_row = new_row[ :k]
+            else:
+                last_row = new_row
+        return last_row[k - 1]
+最小堆
+解题思路
+可以先看下264. 丑数 II这道题。T264的最小堆解法：每次从堆中弹出最小的丑数，然后把该丑数能生成的新的丑数push入堆。
+本题也很相似，每次从堆中弹出最小的数组和curr_sum和对应的指针pointers，然后轮流将指针pointers的每个索引向后移动一位，生成新的new_sum，加入堆中。
+算法流程
+最小堆存储的是[curr_sum, pointers]二元组，pointers是指针数组，curr_sum是该pointers指向的元素的和。初始化pointers全为0，求出相应的curr_sum，并将其入堆。
+重复下列步骤k次：
+从堆中pop出curr_sum和pointers。
+遍历pointers的每个索引，将该索引加一，求出新的和，如果没有出现过，push入堆。
+栗子
+以mat = [[1,10,10],[1,4,5],[2,3,6]]，k = 7为例，初始化pointers = (0, 0, 0)，curr_sum = 4，哈希表seen加入(0, 0, 0)
+从堆中pop出最小和，pointers = (0, 0, 0)，curr_sum = 4。新生成[13, (1, 0, 0)],[7, (0, 1, 0)]，[5, (0, 0, 1)]，在seen中做好标记，然后将三者入堆。重复该步骤k次。
+复杂度分析
+时间复杂度：O(kmlog(k))O(kmlog(k))。执行k次循环：每次循环时，出堆操作，是O(log(k)))O(log(k)))；每次出堆后，要生成m个新的数组和，是O(n)O(n)；将新生成的的数组和入队，是O(logk)O(logk)。所以总的是O(k * (log(k) + m * log(k))) = O(kmlog(k)))O(k∗(log(k)+m∗log(k)))=O(kmlog(k)))
+空间复杂度：O(km^2)O(km 
+2
+ )。堆中的元素个数不会超过km个，每个的空间是O(m)O(m)
+代码
+import heapq
+
+class Solution:
+    def kthSmallest(self, mat, k: int) -> int:
+        m, n = len(mat), len(mat[0])
+        # 初始化指针
+        pointers = [0] * m 
+        # 初始化heap
+        heap = []
+        curr_sum = 0
+        for i in range(m):
+            curr_sum += mat[i][0]
+        heapq.heappush(heap, [curr_sum, tuple(pointers)])
+        # 初始化seen
+        seen = set()
+        seen.add(tuple(pointers))
+        # 执行k次
+        for _ in range(k):
+            # 从堆中pop出curr_sum(最小数组和)和pointers(指针数组)
+            curr_sum, pointers = heapq.heappop(heap)
+            # 每个指针轮流后移一位，将new_sum(新的数组和)和new_pointers(新的指针数组)push入堆
+            for i, j in enumerate(pointers):
+                if j < n - 1:
+                    new_pointers = list(pointers)
+                    new_pointers[i] = j + 1
+                    new_pointers = tuple(new_pointers)
+                    if new_pointers not in seen:
+                        new_sum = curr_sum + mat[i][j + 1]- mat[i][j]
+                        heapq.heappush(heap, [new_sum, new_pointers])
+                        seen.add(new_pointers)
+        return curr_sum
+
+作者：coldme-2
+链接：https://leetcode-cn.com/problems/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/solution/bao-li-jie-fa-zui-xiao-dui-by-coldme-2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
