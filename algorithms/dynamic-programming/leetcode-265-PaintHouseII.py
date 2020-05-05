050843b42aa770274145422506590876882d1315
MecaCho <1099833328@qq.com>
2020-05-06 02:59:08 +0800

    add leetcode 265. Paint House II;

---
+++ algorithms/dynamic-programming/leetcode-265-PaintHouseII.py
@@ -0,0 +1,64 @@
+
+'''
+265. 粉刷房子 II
+假如有一排房子，共 n 个，每个房子可以被粉刷成 k 种颜色中的一种，你需要粉刷所有的房子并且使其相邻的两个房子颜色不能相同。
+
+当然，因为市场上不同颜色油漆的价格不同，所以房子粉刷成不同颜色的花费成本也是不同的。每个房子粉刷成不同颜色的花费是以一个 n x k 的矩阵来表示的。
+
+例如，costs[0][0] 表示第 0 号房子粉刷成 0 号颜色的成本花费；costs[1][2] 表示第 1 号房子粉刷成 2 号颜色的成本花费，以此类推。请你计算出粉刷完所有房子最少的花费成本。
+
+注意：
+
+所有花费均为正整数。
+
+示例：
+
+输入: [[1,5,3],[2,9,4]]
+输出: 5
+解释: 将 0 号房子粉刷成 0 号颜色，1 号房子粉刷成 2 号颜色。最少花费: 1 + 4 = 5;
+     或者将 0 号房子粉刷成 2 号颜色，1 号房子粉刷成 0 号颜色。最少花费: 3 + 2 = 5.
+进阶：
+您能否在 O(nk) 的时间复杂度下解决此问题？
+
+265. Paint House II
+There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+
+Example:
+
+Input: [[1,5,3],[2,9,4]]
+Output: 5
+Explanation: Paint house 0 into color 0, paint house 1 into color 2. Minimum cost: 1 + 4 = 5;
+             Or paint house 0 into color 2, paint house 1 into color 0. Minimum cost: 3 + 2 = 5.
+Follow up:
+Could you solve it in O(nk) runtime?
+'''
+
+
+
+class Solution(object):
+    def minCostII(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        if not costs:
+            return 0
+        dp = [[0  for _ in range(len(costs[0]))] for _ in range(len(costs))]
+        if len(costs[0]) < 2:
+            if len(costs) > 1:
+                return 2147483647
+            else:
+                return costs[0][0]
+
+        for i in range(len(costs)):
+            for j in range(len(costs[0])):
+                colors = [dp[i - 1][k] for k in range(len(costs[0])) if k != j]
+                min_color = min(colors)
+                dp[i][j] = min_color + costs[i][j]
+
+        return min(dp[-1]) if len(dp) > 1 else min(costs[0])
