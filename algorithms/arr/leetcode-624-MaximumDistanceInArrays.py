e4b4fece62b770f784370178d011513548bd1c83
MecaCho <1099833328@qq.com>
2020-05-06 03:54:45 +0800

    add leetcode 624. Maximum Distance in Arrays;

---
+++ algorithms/arr/leetcode-624-MaximumDistanceInArrays.py
@@ -0,0 +1,113 @@
+'''
+624. 数组列表中的最大距离
+给定 m 个数组，每个数组都已经按照升序排好序了。现在你需要从两个不同的数组中选择两个整数（每个数组选一个）并且计算它们的距离。两个整数 a 和 b 之间的距离定义为它们差的绝对值 |a-b| 。你的任务就是去找到最大距离
+
+示例 1：
+
+输入：
+[[1,2,3],
+ [4,5],
+ [1,2,3]]
+输出： 4
+解释：
+一种得到答案 4 的方法是从第一个数组或者第三个数组中选择 1，同时从第二个数组中选择 5 。
+
+
+注意：
+
+每个给定数组至少会有 1 个数字。列表中至少有两个非空数组。
+所有 m 个数组中的数字总数目在范围 [2, 10000] 内。
+m 个数组中所有整数的范围在 [-10000, 10000] 内。
+
+
+624. Maximum Distance in Arrays
+Given m arrays, and each array is sorted in ascending order. Now you can pick up two integers from two different arrays (each array picks one) and calculate the distance. We define the distance between two integers a and b to be their absolute difference |a-b|. Your task is to find the maximum distance.
+
+Example 1:
+Input:
+[[1,2,3],
+ [4,5],
+ [1,2,3]]
+Output: 4
+Explanation:
+One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+Note:
+Each given array will have at least 1 number. There will be at least two non-empty arrays.
+The total number of the integers in all the m arrays will be in the range of [2, 10000].
+The integers in the m arrays will be in the range of [-10000, 10000].
+'''
+
+
+class Solution(object):
+    def maxDistance(self, arrays):
+        """
+        :type arrays: List[List[int]]
+        :rtype: int
+        """
+        max_val = -float("inf")
+        min_val = float("inf")
+        res = 0
+        for i in range(len(arrays)):
+            vals = (min(arrays[i]), max(arrays[i]))
+            res = max(res, max(vals[1] - min_val, max_val - vals[0]))
+            max_val = vals[1] if vals[1] > max_val else max_val
+            min_val = vals[0] if vals[0] < min_val else min_val
+        return res
+
+
+'''
+方法一：枚举 [超出时间限制]
+对于每一个数组，只有第一个元素（最小的元素）和最后一个元素（最大的元素）是有意义的，因此我们可以枚举选择的两个数组 m[i] 和 m[j]，随后计算出 m[i] 的第一个元素和 m[j] 的最后一个元素的差的绝对值，以及 m[i] 的最后一个元素和 m[j] 的第一个元素的差的绝对值。在这些绝对值中，找出最大的距离。
+
+Java
+public class Solution {
+    public int maxDistance(int[][] list) {
+        int res = 0;
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = i + 1; j < list.length; j++) {
+                res = Math.max(res, Math.abs(list[i][0] - list[j][list[j].length - 1]));
+                res = Math.max(res, Math.abs(list[j][0] - list[i][list[i].length - 1]));
+            }
+        }
+        return res;
+    }
+}
+复杂度分析
+
+时间复杂度：O(M^2)O(M 
+2
+ )，其中 MM 是数组的个数。我们需要使用二重循环来枚举 m[i] 和 m[j]。
+
+空间复杂度；O(1)O(1)。
+
+方法二：线性扫描
+方法一告诉我们，当我们枚举了两个数组之后，只有这两个数组的最小值和最大值才是有意义的。同理，假设我们确定了第一个数要从第 i 个数组中选择，第二个数要从前 i - 1 个数组中选择，如果我们把前 i - 1 个数组看成一个大的数组，那么只有这 i - 1 个数组中的最小值和最大值才是有意义的。
+
+因此我们找到了可以方法一中可以优化的地方。我们顺序扫描这 m 个数组，当扫描到第 i 个数组时，我们计算出 m[i] 中最小的元素和前 i - 1 个数组中最大的元素 max_val 的差的绝对值，以及 m[i] 中最大的元素和前 i - 1 个数组中最小的元素 min_val 的差的绝对值。在这之后，我们更新 min_val 和 max_val，使其变为前 i 个数组中最小和最大的元素。
+
+
+1 / 8
+
+Java
+public class Solution {
+    public int maxDistance(int[][] list) {
+        int res = 0, min_val = list[0][0], max_val = list[0][list[0].length - 1];
+        for (int i = 1; i < list.length; i++) {
+            res = Math.max(res, Math.max(Math.abs(list[i][list[i].length - 1] - min_val), Math.abs(max_val - list[i][0])));
+            min_val = Math.min(min_val, list[i][0]);
+            max_val = Math.max(max_val, list[i][list[i].length - 1]);
+        }
+        return res;
+    }
+}
+复杂度分析
+
+时间复杂度：O(M)O(M)，其中 MM 是数组的个数。
+
+空间复杂度：O(1)O(1)。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/maximum-distance-in-arrays/solution/shu-zu-lie-biao-zhong-de-zui-da-ju-chi-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
--- design/leetcode-604-DesignCompressedStringIterator.py
+++ design/leetcode-604-DesignCompressedStringIterator.py
@@ -102,3 +102,154 @@ class StringIterator(object):
 # obj = StringIterator(compressedString)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
+
+
+'''
+方法 1：解压缩字符串 [Time Limit Exceeded]
+算法
+
+在这种方法中，我们将字符串预处理。我们假设已经将 compressedStringcompressedString 解压缩到 resres 字符串构造器中，为了求得 resres 中的解压字符串，我们遍历一遍 compressedStringcompressedString，一旦遇到一个字符，我们找到它后面的数字。然后我们就可以得到当前解压字符的两个元素（字符和次数）。
+
+现在我们看看 next() 和 hasNext() 操作具体是怎么实现的：
+
+next()：我们首先检车压缩字符串是否还有没解压的字符，如果没有了，那么 hasNext() 函数返回 False ，next() 函数返回 ' ' 。否则我们用指针 ptrptr 返回一个字符， ptrptr 指向的是下一个待返回的字符。在返回字符之前我们要在 resres 中更新 ptrptr 指针。
+
+hasNext()： 如果 ptrptr 指向的地方超过了 resres 数组最后一个位置，说明没有更多的待解压字符，我们返回 False，否则返回 True。
+
+Java
+public class StringIterator {
+    StringBuilder res=new StringBuilder();
+    int ptr=0;
+    public StringIterator(String s) {
+        int i = 0;
+        while (i < s.length()) {
+                char ch = s.charAt(i++);
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                for (int j = 0; j < num; j++)
+                    res.append(ch);
+        }
+    }
+    public char next() {
+        if (!hasNext())
+            return ' ';
+        return res.charAt(ptr++);
+    }
+    public boolean hasNext() {
+        return ptr!=res.length();
+    }
+}
+表现分析
+
+我们预处理了解压缩后的字符串，所以需要空间为 O(m)O(m)，这里 mm 是解压缩后字符串的长度。
+
+预处理的时间复杂度是 O(m)O(m)，因为解压缩后字符串的长度是 mm。
+
+一旦预处理过程完成后，操作 next() 和 hasNexts() 的时间复杂度都是 O(1)O(1)。
+
+这个方法可以很轻易地扩展更多操作，比方说 previous()，last()，find() 和 hasPrevious() 等等，而且由于这些操作都可以基于一个索引进行，所以时间复杂度都是 O(1)O(1) 的。
+
+由于预处理完成后，next() 操作只需要 O(1)O(1) 的时间，如果有大量的 next() 操作，这个方法的优势非常明显。但是如果有大量的 hasNext() 操作，这个方法的优势不明显，因为预处理总是要被执行的。
+
+这个方法的一个潜在问题是如果未解压字符串你的长度非常大，那么可能会导致内存溢出的问题。
+
+方法 2：预处理 [Accepted]
+算法
+
+这个方法中，我们首先将 compressedStringcompressedString 中的字母和后面的重复次数分别保存在 charschars 和 numsnums 数组中。我们使用正则表达式来完成这个过程。
+
+正则表达式是一串特殊的字符串，它通过一些匹配模式，找到需要匹配的字符串或者一系列字符串，它可以被用在搜索、编辑和修改文字和数据等等方面。
+
+用正则表达式进行分割字符和数字可以在预处理过程中完成，我们来看看 next() 和 hasNext() 操作分别如何完成。
+
+next()：每次执行 next() 操作时，首先我们使用函数 hasNext() 检查是否还有没解压的字符，如果没有了，我们返回 ' '。我们通过 ptrptr 记录 compressedStringcompressedString 中需要返回的下一个字符。如果有剩余未解压字符，我们返回 ptrptr 当前指向的字符，我们同时减少 nums[ptr]nums[ptr] 条目，表示当前待解压的字符数目减少一个。如果 nums[ptr]nums[ptr] 变成了 0 ，表示当前字符已经完全被解压了，我们就将 ptrptr 指向下一个字母。
+
+hasNext()：执行 hasNext() 时，我们只需要检查 ptrptr 是否超出了 charschars 数组的范围，如果超出了，说明compressedStringcompressedString 中没有更多字符了，此时我们返回 False，否则我们返回 True。
+
+Java
+import java.util.regex.Pattern;
+public class StringIterator {
+    int ptr = 0;
+    String[] chars;int[] nums;
+    public StringIterator(String compressedString) {
+        nums = Arrays.stream(compressedString.substring(1).split("[a-zA-Z]+")).mapToInt(Integer::parseInt).toArray();;
+        chars = compressedString.split("[0-9]+");
+    }
+    public char next() {
+        if (!hasNext())
+            return ' ';
+        nums[ptr]--;
+        char res=chars[ptr].charAt(0);
+        if(nums[ptr]==0)
+            ptr++;
+        return res;
+    }
+    public boolean hasNext() {
+        return ptr != chars.length;
+    }
+}
+表现分析
+
+保存预处理结果的空间为 O(n)O(n)，这里 nn 是压缩字符串的长度。 numsnums 数组和 charschars 数组都包含 nn 个元素。
+
+预处理过程需要 O(n)O(n) 的时间。所以如果大部分时间在执行 hasNext() 操作，这个预处理方法也会没有优势。
+
+一旦预处理操作完成， hasNext() 和 next() 只需要 O(1)O(1) 的时间。
+
+这个方法可以扩展到 previous() 和 hasPrevious() 操作，但需要在当前的实现上进行一些简单的修改。
+
+方法 3：需求计算 [Accepted]
+算法
+
+在这个方法中，我们不使用正则表达式来找到 compressedStringcompressedString 中每一个部分，我们也不做任何形式的预处理，当一个操作被执行的时候，我们才去执行相应的操作获得对应的结果。所以操作是根据需要进行的。
+
+我们来看一下这个方法的具体实现：
+
+next()：我们使用一个全局 ptrptr 记录 compressedStringcompressedString 中下一个要生成的字符，我们还使用 numnum 记录当前字符剩余的数目。一旦 next() 操作被执行，我们首先检查是否还有没被解压的字符，如果没有，我们返回 ' ' ，否则我们检查当前字符是否还有剩余数目，如果有我们减少当前字符剩余数目并直接返回当前字符。如果没有剩余数目了，我们更新 ptrptr 指向 compressedStringcompressedString 中下一个字符并从 compressedStringcompressedString 中获取当前字符的数目，然后更新 numnum。这个数字可以通过数值方法获得。
+
+hasNext()：如果 ptrptr 指向的下标超过了 compressedStringcompressedString 的范围，这表示已经没有更多未解压字符，我们返回 False，否则返回 True 表示还有未解压字符。
+
+Java
+public class StringIterator {
+    String res;
+    int ptr = 0, num = 0;
+    char ch = ' ';
+    public StringIterator(String s) {
+        res = s;
+    }
+    public char next() {
+        if (!hasNext())
+            return ' ';
+        if (num == 0) {
+            ch = res.charAt(ptr++);
+            while (ptr < res.length() && Character.isDigit(res.charAt(ptr))) {
+                num = num * 10 + res.charAt(ptr++) - '0';
+            }
+        }
+        num--;
+        return ch;
+    }
+    public boolean hasNext() {
+        return ptr != res.length() || num != 0;
+    }
+}
+表现分析
+
+由于没有进行预处理，所以只需要常数的空间。
+
+执行 next() 操作的时间复杂度为 O(1)O(1)。
+
+执行 hasNexts() 的时间复杂度是 O(1)O(1)。
+
+由于不需要预处理，hasNext() 只需要 O(1)O(1) 的时间，如果有大量的 hasNext() 操作，这个方法优势非常明显。
+
+这个方法可以扩展包括 previous() 和 hasPrevious()，但是需要一些额外的变量。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/design-compressed-string-iterator/solution/die-dai-ya-suo-zi-fu-chuan-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
