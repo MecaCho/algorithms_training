a3cf2b6baf773c9e2446d2eb7b8c69cb10677c3b
MecaCho <1099833328@qq.com>
2020-05-06 02:44:18 +0800

    add leetcode 256. Paint House;

---
+++ algorithms/dynamic-programming/leetcode-256-PaintHouse.py
@@ -0,0 +1,52 @@
+'''
+256. 粉刷房子
+假如有一排房子，共 n 个，每个房子可以被粉刷成红色、蓝色或者绿色这三种颜色中的一种，你需要粉刷所有的房子并且使其相邻的两个房子颜色不能相同。
+
+当然，因为市场上不同颜色油漆的价格不同，所以房子粉刷成不同颜色的花费成本也是不同的。每个房子粉刷成不同颜色的花费是以一个 n x 3 的矩阵来表示的。
+
+例如，costs[0][0] 表示第 0 号房子粉刷成红色的成本花费；costs[1][2] 表示第 1 号房子粉刷成绿色的花费，以此类推。请你计算出粉刷完所有房子最少的花费成本。
+
+注意：
+
+所有花费均为正整数。
+
+示例：
+
+输入: [[17,2,17],[16,16,5],[14,3,19]]
+输出: 10
+解释: 将 0 号房子粉刷成蓝色，1 号房子粉刷成绿色，2 号房子粉刷成蓝色。
+     最少花费: 2 + 5 + 3 = 10。
+
+256. Paint House
+There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+
+Example:
+
+Input: [[17,2,17],[16,16,5],[14,3,19]]
+Output: 10
+Explanation: Paint house 0 into blue, paint house 1 into green, paint house 2 into blue.
+             Minimum cost: 2 + 5 + 3 = 10.
+'''
+
+
+class Solution(object):
+    def minCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        if not costs:
+            return 0
+        dp = [[0 ,0 ,0] for _ in range(len(costs))]
+        pre = None
+        for i in range(len(costs)):
+            dp[i][0] = min(dp[ i -1][1], dp[ i -1][2]) + costs[i][0]
+            dp[i][1] = min(dp[ i -1][0], dp[ i -1][2]) + costs[i][1]
+            dp[i][2] = min(dp[ i -1][0], dp[ i -1][1]) + costs[i][2]
+        # print(dp)
+        return min(dp[-1]) if len(dp) > 1 else min(costs[0])
--- algorithms/dynamic-programming/leetcode-983-MinimumCostForTickets.py
+++ algorithms/dynamic-programming/leetcode-983-MinimumCostForTickets.py
@@ -113,3 +113,147 @@ class Solution(object):
                 dp[i] = dp[i-1]
         # print(dp)
         return dp[days[-1]]
+
+
+'''
+方法一：记忆化搜索（日期变量型）
+思路和算法
+
+我们用 \textit{dp}(i)dp(i) 来表示从第 ii 天开始到一年的结束，我们需要花的钱。考虑到一张通行证可以让我们在「接下来」的若干天进行旅行，所以我们「从后往前」倒着进行动态规划。
+
+对于一年中的任意一天：
+
+如果这一天不是必须出行的日期，那我们可以贪心地选择不买。这是因为如果今天不用出行，那么也不必购买通行证，并且通行证越晚买越好。所以有 \textit{dp}(i) = \textit{dp}(i + 1)dp(i)=dp(i+1)；
+
+如果这一天是必须出行的日期，我们可以选择买 11，77 或 3030 天的通行证。若我们购买了 jj 天的通行证，那么接下来的 j - 1j−1 天，我们都不再需要购买通行证，只需要考虑第 i + ji+j 天及以后即可。因此，我们有
+
+\textit{dp}(i) = \textit{cost}(j) + \max\{\textit{dp}(i + j)\}, \quad j \in \{1, 7, 30\}
+dp(i)=cost(j)+max{dp(i+j)},j∈{1,7,30}
+
+其中 \textit{cost}(j)cost(j) 表示 jj 天通行证的价格。为什么我们只需要考虑第 i+ji+j 天及以后呢？这里和第一条的贪心思路是一样的，如果我们需要购买通行证，那么一定越晚买越好，在握着一张有效的通行证的时候购买其它的通行证显然是不划算的。
+
+由于我们是倒着进行动态规划的，因此我们可以使用记忆化搜索，减少代码的编写难度。我们使用一个长度为 366366 的数组（因为天数是 [1, 365][1,365]，而数组的下标是从 00 开始的）存储所有的动态规划结果，这样所有的 \textit{dp}(i)dp(i) 只会被计算一次（和普通的动态规划相同），时间复杂度不会增大。
+
+最终的答案记为 \textit{dp}(1)dp(1)。
+
+JavaPython3C++Golang
+class Solution {
+    int[] costs;
+    Integer[] memo;
+    Set<Integer> dayset;
+
+    public int mincostTickets(int[] days, int[] costs) {
+        this.costs = costs;
+        memo = new Integer[366];
+        dayset = new HashSet();
+        for (int d: days) {
+            dayset.add(d);
+        }
+        return dp(1);
+    }
+
+    public int dp(int i) {
+        if (i > 365) {
+            return 0;
+        }
+        if (memo[i] != null) {
+            return memo[i];
+        }
+        if (dayset.contains(i)) {
+            memo[i] = Math.min(Math.min(dp(i + 1) + costs[0], dp(i + 7) + costs[1]), dp(i + 30) + costs[2]);
+        }
+        else {
+            memo[i] = dp(i + 1);
+        }
+        return memo[i];
+    }
+}
+复杂度分析
+
+时间复杂度：O(W)O(W)，其中 W = 365W=365 是旅行计划中日期的最大值，我们需要计算 WW 个解，而每个解最多需要查询 33 个其他的解，因此计算量为 O(3 * W)=O(W)O(3∗W)=O(W)。
+
+空间复杂度：O(W)O(W)，我们需要长度为 O(W)O(W) 的数组来存储所有的解。
+
+方法二：记忆化搜索（窗口变量型）
+思路
+
+方法一需要遍历一年中所有的天数，无论 \textit{days}days 的长度是多少。
+
+但是观察方法一的递推式，我们可以看到，如果我们查询 \textit{dp}(i)dp(i)，而第 ii 天我们又不需要出行的话，那么 \textit{dp}dp 函数会一直向后计算 \textit{dp}(i + 1) = \textit{dp}(i + 2) = \textit{dp}(i + 3)dp(i+1)=dp(i+2)=dp(i+3) 一直到一年结束或者有一天我们需要出行为止。那么我们其实可以直接跳过这些不需要出行的日期，直接找到下一个需要出行的日期。
+
+算法
+
+现在，我们令 \textit{dp}(i)dp(i) 表示能够完成从第 \textit{days}[i]days[i] 天到最后的旅行计划的最小花费（注意，不再是第 ii 天到最后的最小花费）。令 j_1j 
+1
+​	
+  是满足 \textit{days}[j_1] >= \textit{days}[i] + 1days[j 
+1
+​	
+ ]>=days[i]+1 的最小下标，j_7j 
+7
+​	
+  是满足 \textit{days}[j_7] >= \textit{days}[i] + 7days[j 
+7
+​	
+ ]>=days[i]+7 的最小下标， j_{30}j 
+30
+​	
+  是满足 \textit{days}[j_{30}] >= \textit{days}[i] + 30days[j 
+30
+​	
+ ]>=days[i]+30 的最小下标，那么就有：
+
+\textit{dp}(i) = \min(\textit{dp}(j_1) + \textit{costs}[0], \textit{dp}(j_7) + \textit{costs}[1], \textit{dp}(j_{30}) + \textit{costs}[2])
+dp(i)=min(dp(j 
+1
+​	
+ )+costs[0],dp(j 
+7
+​	
+ )+costs[1],dp(j 
+30
+​	
+ )+costs[2])
+
+JavaPython3C++Golang
+class Solution {
+    int[] days, costs;
+    Integer[] memo;
+    int[] durations = new int[]{1, 7, 30};
+
+    public int mincostTickets(int[] days, int[] costs) {
+        this.days = days;
+        this.costs = costs;
+        memo = new Integer[days.length];
+        return dp(0);
+    }
+
+    public int dp(int i) {
+        if (i >= days.length) {
+            return 0;
+        }
+        if (memo[i] != null) {
+            return memo[i];
+        }
+        memo[i] = Integer.MAX_VALUE;
+        int j = i;
+        for (int k = 0; k < 3; ++k) {
+            while (j < days.length && days[j] < days[i] + durations[k]) {
+                j++;
+            }
+            memo[i] = Math.min(memo[i], dp(j) + costs[k]);
+        }
+        return memo[i];
+    }
+}
+复杂度分析
+
+时间复杂度：O(N)O(N)，其中 NN 是出行日期的数量，我们需要计算 NN 个解，而计算每个解的过程中最多将指针挪动 3030 步，计算量为 O(30 * N)=O(N)O(30∗N)=O(N)。
+
+空间复杂度：O(N)O(N)，我们需要长度为 O(N)O(N) 的数组来存储所有的解。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/minimum-cost-for-tickets/solution/zui-di-piao-jie-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
