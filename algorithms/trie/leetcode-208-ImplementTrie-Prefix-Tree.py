186a85ca841e07b877264171365f9ceecc85c3bb
MecaCho <1099833328@qq.com>
2020-05-06 16:44:58 +0800

    add leetcode 208. Implement Trie (Prefix Tree);

---
+++ algorithms/trie/leetcode-208-ImplementTrie-Prefix-Tree.py
@@ -0,0 +1,98 @@
+'''
+208. 实现 Trie (前缀树)
+实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+
+示例:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // 返回 true
+trie.search("app");     // 返回 false
+trie.startsWith("app"); // 返回 true
+trie.insert("app");
+trie.search("app");     // 返回 true
+说明:
+
+你可以假设所有的输入都是由小写字母 a-z 构成的。
+保证所有输入均为非空字符串。
+
+208. Implement Trie (Prefix Tree)
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+'''
+
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: None
+        """
+        tree = self.root
+        for w in word:
+            if tree is None or not tree.get(w):
+                tree[w] = {}
+            tree = tree.get(w)
+        tree["end"] = True
+
+
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        tree = self.root
+        for w in word:
+            tree = tree.get(w)
+            if tree is None:
+                return False
+        return True if tree.get("end") else False
+
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        tree = self.root
+        for w in prefix:
+            tree = tree.get(w)
+            if tree is None:
+                return False
+        return True
+
+
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
