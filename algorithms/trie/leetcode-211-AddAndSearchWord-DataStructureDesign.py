628ecd7806d3eb8441436ebe854db9b5ca9df39e
MecaCho <1099833328@qq.com>
2020-05-06 17:51:45 +0800

    add leetcode 211. Add and Search Word - Data structure design ;

---
+++ algorithms/trie/leetcode-211-AddAndSearchWord-DataStructureDesign.py
@@ -0,0 +1,107 @@
+
+'''
+211. 添加与搜索单词 - 数据结构设计
+设计一个支持以下两种操作的数据结构：
+
+void addWord(word)
+bool search(word)
+search(word) 可以搜索文字或正则表达式字符串，字符串只包含字母 . 或 a-z 。 . 可以表示任何一个字母。
+
+示例:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+说明:
+
+你可以假设所有单词都是由小写字母 a-z 组成的。
+
+211. Add and Search Word - Data structure design
+Design a data structure that supports the following two operations:
+
+void addWord(word)
+bool search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+
+Example:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+Note:
+You may assume that all words are consist of lowercase letters a-z.
+'''
+
+
+
+
+class WordDictionary(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+
+
+    def addWord(self, word):
+        """
+        Adds a word into the data structure.
+        :type word: str
+        :rtype: None
+        """
+        tree = self.root
+        for w in word:
+            if tree is None or not tree.get(w):
+                tree[w] = {}
+            tree = tree.get(w)
+        tree["end"] = True
+
+
+    def search(self, word):
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        :type word: str
+        :rtype: bool
+        """
+        tree = self.root
+        def find(word, tree):
+            for w in word:
+                if tree.get(w) is None:
+                    return False
+                tree = tree.get(w)
+            return True if tree.get("end") else False
+        if "." in word:
+            self.vals = []
+            def bk(res, d):
+                if len(res) == len(word):
+                    if d.get("end"):
+                        self.vals.append(res)
+                else:
+                    for k, v in d.items():
+                        new_res = res+k
+                        flag = True
+                        for i in range(len(new_res)):
+                            if i >= len(word) or word[i] != "." and new_res[i] != word[i]:
+                                flag = False
+                        if flag and isinstance(v, dict):
+                            bk(new_res, v)
+            bk("", self.root)
+            return self.vals != []
+        else:
+            return find(word, tree)
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
