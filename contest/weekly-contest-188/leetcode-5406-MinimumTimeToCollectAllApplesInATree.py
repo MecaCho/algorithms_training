e1c0cffd3805e3088bc8db949fc4e0326780efb1
MecaCho <1099833328@qq.com>
2020-05-10 13:32:31 +0800

    add leetcode 5406. Minimum Time to Collect All Apples in a Tree;

---
+++ contest/weekly-contest-188/leetcode-5406-MinimumTimeToCollectAllApplesInATree.py
@@ -0,0 +1,130 @@
+'''
+5406. 收集树上所有苹果的最少时间
+给你一棵有 n 个节点的无向树，节点编号为 0 到 n-1 ，它们中有一些节点有苹果。通过树上的一条边，需要花费 1 秒钟。你从 节点 0 出发，请你返回最少需要多少秒，可以收集到所有苹果，并回到节点 0 。
+
+无向树的边由 edges 给出，其中 edges[i] = [fromi, toi] ，表示有一条边连接 from 和 toi 。除此以外，还有一个布尔数组 hasApple ，其中 hasApple[i] = true 代表节点 i 有一个苹果，否则，节点 i 没有苹果。
+
+
+
+示例 1：
+
+
+
+输入：n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
+输出：8
+解释：上图展示了给定的树，其中红色节点表示有苹果。一个能收集到所有苹果的最优方案由绿色箭头表示。
+示例 2：
+
+
+
+输入：n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,false,true,false]
+输出：6
+解释：上图展示了给定的树，其中红色节点表示有苹果。一个能收集到所有苹果的最优方案由绿色箭头表示。
+示例 3：
+
+输入：n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,false,false,false,false,false]
+输出：0
+
+
+提示：
+
+1 <= n <= 10^5
+edges.length == n-1
+edges[i].length == 2
+0 <= fromi, toi <= n-1
+fromi < toi
+hasApple.length == n
+
+5406. Minimum Time to Collect All Apples in a Tree
+Given an undirected tree consisting of n vertices numbered from 0 to n-1, which has some apples in their vertices. You spend 1 second to walk over one edge of the tree. Return the minimum time in seconds you have to spend in order to collect all apples in the tree starting at vertex 0 and coming back to this vertex.
+
+The edges of the undirected tree are given in the array edges, where edges[i] = [fromi, toi] means that exists an edge connecting the vertices fromi and toi. Additionally, there is a boolean array hasApple, where hasApple[i] = true means that vertex i has an apple, otherwise, it does not have any apple.
+
+
+
+Example 1:
+
+
+
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
+Output: 8
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.
+Example 2:
+
+
+
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,false,true,false]
+Output: 6
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.
+Example 3:
+
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,false,false,false,false,false]
+Output: 0
+
+
+Constraints:
+
+1 <= n <= 10^5
+edges.length == n-1
+edges[i].length == 2
+0 <= fromi, toi <= n-1
+fromi < toi
+hasApple.length == n
+'''
+
+
+
+
+
+class Solution(object):
+    def minTime(self, n, edges, hasApple):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type hasApple: List[bool]
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(edges)-1, 0, -1):
+            if hasApple[edges[i][1]]:
+                hasApple[edges[i][0]] = True
+        for i in range(len(edges)):
+            if hasApple[edges[i][1]]:
+                res += 2
+        return res
+
+
+# undo
+class Solution1(object):
+    def minTime(self, n, edges, hasApple):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type hasApple: List[bool]
+        :rtype: int
+        """
+        edges = [[0, 0]] + edges
+        length = len(edges)
+        marked = [None for i in range(length)]
+
+        def dfs(i):
+            if i == 0:
+                marked[i] = True
+                return 0
+            else:
+                marked[i] = True
+                return dfs(edges[i][0]) + 1
+
+        res = 0
+        for i in range(len(hasApple) - 1, -1, -1):
+            if hasApple[i] and not marked[i]:
+                res += dfs(i)
+                print(edges[i], dfs(i))
+        return res * 2
+
+# tips
+'''
+Note that if a node u contains an apple then all edges in the path from the root to the node u have to be used forward and backward (2 times).
+
+Therefore use a depth-first search (DFS) to check if an edge will be used or not.
+'''
