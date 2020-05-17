ddbc6cfa1c2b37c6d36d389fa28fd79e2d7c48dc
MecaCho <1099833328@qq.com>
2020-05-18 05:04:50 +0800

    add leetcode 62. Unique Paths

---
+++ algorithms/dynamic-programming/leetcode-62-UniquePaths.py
@@ -0,0 +1,158 @@
+
+
+
+'''
+62. 不同路径
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+问总共有多少条不同的路径？
+
+
+
+例如，上图是一个7 x 3 的网格。有多少可能的路径？
+
+
+
+示例 1:
+
+输入: m = 3, n = 2
+输出: 3
+解释:
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+示例 2:
+
+输入: m = 7, n = 3
+输出: 28
+
+
+提示：
+
+1 <= m, n <= 100
+题目数据保证答案小于等于 2 * 10 ^ 9
+
+62. Unique Paths
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+
+
+Constraints:
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+'''
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m + n < 2:
+            return 0
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 2000000000
+        return dp[-1][-1]
+
+
+# tips
+
+'''
+思路一：排列组合
+
+因为机器到底右下角，向下几步，向右几步都是固定的，
+
+比如，m=3, n=2，我们只要向下 1 步，向右 2 步就一定能到达终点。
+
+所以有 C_{m+n-2}^{m-1}C 
+m+n−2
+m−1
+​	
+ 
+
+Python
+def uniquePaths(self, m: int, n: int) -> int:
+        return int(math.factorial(m+n-2)/math.factorial(m-1)/math.factorial(n-1))
+思路二：动态规划
+
+我们令 dp[i][j] 是到达 i, j 最多路径
+
+动态方程：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+注意，对于第一行 dp[0][j]，或者第一列 dp[i][0]，由于都是在边界，所以只能为 1
+
+时间复杂度：O(m*n)O(m∗n)
+
+空间复杂度：O(m * n)O(m∗n)
+
+优化：因为我们每次只需要 dp[i-1][j],dp[i][j-1]
+
+所以我们只要记录这两个数，直接看代码吧！
+
+代码
+思路二：
+
+PythonJava
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1]*n] + [[1]+[0] * (n-1) for _ in range(m-1)]
+        #print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+优化1：空间复杂度 O(2n)O(2n)
+
+PythonJava
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        pre = [1] * n
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] = pre[j] + cur[j-1]
+            pre = cur[:]
+        return pre[-1]
+优化2：空间复杂度 O(n)O(n)
+
+PythonJava
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
+
+'''
