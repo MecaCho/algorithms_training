6a7739a599b8751b0f94da3121c2df4f33c01735
MecaCho <1099833328@qq.com>
2020-05-25 15:19:30 +0800

    add leetcode 1139. Largest 1-Bordered Square

---
+++ algorithms/dynamic-programming/leetcode-1139-Largest1-BorderedSquare.py
@@ -0,0 +1,84 @@
+'''
+1139. 最大的以 1 为边界的正方形
+给你一个由若干 0 和 1 组成的二维网格 grid，请你找出边界全部由 1 组成的最大 正方形 子网格，并返回该子网格中的元素数量。如果不存在，则返回 0。
+
+
+
+示例 1：
+
+输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+输出：9
+示例 2：
+
+输入：grid = [[1,1,0,0]]
+输出：1
+
+
+提示：
+
+1 <= grid.length <= 100
+1 <= grid[0].length <= 100
+grid[i][j] 为 0 或 1
+
+
+1139. Largest 1-Bordered Square
+Given a 2D grid of 0s and 1s, return the number of elements in the largest square subgrid that has all 1s on its border, or 0 if such a subgrid doesn't exist in the grid.
+
+
+
+Example 1:
+
+Input: grid = [[1,1,1],[1,0,1],[1,1,1]]
+Output: 9
+Example 2:
+
+Input: grid = [[1,1,0,0]]
+Output: 1
+
+
+Constraints:
+
+1 <= grid.length <= 100
+1 <= grid[0].length <= 100
+grid[i][j] is 0 or 1
+'''
+
+
+class Solution(object):
+    def largest1BorderedSquare(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        max_val = 0
+        def check_square(i, j, v):
+            if i + v >= len(grid) or j + v >= len(grid[0]):
+                return False
+            if grid[ i +v][j] == 1 and grid[i][ j +v] == 1 and grid[ i +v][ j +v] == 1:
+                for k in range(v):
+                    if grid[ i +k][j] != 1 or grid[i][ j +k] != 1 or grid[ i +v][ j +k] != 1 or grid[ i +k][ j +v] != 1:
+                        return False
+                return True
+            return False
+        max_len = max(len(grid), len(grid[0]))
+        for i in range(len(grid)):
+            # print(grid[i])
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+
+                    cur = max_len
+                    while cur >= max_val:
+                        if check_square(i, j, cur):
+                            max_val = cur + 1
+                            break
+                        cur -= 1
+        return max_val * max_val
+
+
+# tips
+
+'''
+For each square, know how many ones are up, left, down, and right of this square. You can find it in O(N^2) using dynamic programming.
+
+Now for each square ( O(N^3) ), we can evaluate whether that square is 1-bordered in O(1).
+'''
