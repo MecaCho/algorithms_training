5ec7691569660d1d61ddf1c50e48692ab26367fa
MecaCho <1099833328@qq.com>
2020-05-28 23:58:27 +0800

    add leetcode 203. Remove Linked List Elements python solution;

--- algorithms/linked_list/leetcode-203-RemoveLinkedListElements.py
+++ algorithms/linked_list/leetcode-203-RemoveLinkedListElements.py
@@ -20,6 +20,30 @@ Output: 1->2->3->4->5
 '''
 
 
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        pre = dummy
+        while head:
+            if head.val == val:
+                pre.next = head.next
+            else:
+                pre = pre.next
+            head = head.next
+        return dummy.next
+
 
 
 
