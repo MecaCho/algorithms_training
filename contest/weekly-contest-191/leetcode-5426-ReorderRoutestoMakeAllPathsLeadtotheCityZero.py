6891748e381947addb96d5e483794b15cbb3acf9
MecaCho <1099833328@qq.com>
2020-05-31 23:21:44 +0800

    add weekly contest 5426. Reorder Routes to Make All Paths Lead to the City Zero;

---
+++ contest/weekly-contest-191/leetcode-5426-ReorderRoutestoMakeAllPathsLeadtotheCityZero.py
@@ -0,0 +1,106 @@
+
+'''
+5426. 重新规划路线
+n 座城市，从 0 到 n-1 编号，其间共有 n-1 条路线。因此，要想在两座不同城市之间旅行只有唯一一条路线可供选择（路线网形成一颗树）。去年，交通运输部决定重新规划路线，以改变交通拥堵的状况。
+
+路线用 connections 表示，其中 connections[i] = [a, b] 表示从城市 a 到 b 的一条有向路线。
+
+今年，城市 0 将会举办一场大型比赛，很多游客都想前往城市 0 。
+
+请你帮助重新规划路线方向，使每个城市都可以访问城市 0 。返回需要变更方向的最小路线数。
+
+题目数据 保证 每个城市在重新规划路线方向后都能到达城市 0 。
+
+
+
+示例 1：
+
+
+
+输入：n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+输出：3
+解释：更改以红色显示的路线的方向，使每个城市都可以到达城市 0 。
+示例 2：
+
+
+
+输入：n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+输出：2
+解释：更改以红色显示的路线的方向，使每个城市都可以到达城市 0 。
+示例 3：
+
+输入：n = 3, connections = [[1,0],[2,0]]
+输出：0
+
+
+提示：
+
+2 <= n <= 5 * 10^4
+connections.length == n-1
+connections[i].length == 2
+0 <= connections[i][0], connections[i][1] <= n-1
+connections[i][0] != connections[i][1]
+
+5426. Reorder Routes to Make All Paths Lead to the City Zero
+There are n cities numbered from 0 to n-1 and n-1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+
+Roads are represented by connections where connections[i] = [a, b] represents a road from city a to b.
+
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+
+Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+
+It's guaranteed that each city can reach the city 0 after reorder.
+
+
+
+Example 1:
+
+
+
+Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+Output: 3
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+Example 2:
+
+
+
+Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+Output: 2
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+Example 3:
+
+Input: n = 3, connections = [[1,0],[2,0]]
+Output: 0
+
+
+Constraints:
+
+2 <= n <= 5 * 10^4
+connections.length == n-1
+connections[i].length == 2
+0 <= connections[i][0], connections[i][1] <= n-1
+connections[i][0] != connections[i][1]
+'''
+
+
+class Solution(object):
+    def minReorder(self, n, connections):
+        """
+        :type n: int
+        :type connections: List[List[int]]
+        :rtype: int
+        """
+        hash_map = {}
+        res = 0
+        for i in range(len(connections)):
+            if connections[i][0] == 0 or connections[i][0] in hash_map:
+                res += 1
+                connections[i] = connections[i][::-1]
+            hash_map[connections[i][0]] = connections[i][1]
+
+        return res
+
+'''
+Treat the graph as undirected. Start a dfs from the root, if you come across an edge in the forward direction, you need to reverse the edge.
+'''
