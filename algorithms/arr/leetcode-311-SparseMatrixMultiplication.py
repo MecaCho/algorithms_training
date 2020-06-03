cd42e604a7180fcad95846e8eb0b3745924f5f05
MecaCho <1099833328@qq.com>
2020-06-03 23:16:10 +0800

    add leetcode 311. Sparse Matrix Multiplication;

---
+++ algorithms/arr/leetcode-311-SparseMatrixMultiplication.py
@@ -0,0 +1,76 @@
+'''
+311. 稀疏矩阵的乘法
+给你两个 稀疏矩阵 A 和 B，请你返回 AB 的结果。你可以默认 A 的列数等于 B 的行数。
+
+请仔细阅读下面的示例。
+
+
+
+示例：
+
+输入：
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+输出：
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+
+311. Sparse Matrix Multiplication
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+Input:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+Output:
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+'''
+
+
+
+
+
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        C = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+        for i in range(len(A)):
+            for j in range(len(B[0])):
+                res = 0
+                for k in range(len(A[i])):
+                    res += A[i][k] * B[k][j]
+                C[i][j] = res
+
+        return C
