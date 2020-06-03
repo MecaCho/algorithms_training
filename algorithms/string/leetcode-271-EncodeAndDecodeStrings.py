80b276388de58c6428f251494294bc9cd56cf034
MecaCho <1099833328@qq.com>
2020-06-03 23:35:30 +0800

    add leetcode 271. Encode and Decode Strings;

---
+++ algorithms/string/leetcode-271-EncodeAndDecodeStrings.py
@@ -0,0 +1,95 @@
+
+
+
+
+'''
+271. 字符串的编码与解码
+请你设计一个算法，可以将一个 字符串列表 编码成为一个 字符串。这个编码后的字符串是可以通过网络进行高效传送的，并且可以在接收端被解码回原来的字符串列表。
+
+1 号机（发送方）有如下函数：
+
+string encode(vector<string> strs) {
+  // ... your code
+  return encoded_string;
+}
+2 号机（接收方）有如下函数：
+
+vector<string> decode(string s) {
+  //... your code
+  return strs;
+}
+1 号机（发送方）执行：
+
+string encoded_string = encode(strs);
+2 号机（接收方）执行：
+
+vector<string> strs2 = decode(encoded_string);
+此时，2 号机（接收方）的 strs2 需要和 1 号机（发送方）的 strs 相同。
+
+请你来实现这个 encode 和 decode 方法。
+
+注意：
+
+因为字符串可能会包含 256 个合法 ascii 字符中的任何字符，所以您的算法必须要能够处理任何可能会出现的字符。
+请勿使用 “类成员”、“全局变量” 或 “静态变量” 来存储这些状态，您的编码和解码算法应该是非状态依赖的。
+请不要依赖任何方法库，例如 eval 又或者是 serialize 之类的方法。本题的宗旨是需要您自己实现 “编码” 和 “解码” 算法。
+
+
+271. Encode and Decode Strings
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+Machine 1 (sender) has the function:
+
+string encode(vector<string> strs) {
+  // ... your code
+  return encoded_string;
+}
+Machine 2 (receiver) has the function:
+vector<string> decode(string s) {
+  //... your code
+  return strs;
+}
+So Machine 1 does:
+
+string encoded_string = encode(strs);
+and Machine 2 does:
+
+vector<string> strs2 = decode(encoded_string);
+strs2 in Machine 2 should be the same as strs in Machine 1.
+
+Implement the encode and decode methods.
+
+
+
+Note:
+
+The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
+Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+'''
+
+import json
+
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+
+        :type strs: List[str]
+        :rtype: str
+        """
+        # return ",".join(strs)
+        return json.dumps(strs)
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        # return s.split(",")
+        return json.loads(s)
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
