2d8a4e33d8e784154d825011637631d3833fdb1f
MecaCho <1099833328@qq.com>
2020-06-11 01:02:16 +0800

    add leetcode 144. Binary Tree Preorder Traversal;

---
+++ algorithms/tree/leetcode-144-BinaryTreePreorderTraversal.py
@@ -0,0 +1,193 @@
+'''
+144. 二叉树的前序遍历
+给定一个二叉树，返回它的 前序 遍历。
+
+ 示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,2,3]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+144. Binary Tree Preorder Traversal
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # if not root:
+            # return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right) if root else []
+
+
+# golang solutions
+
+'''
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+
+    if root == nil{
+        return []int{}
+    }
+    res := []int{root.Val}
+
+    res = append(res, preorderTraversal(root.Left)...)
+    res = append(res, preorderTraversal(root.Right)...)
+
+    return res
+}
+'''
+
+# solutions
+
+'''
+有两种通用的遍历树的策略：
+
+深度优先搜索（DFS）
+
+在这个策略中，我们采用深度作为优先级，以便从跟开始一直到达某个确定的叶子，然后再返回根到达另一个分支。
+
+深度优先搜索策略又可以根据根节点、左孩子和右孩子的相对顺序被细分为前序遍历，中序遍历和后序遍历。
+
+宽度优先搜索（BFS）
+
+我们按照高度顺序一层一层的访问整棵树，高层次的节点将会比低层次的节点先被访问到。
+
+下图中的顶点按照访问的顺序编号，按照 1-2-3-4-5 的顺序来比较不同的策略。
+
+
+
+本问题就是用宽度优先搜索遍历来划分层次：[[1], [2, 3], [4, 5]]。
+
+方法 1：迭代
+算法
+
+首先，定义树的存储结构 TreeNode。
+
+JavaPython
+
+class TreeNode(object):
+    """ Definition of a binary tree node."""
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+从根节点开始，每次迭代弹出当前栈顶元素，并将其孩子节点压入栈中，先压右孩子再压左孩子。
+
+在这个算法中，输出到最终结果的顺序按照 Top->Bottom 和 Left->Right，符合前序遍历的顺序。
+
+JavaPython
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        
+        stack, output = [root, ], []
+        
+        while stack:
+            root = stack.pop()
+            if root is not None:
+                output.append(root.val)
+                if root.right is not None:
+                    stack.append(root.right)
+                if root.left is not None:
+                    stack.append(root.left)
+        
+        return output
+算法复杂度
+
+时间复杂度：访问每个节点恰好一次，时间复杂度为 O(N)O(N) ，其中 NN 是节点的个数，也就是树的大小。
+空间复杂度：取决于树的结构，最坏情况存储整棵树，因此空间复杂度是 O(N)O(N)。
+方法 2：莫里斯遍历
+方法基于 莫里斯的文章，可以优化空间复杂度。算法不会使用额外空间，只需要保存最终的输出结果。如果实时输出结果，那么空间复杂度是 O(1)O(1)。
+
+算法
+
+算法的思路是从当前节点向下访问先序遍历的前驱节点，每个前驱节点都恰好被访问两次。
+
+首先从当前节点开始，向左孩子走一步然后沿着右孩子一直向下访问，直到到达一个叶子节点（当前节点的中序遍历前驱节点），所以我们更新输出并建立一条伪边 predecessor.right = root 更新这个前驱的下一个点。如果我们第二次访问到前驱节点，由于已经指向了当前节点，我们移除伪边并移动到下一个顶点。
+
+如果第一步向左的移动不存在，就直接更新输出并向右移动。
+
+
+1 / 16
+
+JavaPython
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        node, output = root, []
+        while node:  
+            if not node.left: 
+                output.append(node.val)
+                node = node.right 
+            else: 
+                predecessor = node.left 
+
+                while predecessor.right and predecessor.right is not node: 
+                    predecessor = predecessor.right 
+
+                if not predecessor.right:
+                    output.append(node.val)
+                    predecessor.right = node  
+                    node = node.left  
+                else:
+                    predecessor.right = None
+                    node = node.right         
+
+        return output
+算法复杂度
+
+时间复杂度：每个前驱恰好访问两次，因此复杂度是 O(N)O(N)，其中 NN 是顶点的个数，也就是树的大小。
+空间复杂度：我们在计算中不需要额外空间，但是输出需要包含 NN 个元素，因此空间复杂度为 O(N)O(N)。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/er-cha-shu-de-qian-xu-bian-li-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
