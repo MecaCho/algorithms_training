341565670b1fc9a835001d8ef73a936c9dca36c7
MecaCho <1099833328@qq.com>
2020-06-16 22:31:29 +0800

    add leetcode 1. Two Sum;

--- algorithms/old/leetcode-001-two-num-sum.py
+++ algorithms/old/leetcode-001-two-num-sum.py
@@ -1,3 +1,19 @@
+'''
+1. Two Sum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -11,3 +27,19 @@ class Solution(object):
                 return num_dict[target - num], i
             num_dict[num] = i
         return None
+
+
+class Solution1(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in hash_map:
+                hash_map[target - nums[i]] = i
+            else:
+                return [hash_map[nums[i]], i]
