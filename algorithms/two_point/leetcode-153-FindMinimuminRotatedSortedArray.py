1a9443054acd743769e0a74ff7951e3964be5d47
MecaCho <1099833328@qq.com>
2020-06-17 12:09:41 +0800

    add leetcode 153. Find Minimum in Rotated Sorted Array;

---
+++ algorithms/two_point/leetcode-153-FindMinimuminRotatedSortedArray.py
@@ -0,0 +1,149 @@
+'''
+153. 寻找旋转排序数组中的最小值
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+请找出其中最小的元素。
+
+你可以假设数组中不存在重复元素。
+
+示例 1:
+
+输入: [3,4,5,1,2]
+输出: 1
+示例 2:
+
+输入: [4,5,6,7,0,1,2]
+输出: 0
+
+153. Find Minimum in Rotated Sorted Array
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+Example 1:
+
+Input: [3,4,5,1,2]
+Output: 1
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+'''
+
+
+
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i, j = 0, len(nums) - 1
+        while i < j:
+            # print(i, j)
+            mid = (i + j) / 2
+            if nums[mid] > nums[j]:
+                i = mid + 1
+            else:
+                j = mid
+        # print(i, j)
+        return nums[i]
+
+# solution
+
+class Solution1:
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # set left and right bounds
+        left, right = 0, len(nums) - 1
+
+        # left and right both converge to the minimum index;
+        # DO NOT use left <= right because that would loop forever
+        while left < right:
+            # find the middle value between the left and right bounds (their average);
+            # can equivalently do: mid = left + (right - left) // 2,
+            # if we are concerned left + right would cause overflow (which would occur
+            # if we are searching a massive array using a language like Java or C that has
+            # fixed size integer types)
+            mid = (left + right) // 2
+
+            # the main idea for our checks is to converge the left and right bounds on the start
+            # of the pivot, and never disqualify the index for a possible minimum value.
+
+            # in normal binary search, we have a target to match exactly,
+            # and would have a specific branch for if nums[mid] == target.
+            # we do not have a specific target here, so we just have simple if/else.
+
+            if nums[mid] > nums[right]:
+                # we KNOW the pivot must be to the right of the middle:
+                # if nums[mid] > nums[right], we KNOW that the
+                # pivot/minimum value must have occurred somewhere to the right
+                # of mid, which is why the values wrapped around and became smaller.
+
+                # example:  [3,4,5,6,7,8,9,1,2]
+                # in the first iteration, when we start with mid index = 4, right index = 9.
+                # if nums[mid] > nums[right], we know that at some point to the right of mid,
+                # the pivot must have occurred, which is why the values wrapped around
+                # so that nums[right] is less then nums[mid]
+
+                # we know that the number at mid is greater than at least
+                # one number to the right, so we can use mid + 1 and
+                # never consider mid again; we know there is at least
+                # one value smaller than it on the right
+                left = mid + 1
+
+            else:
+                # here, nums[mid] <= nums[right]:
+                # we KNOW the pivot must be at mid or to the left of mid:
+                # if nums[mid] <= nums[right], we KNOW that the pivot was not encountered
+                # to the right of middle, because that means the values would wrap around
+                # and become smaller (which is caught in the above if statement).
+                # this leaves the possible pivot point to be at index <= mid.
+
+                # example: [8,9,1,2,3,4,5,6,7]
+                # in the first iteration, when we start with mid index = 4, right index = 9.
+                # if nums[mid] <= nums[right], we know the numbers continued increasing to
+                # the right of mid, so they never reached the pivot and wrapped around.
+                # therefore, we know the pivot must be at index <= mid.
+
+                # we know that nums[mid] <= nums[right].
+                # therefore, we know it is possible for the mid index to store a smaller
+                # value than at least one other index in the list (at right), so we do
+                # not discard it by doing right = mid - 1. it still might have the minimum value.
+                right = mid
+
+        # at this point, left and right converge to a single index (for minimum value) since
+        # our if/else forces the bounds of left/right to shrink each iteration:
+
+        # when left bound increases, it does not disqualify a value
+        # that could be smaller than something else (we know nums[mid] > nums[right],
+        # so nums[right] wins and we ignore mid and everything to the left of mid).
+
+        # when right bound decreases, it also does not disqualify a
+        # value that could be smaller than something else (we know nums[mid] <= nums[right],
+        # so nums[mid] wins and we keep it for now).
+
+        # so we shrink the left/right bounds to one value,
+        # without ever disqualifying a possible minimum
+        return nums[left]
+
+# tips
+
+'''
+Array was originally in ascending order. Now that the array is rotated, there would be a point in the array where there is a small deflection from the increasing sequence. eg. The array would be something like [4, 5, 6, 7, 0, 1, 2].
+
+You can divide the search space into two and see which direction to go. Can you think of an algorithm which has O(logN) search complexity?
+
+All the elements to the left of inflection point > first element of the array.
+All the elements to the right of inflection point < first element of the array.
+'''
