ee6b92c76fb9f72e39f3ed188923ce82ea778be7
MecaCho <1099833328@qq.com>
2020-06-20 18:30:07 +0800

    add leetcode 234. Palindrome Linked List;

---
+++ algorithms/linked_list/leetcode-234-PalindromeLinkedList.py
@@ -0,0 +1,81 @@
+'''
+234. 回文链表
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+
+234. Palindrome Linked List
+Easy
+
+3013
+
+347
+
+Add to List
+
+Share
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+Follow up:
+Could you do it in O(n) time and O(1) space?
+
+
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return True
+        fast, slow = head, head
+        pre = ListNode(0)
+        pre.next = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+
+            tmp = slow.next
+            slow.next = pre
+            pre = slow
+
+            slow = tmp
+
+        if fast:
+            slow = slow.next
+
+        while slow and pre:
+            if pre.val != slow.val:
+                return False
+            slow = slow.next
+            pre = pre.next
+        return True
+# Runtime: 60 ms, faster than 98.24% of Python online submissions for Palindrome Linked List.
+# Memory Usage: 31.7 MB, less than 85.78% of Python online submissions for Palindrome Linked List.
+
