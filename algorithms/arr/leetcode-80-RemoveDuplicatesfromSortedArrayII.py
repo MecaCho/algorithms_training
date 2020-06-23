cb117cd4f646bb1031888c5bc41cdb43c915dbaf
MecaCho <1099833328@qq.com>
2020-06-23 23:56:10 +0800

    add leetcode 80. Remove Duplicates from Sorted Array II;

--- algorithms/arr/leetcode-80-RemoveDuplicatesfromSortedArrayII.py
+++ algorithms/arr/leetcode-80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,32 +1,28 @@
 '''
-26. 删除排序数组中的重复项
-给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+80. 删除排序数组中的重复项 II
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
 
-不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
-
-
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
 
 示例 1:
 
-给定数组 nums = [1,1,2],
+给定 nums = [1,1,1,2,2,3],
 
-函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。
 
 你不需要考虑数组中超出新长度后面的元素。
 示例 2:
 
-给定 nums = [0,0,1,1,1,2,2,3,3,4],
+给定 nums = [0,0,1,1,1,1,2,3,3],
 
-函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。
 
 你不需要考虑数组中超出新长度后面的元素。
-
-
 说明:
 
 为什么返回数值是整数，但输出的答案是数组呢?
 
-请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
 
 你可以想象内部操作如下:
 
@@ -39,23 +35,23 @@ for (int i = 0; i < len; i++) {
     print(nums[i]);
 }
 
-26. Remove Duplicates from Sorted Array
-Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+80. Remove Duplicates from Sorted Array II
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
 Example 1:
 
-Given nums = [1,1,2],
+Given nums = [1,1,1,2,2,3],
 
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
 
 It doesn't matter what you leave beyond the returned length.
 Example 2:
 
-Given nums = [0,0,1,1,1,2,2,3,3,4],
+Given nums = [0,0,1,1,1,1,2,3,3],
 
-Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
 
 It doesn't matter what values are set beyond the returned length.
 Clarification:
@@ -77,6 +73,7 @@ for (int i = 0; i < len; i++) {
 '''
 
 
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -84,21 +81,24 @@ class Solution(object):
         :rtype: int
         """
         i = 0
-        n = len(nums)
-        while i < len(nums) - 1:
-            if nums[i] == nums[i+1]:
-                nums.pop(i)
-            else:
+        for j, num in enumerate(nums):
+            if j < 2 or num != nums[i-2]:
+                nums[i] = num
                 i += 1
+        return i
 
 
-# golang solution
+
+# golang
+
 
 '''
 func removeDuplicates(nums []int) int {
+
     i := 0
-    for j := range nums {
-        if j == 0 || nums[j] != nums[j-1]{
+
+    for j := range nums{
+        if j == 0 || j == 1 ||  nums[j] != nums[i-2]{
             nums[i] = nums[j]
             i++
         }
@@ -107,8 +107,3 @@ func removeDuplicates(nums []int) int {
 }
 '''
 
-# tips
-
-'''
-
-'''
