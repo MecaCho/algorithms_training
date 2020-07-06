2e777ab63b62f6b05063d55b9d7cae421186117e
MecaCho <1099833328@qq.com>
2020-07-06 12:40:32 +0800

    add leetcode UniqePath II tips;

--- algorithms/dynamic-programming/leetcode-63-UniquePathsII.py
+++ algorithms/dynamic-programming/leetcode-63-UniquePathsII.py
@@ -84,6 +84,25 @@ class Solution(object):
         return dp[-1][-1]
 
 
+# tips
+
+'''
+The robot can only move either down or right. Hence any cell in the first row can only be reached from the cell left to it. However, if any cell has an obstacle, you don't let that cell contribute to any path. So, for the first row, the number of ways will simply be
+if obstacleGrid[i][j] is not an obstacle
+     obstacleGrid[i,j] = obstacleGrid[i,j - 1] 
+else
+     obstacleGrid[i,j] = 0
+You can do a similar processing for finding out the number of ways of reaching the cells in the first column.
+
+For any other cell, we can find out the number of ways of reaching it, by making use of the number of ways of reaching the cell directly above it and the cell to the left of it in the grid. This is because these are the only two directions from which the robot can come to the current cell.
+
+Since we are making use of pre-computed values along the iteration, this becomes a dynamic programming problem.
+if obstacleGrid[i][j] is not an obstacle
+     obstacleGrid[i,j] = obstacleGrid[i,j - 1]  + obstacleGrid[i - 1][j]
+else
+     obstacleGrid[i,j] = 0
+'''
+
 # solutions
 
 
