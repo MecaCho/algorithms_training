7ed7392a4898f217803eb4eb8f80f3bd0a807db1
MecaCho <1099833328@qq.com>
2020-07-14 23:37:22 +0800

    add leetcode contest 1510. Stone Game IV golang solution;

--- contest/bi-weekly-contest-30/leetcode-1510-StoneGameIV.py
+++ contest/bi-weekly-contest-30/leetcode-1510-StoneGameIV.py
@@ -112,6 +112,26 @@ class Solution(object):
         return dp[n] == 1
 
 
+# golang solution
+
+'''
+func winnerSquareGame(n int) bool {
+
+    dp := []int{0}
+
+    for i := 1; i <= n; i++{
+        dp = append(dp, 0)
+        for j := 1; j*j <= i;j++{
+            if dp[i-j*j] == 0{
+                dp[i] = 1
+                break
+            }
+        }
+    }
+    return dp[n] == 1
+}
+'''
+
 # tips
 
 '''
