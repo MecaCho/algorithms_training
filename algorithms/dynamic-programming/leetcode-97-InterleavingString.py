6be19ee2336752a8a9fdf3481b4d8bab72a23f72
MecaCho <1099833328@qq.com>
2020-07-18 20:04:26 +0800

    add leetcode 97. Interleaving String;

---
+++ algorithms/dynamic-programming/leetcode-97-InterleavingString.py
@@ -0,0 +1,95 @@
+'''
+97. Interleaving String
+Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+Example 1:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+
+97. 交错字符串
+给定三个字符串 s1, s2, s3, 验证 s3 是否是由 s1 和 s2 交错组成的。
+
+示例 1:
+
+输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+输出: true
+示例 2:
+
+输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+输出: false
+'''
+
+
+
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        if len(s3) != len(s1) + len(s2):
+            return False
+        if not s1 or not s2:
+            return s1+s2 == s3
+        dp = []
+        for i in range(len(s1)+1):
+            dp.append([])
+            for j in range(len(s2)+1):
+                dp[i].append(False)
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                else:
+                    dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or  (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+        # print(dp)
+
+        return dp[-1][-1]
+
+
+# solutions
+
+'''
+ bool isInterleave(string s1, string s2, string s3) {
+    
+    if(s3.length() != s1.length() + s2.length())
+        return false;
+    
+    bool table[s1.length()+1][s2.length()+1];
+    
+    for(int i=0; i<s1.length()+1; i++)
+        for(int j=0; j< s2.length()+1; j++){
+            if(i==0 && j==0)
+                table[i][j] = true;
+            else if(i == 0)
+                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+            else if(j == 0)
+                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+            else
+                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+        }
+        
+    return table[s1.length()][s2.length()];
+}
+Here is some explanation:
+
+DP table represents if s3 is interleaving at (i+j)th position when s1 is at ith position, and s2 is at jth position. 0th position means empty string.
+
+So if both s1 and s2 is currently empty, s3 is empty too, and it is considered interleaving. 
+If only s1 is empty, then if previous s2 position is interleaving and current s2 position 
+char is equal to s3 current position char, it is considered interleaving. 
+similar idea applies to when s2 is empty. 
+when both s1 and s2 is not empty, then if we arrive i, j from i-1, j, 
+then if i-1,j is already interleaving and i and current s3 position equal, 
+it s interleaving. If we arrive i,j from i, j-1, then if i, j-1 is already interleaving 
+and j and current s3 position equal. it is interleaving.
+'''
