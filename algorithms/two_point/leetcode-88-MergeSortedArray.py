c5516a050ccdb5fc45d664aa79f675213afc3f46
MecaCho <1099833328@qq.com>
2020-07-23 22:17:34 +0800

    add merge arr tips;

--- algorithms/two_point/leetcode-88-MergeSortedArray.py
+++ algorithms/two_point/leetcode-88-MergeSortedArray.py
@@ -65,3 +65,10 @@ class Solution(object):
         return nums1
 
 
+# tips
+
+'''
+You can easily solve this problem if you simply think about two elements at a time rather than two arrays. We know that each of the individual arrays is sorted. What we don't know is how they will intertwine. Can we take a local decision and arrive at an optimal solution?
+
+If you simply consider one element each at a time from the two arrays and make a decision and proceed accordingly, you will arrive at the optimal solution.
+'''
