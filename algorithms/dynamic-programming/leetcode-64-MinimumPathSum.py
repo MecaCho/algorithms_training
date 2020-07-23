d1acc943d054a5c813b47aad4215385f462ee43a
MecaCho <1099833328@qq.com>
2020-07-23 12:22:25 +0800

    add 64. Minimum Path Sum golang solution;

--- algorithms/dynamic-programming/leetcode-64-MinimumPathSum.py
+++ algorithms/dynamic-programming/leetcode-64-MinimumPathSum.py
@@ -50,6 +50,8 @@ class Solution1(object):
         return grid[-1][-1] if grid and grid[0] else None
 
 
+
+
 class Solution(object):
     def minPathSum(self, grid):
         """
@@ -79,6 +81,35 @@ class Solution(object):
         print(dp)
         return dp[-1][-1]
 
+# golang solution
+
+'''
+func minPathSum(grid [][]int) int {
+	colLen := len(grid)
+	rowLen := len(grid[0])
+	if colLen+rowLen < 2 {
+		return 0
+	}
+
+	for i := 0; i < colLen; i++ {
+		for j := 0; j < rowLen; j++ {
+			if i == 0 && j != 0 {
+				grid[i][j] += grid[i][j-1]
+			} else if j == 0 && i != 0 {
+				grid[i][j] += grid[i-1][j]
+			} else if i != 0 && j != 0 {
+				if grid[i][j-1] < grid[i-1][j] {
+					grid[i][j] += grid[i][j-1]
+				} else {
+					grid[i][j] += grid[i-1][j]
+				}
+			}
+
+		}
+	}
+	return grid[colLen-1][rowLen-1]
+}
+'''
 
 '''
 概述
