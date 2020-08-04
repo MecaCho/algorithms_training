b5a89a7d1c3625b1f164692285c71aa0b02f8b88
MecaCho <1099833328@qq.com>
2020-08-05 00:03:35 +0800

    add rob;

--- algorithms/leetcode-337-houseRob3.py
+++ algorithms/leetcode-337-houseRob3.py
@@ -89,3 +89,84 @@ class Solution(object):
             return [do_it, not_do_it]
 
         return max(deep_traval_tree(root))
+
+# solution
+
+'''
+方法一：动态规划
+思路与算法
+
+简化一下这个问题：一棵二叉树，树上的每个点都有对应的权值，每个点有两种状态（选中和不选中），问在不能同时选中有父子关系的点的情况下，能选中的点的最大权值和是多少。
+
+我们可以用 f(o)f(o) 表示选择 oo 节点的情况下，oo 节点的子树上被选择的节点的最大权值和；g(o)g(o) 表示不选择 oo 节点的情况下，oo 节点的子树上被选择的节点的最大权值和；ll 和 rr 代表 oo 的左右孩子。
+
+当 oo 被选中时，oo 的左右孩子都不能被选中，故 oo 被选中情况下子树上被选中点的最大权值和为 ll 和 rr 不被选中的最大权值和相加，即 f(o) = g(l) + g(r)f(o)=g(l)+g(r)。
+当 oo 不被选中时，oo 的左右孩子可以被选中，也可以不被选中。对于 oo 的某个具体的孩子 xx，它对 oo 的贡献是 xx 被选中和不被选中情况下权值和的较大值。故 g(o) = \max \{ f(l) , g(l)\}+\max\{ f(r) , g(r) \}g(o)=max{f(l),g(l)}+max{f(r),g(r)}。
+至此，我们可以用哈希映射来存 ff 和 gg 的函数值，用深度优先搜索的办法后序遍历这棵二叉树，我们就可以得到每一个节点的 ff 和 gg。根节点的 ff 和 gg 的最大值就是我们要找的答案。
+
+我们不难给出这样的实现：
+
+C++JavaJavaScript
+
+class Solution {
+public:
+    unordered_map <TreeNode*, int> f, g;
+
+    void dfs(TreeNode* o) {
+        if (!o) {
+            return;
+        }
+        dfs(o->left);
+        dfs(o->right);
+        f[o] = o->val + g[o->left] + g[o->right];
+        g[o] = max(f[o->left], g[o->left]) + max(f[o->right], g[o->right]);
+    }
+
+    int rob(TreeNode* o) {
+        dfs(o);
+        return max(f[o], g[o]);
+    }
+};
+假设二叉树的节点个数为 nn。
+
+我们可以看出，以上的算法对二叉树做了一次后序遍历，时间复杂度是 O(n)O(n)；由于递归会使用到栈空间，空间代价是 O(n)O(n)，哈希映射的空间代价也是 O(n)O(n)，故空间复杂度也是 O(n)O(n)。
+
+我们可以做一个小小的优化，我们发现无论是 f(o)f(o) 还是 g(o)g(o)，他们最终的值只和 f(l)f(l)、g(l)g(l)、f(r)f(r)、g(r)g(r) 有关，所以对于每个节点，我们只关心它的孩子节点们的 ff 和 gg 是多少。我们可以设计一个结构，表示某个节点的 ff 和 gg 值，在每次递归返回的时候，都把这个点对应的 ff 和 gg 返回给上一级调用，这样可以省去哈希映射的空间。
+
+代码如下。
+
+代码
+
+C++JavaJavaScriptGolangC
+
+func rob(root *TreeNode) int {
+    val := dfs(root)
+    return max(val[0], val[1])
+}
+
+func dfs(node *TreeNode) []int {
+    if node == nil {
+        return []int{0, 0}
+    }
+    l, r := dfs(node.Left), dfs(node.Right)
+    selected := node.Val + l[1] + r[1]
+    notSelected := max(l[0], l[1]) + max(r[0], r[1])
+    return []int{selected, notSelected}
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+复杂度分析
+
+时间复杂度：O(n)O(n)。上文中已分析。
+空间复杂度：O(n)O(n)。虽然优化过的版本省去了哈希映射的空间，但是栈空间的使用代价依旧是 O(n)O(n)，故空间复杂度不变。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/house-robber-iii/solution/da-jia-jie-she-iii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
