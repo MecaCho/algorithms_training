ce2e6eeeab8ecfbf7cb0e6311c103d42101991ea
MecaCho <1099833328@qq.com>
2020-08-08 23:31:59 +0800

    add 99. Recover Binary Search Tree;

---
+++ algorithms/tree/leetcode-99-RecoverBinarySearchTree.py
@@ -0,0 +1,124 @@
+'''
+99. Recover Binary Search Tree
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+
+Example 1:
+
+Input: [1,3,null,null,2]
+
+   1
+  /
+ 3
+  \
+   2
+
+Output: [3,1,null,null,2]
+
+   3
+  /
+ 1
+  \
+   2
+Example 2:
+
+Input: [3,1,4,null,null,2]
+
+  3
+ / \
+1   4
+   /
+  2
+
+Output: [2,1,4,null,null,3]
+
+  2
+ / \
+1   4
+   /
+  3
+Follow up:
+
+A solution using O(n) space is pretty straight forward.
+Could you devise a constant space solution?
+
+99. 恢复二叉搜索树
+二叉搜索树中的两个节点被错误地交换。
+
+请在不改变其结构的情况下，恢复这棵树。
+
+示例 1:
+
+输入: [1,3,null,null,2]
+
+   1
+  /
+ 3
+  \
+   2
+
+输出: [3,1,null,null,2]
+
+   3
+  /
+ 1
+  \
+   2
+示例 2:
+
+输入: [3,1,4,null,null,2]
+
+  3
+ / \
+1   4
+   /
+  2
+
+输出: [2,1,4,null,null,3]
+
+  2
+ / \
+1   4
+   /
+  3
+进阶:
+
+使用 O(n) 空间复杂度的解法很容易实现。
+你能想出一个只使用常数空间的解决方案吗？
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        # 1 2 3 4 5 6
+        # 1 3 2 4 5 6
+        # 1 4 3 2 5 6
+        # 6 2 3 4 5 1
+        self.pre = None
+        self.nodes = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            if self.pre and self.pre.val > root.val:
+                self.nodes.append(self.pre)
+                self.nodes.append(root)
+            self.pre = root
+            inorder(root.right)
+
+        inorder(root)
+
+        self.nodes[0].val, self.nodes[-1].val = self.nodes[-1].val, self.nodes[0].val
+
