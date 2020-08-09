afb82c708eba96ca20e459f43995aa1771fbdbdc
MecaCho <1099833328@qq.com>
2020-08-10 00:20:05 +0800

    add leetcode 696. Count Binary Substrings;

---
+++ algorithms/arr/leetcode-696-CountBinarySubstrings.py
@@ -0,0 +1,75 @@
+'''
+696. Count Binary Substrings
+Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+Substrings that occur multiple times are counted the number of times they occur.
+
+Example 1:
+Input: "00110011"
+Output: 6
+Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+
+Notice that some of these substrings repeat and are counted the number of times they occur.
+
+Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+Example 2:
+Input: "10101"
+Output: 4
+Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+Note:
+
+s.length will be between 1 and 50,000.
+s will only consist of "0" or "1" characters.
+
+696. 计数二进制子串
+给定一个字符串 s，计算具有相同数量0和1的非空(连续)子字符串的数量，并且这些子字符串中的所有0和所有1都是组合在一起的。
+
+重复出现的子串要计算它们出现的次数。
+
+示例 1 :
+
+输入: "00110011"
+输出: 6
+解释: 有6个子串具有相同数量的连续1和0：“0011”，“01”，“1100”，“10”，“0011” 和 “01”。
+
+请注意，一些重复出现的子串要计算它们出现的次数。
+
+另外，“00110011”不是有效的子串，因为所有的0（和1）没有组合在一起。
+示例 2 :
+
+输入: "10101"
+输出: 4
+解释: 有4个子串：“10”，“01”，“10”，“01”，它们具有相同数量的连续1和0。
+注意：
+
+s.length 在1到50,000之间。
+s 只包含“0”或“1”字符。
+'''
+
+
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        pre, cur = 0, 0
+        for i in range(len(s)):
+            # if i == 0:
+            #     cur += 1
+            if i == 0 or s[i] == s[i-1]:
+                cur += 1
+            else:
+                pre = cur
+                cur = 1
+            if pre >= cur:
+                res += 1
+        return res
+
+
+# tips
+
+'''
+How many valid binary substrings exist in "000111", and how many in "11100"? What about "00011100"?
+'''
