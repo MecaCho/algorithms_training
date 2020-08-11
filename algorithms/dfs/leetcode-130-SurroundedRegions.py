b4f132dc474612a929a79f3812e12587e96098ff
MecaCho <1099833328@qq.com>
2020-08-11 21:25:35 +0800

    add 130. Surrounded Regions;

---
+++ algorithms/dfs/leetcode-130-SurroundedRegions.py
@@ -0,0 +1,81 @@
+'''
+130. Surrounded Regions
+Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+Example:
+
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+Explanation:
+
+Surrounded regions shouldn’t be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+
+130. 被围绕的区域
+给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+
+找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+
+示例:
+
+X X X X
+X O O X
+X X O X
+X O X X
+运行你的函数后，矩阵变为：
+
+X X X X
+X X X X
+X X X X
+X O X X
+解释:
+
+被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+'''
+
+
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]:
+            return
+        m, n = len(board), len(board[0])
+        o_char = "O"
+
+        def dfs(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or board[x][y] != o_char:
+                return
+            board[x][y] = "#"
+            # print(x, y)
+            dfs(x - 1, y)
+            dfs(x + 1, y)
+            dfs(x, y - 1)
+            dfs(x, y + 1)
+
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+
+        for i in range(n - 1):
+            dfs(0, i)
+            dfs(m - 1, i)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "#":
+                    board[i][j] = o_char
+                elif board[i][j] == o_char:
+                    board[i][j] = "X"
+
