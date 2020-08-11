888bfb01451118ad885e55c9a6ff9dbff75ad731
MecaCho <1099833328@qq.com>
2020-08-11 23:36:04 +0800

    add 130. Surrounded Regions solution;

--- algorithms/dfs/leetcode-130-SurroundedRegions.py
+++ algorithms/dfs/leetcode-130-SurroundedRegions.py
@@ -79,3 +79,117 @@ class Solution(object):
                 elif board[i][j] == o_char:
                     board[i][j] = "X"
 
+# solutions
+
+'''
+写在前面
+本题给定的矩阵中有三种元素：
+
+字母 X；
+被字母 X 包围的字母 O；
+没有被字母 X 包围的字母 O。
+本题要求将所有被字母 X 包围的字母 O都变为字母 X ，但很难判断哪些 O 是被包围的，哪些 O 不是被包围的。
+
+注意到题目解释中提到：任何边界上的 O 都不会被填充为 X。 我们可以想到，所有的不被包围的 O 都直接或间接与边界上的 O 相连。我们可以利用这个性质判断 O 是否在边界上，具体地说：
+
+对于每一个边界上的 O，我们以它为起点，标记所有与它直接或间接相连的字母 O；
+最后我们遍历这个矩阵，对于每一个字母：
+如果该字母被标记过，则该字母为没有被字母 X 包围的字母 O，我们将其还原为字母 O；
+如果该字母没有被标记过，则该字母为被字母 X 包围的字母 O，我们将其修改为字母 X。
+方法一：深度优先搜索
+思路及解法
+
+我们可以使用深度优先搜索实现标记操作。在下面的代码中，我们把标记过的字母 O 修改为字母 A。
+
+代码
+
+C++JavaPython3Golang
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return
+        
+        n, m = len(board), len(board[0])
+
+        def dfs(x, y):
+            if not 0 <= x < n or not 0 <= y < m or board[x][y] != 'O':
+                return
+            
+            board[x][y] = "A"
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+        
+        for i in range(n):
+            dfs(i, 0)
+            dfs(i, m - 1)
+        
+        for i in range(m - 1):
+            dfs(0, i)
+            dfs(n - 1, i)
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "A":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+复杂度分析
+
+时间复杂度：O(n \times m)O(n×m)，其中 nn 和 mm 分别为矩阵的行数和列数。深度优先搜索过程中，每一个点至多只会被标记一次。
+
+空间复杂度：O(n \times m)O(n×m)，其中 nn 和 mm 分别为矩阵的行数和列数。主要为深度优先搜索的栈的开销。
+
+方法二：广度优先搜索
+思路及解法
+
+我们可以使用广度优先搜索实现标记操作。在下面的代码中，我们把标记过的字母 O 修改为字母 A。
+
+代码
+
+C++JavaPython3CGolang
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return
+        
+        n, m = len(board), len(board[0])
+        que = collections.deque()
+        for i in range(n):
+            if board[i][0] == "O":
+                que.append((i, 0))
+            if board[i][m - 1] == "O":
+                que.append((i, m - 1))
+        for i in range(m - 1):
+            if board[0][i] == "O":
+                que.append((0, i))
+            if board[n - 1][i] == "O":
+                que.append((n - 1, i))
+        
+        while que:
+            x, y = que.popleft()
+            board[x][y] = "A"
+            for mx, my in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                if 0 <= mx < n and 0 <= my < m and board[mx][my] == "O":
+                    que.append((mx, my))
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "A":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+复杂度分析
+
+时间复杂度：O(n \times m)O(n×m)，其中 nn 和 mm 分别为矩阵的行数和列数。广度优先搜索过程中，每一个点至多只会被标记一次。
+
+空间复杂度：O(n \times m)O(n×m)，其中 nn 和 mm 分别为矩阵的行数和列数。主要为广度优先搜索的队列的开销。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/surrounded-regions/solution/bei-wei-rao-de-qu-yu-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
