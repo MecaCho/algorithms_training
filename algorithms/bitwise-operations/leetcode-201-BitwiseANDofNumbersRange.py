fde8270ba4e0ae6f614917244e1ec7e2e5259d78
MecaCho <1099833328@qq.com>
2020-08-23 01:02:31 +0800

    add leetcode 201. Bitwise AND of Numbers Range;

---
+++ algorithms/bitwise-operations/leetcode-201-BitwiseANDofNumbersRange.py
@@ -0,0 +1,45 @@
+'''
+201. Bitwise AND of Numbers Range
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+
+Input: [5,7]
+Output: 4
+Example 2:
+
+Input: [0,1]
+Output: 0
+
+201. 数字范围按位与
+给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+
+示例 1:
+
+输入: [5,7]
+输出: 4
+示例 2:
+
+输入: [0,1]
+输出: 0
+'''
+
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # i = 0
+        # while m != n:
+        #     m >>= 1
+        #     n >>= 1
+        #     i += 1
+        # return m << i
+
+        while n > m:
+            n &= n - 1
+        return n
+
