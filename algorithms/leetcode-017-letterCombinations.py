46dd972883cd301a9b7cfb4fad6c3bcc29910ec6
MecaCho <1099833328@qq.com>
2020-08-26 22:32:05 +0800

    add 17. Letter Combinations of a Phone Number solution;

--- algorithms/leetcode-017-letterCombinations.py
+++ algorithms/leetcode-017-letterCombinations.py
@@ -1,5 +1,5 @@
 
-
+'''
 17. Letter Combinations of a Phone Number
 Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
 
@@ -28,8 +28,10 @@ Although the above answer is in lexicographical order, your answer could be in a
 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 说明:
 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+'''
 
-```
+
+
 class Solution(object):
     def letterCombinations(self, digits):
         """
@@ -50,4 +52,67 @@ class Solution(object):
         for digist in digits:
             res = [key + now for key in res for now in list(str_map[digist])]
         return res
- ```
+
+
+'''
+方法一：回溯
+首先使用哈希表存储每个数字对应的所有可能的字母，然后进行回溯操作。
+
+回溯过程中维护一个字符串，表示已有的字母排列（如果未遍历完电话号码的所有数字，则已有的字母排列是不完整的）。该字符串初始为空。每次取电话号码的一位数字，从哈希表中获得该数字对应的所有可能的字母，并将其中的一个字母插入到已有的字母排列后面，然后继续处理电话号码的后一位数字，直到处理完电话号码中的所有数字，即得到一个完整的字母排列。然后进行回退操作，遍历其余的字母排列。
+
+回溯算法用于寻找所有的可行解，如果发现一个解不可行，则会舍弃不可行的解。在这道题中，由于每个数字对应的每个字母都可能进入字母组合，因此不存在不可行的解，直接穷举所有的解即可。
+
+
+1 / 14
+
+JavaC++GolangCPython3Python3_oneliner
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+        
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
+复杂度分析
+
+时间复杂度：O(3^m \times 4^n)O(3 
+m
+ ×4 
+n
+ )，其中 mm 是输入中对应 33 个字母的数字个数（包括数字 22、33、44、55、66、88），nn 是输入中对应 44 个字母的数字个数（包括数字 77、99），m+nm+n 是输入数字的总个数。当输入包含 mm 个对应 33 个字母的数字和 nn 个对应 44 个字母的数字时，不同的字母组合一共有 3^m \times 4^n3 
+m
+ ×4 
+n
+  种，需要遍历每一种字母组合。
+
+空间复杂度：O(m+n)O(m+n)，其中 mm 是输入中对应 33 个字母的数字个数，nn 是输入中对应 44 个字母的数字个数，m+nm+n 是输入数字的总个数。除了返回值以外，空间复杂度主要取决于哈希表以及回溯过程中的递归调用层数，哈希表的大小与输入无关，可以看成常数，递归调用层数最大为 m+nm+n。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/solution/dian-hua-hao-ma-de-zi-mu-zu-he-by-leetcode-solutio/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
