f6ecd9eac44997106ee7555018eff621f3409a36
MecaCho <1099833328@qq.com>
2020-09-04 01:25:52 +0800

    add 257. Binary Tree Paths solution;

--- algorithms/tree/leetcode-257.py
+++ algorithms/tree/leetcode-257.py
@@ -40,6 +40,37 @@ Explanation: All root-to-leaf paths are: 1->2->5, 1->3
 '''
 
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        self.vals = []
+        def dfs(root, val):
+            if not root:
+                return
+            val.append(root.val)
+            if not root.left and not root.right:
+                self.vals.append(val)
+            dfs(root.left, val[:])
+            dfs(root.right, val[:])
+
+        dfs(root, [])
+        # print(self.vals)
+
+        return ["->".join([str(i) for i in val]) for val in self.vals]
+
+
+
+
 
 # solution
 
