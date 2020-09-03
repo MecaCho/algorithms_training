b32ad869e20420ff358be07e140d50d9fe65a109
MecaCho <1099833328@qq.com>
2020-09-03 19:39:51 +0800

    add 51. N-Queens;

---
+++ algorithms/dfs/leetcode-51-N-Queens.py
@@ -0,0 +1,94 @@
+'''
+51. N-Queens
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+Example:
+
+Input: 4
+Output: [
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+
+51. N 皇后
+n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+
+
+上图为 8 皇后问题的一种解法。
+
+给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+
+每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+
+
+
+示例：
+
+输入：4
+输出：[
+ [".Q..",  // 解法 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // 解法 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+解释: 4 皇后问题存在两个不同的解法。
+
+
+提示：
+
+皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。
+'''
+
+
+
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        self.res = []
+
+        # def check(x, y):
+        #     for i in range()
+
+        def dfs(queens, diff, sums):
+            seq = len(queens)
+            if seq == n:
+                self.res.append(queens)
+            else:
+                for i in range(n):
+                    sum_ = (seq - 1) + i
+                    diff_ = (seq - 1) - i
+                    if i not in queens and diff_ not in diff and sum_ not in sums:
+                        dfs(queens + [i], diff + [diff_], sums + [sum_])
+
+        dfs([], [], [])
+        res = []
+        for row in self.res:
+            res.append(["." * k + "Q" + "." * (n - k - 1) for k in row])
+        return res
+
+
+
+
