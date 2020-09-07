fc602839e0507a587ce9d229e5d12f4834be4eae
MecaCho <1099833328@qq.com>
2020-09-07 08:50:53 +0800

    add 347. Top K Frequent Elements solution;

--- algorithms/arr/leetcode_347_topKFrequent/leetcode_347_topKFrequent.py
+++ algorithms/arr/leetcode_347_topKFrequent/leetcode_347_topKFrequent.py
@@ -1,3 +1,48 @@
+'''
+
+347. Top K Frequent Elements
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order.
+
+
+347. 前 K 个高频元素
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+
+
+
+示例 1:
+
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+示例 2:
+
+输入: nums = [1], k = 1
+输出: [1]
+
+
+提示：
+
+你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+你可以按任意顺序返回答案。
+'''
+
+
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -21,15 +66,19 @@ class Solution(object):
         return ret
 
 
-class Solution1(object):
+import collections
+
+
+class Solution2(object):
     def topKFrequent(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
         """
-        from collections import Counter
-        return [k[0] for k in Counter(nums).most_common(k)]
+        counter = collections.Counter(nums)
+        return [key for key, _ in counter.most_common(k)]
+
 
 if __name__ == '__main__':
     test_list = [1, 1, 1, 2, 2, 3, 4, 5, 6, 9, 10]
@@ -37,6 +86,6 @@ if __name__ == '__main__':
 
     print(demo.topKFrequent(test_list, 10))
 
-    demo1 = Solution1()
+    demo1 = Solution2()
 
     print(demo1.topKFrequent(test_list, 10))
