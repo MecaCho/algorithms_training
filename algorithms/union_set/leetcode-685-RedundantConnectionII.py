193e4642b4613526416a87edca38e6f36abdb4a1
MecaCho <1099833328@qq.com>
2020-09-17 09:54:26 +0800

    add leetcode 685. Redundant Connection II;

---
+++ algorithms/union_set/leetcode-685-RedundantConnectionII.py
@@ -0,0 +1,124 @@
+'''
+685. Redundant Connection II
+In this problem, a rooted tree is a directed graph such that, there is exactly one node (the root) for which all other nodes are descendants of this node, plus every node has exactly one parent, except for the root node which has no parents.
+
+The given input is a directed graph that started as a rooted tree with N nodes (with distinct values 1, 2, ..., N), with one additional directed edge added. The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
+
+The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] that represents a directed edge connecting nodes u and v, where u is a parent of child v.
+
+Return an edge that can be removed so that the resulting graph is a rooted tree of N nodes. If there are multiple answers, return the answer that occurs last in the given 2D-array.
+
+Example 1:
+Input: [[1,2], [1,3], [2,3]]
+Output: [2,3]
+Explanation: The given directed graph will be like this:
+  1
+ / \
+v   v
+2-->3
+Example 2:
+Input: [[1,2], [2,3], [3,4], [4,1], [1,5]]
+Output: [4,1]
+Explanation: The given directed graph will be like this:
+5 <- 1 -> 2
+     ^    |
+     |    v
+     4 <- 3
+Note:
+The size of the input 2D-array will be between 3 and 1000.
+Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
+
+685. 冗余连接 II
+在本问题中，有根树指满足以下条件的有向图。该树只有一个根节点，所有其他节点都是该根节点的后继。每一个节点只有一个父节点，除了根节点没有父节点。
+
+输入一个有向图，该图由一个有着N个节点 (节点值不重复1, 2, ..., N) 的树及一条附加的边构成。附加的边的两个顶点包含在1到N中间，这条附加的边不属于树中已存在的边。
+
+结果图是一个以边组成的二维数组。 每一个边 的元素是一对 [u, v]，用以表示有向图中连接顶点 u 和顶点 v 的边，其中 u 是 v 的一个父节点。
+
+返回一条能删除的边，使得剩下的图是有N个节点的有根树。若有多个答案，返回最后出现在给定二维数组的答案。
+
+示例 1:
+
+输入: [[1,2], [1,3], [2,3]]
+输出: [2,3]
+解释: 给定的有向图如下:
+  1
+ / \
+v   v
+2-->3
+示例 2:
+
+输入: [[1,2], [2,3], [3,4], [4,1], [1,5]]
+输出: [4,1]
+解释: 给定的有向图如下:
+5 <- 1 -> 2
+     ^    |
+     |    v
+     4 <- 3
+注意:
+
+二维数组大小的在3到1000范围内。
+二维数组中的每个整数在1到N之间，其中 N 是二维数组的大小。
+'''
+
+
+# class UnionFind:
+#     def __init__(self, n):
+#         self.parent = list(range(n))
+
+#     def find(self, index):
+#         if index == self.parent[index]:
+#             return index
+#         self.parent[index] = self.find(self.parent[index])
+#         return self.parent[index]
+
+#     def union(self, index1, index2):
+#         self.parent[self.find(index1)] = self.find(index2)
+
+
+class UnionFind(object):
+
+    def __init__(self, n):
+        self.pre = range(n)
+
+    def find(self, x):
+        if x != self.pre[x]:
+            self.pre[x] = self.find(self.pre[x])
+        return self.pre[x]
+
+    def union(self, x1, x2):
+        root1 = self.find(x1)
+        root2 = self.find(x2)
+        if root1 != root2:
+            self.pre[root2] = root1
+            return False
+
+        return True
+
+
+class Solution(object):
+    def findRedundantDirectedConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        n = len(edges)
+        uf = UnionFind(n + 1)
+
+        last = []
+        parent = {}
+        candidates = []
+        for st, ed in edges:
+            if ed in parent:
+                candidates.append([parent[ed], ed])
+                candidates.append([st, ed])
+            else:
+                parent[ed] = st
+                if uf.union(st, ed):
+                    last = [st, ed]
+
+        if not candidates:
+            return last
+
+        return candidates[0] if last else candidates[1]
+
