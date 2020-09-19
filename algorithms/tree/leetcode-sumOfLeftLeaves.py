041eec2c392ea66fcb1acfa2c8495f08907fb76c
MecaCho <1099833328@qq.com>
2020-09-19 17:12:04 +0800

    add 404. Sum of Left Leaves;

---
+++ algorithms/tree/leetcode-sumOfLeftLeaves.py
@@ -0,0 +1,52 @@
+'''
+404. Sum of Left Leaves
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+404. 左叶子之和
+计算给定二叉树的所有左叶子之和。
+
+示例：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+
+'''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if root.left and not root.left.left and not root.left.right:
+            return self.sumOfLeftLeaves(root.right) + root.left.val
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+
