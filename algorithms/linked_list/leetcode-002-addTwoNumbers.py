1cd05a53c5f3cb311395df9733e3ef74db5dad7f
MecaCho <1099833328@qq.com>
2020-10-04 19:40:38 +0800

    add leetcode 2. Add Two Numbers solution;

--- algorithms/linked_list/leetcode-002-addTwoNumbers.py
+++ algorithms/linked_list/leetcode-002-addTwoNumbers.py
@@ -104,5 +104,36 @@ def main():
         except StopIteration:
             break
 
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution20201004(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        pre = 0
+        res = head = ListNode(0)
+        # head = res
+        while l1 or l2 or pre:
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
+
+            node = ListNode((num1 + num2 + pre) % 10)
+            res.next = node
+            res = res.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            pre = (num1 + num2 + pre) / 10
+
+        return head.next
+
+
 if __name__ == '__main__':
     main()
