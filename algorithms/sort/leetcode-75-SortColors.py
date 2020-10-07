e3a8727505f5464662ed90622fa41714392d7992
MecaCho <1099833328@qq.com>
2020-10-07 23:56:39 +0800

    add 75. Sort Colors solution;

--- algorithms/sort/leetcode-75-SortColors.py
+++ algorithms/sort/leetcode-75-SortColors.py
@@ -63,3 +63,27 @@ class Solution(object):
         # return nums.sort()
 
 
+class Solution1(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # return nums.sort()
+        l, r = 0, len(nums) - 1
+        i = 0
+
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                # i += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                i -= 1
+            i += 1
+
+        return
+
+
