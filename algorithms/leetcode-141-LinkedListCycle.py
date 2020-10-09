924e79950904ea72d0cf0a517d69fb9dd5d3035d
MecaCho <1099833328@qq.com>
2020-10-09 10:35:20 +0800

    add 141. Linked List Cycle solution;

--- algorithms/leetcode-141-LinkedListCycle.py
+++ algorithms/leetcode-141-LinkedListCycle.py
@@ -93,6 +93,32 @@ class Solution(object):
         return False
 
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution20201009(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        slow, fast = head, head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if fast == slow:
+                return True
+        return False
+
+
+
 '''
 方法一：哈希表
 思路
