7b2f4e9bb09b8c29adbf2057e4b85dce4084cf9f
MecaCho <1099833328@qq.com>
2020-10-11 22:03:28 +0800

    add leetcode 416. Partition Equal Subset Sum;

---
+++ algorithms/tree/leetcode-416-PartitionEqualSubsetSum.py
@@ -0,0 +1,70 @@
+'''
+416. Partition Equal Subset Sum
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+
+416. 分割等和子集
+给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+
+注意:
+
+每个数组中的元素不会超过 100
+数组的大小不会超过 200
+示例 1:
+
+输入: [1, 5, 11, 5]
+
+输出: true
+
+解释: 数组可以分割成 [1, 5, 5] 和 [11].
+
+
+示例 2:
+
+输入: [1, 2, 3, 5]
+
+输出: false
+
+解释: 数组不能分割成两个元素和相等的子集.
+'''
+
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) < 2:
+            return False
+        sum_ = sum(nums)
+        if sum_ % 2 == 1:
+            return False
+
+        target = sum_ / 2
+
+        dp = [True] + [False] * target
+        for i in range(len(nums)):
+            for j in range(target, nums[i] - 1, -1):
+                dp[j] = dp[j] or dp[j - nums[i]]
+
+        return dp[target]
+
