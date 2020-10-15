25d556bf4d015f59e2a00f7dbb896cbaff660d59
MecaCho <1099833328@qq.com>
2020-10-16 00:00:44 +0800

    add 116. Populating Next Right Pointers in Each Node solutions;

--- algorithms/tree/leetcode-116-PopulatingNextRightPointersinEachNode.py
+++ algorithms/tree/leetcode-116-PopulatingNextRightPointersinEachNode.py
@@ -106,3 +106,167 @@ class Solution(object):
         return root
 
 
+# solutions
+
+'''
+方法一：层次遍历
+思路与算法
+
+题目本身希望我们将二叉树的每一层节点都连接起来形成一个链表。因此直观的做法我们可以对二叉树进行层次遍历，在层次遍历的过程中将我们将二叉树每一层的节点拿出来遍历并连接。
+
+层次遍历基于广度优先搜索，它与广度优先搜索的不同之处在于，广度优先搜索每次只会取出一个节点来拓展，而层次遍历会每次将队列中的所有元素都拿出来拓展，这样能保证每次从队列中拿出来遍历的元素都是属于同一层的，因此我们可以在遍历的过程中修改每个节点的 next 指针，同时拓展下一层的新队列。
+
+
+1 / 15
+
+代码
+
+JavaPython3C++JavaScriptGolangC
+
+import collections 
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+        # 初始化队列同时将第一层节点加入队列中，即根节点
+        Q = collections.deque([root])
+        
+        # 外层的 while 循环迭代的是层数
+        while Q:
+            
+            # 记录当前队列大小
+            size = len(Q)
+            
+            # 遍历这一层的所有节点
+            for i in range(size):
+                
+                # 从队首取出元素
+                node = Q.popleft()
+                
+                # 连接
+                if i < size - 1:
+                    node.next = Q[0]
+                
+                # 拓展下一层节点
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+        
+        # 返回根节点
+        return root
+复杂度分析
+
+时间复杂度：O(N)O(N)。每个节点会被访问一次且只会被访问一次，即从队列中弹出，并建立 next 指针。
+
+空间复杂度：O(N)O(N)。这是一棵完美二叉树，它的最后一个层级包含 N/2N/2 个节点。广度优先遍历的复杂度取决于一个层级上的最大元素数量。这种情况下空间复杂度为 O(N)O(N)。
+
+方法二：使用已建立的 \text{next}next 指针
+思路
+
+一棵树中，存在两种类型的 next 指针。
+
+第一种情况是连接同一个父节点的两个子节点。它们可以通过同一个节点直接访问到，因此执行下面操作即可完成连接。
+
+Java
+
+node.left.next = node.right
+
+
+第二种情况在不同父亲的子节点之间建立连接，这种情况不能直接连接。
+
+
+
+如果每个节点有指向父节点的指针，可以通过该指针找到 next 节点。如果不存在该指针，则按照下面思路建立连接：
+
+第 NN 层节点之间建立 next 指针后，再建立第 N+1N+1 层节点的 next 指针。可以通过 next 指针访问同一层的所有节点，因此可以使用第 NN 层的 next 指针，为第 N+1N+1 层节点建立 next 指针。
+
+算法
+
+从根节点开始，由于第 00 层只有一个节点，所以不需要连接，直接为第 11 层节点建立 next 指针即可。该算法中需要注意的一点是，当我们为第 NN 层节点建立 next 指针时，处于第 N-1N−1 层。当第 NN 层节点的 next 指针全部建立完成后，移至第 NN 层，建立第 N+1N+1 层节点的 next 指针。
+
+遍历某一层的节点时，这层节点的 next 指针已经建立。因此我们只需要知道这一层的最左节点，就可以按照链表方式遍历，不需要使用队列。
+
+上面思路的伪代码如下：
+
+Java
+
+leftmost = root
+while (leftmost.left != null) {
+    head = leftmost
+    while (head.next != null) {
+        1) Establish Connection 1
+        2) Establish Connection 2 using next pointers
+        head = head.next
+    }
+    leftmost = leftmost.left
+}
+
+
+两种类型的 next 指针。
+
+第一种情况两个子节点属于同一个父节点，因此直接通过父节点建立两个子节点的 next 指针即可。
+
+Java
+
+node.left.next = node.right
+
+
+第二种情况是连接不同父节点之间子节点的情况。更具体地说，连接的是第一个父节点的右孩子和第二父节点的左孩子。由于已经在父节点这一层建立了 next 指针，因此可以直接通过第一个父节点的 next 指针找到第二个父节点，然后在它们的孩子之间建立连接。
+
+Java
+
+node.right.next = node.next.left
+
+
+完成当前层的连接后，进入下一层重复操作，直到所有的节点全部连接。进入下一层后需要更新最左节点，然后从新的最左节点开始遍历该层所有节点。因为是完美二叉树，因此最左节点一定是当前层最左节点的左孩子。如果当前最左节点的左孩子不存在，说明已经到达该树的最后一层，完成了所有节点的连接。
+
+
+
+代码
+
+JavaPython3C++JavaScriptGolangC
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+        # 从根节点开始
+        leftmost = root
+        
+        while leftmost.left:
+            
+            # 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            head = leftmost
+            while head:
+                
+                # CONNECTION 1
+                head.left.next = head.right
+                
+                # CONNECTION 2
+                if head.next:
+                    head.right.next = head.next.left
+                
+                # 指针向后移动
+                head = head.next
+            
+            # 去下一层的最左的节点
+            leftmost = leftmost.left
+        
+        return root 
+复杂度分析
+
+时间复杂度：O(N)O(N)，每个节点只访问一次。
+
+空间复杂度：O(1)O(1)，不需要存储额外的节点。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/solution/tian-chong-mei-ge-jie-dian-de-xia-yi-ge-you-ce-2-4/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
