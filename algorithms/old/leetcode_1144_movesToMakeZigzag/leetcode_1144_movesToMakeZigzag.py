66523154bc4cc7f0151f22fea2d7d4a61b232991
MecaCho <1099833328@qq.com>
2020-10-25 20:09:06 +0800

    add leetcode 845. Longest Mountain in Array;

--- algorithms/old/leetcode_1144_movesToMakeZigzag/leetcode_1144_movesToMakeZigzag.py
+++ algorithms/old/leetcode_1144_movesToMakeZigzag/leetcode_1144_movesToMakeZigzag.py
@@ -6,10 +6,11 @@ class Solution(object):
         """
         nums = [float('inf')] + nums + [float('inf')]
         res = [0, 0]
-        for i in xrange(1, len(nums) - 1):
+        for i in range(1, len(nums) - 1):
             res[i % 2] += max(0, nums[i] - min(nums[i - 1], nums[i + 1]) + 1)
         return min(res)
 
 if __name__ == '__main__':
     demo = Solution()
-    print demo.movesToMakeZigzag([1, 2, 3, 4, 5])
+    print(demo.movesToMakeZigzag([1, 2, 3, 4, 5]))
+
