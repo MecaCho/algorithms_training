8ee405e07e6af531e592fe9eb429cbc075056c1b
MecaCho <1099833328@qq.com>
2020-10-27 12:01:13 +0800

    add 144. Binary Tree Preorder Traversal solution;

--- algorithms/tree/leetcode-144-BinaryTreePreorderTraversal.py
+++ algorithms/tree/leetcode-144-BinaryTreePreorderTraversal.py
@@ -75,6 +75,23 @@ class Solution1(object):
                     stack_t.append(node.left)
         return res
 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution20201027(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right) if root else []
+
+
 # golang solutions
 
 '''
