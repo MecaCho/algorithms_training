45e2a6f009d6fa924616d0a6ab8b18605700a3a6
MecaCho <1099833328@qq.com>
2020-11-16 15:14:13 +0800

    406. Queue Reconstruction by Height;

---
+++ algorithms/sort/leetcode-406-QueueReconstructionbyHeight.py
@@ -0,0 +1,46 @@
+'''
+406. Queue Reconstruction by Height
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+
+406. 根据身高重建队列
+假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
+
+注意：
+总人数少于1100人。
+
+示例
+
+输入:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+输出:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+'''
+
+
+
+class Solution(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        people.sort(key=lambda x: (-x[0], x[1]))
+
+        res = []
+        for i in people:
+            res.insert(i[1], i)
+        return res
+
