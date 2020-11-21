6890ff67225d4113dfd199750ae9164d274a3a5c
MecaCho <1099833328@qq.com>
2020-11-21 22:23:38 +0800

    add 148. Sort List solution;

--- algorithms/linked_list/leetcode-148-SortList.py
+++ algorithms/linked_list/leetcode-148-SortList.py
@@ -29,10 +29,10 @@ Output: -1->0->3->4->5
 
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def merge_list(self, l1, l2):
@@ -66,6 +66,35 @@ class Solution(object):
 
         res = self.merge_list(l1, l2)
         return res
+
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution20201121(object):
+
+    def sortList(self, head):
+        if not head or not head.next: return head # termination.
+        # cut the LinkedList at the mid index.
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None # save and cut.
+        # recursive for cutting.
+        left, right = self.sortList(head), self.sortList(mid)
+        # merge `left` and `right` linked list and return it.
+        h = res = ListNode(0)
+        while left and right:
+            if left.val < right.val:
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
 
 
 '''
