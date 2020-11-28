de5fae72ec84e80ee9b816ec8f368d8b9807d3f8
MecaCho <1099833328@qq.com>
2020-11-28 21:58:38 +0800

    add 493. Reverse Pairs;

--- algorithms/hash/leetcode-454-fourSumCount.py
+++ algorithms/hash/leetcode-454-fourSumCount.py
@@ -74,4 +74,50 @@ class Solution(object):
 # solutions
 
 '''
+方法一：分组 + 哈希表
+思路与算法
+
+我们可以将四个数组分成两部分，AA 和 BB 为一组，CC 和 DD 为另外一组。
+
+对于 AA 和 BB，我们使用二重循环对它们进行遍历，得到所有 A[i]+B[j]A[i]+B[j] 的值并存入哈希映射中。对于哈希映射中的每个键值对，每个键表示一种 A[i]+B[j]A[i]+B[j]，对应的值为 A[i]+B[j]A[i]+B[j] 出现的次数。
+
+对于 CC 和 DD，我们同样使用二重循环对它们进行遍历。当遍历到 C[k]+D[l]C[k]+D[l] 时，如果 -(C[k]+D[l])−(C[k]+D[l]) 出现在哈希映射中，那么将 -(C[k]+D[l])−(C[k]+D[l]) 对应的值累加进答案中。
+
+最终即可得到满足 A[i]+B[j]+C[k]+D[l]=0A[i]+B[j]+C[k]+D[l]=0 的四元组数目。
+
+代码
+
+C++JavaPython3JavaScriptGolangC
+
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        countAB = collections.Counter(u + v for u in A for v in B)
+        ans = 0
+        for u in C:
+            for v in D:
+                if -u - v in countAB:
+                    ans += countAB[-u - v]
+        return ans
+复杂度分析
+
+时间复杂度：O(n^2)O(n 
+2
+ )。我们使用了两次二重循环，时间复杂度均为 O(n^2)O(n 
+2
+ )。在循环中对哈希映射进行的修改以及查询操作的期望时间复杂度均为 O(1)O(1)，因此总时间复杂度为 O(n^2)O(n 
+2
+ )。
+
+空间复杂度：O(n^2)O(n 
+2
+ )，即为哈希映射需要使用的空间。在最坏的情况下，A[i]+B[j]A[i]+B[j] 的值均不相同，因此值的个数为 n^2n 
+2
+ ，也就需要 O(n^2)O(n 
+2
+ ) 的空间。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/4sum-ii/solution/si-shu-xiang-jia-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
