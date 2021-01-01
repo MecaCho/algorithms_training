eb63e2f63252f9f74461b9fd7e3f08e8592ba3cb
MecaCho <1099833328@qq.com>
2021-01-01 21:23:26 +0800

    605. Can Place Flowers

---
+++ algorithms/greedy-alogrithms/leetcode-605-CanPlaceFlowers.py
@@ -0,0 +1,62 @@
+'''
+605. Can Place Flowers
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+
+
+605. 种花问题
+假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+
+给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+
+示例 1:
+
+输入: flowerbed = [1,0,0,0,1], n = 1
+输出: True
+示例 2:
+
+输入: flowerbed = [1,0,0,0,1], n = 2
+输出: False
+注意:
+
+数组内已种好的花不会违反种植规则。
+输入的数组长度范围为 [1, 20000]。
+n 是非负整数，且不会超过输入数组的大小。
+'''
+
+
+
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        tmp = [0]+flowerbed+[0]
+        for i in range(1, len(tmp)-1):
+            if tmp[i-1] == 0 and tmp[i] == 0 and tmp[i+1] == 0:
+                tmp[i] = 1
+                n -= 1
+        return n <= 0
+
