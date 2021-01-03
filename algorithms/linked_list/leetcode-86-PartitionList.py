18fdd8b78a741ccd94f3855bedc70ab420996a30
MecaCho <1099833328@qq.com>
2021-01-03 22:45:24 +0800

    add 86. Partition List;

---
+++ algorithms/linked_list/leetcode-86-PartitionList.py
@@ -0,0 +1,61 @@
+# encoding=utf8
+
+
+'''
+86. Partition List
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example:
+
+Input: head = 1->4->3->2->5->2, x = 3
+Output: 1->2->2->4->3->5
+
+
+86. 分隔链表
+给你一个链表和一个特定值 x ，请你对链表进行分隔，使得所有小于 x 的节点都出现在大于或等于 x 的节点之前。
+
+你应当保留两个分区中每个节点的初始相对位置。
+
+
+
+示例：
+
+输入：head = 1->4->3->2->5->2, x = 3
+输出：1->2->2->4->3->5
+'''
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        before = before_head = ListNode(0)
+        after = after_head = ListNode(0)
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+
+            head = head.next
+
+
+        after.next = None
+        before.next = after_head.next
+
+        return before_head.next
+
