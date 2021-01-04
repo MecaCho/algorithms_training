3b681f059e8859b14d3769d17758977dd6d591e2
MecaCho <1099833328@qq.com>
2021-01-04 09:17:56 +0800

    add 509. Fibonacci Number solution;

--- algorithms/dynamic-programming/leetcode-509-FibonacciNumber.py
+++ algorithms/dynamic-programming/leetcode-509-FibonacciNumber.py
@@ -1,5 +1,4 @@
-
-
+# encoding=utf8
 '''
 509. 斐波那契数
 斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
@@ -62,6 +61,7 @@ Note:
 0 ≤ N ≤ 30.
 '''
 
+
 class Solution(object):
     def fib(self, N):
         """
@@ -74,6 +74,22 @@ class Solution(object):
         for i in range(2, N+1):
             dp.append(dp[i-1] + dp[i-2])
         return dp[N]
+
+
+class Solution20200104(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a,b = 0, 1
+        for i in range(n):
+            a, b = b, a+b
+
+        return a
+
+
+# solutions
 
 '''
 递归
