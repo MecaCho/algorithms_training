6837e0f53d52aef209841afc3baeae53519e7349
MecaCho <1099833328@qq.com>
2021-01-15 09:51:54 +0800

    add 947. Most Stones Removed with Same Row or Column solution;

--- algorithms/union_find_set/leetcode-947-MostStonesRemovedwithSameRoworColumn.py
+++ algorithms/union_find_set/leetcode-947-MostStonesRemovedwithSameRoworColumn.py
@@ -116,5 +116,108 @@ class Solution(object):
 
 
 
+# solution
 
+
+'''
+把二维坐标平面上的石头想象成图的顶点，如果两个石头横坐标相同、或者纵坐标相同，在它们之间形成一条边。
+
+
+
+根据可以移除石头的规则：如果一块石头的 同行或者同列 上有其他石头存在，那么就可以移除这块石头。可以发现：一定可以把一个连通图里的所有顶点根据这个规则删到只剩下一个顶点。
+
+为什么这么说呢？既然这些顶点在一个连通图里，可以通过遍历的方式（深度优先遍历或者广度优先遍历）遍历到这个连通图的所有顶点。那么就可以按照遍历的方式 逆向 移除石头，最后只剩下一块石头。所以：最多可以移除的石头的个数 = 所有石头的个数 - 连通分量的个数。
+
+题目没有让我们给出具体移除石头的方案，可以考虑使用并查集。
+
+方法：并查集
+删到最后，留在图中的顶点一定位于不同的行和不同的列。因此，并查集里的元素是 描述「横坐标」和「纵坐标」的数值。因此我们需要遍历数组 stones，将每个 stone 的横坐标和纵坐标在并查集中进行合并。理解合并的语义十分重要。
+
+「合并」的语义
+「合并」的语义是：所有横坐标为 x 的石头和所有纵坐标为 y 的石头都属于同一个连通分量。
+
+并查集里如何区分横纵坐标
+然而会遇到这样一个问题：石头的位置是「有序数对（二维）」，并查集的底层是「一维数组」，我们在并查集里应该如何区分横纵坐标呢？
+
+例如：如果一块石头的坐标为 [3, 3] ，那么所有横坐标为 3 的石头和所有纵坐标为 3 的石头都在一个连通分量中，但是我们需要在并查集里区分「横坐标」和「纵坐标」，它们在并查集里不能相等，根据题目的提示 0 <= x_i, y_i <= 10^40<=x 
+i
+​	
+ ,y 
+i
+​	
+ <=10 
+4
+ ，可以把其中一个坐标 映射 到另一个与 [0, 10000] 不重合的区间，可以的做法是把横坐标全部减去 1000010000 或者全部加上 1000010000 ，或者按位取反。
+
+在并查集里我们需要维护连通分量的个数，新创建顶点的时候连通分量加 11；合并不在同一个连通分量中的两个并查集的时候，连通分量减 11。
+
+参考代码：
+
+Java
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int removeStones(int[][] stones) {
+        UnionFind unionFind = new UnionFind();
+
+        for (int[] stone : stones) {
+            // 下面这三种写法任选其一
+            // unionFind.union(~stone[0], stone[1]);
+            // unionFind.union(stone[0] - 10000, stone[1]);
+            unionFind.union(stone[0] + 10000, stone[1]);
+        }
+        return stones.length - unionFind.getCount();
+    }
+
+    private class UnionFind {
+
+        private Map<Integer, Integer> parent;
+        private int count;
+
+        public UnionFind() {
+            this.parent = new HashMap<>();
+            this.count = 0;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public int find(int x) {
+            if (!parent.containsKey(x)) {
+                parent.put(x, x);
+                count++;
+            }
+
+            if (x != parent.get(x)) {
+                parent.put(x, find(parent.get(x)));
+            }
+            return parent.get(x);
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            parent.put(rootX, rootY);
+            count--;
+        }
+    }
+}
+复杂度分析：
+
+时间复杂度：O(n \log(A))O(nlog(A))，其中 nn 为石子的数量，AA 是数组 stones 里横纵坐标不同值的总数；
+空间复杂度：O(A)O(A)，并查集的底层哈希表的长度为 AA。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/solution/947-yi-chu-zui-duo-de-tong-xing-huo-tong-ezha/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
 
