64ad803dbe52b80e95c3209d22bcffb9fa7895b8
MecaCho <1099833328@qq.com>
2021-01-17 22:03:47 +0800

    add 1232. Check If It Is a Straight Line solution;

--- algorithms/arr/leetcode-1232-CheckIfItIsaStraightLine.py
+++ algorithms/arr/leetcode-1232-CheckIfItIsaStraightLine.py
@@ -82,3 +82,127 @@ class Solution(object):
         return True
 
 
+
+# solution
+
+'''
+方法一：数学
+思路
+
+记数组 \textit{coordinates}coordinates 中的点为 P_0, P_1, \dots, P_{n-1}P 
+0
+​	
+ ,P 
+1
+​	
+ ,…,P 
+n−1
+​	
+ 。为方便后续计算，将所有点向 (-P_{0x}, -P_{0y})(−P 
+0x
+​	
+ ,−P 
+0y
+​	
+ ) 方向平移。记平移后的点为 P_0', P_1', \dots, P_{n-1}'P 
+0
+′
+​	
+ ,P 
+1
+′
+​	
+ ,…,P 
+n−1
+′
+​	
+ ，其中 P_i'=(P_{ix}-P_{0x}, P_{iy}-P_{0y})P 
+i
+′
+​	
+ =(P 
+ix
+​	
+ −P 
+0x
+​	
+ ,P 
+iy
+​	
+ −P 
+0y
+​	
+ )，P_0'P 
+0
+′
+​	
+  位于坐标系原点 OO 上。
+
+由于经过两点的直线有且仅有一条，我们以 P_0'P 
+0
+′
+​	
+  和 P_1'P 
+1
+′
+​	
+  来确定这条直线。
+
+因为 P_0'P 
+0
+′
+​	
+  位于坐标系原点 OO 上，直线过原点，故设其方程为 Ax+By=0Ax+By=0，将 P_1'P 
+1
+′
+​	
+  坐标代入可得 A=P_{1y}',B=-P_{1x}'A=P 
+1y
+′
+​	
+ ,B=−P 
+1x
+′
+​	
+ .
+
+然后依次判断 P_2', \dots, P_{n-1}'P 
+2
+′
+​	
+ ,…,P 
+n−1
+′
+​	
+  是否在这条直线上，将其坐标代入直线方程即可。
+
+代码
+
+C++GolangJavaJavaScriptC
+
+func checkStraightLine(coordinates [][]int) bool {
+    deltaX, deltaY := coordinates[0][0], coordinates[0][1]
+    for _, p := range coordinates {
+        p[0] -= deltaX
+        p[1] -= deltaY
+    }
+    A, B := coordinates[1][1], -coordinates[1][0]
+    for _, p := range coordinates[2:] {
+        x, y := p[0], p[1]
+        if A*x+B*y != 0 {
+            return false
+        }
+    }
+    return true
+}
+复杂度分析
+
+时间复杂度：O(n)O(n)，其中 nn 是数组中的元素数量。
+
+时间复杂度：O(1)O(1)
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/check-if-it-is-a-straight-line/solution/zhui-dian-cheng-xian-by-leetcode-solutio-lpt6/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
