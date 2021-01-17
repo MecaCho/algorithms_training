1773d882078e320bff99e7740ccf10f4be2c44a9
MecaCho <1099833328@qq.com>
2021-01-17 22:00:41 +0800

    add 1232. Check If It Is a Straight Line;

---
+++ algorithms/arr/leetcode-1232-CheckIfItIsaStraightLine.py
@@ -0,0 +1,84 @@
+# encoding=utf8
+
+'''
+1232. Check If It Is a Straight Line
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+
+
+
+
+Example 1:
+
+
+
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+Example 2:
+
+
+
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+
+
+Constraints:
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+
+
+1232. 缀点成线
+在一个 XY 坐标系中有一些点，我们用数组 coordinates 来分别记录它们的坐标，其中 coordinates[i] = [x, y] 表示横坐标为 x、纵坐标为 y 的点。
+
+请你来判断，这些点是否在该坐标系中属于同一条直线上，是则返回 true，否则请返回 false。
+
+
+
+示例 1：
+
+
+
+输入：coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+输出：true
+示例 2：
+
+
+
+输入：coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+输出：false
+
+
+提示：
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates 中不含重复的点
+'''
+
+
+class Solution(object):
+    def checkStraightLine(self, coordinates):
+        """
+        :type coordinates: List[List[int]]
+        :rtype: bool
+        """
+        if len(coordinates) <= 2:
+            return True
+        x1 = coordinates[0][0]
+        y1 = coordinates[0][1]
+
+        x2 = coordinates[1][0]
+        y2 = coordinates[1][1]
+
+        for i in range(2, len(coordinates)):
+            x, y = coordinates[i][0], coordinates[i][1]
+            if (y - y1) * (x2 - x1) != (x - x1) * (y2 - y1):
+                return False
+
+        return True
+
+
