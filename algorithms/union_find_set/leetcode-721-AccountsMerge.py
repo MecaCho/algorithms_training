578ab214ebe8f2577b77a088f639cbfdeef87b84
MecaCho <1099833328@qq.com>
2021-01-19 12:21:57 +0800

    add 721. Accounts Merge solutions;

--- algorithms/union_find_set/leetcode-721-AccountsMerge.py
+++ algorithms/union_find_set/leetcode-721-AccountsMerge.py
@@ -127,3 +127,73 @@ class Solution1():
             ans.append([emailToName[emails[0]]] + sorted(emails))
         return ans
 
+# solutions
+
+'''
+方法一：哈希表 + 并查集
+两个账户需要合并，当且仅当两个账户至少有一个共同的邮箱地址，因此这道题的实质是判断所有的邮箱地址中有哪些邮箱地址必定属于同一人，可以使用并查集实现。
+
+为了使用并查集实现账户合并，需要知道一共有多少个不同的邮箱地址，以及每个邮箱对应的名称，因此需要使用两个哈希表分别记录每个邮箱对应的编号和每个邮箱对应的名称，遍历所有的账户并在两个哈希表中记录相应的信息。虽然同一个邮箱地址可能在多个账户中出现，但是同一个邮箱地址在两个哈希表中都只能存储一次。
+
+然后使用并查集进行合并操作。由于同一个账户中的邮箱地址一定属于同一个人，因此遍历每个账户，对账户中的邮箱地址进行合并操作。并查集存储的是每个邮箱地址对应的编号，合并操作也是针对编号进行合并。
+
+完成并查集的合并操作之后，即可知道合并后有多少个不同的账户。遍历所有的邮箱地址，对于每个邮箱地址，通过并查集得到该邮箱地址属于哪个合并后的账户，即可整理出每个合并后的账户包含哪些邮箱地址。
+
+对于每个合并后的账户，需要整理出题目要求的返回账户的格式，具体做法是：将邮箱地址排序，账户的名称可以通过在哈希表中查找任意一个邮箱对应的名称得到，将名称和排序后的邮箱地址整理成一个账户列表。对所有合并后的账户整理出账户列表，即可得到最终答案。
+
+JavaC++JavaScriptGolangPython3
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def union(self, index1: int, index2: int):
+        self.parent[self.find(index2)] = self.find(index1)
+
+    def find(self, index: int) -> int:
+        if self.parent[index] != index:
+            self.parent[index] = self.find(self.parent[index])
+        return self.parent[index]
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        emailToIndex = dict()
+        emailToName = dict()
+
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                if email not in emailToIndex:
+                    emailToIndex[email] = len(emailToIndex)
+                    emailToName[email] = name
+        
+        uf = UnionFind(len(emailToIndex))
+        for account in accounts:
+            firstIndex = emailToIndex[account[1]]
+            for email in account[2:]:
+                uf.union(firstIndex, emailToIndex[email])
+        
+        indexToEmails = collections.defaultdict(list)
+        for email, index in emailToIndex.items():
+            index = uf.find(index)
+            indexToEmails[index].append(email)
+        
+        ans = list()
+        for emails in indexToEmails.values():
+            ans.append([emailToName[emails[0]]] + sorted(emails))
+        return ans
+复杂度分析
+
+时间复杂度：O(n \log n)O(nlogn)，其中 nn 是不同邮箱地址的数量。
+需要遍历所有邮箱地址，在并查集内进行查找和合并操作，对于两个不同的邮箱地址，如果它们的祖先不同则需要进行合并，需要进行 22 次查找和最多 11 次合并。一共需要进行 2n2n 次查找和最多 nn 次合并，因此时间复杂度是 O(2n \log n)=O(n \log n)O(2nlogn)=O(nlogn)。这里的并查集使用了路径压缩，但是没有使用按秩合并，最坏情况下的时间复杂度是 O(n \log n)O(nlogn)，平均情况下的时间复杂度依然是 O(n \alpha (n))O(nα(n))，其中 \alphaα 为阿克曼函数的反函数，\alpha (n)α(n) 可以认为是一个很小的常数。
+整理出题目要求的返回账户的格式时需要对邮箱地址排序，时间复杂度是 O(n \log n)O(nlogn)。
+其余操作包括遍历所有邮箱地址，在哈希表中记录相应的信息，时间复杂度是 O(n)O(n)，在渐进意义下 O(n)O(n) 小于 O(n \log n)O(nlogn)。
+因此总时间复杂度是 O(n \log n)O(nlogn)。
+
+空间复杂度：O(n)O(n)，其中 nn 是不同邮箱地址的数量。空间复杂度主要取决于哈希表和并查集，每个哈希表存储的邮箱地址的数量为 nn，并查集的大小为 nn。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/accounts-merge/solution/zhang-hu-he-bing-by-leetcode-solution-3dyq/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
