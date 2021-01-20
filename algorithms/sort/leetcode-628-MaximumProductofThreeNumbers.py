5487f907f8970ba77ae95b7e2f15f5ee8e6b4bd0
MecaCho <1099833328@qq.com>
2021-01-20 17:47:02 +0800

    add 628. Maximum Product of Three Numbers solution;

--- algorithms/sort/leetcode-628-MaximumProductofThreeNumbers.py
+++ algorithms/sort/leetcode-628-MaximumProductofThreeNumbers.py
@@ -49,6 +49,15 @@ class Solution20210120(object):
         return max(nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[2], nums[0]*nums[1]*nums[-1])
 
 
+class Solution202101201(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return max(nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[-1])
+
 
 class Solution(object):
     def maximumProduct(self, nums):
