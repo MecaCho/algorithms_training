c3e793f40c436495096cbe3531296130d03f55e5
MecaCho <1099833328@qq.com>
2021-01-22 09:38:02 +0800

    add 989. Add to Array-Form of Integer solutions;

--- algorithms/arr/leetcode-989-AddtoArray-FormofInteger.py
+++ algorithms/arr/leetcode-989-AddtoArray-FormofInteger.py
@@ -97,6 +97,76 @@ class Solution(object):
         return A
 
 
+# solutions
+
+'''
+方法一：逐位相加
+思路
+
+让我们逐位将数字加在一起。例如计算 123+912123+912，我们从低位到高位依次计算 3+23+2、2+12+1 和 1+91+9。任何时候，若加法的结果大于等于 1010，把进位的 11 加入到下一位的计算中，所以最终结果为 10351035。
+
+代码
+
+C++JavaGolangCJavaScript
+
+func addToArrayForm(A []int, K int) (ans []int) {
+    for i := len(A) - 1; i >= 0; i-- {
+        sum := A[i] + K%10
+        K /= 10
+        if sum >= 10 {
+            K++
+            sum -= 10
+        }
+        ans = append(ans, sum)
+    }
+    for ; K > 0; K /= 10 {
+        ans = append(ans, K%10)
+    }
+    reverse(ans)
+    return
+}
+
+func reverse(A []int) {
+    for i, n := 0, len(A); i < n/2; i++ {
+        A[i], A[n-1-i] = A[n-1-i], A[i]
+    }
+}
+另一个思路是将整个加数 KK 加入数组表示的数的最低位。
+
+上面的例子 123+912123+912，我们把它表示成 [1,2,3+912][1,2,3+912]。然后，我们计算 3+912=9153+912=915。55 留在当前这一位，将 910/10=91910/10=91 以进位的形式加入下一位。
+
+然后，我们再重复这个过程，计算 [1,2+91,5][1,2+91,5]。我们得到 9393，33 留在当前位，将 90/10=990/10=9 以进位的形式加入下一位。继而又得到 [1+9,3,5][1+9,3,5]，重复这个过程之后，最终得到结果 [1,0,3,5][1,0,3,5]。
+
+C++JavaGolangCJavaScript
+
+func addToArrayForm(A []int, K int) (ans []int) {
+    for i := len(A) - 1; i >= 0 || K > 0; i-- {
+        if i >= 0 {
+            K += A[i]
+        }
+        ans = append(ans, K%10)
+        K /= 10
+    }
+    reverse(ans)
+    return
+}
+
+func reverse(A []int) {
+    for i, n := 0, len(A); i < n/2; i++ {
+        A[i], A[n-1-i] = A[n-1-i], A[i]
+    }
+}
+复杂度分析
+
+时间复杂度：O(\max(n,\log K))O(max(n,logK))，其中 nn 为数组的长度。
+
+空间复杂度：O(\max(n,\log K))O(max(n,logK))。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/add-to-array-form-of-integer/solution/shu-zu-xing-shi-de-zheng-shu-jia-fa-by-l-jljp/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
 
 
 
