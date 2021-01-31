d6bf6575dbc009167351ca30acd47298e35cef27
MecaCho <1099833328@qq.com>
2021-01-31 21:57:35 +0800

    add leetcode 839. Similar String Groups solutions;

--- algorithms/union_find_set/leetcode-839-SimilarStringGroups.py
+++ algorithms/union_find_set/leetcode-839-SimilarStringGroups.py
@@ -108,4 +108,67 @@ class Solution(object):
 
         return sum([1 if i == self.p[i] else 0 for i in range(n)])
 
+# solutions
+
+'''
+方法一：并查集
+思路及解法
+
+我们把每一个字符串看作点，字符串之间是否相似看作边，那么可以发现本题询问的是给定的图中有多少连通分量。于是可以想到使用并查集维护节点间的连通性。
+
+我们枚举给定序列中的任意一对字符串，检查其是否具有相似性，如果相似，那么我们就将这对字符串相连。
+
+在实际代码中，我们可以首先判断当前这对字符串是否已经连通，如果没有连通，我们再检查它们是否具有相似性，可以优化一定的时间复杂度的常数。
+
+代码
+
+C++JavaJavaScriptPython3GolangC
+
+class Solution:
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        n = len(strs)
+        f = list(range(n))
+
+        def find(x: int) -> int:
+            if f[x] == x:
+                return x
+            f[x] = find(f[x])
+            return f[x]
+        
+        def check(a: str, b: str) -> bool:
+            num = 0
+            for ac, bc in zip(a, b):
+                if ac != bc:
+                    num += 1
+                    if num > 2:
+                        return False
+            return True
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                fi, fj = find(i), find(j)
+                if fi == fj:
+                    continue
+                if check(strs[i], strs[j]):
+                    f[fi] = fj
+        
+        ret = sum(1 for i in range(n) if f[i] == i)
+        return ret
+复杂度分析
+
+时间复杂度：O(n^2m + n \log n))O(n 
+2
+ m+nlogn))，其中 nn 是字符串的数量。我们需要 O(n^2)O(n 
+2
+ ) 地枚举任意一对字符串之间的关系，对于任意一对字符串，我们需要 O(m)O(m) 的时间检查字符串是否相同。在最坏情况下我们需要对并查集执行 O(n)O(n) 次合并，合并的均摊时间复杂度 O(\log n)O(logn)。综上，总的时间复杂度为 O(n^2m + n \log n))O(n 
+2
+ m+nlogn))
+
+空间复杂度：O(n)O(n)，其中 nn 是字符串的数量。并查集需要 O(n)O(n) 的空间。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/similar-string-groups/solution/xiang-si-zi-fu-chuan-zu-by-leetcode-solu-8jt9/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
 
