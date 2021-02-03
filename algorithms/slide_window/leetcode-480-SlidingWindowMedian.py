ccc2057ac1ba64764cbbba665481f2a7e1f4e5be
MecaCho <1099833328@qq.com>
2021-02-03 09:46:47 +0800

    add leetcode 480. Sliding Window Median tips;

--- algorithms/slide_window/leetcode-480-SlidingWindowMedian.py
+++ algorithms/slide_window/leetcode-480-SlidingWindowMedian.py
@@ -73,4 +73,13 @@ class Solution(object):
         return [sorted(nums[i:i+k])[k//2] for i in range(len(nums)-k+1)] if k%2 else [(sorted(nums[i:i+k])[k//2]+sorted(nums[i:i+k])[(k-1)//2])/2.0 for i in range(len(nums)-k+1)]
 
 
+# tips
+
+'''
+The simplest of solutions comes from the basic idea of finding the median given a set of numbers. We know that by definition, a median is the center element (or an average of the two center elements). Given an unsorted list of numbers, how do we find the median element? If you know the answer to this question, can we extend this idea to every sliding window that we come across in the array?
+
+Is there a better way to do what we are doing in the above hint? Don't you think there is duplication of calculation being done there? Is there some sort of optimization that we can do to achieve the same result? This approach is merely a modification of the basic approach except that it simply reduces duplication of calculations once done.
+
+The third line of thought is also based on this same idea but achieving the result in a different way. We obviously need the window to be sorted for us to be able to find the median. Is there a data-structure out there that we can use (in one or more quantities) to obtain the median element extremely fast, say O(1) time while having the ability to perform the other operations fairly efficiently as well?
+'''
 
