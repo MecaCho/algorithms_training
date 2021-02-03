207ed9edaafca92f6f80c40788614915a503a954
MecaCho <1099833328@qq.com>
2021-02-03 09:49:00 +0800

    add leetcode 480. Sliding Window Median solutions;

--- algorithms/slide_window/leetcode-480-SlidingWindowMedian.py
+++ algorithms/slide_window/leetcode-480-SlidingWindowMedian.py
@@ -83,3 +83,121 @@ Is there a better way to do what we are doing in the above hint? Don't you think
 The third line of thought is also based on this same idea but achieving the result in a different way. We obviously need the window to be sorted for us to be able to find the median. Is there a data-structure out there that we can use (in one or more quantities) to obtain the median element extremely fast, say O(1) time while having the ability to perform the other operations fairly efficiently as well?
 '''
 
+
+'''
+前言
+本题和 295. 数据流的中位数 非常相似。
+求a的中位数的思路与代码如下：
+
+python3思路
+
+a[len(a)//2] if len(a)%2 else (a[len(a)//2-1] + a[len(a)//2]) / 2
+解法一：数组+暴力法
+思路
+这个办法不难想到，就是将每一组的中位数加入返回结果res，再返回。
+
+执行结果
+第一种
+执行用时：4992 ms, 在所有 Python3 提交中击败了25.46%的用户
+内存消耗：16.1 MB, 在所有 Python3 提交中击败了42.07%的用户
+
+第二种
+执行用时：7248 ms, 在所有 Python3 提交中击败了5.09%的用户
+内存消耗：16.4 MB, 在所有 Python3 提交中击败了14.02%的用户
+
+代码
+python3python3
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        median = lambda a: a[len(a)//2] if len(a)%2 else (a[len(a)//2-1] + a[len(a)//2]) / 2
+        res = []
+        for i in range(len(nums)-k+1):
+            res.append(median(sorted(nums[i:i+k])))
+        return res
+时间复杂度
+时间复杂度：O(n k log k)O(nklogk)，排序的时间复杂度为O(k log k)O(klogk)，总共执行了nn次。
+
+解法二：数组+二分查找
+思路
+维护一个数组a，它保存当前数组，最开始等于排好序的nums的前k个元素。注意：a是排好序的。
+用res保存返回结果，首先将a加入。
+用i, j循环nums，它们分别表示删除值和加入值，每一对i, j都要执行下面步骤：
+将a中值为i的元素删除；
+将j增加在合适的位置，使用二分查找库（bisect）；
+得到中位数并增加到res的最后。
+返回。
+举例
+输入：nums = [1, 3, -1, -3, 5], k = 2
+步骤：
+
+a = [1, 3]，res = [a的中位数] = [2]
+a = [1, 3]，删除1，增加-1，a = [-1, 3]，res增加a的中位数 = 1，res = [2, 1]
+a = [-1, 3]，删除3，增加-3，a = [-3, -1]，res增加a的中位数 = -2，res = [2, 1, -2]
+a = [-3, -1]，删除-1，增加5，a = [-3, 5]，res增加a的中位数 = 1，res = [2, 1, -2, 1]
+返回res
+执行结果
+执行用时：308 ms, 在所有 Python3 提交中击败了38.91%的用户
+内存消耗：16.1 MB, 在所有 Python3 提交中击败了43.91%的用户
+
+python3
+
+import bisect
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        median = lambda a: a[len(a)//2] if len(a)%2 else (a[len(a)//2-1] + a[len(a)//2]) / 2
+        a = sorted(nums[:k])
+        res = [median(a)]
+        for i, j in zip(nums[:-k], nums[k:]):
+            a.remove(i)
+            a.insert(bisect.bisect_left(a, j), j)
+            res.append(median(a))
+        return res
+时间复杂度
+时间复杂度：O(n^2)O(n 
+2
+ )
+
+解法三：数组+二分查找（优化）
+思路
+和解法二思路一致，思路改变部分加粗了，思路如下：
+
+维护一个数组a，它保存当前数组，最开始等于排好序的nums的前k个元素。注意：a是排好序的。
+用res保存返回结果，首先将a加入。
+用i, j循环nums，它们分别表示删除值和加入值，每一对i, j都要执行下面步骤：
+将值为i的索引从a中删除，使用二分查找库（bisect）；
+将j增加在合适的位置，使用二分查找库（bisect）；
+得到中位数并增加到res的最后。
+返回。
+执行结果
+执行用时：72 ms, 在所有 Python3 提交中击败了100.00%的用户
+内存消耗：16.1 MB, 在所有 Python3 提交中击败了44.28%的用户
+
+python3
+
+import bisect
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        median = lambda a: a[len(a)//2] if len(a)%2 else (a[len(a)//2-1] + a[len(a)//2]) / 2
+        a = sorted(nums[:k])
+        res = [median(a)]
+        for i, j in zip(nums[:-k], nums[k:]):
+            a.pop(bisect.bisect_left(a, i))
+            a.insert(bisect.bisect_left(a, j), j)
+            res.append(median(a))
+        return res
+时间复杂度
+时间复杂度：O(n^2)O(n 
+2
+ )
+
+
+
+作者：ting-ting-28
+链接：https://leetcode-cn.com/problems/sliding-window-median/solution/python3-chao-xiang-xi-duo-jie-fa-bao-li-ga02a/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
+
