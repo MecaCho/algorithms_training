d37dc037bd39a62398e56860ca57e277f0913f4d
MecaCho <1099833328@qq.com>
2021-02-07 09:41:10 +0800

    add leetcode 665. Non-decreasing Array;

---
+++ algorithms/arr/leetcode-665-Non-decreasingArray.py
@@ -0,0 +1,72 @@
+# encoding=utf8
+
+'''
+665. Non-decreasing Array
+Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+
+
+Example 1:
+
+Input: nums = [4,2,3]
+Output: true
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+
+Input: nums = [4,2,1]
+Output: false
+Explanation: You can't get a non-decreasing array by modify at most one element.
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+-105 <= nums[i] <= 105
+
+
+665. 非递减数列
+给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+
+我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+
+
+
+示例 1:
+
+输入: nums = [4,2,3]
+输出: true
+解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+示例 2:
+
+输入: nums = [4,2,1]
+输出: false
+解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+
+
+说明：
+
+1 <= n <= 10 ^ 4
+- 10 ^ 5 <= nums[i] <= 10 ^ 5
+'''
+
+
+
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                count += 1
+                if i > 0 and nums[i+1] < nums[i-1]:
+                    nums[i+1] = nums[i]
+
+        return count < 2
+
+
