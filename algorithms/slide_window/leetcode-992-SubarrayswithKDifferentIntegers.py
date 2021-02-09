341ea6ac969f76dabf08c284b1f397ddfd38eb53
MecaCho <1099833328@qq.com>
2021-02-09 11:42:31 +0800

    add leetcode 992. Subarrays with K Different Integers;

---
+++ algorithms/slide_window/leetcode-992-SubarrayswithKDifferentIntegers.py
@@ -0,0 +1,97 @@
+# encoding=utf8
+
+'''
+992. Subarrays with K Different Integers
+Given an array A of positive integers, call a (contiguous, not necessarily distinct) subarray of A good if the number of different integers in that subarray is exactly K.
+
+(For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.)
+
+Return the number of good subarrays of A.
+
+
+
+Example 1:
+
+Input: A = [1,2,1,2,3], K = 2
+Output: 7
+Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2].
+Example 2:
+
+Input: A = [1,2,1,3,4], K = 3
+Output: 3
+Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+
+
+Note:
+
+1 <= A.length <= 20000
+1 <= A[i] <= A.length
+1 <= K <= A.length
+
+
+992. K 个不同整数的子数组
+给定一个正整数数组 A，如果 A 的某个子数组中不同整数的个数恰好为 K，则称 A 的这个连续、不一定独立的子数组为好子数组。
+
+（例如，[1,2,3,1,2] 中有 3 个不同的整数：1，2，以及 3。）
+
+返回 A 中好子数组的数目。
+
+
+
+示例 1：
+
+输入：A = [1,2,1,2,3], K = 2
+输出：7
+解释：恰好由 2 个不同整数组成的子数组：[1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2].
+示例 2：
+
+输入：A = [1,2,1,3,4], K = 3
+输出：3
+解释：恰好由 3 个不同整数组成的子数组：[1,2,1,3], [2,1,3], [1,3,4].
+
+
+提示：
+
+1 <= A.length <= 20000
+1 <= A[i] <= A.length
+1 <= K <= A.length
+'''
+
+
+class Solution(object):
+    def subarraysWithKDistinct(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+
+        def max_sub_arr(nums, K):
+            l, r = 0, 0
+            res = 0
+            length = len(nums)
+            freq = [0] * (length + 1)
+            count = 0
+            while r < length:
+                if freq[nums[r]] == 0:
+                    count += 1
+
+                freq[nums[r]] += 1
+                r += 1
+
+                while count > K:
+                    freq[nums[l]] -= 1
+                    if freq[nums[l]] == 0:
+                        count -= 1
+                    l += 1
+
+                res += r - l
+
+            return res
+
+        return max_sub_arr(A, K) - max_sub_arr(A, K - 1)
+
+
+
+
+
