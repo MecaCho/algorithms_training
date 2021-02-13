9046caea261e868c47caa039a91b61d2ee96aa94
MecaCho <1099833328@qq.com>
2021-02-13 12:23:39 +0800

    add 448. Find All Numbers Disappeared in an Array solution;

--- algorithms/arr/leetcode-448-FindAllNumbersDisappearedinanArray.py
+++ algorithms/arr/leetcode-448-FindAllNumbersDisappearedinanArray.py
@@ -1,3 +1,6 @@
+# encoding=utf8
+
+
 '''
 448. 找到所有数组中消失的数字
 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
@@ -54,6 +57,27 @@ class Solution(object):
         return res
 
 
+
+class Solution20210213(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        i = 0
+        while i < len(nums):
+            while nums[i] != nums[nums[i]-1]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+
+            i += 1
+
+        res = []
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                res.append(i+1)
+        return res
+
+
 # tips
 
 '''
@@ -62,4 +86,47 @@ This is a really easy problem if you decide to use additional memory. For those 
 However, the trick really is to not use any additional space than what is already available to use. Sometimes, multiple passes over the input array help find the solution. However, there's an interesting piece of information in this problem that makes it easy to re-use the input array itself for the solution.
 
 The problem specifies that the numbers in the array will be in the range [1, n] where n is the number of elements in the array. Can we use this information and modify the array in-place somehow to find what we need?
+'''
+
+# solutions
+
+'''
+方法一：原地修改
+思路及解法
+
+我们可以用一个哈希表记录数组 \textit{nums}nums 中的数字，由于数字范围均在 [1,n][1,n] 中，记录数字后我们再利用哈希表检查 [1,n][1,n] 中的每一个数是否出现，从而找到缺失的数字。
+
+由于数字范围均在 [1,n][1,n] 中，我们也可以用一个长度为 nn 的数组来代替哈希表。这一做法的空间复杂度是 O(n)O(n) 的。我们的目标是优化空间复杂度到 O(1)O(1)。
+
+注意到 \textit{nums}nums 的长度恰好也为 nn，能否让 \textit{nums}nums 充当哈希表呢？
+
+由于 \textit{nums}nums 的数字范围均在 [1,n][1,n] 中，我们可以利用这一范围之外的数字，来表达「是否存在」的含义。
+
+具体来说，遍历 \textit{nums}nums，每遇到一个数 xx，就让 \textit{nums}[x-1]nums[x−1] 增加 nn。由于 \textit{nums}nums 中所有数均在 [1,n][1,n] 中，增加以后，这些数必然大于 nn。最后我们遍历 \textit{nums}nums，若 \textit{nums}[i]nums[i] 未大于 nn，就说明没有遇到过数 i+1i+1。这样我们就找到了缺失的数字。
+
+注意，当我们遍历到某个位置时，其中的数可能已经被增加过，因此需要对 nn 取模来还原出它本来的值。
+
+代码
+
+C++JavaPython3GolangCJavaScript
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for num in nums:
+            x = (num - 1) % n
+            nums[x] += n
+        
+        ret = [i + 1 for i, num in enumerate(nums) if num <= n]
+        return ret
+复杂度分析
+
+时间复杂度：O(n)O(n)。其中 nn 是数组 \textit{nums}nums 的长度。
+
+空间复杂度：O(1)O(1)。返回值不计入空间复杂度。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/solution/zhao-dao-suo-you-shu-zu-zhong-xiao-shi-d-mabl/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
