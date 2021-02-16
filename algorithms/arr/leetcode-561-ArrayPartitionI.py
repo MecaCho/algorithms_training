28025174501c66a8bf4170496e1c89394302b0ec
MecaCho <1099833328@qq.com>
2021-02-16 22:50:29 +0800

    add leetcode 561. Array Partition I solution golang;

--- algorithms/arr/leetcode-561-ArrayPartitionI.py
+++ algorithms/arr/leetcode-561-ArrayPartitionI.py
@@ -69,7 +69,18 @@ class Solution(object):
         """
         return sum(sorted(nums)[::2])
 
+# golang solution
 
+'''
+func arrayPairSum(nums []int) int {
+    sort.Ints(nums)
+    res := 0
+    for i := 0 ; i < len(nums); i += 2{
+        res += nums[i]
+    }
+    return res
+}
+'''
 
 # solutions
 
