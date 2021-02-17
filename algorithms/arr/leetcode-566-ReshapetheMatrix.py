edb66e15da2756617161f4207070852672216ea6
MecaCho <1099833328@qq.com>
2021-02-17 20:29:12 +0800

    add leetcode 566. Reshape the Matrix golang solution;

--- algorithms/arr/leetcode-566-ReshapetheMatrix.py
+++ algorithms/arr/leetcode-566-ReshapetheMatrix.py
@@ -100,6 +100,31 @@ class Solution(object):
 
         return res
 
+# golang solution
+
+'''
+func matrixReshape(nums [][]int, r int, c int) [][]int {
+	if len(nums) == 0 {
+		return nums
+	}
+
+	r0, c0 := len(nums), len(nums[0])
+	if r0*c0 != r*c {
+		return nums
+	}
+
+	res := make([][]int, r)
+	for i, num := range nums {
+		for j, nu := range num {
+			index := (i*c0 + j) / c
+			fmt.Println(index, nu, i*c0 + j, c)
+			res[index] = append(res[index], nu)
+		}
+	}
+	return res
+
+}
+'''
 
 # solutions
 
