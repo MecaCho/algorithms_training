ceec6655838659ad84e9e77319fdce3d194b238c
MecaCho <1099833328@qq.com>
2021-02-25 09:56:30 +0800

    add leetcode 867. Transpose Matrix solutions;

--- algorithms/arr/leetcode-867-TransposeMatrix.py
+++ algorithms/arr/leetcode-867-TransposeMatrix.py
@@ -67,3 +67,37 @@ class Solution(object):
         """
         return map(list, zip(*matrix[:]))
 
+# solutions
+
+
+'''
+方法一：模拟
+如果矩阵 \textit{matrix}matrix 为 mm 行 nn 列，则转置后的矩阵 \textit{matrix}^\text{T}matrix 
+T
+  为 nn 行 mm 列，且对任意 0 \le i<m0≤i<m 和 0 \le j<n0≤j<n，都有 \textit{matrix}^\text{T}[j][i]=\textit{matrix}[i][j]matrix 
+T
+ [j][i]=matrix[i][j]。
+
+创建一个 nn 行 mm 列的新矩阵，根据转置的规则对新矩阵中的每个元素赋值，则新矩阵为转置后的矩阵。
+
+JavaJavaScriptGolangPython3Python3_onelineC++C
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        transposed = [[0] * m for _ in range(n)]
+        for i in range(m):
+            for j in range(n):
+                transposed[j][i] = matrix[i][j]
+        return transposed
+复杂度分析
+
+时间复杂度：O(mn)O(mn)，其中 mm 和 nn 分别是矩阵 \textit{matrix}matrix 的行数和列数。需要遍历整个矩阵，并对转置后的矩阵进行赋值操作。
+
+空间复杂度：O(1)O(1)。除了返回值以外，额外使用的空间为常数。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/transpose-matrix/solution/zhuan-zhi-ju-zhen-by-leetcode-solution-85s2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
