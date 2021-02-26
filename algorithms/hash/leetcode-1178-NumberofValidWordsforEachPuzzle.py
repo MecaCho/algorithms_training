f3d8a49e91f30e60cfd72a3fba4c441413e8a6c6
MecaCho <1099833328@qq.com>
2021-02-26 09:46:15 +0800

    add leetcode 1178. Number of Valid Words for Each Puzzle;

---
+++ algorithms/hash/leetcode-1178-NumberofValidWordsforEachPuzzle.py
@@ -0,0 +1,91 @@
+# encoding=utf8
+
+'''
+1178. Number of Valid Words for Each Puzzle
+With respect to a given puzzle string, a word is valid if both the following conditions are satisfied:
+word contains the first letter of puzzle.
+For each letter in word, that letter is in puzzle.
+For example, if the puzzle is "abcdefg", then valid words are "faced", "cabbage", and "baggage"; while invalid words are "beefed" (doesn't include "a") and "based" (includes "s" which isn't in the puzzle).
+Return an array answer, where answer[i] is the number of words in the given word list words that are valid with respect to the puzzle puzzles[i].
+
+
+Example :
+
+Input:
+words = ["aaaa","asas","able","ability","actt","actor","access"],
+puzzles = ["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"]
+Output: [1,1,3,2,4,0]
+Explanation:
+1 valid word for "aboveyz" : "aaaa"
+1 valid word for "abrodyz" : "aaaa"
+3 valid words for "abslute" : "aaaa", "asas", "able"
+2 valid words for "absoryz" : "aaaa", "asas"
+4 valid words for "actresz" : "aaaa", "asas", "actt", "access"
+There're no valid words for "gaswxyz" cause none of the words in the list contains letter 'g'.
+
+
+Constraints:
+
+1 <= words.length <= 10^5
+4 <= words[i].length <= 50
+1 <= puzzles.length <= 10^4
+puzzles[i].length == 7
+words[i][j], puzzles[i][j] are English lowercase letters.
+Each puzzles[i] doesn't contain repeated characters.
+
+
+
+1178. 猜字谜
+外国友人仿照中国字谜设计了一个英文版猜字谜小游戏，请你来猜猜看吧。
+
+字谜的迷面 puzzle 按字符串形式给出，如果一个单词 word 符合下面两个条件，那么它就可以算作谜底：
+
+单词 word 中包含谜面 puzzle 的第一个字母。
+单词 word 中的每一个字母都可以在谜面 puzzle 中找到。
+例如，如果字谜的谜面是 "abcdefg"，那么可以作为谜底的单词有 "faced", "cabbage", 和 "baggage"；而 "beefed"（不含字母 "a"）以及 "based"（其中的 "s" 没有出现在谜面中）。
+返回一个答案数组 answer，数组中的每个元素 answer[i] 是在给出的单词列表 words 中可以作为字谜迷面 puzzles[i] 所对应的谜底的单词数目。
+
+
+
+示例：
+
+输入：
+words = ["aaaa","asas","able","ability","actt","actor","access"],
+puzzles = ["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"]
+输出：[1,1,3,2,4,0]
+解释：
+1 个单词可以作为 "aboveyz" 的谜底 : "aaaa"
+1 个单词可以作为 "abrodyz" 的谜底 : "aaaa"
+3 个单词可以作为 "abslute" 的谜底 : "aaaa", "asas", "able"
+2 个单词可以作为 "absoryz" 的谜底 : "aaaa", "asas"
+4 个单词可以作为 "actresz" 的谜底 : "aaaa", "asas", "actt", "access"
+没有单词可以作为 "gaswxyz" 的谜底，因为列表中的单词都不含字母 'g'。
+
+
+提示：
+
+1 <= words.length <= 10^5
+4 <= words[i].length <= 50
+1 <= puzzles.length <= 10^4
+puzzles[i].length == 7
+words[i][j], puzzles[i][j] 都是小写英文字母。
+每个 puzzles[i] 所包含的字符都不重复。
+'''
+
+
+from collections import Counter
+from itertools import combinations
+class Solution(object):
+    def findNumOfValidWords(self, words, puzzles):
+        """
+        :type words: List[str]
+        :type puzzles: List[str]
+        :rtype: List[int]
+        """
+        counter, ans = Counter(filter(lambda x: len(x) <= 7, map(frozenset, words))), [0]*(len(puzzles))
+        for index, puzzle in enumerate(puzzles):
+            for comb in list(map(lambda i:combinations(puzzle[1:], i), range(7))):
+                ans[index] += sum(counter[frozenset((puzzle[0],)+ele)] for ele in comb)
+        return ans
+
+
