b600572fcab7bb6cad8f7ef7ae975753da793f8d
MecaCho <1099833328@qq.com>
2021-03-09 09:39:55 +0800

    add leetcode 1047. Remove All Adjacent Duplicates In String solutions;

--- algorithms/stack/leetcode-1047-RemoveAllAdjacentDuplicatesInString.py
+++ algorithms/stack/leetcode-1047-RemoveAllAdjacentDuplicatesInString.py
@@ -64,3 +64,37 @@ class Solution(object):
         return "".join(stack)
 
 
+# solutions
+
+'''
+方法一：栈
+充分理解题意后，我们可以发现，当字符串中同时有多组相邻重复项时，我们无论是先删除哪一个，都不会影响最终的结果。因此我们可以从左向右顺次处理该字符串。
+
+而消除一对相邻重复项可能会导致新的相邻重复项出现，如从字符串 \text{abba}abba 中删除 \text{bb}bb 会导致出现新的相邻重复项 \text{aa}aa 出现。因此我们需要保存当前还未被删除的字符。一种显而易见的数据结构呼之欲出：栈。我们只需要遍历该字符串，如果当前字符和栈顶字符相同，我们就贪心地将其消去，否则就将其入栈即可。
+
+代码
+
+在下面的 \texttt{C++}C++ 代码中，由于 \texttt{std::string}std::string 类本身就提供了类似「入栈」和「出栈」的接口，因此我们直接将需要被返回的字符串作为栈即可。对于其他的语言，如果字符串类没有提供相应的接口，则需要在遍历完成字符串后，使用栈中的字符显式地构造出需要被返回的字符串。
+
+C++JavaJavaScriptPython3GolangC
+
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stk = list()
+        for ch in S:
+            if stk and stk[-1] == ch:
+                stk.pop()
+            else:
+                stk.append(ch)
+        return "".join(stk)
+复杂度分析
+
+时间复杂度：O(n)O(n)，其中 nn 是字符串的长度。我们只需要遍历该字符串一次。
+
+空间复杂度：O(n)O(n) 或 O(1)O(1)，取决于使用的语言提供的字符串类是否提供了类似「入栈」和「出栈」的接口。注意返回值不计入空间复杂度。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/solution/shan-chu-zi-fu-chuan-zhong-de-suo-you-xi-4ohr/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
