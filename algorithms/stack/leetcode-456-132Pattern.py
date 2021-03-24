79f313b6d35a2d0fa00a0e6cf9f444429360accd
MecaCho <1099833328@qq.com>
2021-03-24 11:21:59 +0800

    add leetcode 456. 132 Pattern;

---
+++ algorithms/stack/leetcode-456-132Pattern.py
@@ -0,0 +1,97 @@
+# encoding=utf8
+
+
+'''
+456. 132 Pattern
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+
+Follow up: The O(n^2) is trivial, could you come up with the O(n logn) or the O(n) solution?
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+Example 2:
+
+Input: nums = [3,1,4,2]
+Output: true
+Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+Example 3:
+
+Input: nums = [-1,3,2,0]
+Output: true
+Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+-109 <= nums[i] <= 109
+
+
+
+456. 132模式
+给你一个整数数组 nums ，数组中共有 n 个整数。132 模式的子序列 由三个整数 nums[i]、nums[j] 和 nums[k] 组成，并同时满足：i < j < k 和 nums[i] < nums[k] < nums[j] 。
+
+如果 nums 中存在 132 模式的子序列 ，返回 true ；否则，返回 false 。
+
+
+
+进阶：很容易想到时间复杂度为 O(n^2) 的解决方案，你可以设计一个时间复杂度为 O(n logn) 或 O(n) 的解决方案吗？
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,4]
+输出：false
+解释：序列中不存在 132 模式的子序列。
+示例 2：
+
+输入：nums = [3,1,4,2]
+输出：true
+解释：序列中有 1 个 132 模式的子序列： [1, 4, 2] 。
+示例 3：
+
+输入：nums = [-1,3,2,0]
+输出：true
+解释：序列中有 3 个 132 模式的的子序列：[-1, 3, 2]、[-1, 3, 0] 和 [-1, 2, 0] 。
+
+
+提示：
+
+n == nums.length
+1 <= n <= 104
+-109 <= nums[i] <= 109
+'''
+
+
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        length = len(nums)
+        mins = [nums[0]]
+        for i in range(1, length):
+            mins.append(min(nums[i], mins[-1]))
+
+        stack = []
+        for i in range(length-1, -1, -1):
+            if nums[i] > mins[i]:
+                while stack and stack[-1] <= mins[i]:
+                    stack.pop()
+                if stack and mins[i] < stack[-1] < nums[i]:
+                    return True
+                stack.append(nums[i])
+
+        return False
+
+
