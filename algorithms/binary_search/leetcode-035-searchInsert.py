ef361daaf372dd3645281631eb994eb9fce59ba5
MecaCho <1099833328@qq.com>
2021-03-25 15:30:40 +0800

    update 35. Search Insert Position solution;

--- algorithms/binary_search/leetcode-035-searchInsert.py
+++ algorithms/binary_search/leetcode-035-searchInsert.py
@@ -65,7 +65,28 @@ class Solution(object):
                 j = mid - 1
             elif mid_num < target:
                 i = mid + 1
-        return i 
+        return i
+
+
+
+class Solution20210325(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            mid = (i + j) / 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                j = mid - 1
+            else:
+                i = mid + 1
+        return i
 
 
 # solutions
