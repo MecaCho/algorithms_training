1a7013a9ce3350aa41e1b6099c035e1df3e2ba3c
MecaCho <1099833328@qq.com>
2021-03-28 12:43:04 +0800

    add leetcode 173. Binary Search Tree Iterator solutions;

--- algorithms/tree/leetcode-173-BinarySearchTreeIterator.py
+++ algorithms/tree/leetcode-173-BinarySearchTreeIterator.py
@@ -153,3 +153,88 @@ class BSTIterator(object):
 # param_2 = obj.hasNext()
 
 
+# solutions
+
+
+'''
+前言
+根据二叉搜索树的性质，不难发现，原问题等价于对二叉搜索树进行中序遍历。因此，我们可以采取与「94. 二叉树的中序遍历」类似的方法来解决这一问题。
+
+下面基于「94. 二叉树的中序遍历的官方题解」，给出本题的两种解法。读者将不难发现两篇题解的代码存在诸多相似之处。
+
+方法一：扁平化
+我们可以直接对二叉搜索树做一次完全的递归遍历，获取中序遍历的全部结果并保存在数组中。随后，我们利用得到的数组本身来实现迭代器。
+
+C++JavaGolangJavaScriptC
+
+type BSTIterator struct {
+    arr []int
+}
+
+func Constructor(root *TreeNode) (it BSTIterator) {
+    it.inorder(root)
+    return
+}
+
+func (it *BSTIterator) inorder(node *TreeNode) {
+    if node == nil {
+        return
+    }
+    it.inorder(node.Left)
+    it.arr = append(it.arr, node.Val)
+    it.inorder(node.Right)
+}
+
+func (it *BSTIterator) Next() int {
+    val := it.arr[0]
+    it.arr = it.arr[1:]
+    return val
+}
+
+func (it *BSTIterator) HasNext() bool {
+    return len(it.arr) > 0
+}
+复杂度分析
+
+时间复杂度：初始化需要 O(n)O(n) 的时间，其中 nn 为树中节点的数量。随后每次调用只需要 O(1)O(1) 的时间。
+
+空间复杂度：O(n)O(n)，因为需要保存中序遍历的全部结果。
+
+方法二：迭代
+除了递归的方法外，我们还可以利用栈这一数据结构，通过迭代的方式对二叉树做中序遍历。此时，我们无需预先计算出中序遍历的全部结果，只需要实时维护当前栈的情况即可。
+
+C++JavaGolangJavaScriptC
+
+type BSTIterator struct {
+    stack []*TreeNode
+    cur   *TreeNode
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+    return BSTIterator{cur: root}
+}
+
+func (it *BSTIterator) Next() int {
+    for node := it.cur; node != nil; node = node.Left {
+        it.stack = append(it.stack, node)
+    }
+    it.cur, it.stack = it.stack[len(it.stack)-1], it.stack[:len(it.stack)-1]
+    val := it.cur.Val
+    it.cur = it.cur.Right
+    return val
+}
+
+func (it *BSTIterator) HasNext() bool {
+    return it.cur != nil || len(it.stack) > 0
+}
+复杂度分析
+
+时间复杂度：显然，初始化和调用 \text{hasNext()}hasNext() 都只需要 O(1)O(1) 的时间。每次调用 \text{next()}next() 函数最坏情况下需要 O(n)O(n) 的时间；但考虑到 nn 次调用 \text{next()}next() 函数总共会遍历全部的 nn 个节点，因此总的时间复杂度为 O(n)O(n)，因此单次调用平均下来的均摊复杂度为 O(1)O(1)。
+
+空间复杂度：O(n)O(n)，其中 nn 是二叉树的节点数量。空间复杂度取决于栈深度，而栈深度在二叉树为一条链的情况下会达到 O(n)O(n) 的级别。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/binary-search-tree-iterator/solution/er-cha-sou-suo-shu-die-dai-qi-by-leetcod-4y0y/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
