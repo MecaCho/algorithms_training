f5ad5dccca8d3dee299c7e0ed769f91fad040abc
MecaCho <1099833328@qq.com>
2021-03-29 09:32:24 +0800

    add 190. Reverse Bits solutions;

--- algorithms/math_binary/leetcode-190-ReverseBits.py
+++ algorithms/math_binary/leetcode-190-ReverseBits.py
@@ -1,3 +1,6 @@
+# encoding=utf8
+
+
 '''
 190. 颠倒二进制位
 颠倒给定的 32 位无符号整数的二进制位。
@@ -71,3 +74,94 @@ class Solution:
             n = n/2
             i += 1
         return res
+
+
+class Solution20210329:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+
+        res = 0
+        i = 0
+        while i < 32:
+            res = res * 2 + n % 2
+            n /= 2
+            i += 1
+        return res
+
+
+# solutions
+
+'''
+方法一：逐位颠倒
+思路
+
+将 nn 视作一个长为 3232 的二进制串，从低位往高位枚举 nn 的每一位，将其倒序添加到翻转结果 \textit{rev}rev 中。
+
+代码实现中，每枚举一位就将 nn 右移一位，这样当前 nn 的最低位就是我们要枚举的比特位。当 nn 为 00 时即可结束循环。
+
+需要注意的是，在某些语言（如 \texttt{Java}Java）中，没有无符号整数类型，因此对 nn 的右移操作应使用逻辑右移。
+
+代码
+
+C++JavaGolangJavaScriptC
+
+func reverseBits(n uint32) (rev uint32) {
+    for i := 0; i < 32 && n > 0; i++ {
+        rev |= n & 1 << (31 - i)
+        n >>= 1
+    }
+    return
+}
+复杂度分析
+
+时间复杂度：O(\log n)O(logn)。
+
+空间复杂度：O(1)O(1)。
+
+方法二：位运算分治
+思路
+
+若要翻转一个二进制串，可以将其均分成左右两部分，对每部分递归执行翻转操作，然后将左半部分拼在右半部分的后面，即完成了翻转。
+
+由于左右两部分的计算方式是相似的，利用位掩码和位移运算，我们可以自底向上地完成这一分治流程。
+
+
+
+对于递归的最底层，我们需要交换所有奇偶位：
+
+取出所有奇数位和偶数位；
+将奇数位移到偶数位上，偶数位移到奇数位上。
+类似地，对于倒数第二层，每两位分一组，按组号取出所有奇数组和偶数组，然后将奇数组移到偶数组上，偶数组移到奇数组上。以此类推。
+
+需要注意的是，在某些语言（如 \texttt{Java}Java）中，没有无符号整数类型，因此对 nn 的右移操作应使用逻辑右移。
+
+代码
+
+C++JavaGolangJavaScriptC
+
+const (
+    m1 = 0x55555555 // 01010101010101010101010101010101
+    m2 = 0x33333333 // 00110011001100110011001100110011
+    m4 = 0x0f0f0f0f // 00001111000011110000111100001111
+    m8 = 0x00ff00ff // 00000000111111110000000011111111
+)
+
+func reverseBits(n uint32) uint32 {
+    n = n>>1&m1 | n&m1<<1
+    n = n>>2&m2 | n&m2<<2
+    n = n>>4&m4 | n&m4<<4
+    n = n>>8&m8 | n&m8<<8
+    return n>>16 | n<<16
+}
+复杂度分析
+
+时间复杂度：O(1)O(1)。
+
+空间复杂度：O(1)O(1)。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/reverse-bits/solution/dian-dao-er-jin-zhi-wei-by-leetcode-solu-yhxz/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
