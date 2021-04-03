1fe9130ded54058ed1f91dac052c763e12437df5
MecaCho <1099833328@qq.com>
2021-04-03 22:26:11 +0800

    add 1143. Longest Common Subsequence solutions;

--- algorithms/dynamic-programming/leetcode-1143-LongestCommonSubsequence.py
+++ algorithms/dynamic-programming/leetcode-1143-LongestCommonSubsequence.py
@@ -90,3 +90,184 @@ class Solution(object):
         return dp[-1][-1]
 
 
+# solutions
+
+'''
+方法一：动态规划
+最长公共子序列问题是典型的二维动态规划问题。
+
+假设字符串 \textit{text}_1text 
+1
+​	
+  和 \textit{text}_2text 
+2
+​	
+  的长度分别为 mm 和 nn，创建 m+1m+1 行 n+1n+1 列的二维数组 \textit{dp}dp，其中 \textit{dp}[i][j]dp[i][j] 表示 \textit{text}_1[0:i]text 
+1
+​	
+ [0:i] 和 \textit{text}_2[0:j]text 
+2
+​	
+ [0:j] 的最长公共子序列的长度。
+
+上述表示中，\textit{text}_1[0:i]text 
+1
+​	
+ [0:i] 表示 \textit{text}_1text 
+1
+​	
+  的长度为 ii 的前缀，\textit{text}_2[0:j]text 
+2
+​	
+ [0:j] 表示 \textit{text}_2text 
+2
+​	
+  的长度为 jj 的前缀。
+
+考虑动态规划的边界情况：
+
+当 i=0i=0 时，\textit{text}_1[0:i]text 
+1
+​	
+ [0:i] 为空，空字符串和任何字符串的最长公共子序列的长度都是 00，因此对任意 0 \le j \le n0≤j≤n，有 \textit{dp}[0][j]=0dp[0][j]=0；
+
+当 j=0j=0 时，\textit{text}_2[0:j]text 
+2
+​	
+ [0:j] 为空，同理可得，对任意 0 \le i \le m0≤i≤m，有 \textit{dp}[i][0]=0dp[i][0]=0。
+
+因此动态规划的边界情况是：当 i=0i=0 或 j=0j=0 时，\textit{dp}[i][j]=0dp[i][j]=0。
+
+当 i>0i>0 且 j>0j>0 时，考虑 \textit{dp}[i][j]dp[i][j] 的计算：
+
+当 \textit{text}_1[i-1]=\textit{text}_2[j-1]text 
+1
+​	
+ [i−1]=text 
+2
+​	
+ [j−1] 时，将这两个相同的字符称为公共字符，考虑 \textit{text}_1[0:i-1]text 
+1
+​	
+ [0:i−1] 和 \textit{text}_2[0:j-1]text 
+2
+​	
+ [0:j−1] 的最长公共子序列，再增加一个字符（即公共字符）即可得到 \textit{text}_1[0:i]text 
+1
+​	
+ [0:i] 和 \textit{text}_2[0:j]text 
+2
+​	
+ [0:j] 的最长公共子序列，因此 \textit{dp}[i][j]=\textit{dp}[i-1][j-1]+1dp[i][j]=dp[i−1][j−1]+1。
+
+当 \textit{text}_1[i-1] \ne \textit{text}_2[j-1]text 
+1
+​	
+ [i−1] 
+
+​	
+ =text 
+2
+​	
+ [j−1] 时，考虑以下两项：
+
+\textit{text}_1[0:i-1]text 
+1
+​	
+ [0:i−1] 和 \textit{text}_2[0:j]text 
+2
+​	
+ [0:j] 的最长公共子序列；
+
+\textit{text}_1[0:i]text 
+1
+​	
+ [0:i] 和 \textit{text}_2[0:j-1]text 
+2
+​	
+ [0:j−1] 的最长公共子序列。
+
+要得到 \textit{text}_1[0:i]text 
+1
+​	
+ [0:i] 和 \textit{text}_2[0:j]text 
+2
+​	
+ [0:j] 的最长公共子序列，应取两项中的长度较大的一项，因此 \textit{dp}[i][j]=\max(\textit{dp}[i-1][j],\textit{dp}[i][j-1])dp[i][j]=max(dp[i−1][j],dp[i][j−1])。
+
+由此可以得到如下状态转移方程：
+
+\textit{dp}[i][j] = \begin{cases} \textit{dp}[i-1][j-1]+1, & \textit{text}_1[i-1]=\textit{text}_2[j-1] \\ \max(\textit{dp}[i-1][j],\textit{dp}[i][j-1]), & \textit{text}_1[i-1] \ne \textit{text}_2[j-1] \end{cases}
+dp[i][j]={ 
+dp[i−1][j−1]+1,
+max(dp[i−1][j],dp[i][j−1]),
+​	
+  
+text 
+1
+​	
+ [i−1]=text 
+2
+​	
+ [j−1]
+text 
+1
+​	
+ [i−1] 
+
+​	
+ =text 
+2
+​	
+ [j−1]
+​	
+ 
+
+最终计算得到 \textit{dp}[m][n]dp[m][n] 即为 \textit{text}_1text 
+1
+​	
+  和 \textit{text}_2text 
+2
+​	
+  的最长公共子序列的长度。
+
+
+
+JavaJavaScriptGolangPython3C++C
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[m][n]
+复杂度分析
+
+时间复杂度：O(mn)O(mn)，其中 mm 和 nn 分别是字符串 \textit{text}_1text 
+1
+​	
+  和 \textit{text}_2text 
+2
+​	
+  的长度。二维数组 \textit{dp}dp 有 m+1m+1 行和 n+1n+1 列，需要对 \textit{dp}dp 中的每个元素进行计算。
+
+空间复杂度：O(mn)O(mn)，其中 mm 和 nn 分别是字符串 \textit{text}_1text 
+1
+​	
+  和 \textit{text}_2text 
+2
+​	
+  的长度。创建了 m+1m+1 行 n+1n+1 列的二维数组 \textit{dp}dp。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/longest-common-subsequence/solution/zui-chang-gong-gong-zi-xu-lie-by-leetcod-y7u0/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
