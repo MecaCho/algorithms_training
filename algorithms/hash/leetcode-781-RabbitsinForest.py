5328cb44b91510463673318a28eb2ffa4b147af5
MecaCho <1099833328@qq.com>
2021-04-04 21:24:24 +0800

    add 42. Trapping Rain Water solutions;

--- algorithms/hash/leetcode-781-RabbitsinForest.py
+++ algorithms/hash/leetcode-781-RabbitsinForest.py
@@ -73,5 +73,57 @@ class Solution(object):
         return res
 
 
+# solutions
 
+
+'''
+方法一：贪心
+思路
+
+两只相同颜色的兔子看到的其他同色兔子数必然是相同的。反之，若两只兔子看到的其他同色兔子数不同，那么这两只兔子颜色也不同。
+
+因此，将 \textit{answers}answers 中值相同的元素分为一组，对于每一组，计算出兔子的最少数量，然后将所有组的计算结果累加，就是最终的答案。
+
+例如，现在有 1313 只兔子回答 55。假设其中有一只红色的兔子，那么森林中必然有 66 只红兔子。再假设其中还有一只蓝色的兔子，同样的道理森林中必然有 66 只蓝兔子。为了最小化可能的兔子数量，我们假设这 1212 只兔子都在这 1313 只兔子中。那么还有一只额外的兔子回答 55，这只兔子只能是其他的颜色，这一颜色的兔子也有 66 只。因此这种情况下最少会有 1818 只兔子。
+
+一般地，如果有 xx 只兔子都回答 yy，则至少有 \lceil\dfrac{x}{y+1}\rceil⌈ 
+y+1
+x
+​	
+ ⌉ 种不同的颜色，且每种颜色有 y+1y+1 只兔子，因此兔子数至少为
+
+\lceil\dfrac{x}{y+1}\rceil\cdot(y+1)
+⌈ 
+y+1
+x
+​	
+ ⌉⋅(y+1)
+
+我们可以用哈希表统计 \textit{answers}answers 中各个元素的出现次数，对每个元素套用上述公式计算，并将计算结果累加，即为最终答案。
+
+代码
+
+C++JavaGolangJavaScriptPython3C
+
+func numRabbits(answers []int) (ans int) {
+    count := map[int]int{}
+    for _, y := range answers {
+        count[y]++
+    }
+    for y, x := range count {
+        ans += (x + y) / (y + 1) * (y + 1)
+    }
+    return
+}
+复杂度分析
+
+时间复杂度：O(n)O(n)，其中 nn 是数组 \textit{answers}answers 的长度。
+
+空间复杂度：O(n)O(n)。最坏情况下，哈希表中含有 nn 个元素。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/rabbits-in-forest/solution/sen-lin-zhong-de-tu-zi-by-leetcode-solut-kvla/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
 
