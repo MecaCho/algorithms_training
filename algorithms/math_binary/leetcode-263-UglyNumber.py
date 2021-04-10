cf81684881879c8b8511354b3ae63934e61946ab
MecaCho <1099833328@qq.com>
2021-04-10 20:42:46 +0800

    add 263. Ugly Number solutions;

--- algorithms/math_binary/leetcode-263-UglyNumber.py
+++ algorithms/math_binary/leetcode-263-UglyNumber.py
@@ -98,4 +98,43 @@ class Solution20210410(object):
         return n == 1
 
 
+# solutions
 
+'''
+方法一：数学
+根据丑数的定义，00 和负整数一定不是丑数。
+
+当 n>0n>0 时，若 nn 是丑数，则 nn 可以写成 n = 2^a \times 3^b \times 5^cn=2 
+a
+ ×3 
+b
+ ×5 
+c
+  的形式，其中 a,b,ca,b,c 都是非负整数。特别地，当 a,b,ca,b,c 都是 00 时，n=1n=1。
+
+为判断 nn 是否满足上述形式，可以对 nn 反复除以 2,3,52,3,5，直到 nn 不再包含质因数 2,3,52,3,5。若剩下的数等于 11，则说明 nn 不包含其他质因数，是丑数；否则，说明 nn 包含其他质因数，不是丑数。
+
+JavaJavaScriptGolangPython3C++C
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        factors = [2, 3, 5]
+        for factor in factors:
+            while n % factor == 0:
+                n //= factor
+        
+        return n == 1
+复杂度分析
+
+时间复杂度：O(\log n)O(logn)。时间复杂度取决于对 nn 除以 2,3,52,3,5 的次数，由于每次至少将 nn 除以 22，因此除法运算的次数不会超过 O(\log n)O(logn)。
+
+空间复杂度：O(1)O(1)。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/ugly-number/solution/chou-shu-by-leetcode-solution-fazd/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
