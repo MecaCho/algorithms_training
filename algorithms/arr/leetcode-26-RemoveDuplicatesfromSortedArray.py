63f86ee53871dee33ba16594b9907fa1e37a8ec1
MecaCho <1099833328@qq.com>
2021-04-18 22:30:09 +0800

    update 26. Remove Duplicates from Sorted Array;

--- algorithms/arr/leetcode-26-RemoveDuplicatesfromSortedArray.py
+++ algorithms/arr/leetcode-26-RemoveDuplicatesfromSortedArray.py
@@ -128,6 +128,11 @@ func removeDuplicates(nums []int) int {
 # tips
 
 '''
+In this problem, the key point to focus on is the input array being sorted. As far as duplicate elements are concerned, what is their positioning in the array when the given array is sorted? Look at the image above for the answer. If we know the position of one of the elements, do we also know the positioning of all the duplicate elements?
+
+We need to modify the array in-place and the size of the final array would potentially be smaller than the size of the input array. So, we ought to use a two-pointer approach here. One, that would keep track of the current element in the original array and another one for just the unique elements.
+
+Essentially, once an element is encountered, you simply need to bypass its duplicates and move on to the next unique element.
 
 '''
 
