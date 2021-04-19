34aa23a78ab907dae859f18d38d5289a7153c21e
MecaCho <1099833328@qq.com>
2021-04-19 23:01:52 +0800

    add 27. Remove Element golang solution;

--- algorithms/two_point/leetcode-027-removeElement.py
+++ algorithms/two_point/leetcode-027-removeElement.py
@@ -134,6 +134,26 @@ class Solution20210325(object):
 
         return n
 
+# solutions
+
+'''
+func removeElement(nums []int, val int) int {
+
+	i, n := 0, len(nums)
+
+	for ; i < n; {
+		if nums[i] == val {
+			nums[i] = nums[n-1]
+			n -= 1
+		} else {
+			i += 1
+		}
+	}
+	// fmt.Println(nums)
+	return n
+}
+
+'''
 
 
 # solutions
