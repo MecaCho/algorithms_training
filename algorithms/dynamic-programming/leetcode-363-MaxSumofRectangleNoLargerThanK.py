b69a709fa2e9b3fd59d307f3a3e5bd9d4d778c03
MecaCho <1099833328@qq.com>
2021-04-22 21:55:32 +0800

    add 363. Max Sum of Rectangle No Larger Than K;

---
+++ algorithms/dynamic-programming/leetcode-363-MaxSumofRectangleNoLargerThanK.py
@@ -0,0 +1,68 @@
+# encoding=utf8
+
+'''
+363. Max Sum of Rectangle No Larger Than K
+Given an m x n matrix matrix and an integer k, return the max sum of a rectangle in the matrix such that its sum is no larger than k.
+
+It is guaranteed that there will be a rectangle with a sum no larger than k.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,0,1],[0,-2,3]], k = 2
+Output: 2
+Explanation: Because the sum of the blue rectangle [[0, 1], [-2, 3]] is 2, and 2 is the max number no larger than k (k = 2).
+Example 2:
+
+Input: matrix = [[2,2,-1]], k = 3
+Output: 3
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-100 <= matrix[i][j] <= 100
+-105 <= k <= 105
+ 
+
+Follow up: What if the number of rows is much larger than the number of columns?
+
+
+363. 矩形区域不超过 K 的最大数值和
+给你一个 m x n 的矩阵 matrix 和一个整数 k ，找出并返回矩阵内部矩形区域的不超过 k 的最大数值和。
+
+题目数据保证总会存在一个数值和不超过 k 的矩形区域。
+
+ 
+
+示例 1：
+
+
+输入：matrix = [[1,0,1],[0,-2,3]], k = 2
+输出：2
+解释：蓝色边框圈出来的矩形区域 [[0, 1], [-2, 3]] 的数值和是 2，且 2 是不超过 k 的最大数字（k = 2）。
+示例 2：
+
+输入：matrix = [[2,2,-1]], k = 3
+输出：3
+ 
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-100 <= matrix[i][j] <= 100
+-105 <= k <= 105
+ 
+
+进阶：如果行数远大于列数，该如何设计解决方案？
+'''
+
+
+
+
