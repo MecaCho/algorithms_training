0123ad471b487752e29ba1870025dbf893de759e
MecaCho <1099833328@qq.com>
2021-04-23 23:22:53 +0800

    add 368. Largest Divisible Subset;

---
+++ algorithms/dynamic-programming/leetcode-368-LargestDivisibleSubset.py
@@ -0,0 +1,74 @@
+# encoding=utf8
+
+
+'''
+368. Largest Divisible Subset
+Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+
+answer[i] % answer[j] == 0, or
+answer[j] % answer[i] == 0
+If there are multiple solutions, return any of them.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,2]
+Explanation: [1,3] is also accepted.
+Example 2:
+
+Input: nums = [1,2,4,8]
+Output: [1,2,4,8]
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 2 * 109
+All the integers in nums are unique.
+
+
+368. 最大整除子集
+给你一个由 无重复 正整数组成的集合 nums ，请你找出并返回其中最大的整除子集 answer ，子集中每一元素对 (answer[i], answer[j]) 都应当满足：
+answer[i] % answer[j] == 0 ，或
+answer[j] % answer[i] == 0
+如果存在多个有效解子集，返回其中任何一个均可。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[1,2]
+解释：[1,3] 也会被视为正确答案。
+示例 2：
+
+输入：nums = [1,2,4,8]
+输出：[1,2,4,8]
+ 
+
+提示：
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 2 * 109
+nums 中的所有整数 互不相同
+'''
+
+
+
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        f = [[x] for x in nums]
+        for j in range(len(nums)):
+            for i in range(j):
+                if nums[j]%nums[i]==0 and len(f[i])+1 > len(f[j]):
+                    f[j] = f[i] + [nums[j]]
+        return max(f, key=len)
+
+
