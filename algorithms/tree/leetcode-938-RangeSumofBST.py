ed2ff872fc30ca1c041c8e43ed2ef4f900725437
MecaCho <1099833328@qq.com>
2021-04-27 21:06:56 +0800

    Create leetcode-938-RangeSumofBST.py
    add 938. Range Sum of BST;

---
+++ algorithms/tree/leetcode-938-RangeSumofBST.py
@@ -0,0 +1,88 @@
+# encoding=utf8
+
+
+'''
+938. Range Sum of BST
+
+Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Example 2:
+
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 2 * 104].
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+All Node.val are unique.
+
+938. 二叉搜索树的范围和
+
+给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+
+ 
+
+示例 1：
+
+
+输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+输出：32
+示例 2：
+
+
+输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+输出：23
+ 
+
+提示：
+
+树中节点数目在范围 [1, 2 * 104] 内
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+所有 Node.val 互不相同
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        self.val = 0
+        def check(root):
+            if not root:
+                return 0
+
+            if low <= root.val <= high:
+                self.val += root.val
+                check(root.left)
+                check(root.right)
+            elif root.val < low:
+                check(root.right)
+            elif root.val > high:
+                check(root.left)
+
+        check(root)
+        return self.val
+
+
