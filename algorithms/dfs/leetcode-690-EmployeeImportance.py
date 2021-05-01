9efe335b5f3d41bbc716d832e356217d9953ecf3
MecaCho <1099833328@qq.com>
2021-05-01 23:06:35 +0800

    Update leetcode-690-EmployeeImportance.py
    add 690. Employee Importance Golang solution;

--- algorithms/dfs/leetcode-690-EmployeeImportance.py
+++ algorithms/dfs/leetcode-690-EmployeeImportance.py
@@ -83,4 +83,38 @@ class Solution(object):
 
         return dfs(id)
 
+# golang solution
+
+'''
+/**
+ * Definition for Employee.
+ * type Employee struct {
+ *     Id int
+ *     Importance int
+ *     Subordinates []int
+ * }
+ */
+
+func getImportance(employees []*Employee, id int) int {
+
+    employeesDict := make(map[int]*Employee, len(employees))
+
+    for _, employee := range employees{
+		employeesDict[employee.Id] = employee
+	}
+
+	var dfs func(int) int
+	dfs = func(id int) int {
+		res := employeesDict[id].Importance
+		for _, id_ := range employeesDict[id].Subordinates {
+			res += dfs(id_)
+		}
+		return res
+	}
+
+	return dfs(id)
+    
+}
+'''
+
 
