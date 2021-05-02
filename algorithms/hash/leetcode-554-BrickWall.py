5ba49fd8c4ee6eea7c0e3261a77ad3f701e7809b
MecaCho <1099833328@qq.com>
2021-05-02 21:28:39 +0800

    Create leetcode-554-BrickWall.py
    add 554. Brick Wall;

---
+++ algorithms/hash/leetcode-554-BrickWall.py
@@ -0,0 +1,84 @@
+# encoding=utf8
+
+
+'''
+554. Brick Wall
+
+There is a rectangular brick wall in front of you with n rows of bricks. The ith row has some number of bricks each of the same height (i.e., one unit) but they can be of different widths. The total width of each row is the same.
+
+Draw a vertical line from the top to the bottom and cross the least bricks. If your line goes through the edge of a brick, then the brick is not considered as crossed. You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+
+Given the 2D array wall that contains the information about the wall, return the minimum number of crossed bricks after drawing such a vertical line.
+
+ 
+
+Example 1:
+
+
+Input: wall = [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]
+Output: 2
+Example 2:
+
+Input: wall = [[1],[1],[1]]
+Output: 3
+ 
+
+Constraints:
+
+n == wall.length
+1 <= n <= 104
+1 <= wall[i].length <= 104
+1 <= sum(wall[i].length) <= 2 * 104
+sum(wall[i]) is the same for each row i.
+1 <= wall[i][j] <= 231 - 1
+
+554. 砖墙
+
+你的面前有一堵矩形的、由 n 行砖块组成的砖墙。这些砖块高度相同（也就是一个单位高）但是宽度不同。每一行砖块的宽度之和应该相等。
+
+你现在要画一条 自顶向下 的、穿过 最少 砖块的垂线。如果你画的线只是从砖块的边缘经过，就不算穿过这块砖。你不能沿着墙的两个垂直边缘之一画线，这样显然是没有穿过一块砖的。
+
+给你一个二维数组 wall ，该数组包含这堵墙的相关信息。其中，wall[i] 是一个代表从左至右每块砖的宽度的数组。你需要找出怎样画才能使这条线 穿过的砖块数量最少 ，并且返回 穿过的砖块数量 。
+
+ 
+
+示例 1：
+
+
+输入：wall = [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]
+输出：2
+示例 2：
+
+输入：wall = [[1],[1],[1]]
+输出：3
+ 
+提示：
+
+n == wall.length
+1 <= n <= 104
+1 <= wall[i].length <= 104
+1 <= sum(wall[i].length) <= 2 * 104
+对于每一行 i ，sum(wall[i]) 应当是相同的
+1 <= wall[i][j] <= 231 - 1
+
+'''
+
+
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+
+        wall_dict = dict()
+        for i in range(len(wall)):
+            tmp = 0
+            for j in range(len(wall[i]) - 1):
+                tmp += wall[i][j]
+                if tmp - 1 in wall_dict:
+                    wall_dict[tmp-1] += 1
+                else:
+                    wall_dict[tmp-1] = 1
+        return len(wall) - max(wall_dict.values()) if wall_dict else len(wall)
+
