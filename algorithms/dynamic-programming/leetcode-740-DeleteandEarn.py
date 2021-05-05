76e75c8362459aef6f8a303fb0abdc5644bea836
MecaCho <1099833328@qq.com>
2021-05-05 20:18:37 +0800

    Update leetcode-740-DeleteandEarn.py
    add 740. Delete and Earn Golang solution;

--- algorithms/dynamic-programming/leetcode-740-DeleteandEarn.py
+++ algorithms/dynamic-programming/leetcode-740-DeleteandEarn.py
@@ -85,6 +85,45 @@ class Solution(object):
             dp[i][1] = max(dp[i-1][0] + values[i], dp[i-1][1])
         return dp[-1][1]
 
+       
+     
+# golang solution
+
+'''
+func deleteAndEarn(nums []int) int {
+	maxVal := 0
+	for i := range nums {
+		if nums[i] > maxVal {
+			maxVal = nums[i]
+		}
+	}
+
+	values := make([]int, maxVal+1)
+
+	for _, num := range nums {
+		values[num] += num
+	}
+
+	do_not := values[0]
+	do := values[0]
+	if values[1] > do_not {
+		do = values[1]
+	}
+	for i := 2; i < len(values); i++ {
+		do_not, do = do, max(do_not+values[i], do)
+	}
+	return do
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+'''
+
 # solutions
 
 '''
