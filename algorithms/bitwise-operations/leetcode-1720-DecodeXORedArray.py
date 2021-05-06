17c7ed9f9c9771e417860871d2f2b2abe49b9b1e
MecaCho <1099833328@qq.com>
2021-05-06 22:53:00 +0800

    Update leetcode-1720-DecodeXORedArray.py
    add 1720. Decode XORed Array Golang solution;

--- algorithms/bitwise-operations/leetcode-1720-DecodeXORedArray.py
+++ algorithms/bitwise-operations/leetcode-1720-DecodeXORedArray.py
@@ -77,6 +77,24 @@ class Solution(object):
         return res
 
       
+# golang solution
+
+'''
+func decode(encoded []int, first int) []int {
+
+    res := make([]int, len(encoded)+1)
+
+	res[0] = first
+
+	for i, num := range encoded {
+		res[i+1] = res[i] ^ num
+	}
+
+	return res
+
+}
+'''
+
 # solutions
 
 '''
