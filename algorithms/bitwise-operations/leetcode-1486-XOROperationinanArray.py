a5002ed27e31bebc6dabad1d4483fd9db339f36b
MecaCho <1099833328@qq.com>
2021-05-07 09:36:28 +0800

    Update leetcode-1486-XOROperationinanArray.py
    add 1486. XOR Operation in an Array Golang solution;

--- algorithms/bitwise-operations/leetcode-1486-XOROperationinanArray.py
+++ algorithms/bitwise-operations/leetcode-1486-XOROperationinanArray.py
@@ -92,3 +92,16 @@ class Solution(object):
             start += 2
 
         return res
+       
+# golang solution
+
+
+'''
+func xorOperation(n int, start int) int {
+	res := 0
+	for i := 0; i < n; i++ {
+		res ^= start + i*2
+	}
+	return res
+}
+'''
