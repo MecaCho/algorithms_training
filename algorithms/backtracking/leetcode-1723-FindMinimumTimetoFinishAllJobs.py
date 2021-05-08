cd511d0ef2a70439b019a754aac5fe099c0f6ee2
MecaCho <1099833328@qq.com>
2021-05-08 21:50:48 +0800

    Update leetcode-1723-FindMinimumTimetoFinishAllJobs.py
    add 1723. Find Minimum Time to Finish All Jobs solutions;

--- algorithms/backtracking/leetcode-1723-FindMinimumTimetoFinishAllJobs.py
+++ algorithms/backtracking/leetcode-1723-FindMinimumTimetoFinishAllJobs.py
@@ -93,3 +93,104 @@ class Solution(object):
       
       
       
+# solutions
+
+'''
+It is easy to get TLE, so how should we cut some branches and speed up the searching process?
+We use an array of length n to record the workload assigned to each worker.
+
+The core idea is that assume at certain point of dfs searching,
+we have the following workload for 10 workers,
+workers = [10, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+
+if we want to assign the current task jobs[curr] to someone,
+it makes no difference if we assign it to any worker whose current workload is 5.
+Therefore we can use a set named seen to store searched workload such that we only search 5 once.
+
+There is also another branch cutting step, if the total workload is already larger than self.res,
+we can exit the dfs search, too.
+
+Another trick is to reverse sort all the jobs so that we are more likely to exit earlier.
+
+Two other similar questions are
+https://leetcode.com/problems/matchsticks-to-square/
+https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+
+this question
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+        workers = [0]*k
+        
+        self.res = sys.maxsize
+        # jobs.sort(reverse = True)
+        def dfs(curr):
+            if curr == len(jobs):
+                self.res = min(self.res, max(workers))
+                return
+            
+            seen = set() # record searched workload of workers
+            for i in range(k):
+                if workers[i] in seen: continue # if we have searched the workload of 5, skip it.
+                if workers[i] + jobs[curr] >= self.res: continue # another branch cutting
+                seen.add(workers[i])
+                workers[i] += jobs[curr]
+                dfs(curr+1)
+                workers[i] -= jobs[curr]
+        
+        dfs(0)
+        return self.res
+473. Matchsticks to Square
+https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+
+class Solution:
+    def makesquare(self, nums):
+        if len(nums) < 4 or sum(nums) % 4 != 0 or max(nums) > sum(nums) // 4:
+            return False
+        target = sum(nums)//4
+
+        nums.sort(reverse = True)
+        edge = [0]*4
+
+        def dfs(index):
+            if index == len(nums):
+                return True
+            seen = [] # record the searched matchstick lengths
+            for i in range(4):
+                if edge[i] in seen: continue
+                if edge[i] + nums[index] <= target:
+                    seen.append(edge[i])
+                    edge[i] += nums[index]
+                    if dfs(index + 1):
+                        return True
+                    edge[i] -= nums[index]
+            return False
+
+        return dfs(0)
+698. Partition to K Equal Sum Subsets
+https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        if len(nums) < k or sum(nums) % k != 0 or max(nums) > sum(nums) // k:
+            return False
+        target = sum(nums)//k
+
+        nums.sort(reverse = True)
+        edge = [0]*k
+
+        def dfs(index):
+            if index == len(nums):
+                return True
+            seen = [] # record the searched summ
+            for i in range(k):
+                if edge[i] in seen: continue
+                if edge[i] + nums[index] <= target:
+                    seen.append(edge[i])
+                    edge[i] += nums[index]
+                    if dfs(index + 1):
+                        return True
+                    edge[i] -= nums[index]
+            return False
+
+        return dfs(0)
+'''
