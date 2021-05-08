0dd87021d2c62e6b9c6dd5703195271f6397ab3f
MecaCho <1099833328@qq.com>
2021-05-08 22:15:14 +0800

    Update leetcode-1723-FindMinimumTimetoFinishAllJobs.py
    add 1723. Find Minimum Time to Finish All Jobs Golang solution;

--- algorithms/backtracking/leetcode-1723-FindMinimumTimetoFinishAllJobs.py
+++ algorithms/backtracking/leetcode-1723-FindMinimumTimetoFinishAllJobs.py
@@ -122,6 +122,46 @@ class Solution1(object):
         dfs(0)
         return self.res        
       
+# golang solution
+
+'''
+import "sort"
+
+func minimumTimeRequired(jobs []int, k int) int {
+	n := len(jobs)
+	sort.Sort(sort.Reverse(sort.IntSlice(jobs)))
+	l, r := jobs[0], 0
+	for _, v := range jobs {
+		r += v
+	}
+	return l + sort.Search(r-l, func(limit int) bool {
+		limit += l
+		workloads := make([]int, k)
+		var backtrack func(int) bool
+		backtrack = func(idx int) bool {
+			if idx == n {
+				return true
+			}
+			cur := jobs[idx]
+			for i := range workloads {
+				if workloads[i]+cur <= limit {
+					workloads[i] += cur
+					if backtrack(idx + 1) {
+						return true
+					}
+					workloads[i] -= cur
+				}
+				if workloads[i] == 0 || workloads[i]+cur == limit {
+					break
+				}
+			}
+			return false
+		}
+		return backtrack(0)
+	})
+}
+'''
+
 # solutions
 
 '''
