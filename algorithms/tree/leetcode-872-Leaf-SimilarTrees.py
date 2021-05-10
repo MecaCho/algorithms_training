67fdff054c4cdab1213a1955d6e667092456b9a3
MecaCho <1099833328@qq.com>
2021-05-10 20:59:25 +0800

    Update leetcode-872-Leaf-SimilarTrees.py
    add 872. Leaf-Similar Trees golang solution;

--- algorithms/tree/leetcode-872-Leaf-SimilarTrees.py
+++ algorithms/tree/leetcode-872-Leaf-SimilarTrees.py
@@ -120,6 +120,49 @@ class Solution(object):
         dfs(root2, vals2)
 
         return vals1 == vals2
+       
+       
+# golang solution
+
+'''
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	var dfs func(node *TreeNode, vals *[]int)
+	dfs = func(root *TreeNode, vals *[]int) {
+		if root == nil {
+			return
+		}
+		if root.Left == nil && root.Right == nil {
+			*vals = append(*vals, root.Val)
+		}
+		dfs(root.Left, vals)
+		dfs(root.Right, vals)
+	}
+
+	vals1 := []int{}
+	vals2 := []int{}
+	dfs(root1, &vals1)
+	dfs(root2, &vals2)
+
+	if len(vals1) != len(vals2) {
+		return false
+	}
+
+	for k, v := range vals1 {
+		if v != vals2[k] {
+			return false
+		}
+	}
+	return true
+}
+'''
 
 # solutions
 
