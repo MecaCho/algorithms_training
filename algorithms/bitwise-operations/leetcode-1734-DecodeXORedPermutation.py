a3b06df9460a661f741a23a1720e49ab75094d01
MecaCho <1099833328@qq.com>
2021-05-11 23:16:52 +0800

    Create leetcode-1734-DecodeXORedPermutation.py
    add 1734. Decode XORed Permutation;

---
+++ algorithms/bitwise-operations/leetcode-1734-DecodeXORedPermutation.py
@@ -0,0 +1,84 @@
+# encoding=utf8
+
+'''
+1734. Decode XORed Permutation
+
+There is an integer array perm that is a permutation of the first n positive integers, where n is always odd.
+
+It was encoded into another integer array encoded of length n - 1, such that encoded[i] = perm[i] XOR perm[i + 1]. For example, if perm = [1,3,2], then encoded = [2,1].
+
+Given the encoded array, return the original array perm. It is guaranteed that the answer exists and is unique.
+
+ 
+
+Example 1:
+
+Input: encoded = [3,1]
+Output: [1,2,3]
+Explanation: If perm = [1,2,3], then encoded = [1 XOR 2,2 XOR 3] = [3,1]
+Example 2:
+
+Input: encoded = [6,5,4,6]
+Output: [2,4,1,5,3]
+ 
+
+Constraints:
+
+3 <= n < 105
+n is odd.
+encoded.length == n - 1
+
+
+1734. 解码异或后的排列
+
+给你一个整数数组 perm ，它是前 n 个正整数的排列，且 n 是个 奇数 。
+
+它被加密成另一个长度为 n - 1 的整数数组 encoded ，满足 encoded[i] = perm[i] XOR perm[i + 1] 。比方说，如果 perm = [1,3,2] ，那么 encoded = [2,1] 。
+
+给你 encoded 数组，请你返回原始数组 perm 。题目保证答案存在且唯一。
+
+ 
+
+示例 1：
+
+输入：encoded = [3,1]
+输出：[1,2,3]
+解释：如果 perm = [1,2,3] ，那么 encoded = [1 XOR 2,2 XOR 3] = [3,1]
+示例 2：
+
+输入：encoded = [6,5,4,6]
+输出：[2,4,1,5,3]
+ 
+
+提示：
+
+3 <= n < 105
+n 是奇数。
+encoded.length == n - 1
+'''
+
+
+class Solution(object):
+    def decode(self, encoded):
+        """
+        :type encoded: List[int]
+        :rtype: List[int]
+        """
+        n = len(encoded) + 1
+        total = reduce(xor, range(1, n + 1))
+        
+        odd = 0
+        for i in range(1, n - 1, 2):
+            odd ^= encoded[i]
+        
+        perm = [total ^ odd]
+        for i in range(n - 1):
+            perm.append(perm[-1] ^ encoded[i])
+        
+        return perm
+
+# solutions
+
+'''
+
+'''
