1ff440148a7f179be4b6d49f3ce0d0990d98b593
MecaCho <1099833328@qq.com>
2021-05-11 23:55:45 +0800

    Update leetcode-1734-DecodeXORedPermutation.py
    add 1734. Decode XORed Permutation Golang solution;

--- algorithms/bitwise-operations/leetcode-1734-DecodeXORedPermutation.py
+++ algorithms/bitwise-operations/leetcode-1734-DecodeXORedPermutation.py
@@ -80,5 +80,24 @@ class Solution(object):
 # solutions
 
 '''
+func Decode(encoded []int) []int {
+	n := len(encoded)
+	total := 0
+	for i := 1; i <= n+1; i++ {
+		total ^= i
+	}
 
+	odd := 0
+	for i := 1; i < n; i += 2 {
+		odd ^= encoded[i]
+	}
+
+	res := []int{total ^ odd}
+	for i := 0; i < n; i++ {
+		tmp := res[len(res)-1]
+		res = append(res, tmp^encoded[i])
+	}
+
+	return res
+}
 '''
