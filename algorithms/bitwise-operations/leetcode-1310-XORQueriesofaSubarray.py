7a76eada77e7e967d2a6a80673cdbfa2fd802eaf
MecaCho <1099833328@qq.com>
2021-05-12 21:47:27 +0800

    Create leetcode-1310-XORQueriesofaSubarray.py
    add 1310. XOR Queries of a Subarray;

---
+++ algorithms/bitwise-operations/leetcode-1310-XORQueriesofaSubarray.py
@@ -0,0 +1,99 @@
+# encoding=utf8
+
+'''
+1310. XOR Queries of a Subarray
+
+Given the array arr of positive integers and the array queries where queries[i] = [Li, Ri], for each query i compute the XOR of elements from Li to Ri (that is, arr[Li] xor arr[Li+1] xor ... xor arr[Ri] ). Return an array containing the result for the given queries.
+ 
+
+Example 1:
+
+Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+Output: [2,7,14,8] 
+Explanation: 
+The binary representation of the elements in the array are:
+1 = 0001 
+3 = 0011 
+4 = 0100 
+8 = 1000 
+The XOR values for queries are:
+[0,1] = 1 xor 3 = 2 
+[1,2] = 3 xor 4 = 7 
+[0,3] = 1 xor 3 xor 4 xor 8 = 14 
+[3,3] = 8
+Example 2:
+
+Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+Output: [8,0,4,4]
+ 
+
+Constraints:
+
+1 <= arr.length <= 3 * 10^4
+1 <= arr[i] <= 10^9
+1 <= queries.length <= 3 * 10^4
+queries[i].length == 2
+0 <= queries[i][0] <= queries[i][1] < arr.length
+
+
+1310. 子数组异或查询
+
+有一个正整数数组 arr，现给你一个对应的查询数组 queries，其中 queries[i] = [Li, Ri]。
+
+对于每个查询 i，请你计算从 Li 到 Ri 的 XOR 值（即 arr[Li] xor arr[Li+1] xor ... xor arr[Ri]）作为本次查询的结果。
+
+并返回一个包含给定查询 queries 所有结果的数组。
+
+ 
+
+示例 1：
+
+输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+输出：[2,7,14,8] 
+解释：
+数组中元素的二进制表示形式是：
+1 = 0001 
+3 = 0011 
+4 = 0100 
+8 = 1000 
+查询的 XOR 值为：
+[0,1] = 1 xor 3 = 2 
+[1,2] = 3 xor 4 = 7 
+[0,3] = 1 xor 3 xor 4 xor 8 = 14 
+[3,3] = 8
+示例 2：
+
+输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+输出：[8,0,4,4]
+ 
+
+提示：
+
+1 <= arr.length <= 3 * 10^4
+1 <= arr[i] <= 10^9
+1 <= queries.length <= 3 * 10^4
+queries[i].length == 2
+0 <= queries[i][0] <= queries[i][1] < arr.length
+'''
+
+
+
+class Solution(object):
+    def xorQueries(self, arr, queries):
+        """
+        :type arr: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        xors = [0]
+        for i in range(len(arr)):
+            xors.append(xors[-1] ^ arr[i])
+
+        res = []
+        for l, r in queries:
+            res.append(xors[l]^xors[r+1])
+
+        return res
+
+      
+      
