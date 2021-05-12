dfb8c6bb7c718683a9b270472696b05c85ef7c98
MecaCho <1099833328@qq.com>
2021-05-12 22:05:36 +0800

    Update leetcode-1310-XORQueriesofaSubarray.py
    add 1310. XOR Queries of a Subarray solutions;

--- algorithms/bitwise-operations/leetcode-1310-XORQueriesofaSubarray.py
+++ algorithms/bitwise-operations/leetcode-1310-XORQueriesofaSubarray.py
@@ -94,7 +94,26 @@ class Solution(object):
             res.append(xors[l]^xors[r+1])
 
         return res
+       
+# golang solution
 
+'''
+func xorQueries(arr []int, queries [][]int) []int {
+	xors := make([]int, len(arr)+1)
+
+	for i, a := range arr {
+		xors[i+1] = xors[i] ^ a
+	}
+
+	res := make([]int, len(queries))
+
+	for i, vals := range queries {
+		res[i] = xors[vals[0]] ^ xors[vals[1]+1]
+	}
+
+	return res
+}
+'''
       
  # solutions
  
