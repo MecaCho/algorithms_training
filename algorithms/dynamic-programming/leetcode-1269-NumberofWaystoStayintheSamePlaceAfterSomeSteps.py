7d5b602f5f70dbfe696e74d1ed9821cc712edbb6
MecaCho <1099833328@qq.com>
2021-05-13 22:38:00 +0800

    Update leetcode-1269-NumberofWaystoStayintheSamePlaceAfterSomeSteps.py
    add 1269. Number of Ways to Stay in the Same Place After Some Steps Golang solution;

--- algorithms/dynamic-programming/leetcode-1269-NumberofWaystoStayintheSamePlaceAfterSomeSteps.py
+++ algorithms/dynamic-programming/leetcode-1269-NumberofWaystoStayintheSamePlaceAfterSomeSteps.py
@@ -104,3 +104,41 @@ class Solution(object):
 
         return dp[-1][0]
 
+# golang solution
+
+
+'''
+func numWays(steps int, arrLen int) int {
+	MOD := 1000000007
+	maxLen := min(steps/2+1, arrLen)
+
+	dp := make([][]int, steps+1)
+	for i := 0; i < steps+1; i++ {
+		dp[i] = make([]int, maxLen+1)
+	}
+	dp[0][0] = 1
+
+	for i := 1; i < steps+1; i++ {
+		for j := 0; j < maxLen+1; j++ {
+			dp[i][j] = dp[i-1][j]
+			if j > 0 {
+				dp[i][j] += dp[i-1][j-1]
+			}
+			if j < maxLen - 1 {
+				dp[i][j] += dp[i-1][j+1]
+			}
+			dp[i][j] %= MOD
+		}
+	}
+    // [[1, 0, 0], [1, 1, 0], [2, 2, 1], [4, 4, 3], [8, 8, 7], [16, 16, 15], [32, 32, 31], [64, 64, 63]]
+	// fmt.Println(dp)
+	return dp[steps][0]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+'''
