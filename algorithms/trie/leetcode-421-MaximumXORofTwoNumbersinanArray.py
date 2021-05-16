b3ac7c2e4887d67f06f0c90538717a0bab33355b
MecaCho <1099833328@qq.com>
2021-05-16 22:42:26 +0800

    Create leetcode-421-MaximumXORofTwoNumbersinanArray.py
    add 421. Maximum XOR of Two Numbers in an Array Golang solution;

---
+++ algorithms/trie/leetcode-421-MaximumXORofTwoNumbersinanArray.py
@@ -0,0 +1,96 @@
+# encoding=utf8
+
+'''
+421. Maximum XOR of Two Numbers in an Array
+
+Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 ≤ i ≤ j < n.
+
+Follow up: Could you do this in O(n) runtime?
+
+ 
+
+Example 1:
+
+Input: nums = [3,10,5,25,2,8]
+Output: 28
+Explanation: The maximum result is 5 XOR 25 = 28.
+Example 2:
+
+Input: nums = [0]
+Output: 0
+Example 3:
+
+Input: nums = [2,4]
+Output: 6
+Example 4:
+
+Input: nums = [8,10,2]
+Output: 10
+Example 5:
+
+Input: nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+Output: 127
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+0 <= nums[i] <= 231 - 1
+
+421. 数组中两个数的最大异或值
+
+给你一个整数数组 nums ，返回 nums[i] XOR nums[j] 的最大运算结果，其中 0 ≤ i ≤ j < n 。
+
+进阶：你可以在 O(n) 的时间解决这个问题吗？
+
+ 
+
+示例 1：
+
+输入：nums = [3,10,5,25,2,8]
+输出：28
+解释：最大运算结果是 5 XOR 25 = 28.
+示例 2：
+
+输入：nums = [0]
+输出：0
+示例 3：
+
+输入：nums = [2,4]
+输出：6
+示例 4：
+
+输入：nums = [8,10,2]
+输出：10
+示例 5：
+
+输入：nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+输出：127
+ 
+
+提示：
+
+1 <= nums.length <= 2 * 104
+0 <= nums[i] <= 231 - 1
+'''
+
+# golang solution
+
+'''
+func findMaximumXOR(nums []int) int {
+
+    var res int
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			tmp := nums[i] ^ nums[j]
+			if tmp > res {
+				res = nums[i] ^ nums[j]
+			}
+		}
+	}
+
+	return res
+}
+'''
+
