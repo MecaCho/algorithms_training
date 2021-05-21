890ee3e5e7846bf85942beadd1cf880ba921b07d
MecaCho <1099833328@qq.com>
2021-05-22 00:13:49 +0800

    Update leetcode-1035-UncrossedLines.py
    add 1035. Uncrossed Lines Golang solution;

--- algorithms/dynamic-programming/leetcode-1035-UncrossedLines.py
+++ algorithms/dynamic-programming/leetcode-1035-UncrossedLines.py
@@ -96,6 +96,37 @@ class Solution(object):
             
         # print(dp)
         return dp[-1][-1]
+       
+       
+# golang solution
+
+'''
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+
+    max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
+'''
 
 # solutions
 
