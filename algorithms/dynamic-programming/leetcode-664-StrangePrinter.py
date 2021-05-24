6469d5145e344268713827d867ceffe9e91b80ef
MecaCho <1099833328@qq.com>
2021-05-24 20:33:10 +0800

    Create leetcode-664-StrangePrinter.py
    Add 664. Strange Printer

---
+++ algorithms/dynamic-programming/leetcode-664-StrangePrinter.py
@@ -0,0 +1,78 @@
+# encoding=utf8
+
+
+'''
+664. Strange Printer
+
+There is a strange printer with the following two special properties:
+
+The printer can only print a sequence of the same character each time.
+At each turn, the printer can print new characters starting from and ending at any place and will cover the original existing characters.
+Given a string s, return the minimum number of turns the printer needed to print it.
+
+ 
+
+Example 1:
+
+Input: s = "aaabbb"
+Output: 2
+Explanation: Print "aaa" first and then print "bbb".
+Example 2:
+
+Input: s = "aba"
+Output: 2
+Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of lowercase English letters.
+
+
+664. 奇怪的打印机
+
+有台奇怪的打印机有以下两个特殊要求：
+
+打印机每次只能打印由 同一个字符 组成的序列。
+每次可以在任意起始和结束位置打印新字符，并且会覆盖掉原来已有的字符。
+给你一个字符串 s ，你的任务是计算这个打印机打印它需要的最少打印次数。
+
+ 
+示例 1：
+
+输入：s = "aaabbb"
+输出：2
+解释：首先打印 "aaa" 然后打印 "bbb"。
+示例 2：
+
+输入：s = "aba"
+输出：2
+解释：首先打印 "aaa" 然后在第二个位置打印 "b" 覆盖掉原来的字符 'a'。
+ 
+
+提示：
+
+1 <= s.length <= 100
+s 由小写英文字母组成
+'''
+
+
+class Solution(object):
+    def strangePrinter(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        dp = [[1 if i==j else float("inf") for i in range(n)] for j in range(n)]
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i][j-1]
+                else:
+                    for k in range(i, j):
+                        dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j])
+
+        return dp[0][n-1]
+
