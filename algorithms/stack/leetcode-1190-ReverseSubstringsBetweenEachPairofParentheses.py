f7570304db8810b34e913dbe68fa7a688cfee5d3
MecaCho <1099833328@qq.com>
2021-05-26 21:42:02 +0800

    Create leetcode-1190-ReverseSubstringsBetweenEachPairofParentheses.py
    Add 1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ algorithms/stack/leetcode-1190-ReverseSubstringsBetweenEachPairofParentheses.py
@@ -0,0 +1,86 @@
+# encoding=utf8
+
+'''
+1190. Reverse Substrings Between Each Pair of Parentheses
+
+You are given a string s that consists of lower case English letters and brackets. 
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+ 
+
+Example 1:
+
+Input: s = "(abcd)"
+Output: "dcba"
+Example 2:
+
+Input: s = "(u(love)i)"
+Output: "iloveu"
+Explanation: The substring "love" is reversed first, then the whole string is reversed.
+Example 3:
+
+Input: s = "(ed(et(oc))el)"
+Output: "leetcode"
+Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+Example 4:
+
+Input: s = "a(bcdefghijkl(mno)p)q"
+Output: "apmnolkjihgfedcbq"
+ 
+
+Constraints:
+
+0 <= s.length <= 2000
+s only contains lower case English characters and parentheses.
+It's guaranteed that all parentheses are balanced.
+
+1190. 反转每对括号间的子串
+
+给出一个字符串 s（仅含有小写英文字母和括号）。
+
+请你按照从括号内到外的顺序，逐层反转每对匹配括号中的字符串，并返回最终的结果。
+
+注意，您的结果中 不应 包含任何括号。
+
+ 
+
+示例 1：
+
+输入：s = "(abcd)"
+输出："dcba"
+示例 2：
+
+输入：s = "(u(love)i)"
+输出："iloveu"
+示例 3：
+
+输入：s = "(ed(et(oc))el)"
+输出："leetcode"
+示例 4：
+
+输入：s = "a(bcdefghijkl(mno)p)q"
+输出："apmnolkjihgfedcbq"
+ 
+
+提示：
+
+0 <= s.length <= 2000
+s 中只有小写英文字母和括号
+我们确保所有括号都是成对出现的
+'''
+
+
+class Solution(object):
+    def reverseParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        while '(' in s:
+            s = re.sub(r'\(([^()]*)\)', lambda x:x.group(1)[::-1], s)
+        return s
+
+      
