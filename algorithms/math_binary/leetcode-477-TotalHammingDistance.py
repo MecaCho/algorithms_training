166fe1a765d830f01ed281fee403f045b6502996
MecaCho <1099833328@qq.com>
2021-05-28 22:33:48 +0800

    Create leetcode-477-TotalHammingDistance.py
    Add 477. Total Hamming Distance

---
+++ algorithms/math_binary/leetcode-477-TotalHammingDistance.py
@@ -0,0 +1,65 @@
+# encoding=utf8
+
+'''
+477. Total Hamming Distance
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,14,2]
+Output: 6
+Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case).
+The answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+Example 2:
+
+Input: nums = [4,14,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+
+477. 汉明距离总和
+
+两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。
+
+计算一个数组中，任意两个数之间汉明距离的总和。
+
+示例:
+
+输入: 4, 14, 2
+
+输出: 6
+
+解释: 在二进制表示中，4表示为0100，14表示为1110，2表示为0010。（这样表示是为了体现后四位之间关系）
+所以答案为：
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+注意:
+
+数组中元素的范围为从 0到 10^9。
+数组的长度不超过 10^4。
+'''
+
+
+class Solution(object):
+    def totalHammingDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        ans = 0
+        for i in range(30):
+            c = sum(((val >> i) & 1) for val in nums)
+            ans += c * (n - c)
+        return ans
+
