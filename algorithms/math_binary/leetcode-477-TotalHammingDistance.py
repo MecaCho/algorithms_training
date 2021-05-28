46b7ef94609f89d6d4d18add46a69ea249dabfad
MecaCho <1099833328@qq.com>
2021-05-28 22:35:51 +0800

    Update leetcode-477-TotalHammingDistance.py
    Add 477. Total Hamming Distance solutions

--- algorithms/math_binary/leetcode-477-TotalHammingDistance.py
+++ algorithms/math_binary/leetcode-477-TotalHammingDistance.py
@@ -62,4 +62,49 @@ class Solution(object):
             c = sum(((val >> i) & 1) for val in nums)
             ans += c * (n - c)
         return ans
+       
+# solutions
 
+'''
+方法一：逐位统计
+在计算汉明距离时，我们考虑的是同一比特位上的值是否不同，而不同比特位之间是互不影响的。
+
+对于数组 \textit{nums}nums 中的某个元素 \textit{val}val，若其二进制的第 ii 位为 11，我们只需统计 \textit{nums}nums 中有多少元素的第 ii 位为 00，即计算出了 \textit{val}val 与其他元素在第 ii 位上的汉明距离之和。
+
+具体地，若长度为 nn 的数组 \textit{nums}nums 的所有元素二进制的第 ii 位共有 cc 个 11，n-cn−c 个 00，则些元素在二进制的第 ii 位上的汉明距离之和为
+
+c\cdot(n-c)
+c⋅(n−c)
+
+我们可以从二进制的最低位到最高位，逐位统计汉明距离。将每一位上得到的汉明距离累加即为答案。
+
+具体实现时，对于整数 \textit{val}val 二进制的第 ii 位，我们可以用代码 (val >> i) & 1 来取出其第 ii 位的值。此外，由于 10^9<2^{30}10 
+9
+ <2 
+30
+ ，我们可以直接从二进制的第 00 位枚举到第 2929 位。
+
+C++JavaC#GolangPython3CJavaScript
+
+func totalHammingDistance(nums []int) (ans int) {
+    n := len(nums)
+    for i := 0; i < 30; i++ {
+        c := 0
+        for _, val := range nums {
+            c += val >> i & 1
+        }
+        ans += c * (n - c)
+    }
+    return
+}
+复杂度分析
+
+时间复杂度：O(n\cdot L)O(n⋅L)。其中 nn 是数组 \textit{nums}nums 的长度，L=30L=30。
+
+空间复杂度：O(1)O(1)。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/total-hamming-distance/solution/yi-ming-ju-chi-zong-he-by-leetcode-solut-t0ev/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
