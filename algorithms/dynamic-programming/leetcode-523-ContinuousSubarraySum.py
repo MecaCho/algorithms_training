aeae59cce9074c3e6e7f876061e533158b22c16d
MecaCho <1099833328@qq.com>
2021-06-02 20:18:42 +0800

    Create leetcode-523-ContinuousSubarraySum.py
    Add 523. Continuous Subarray Sum

---
+++ algorithms/dynamic-programming/leetcode-523-ContinuousSubarraySum.py
@@ -0,0 +1,92 @@
+# encoding=utf8
+
+'''
+523. Continuous Subarray Sum
+
+Given an integer array nums and an integer k, return true if nums has a continuous subarray of size at least two whose elements sum up to a multiple of k, or false otherwise.
+
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+ 
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+Example 2:
+
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+Example 3:
+
+Input: nums = [23,2,6,4,7], k = 13
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+0 <= sum(nums[i]) <= 231 - 1
+1 <= k <= 231 - 1
+
+
+523. 连续的子数组和
+
+给你一个整数数组 nums 和一个整数 k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+
+子数组大小 至少为 2 ，且
+子数组元素总和为 k 的倍数。
+如果存在，返回 true ；否则，返回 false 。
+
+如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
+
+ 
+
+示例 1：
+
+输入：nums = [23,2,4,6,7], k = 6
+输出：true
+解释：[2,4] 是一个大小为 2 的子数组，并且和为 6 。
+示例 2：
+
+输入：nums = [23,2,6,4,7], k = 6
+输出：true
+解释：[23, 2, 6, 4, 7] 是大小为 5 的子数组，并且和为 42 。 
+42 是 6 的倍数，因为 42 = 7 * 6 且 7 是一个整数。
+示例 3：
+
+输入：nums = [23,2,6,4,7], k = 13
+输出：false
+ 
+
+提示：
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+0 <= sum(nums[i]) <= 231 - 1
+1 <= k <= 231 - 1
+
+'''
+
+
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        modset = set()
+        sum_ = 0
+        for num in nums:
+            sum_ += num
+            if sum_ % k in modset:
+                return True
+            modset.add((sum_ - num)%k)
+        return False
+
+
