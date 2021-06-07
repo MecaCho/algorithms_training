fbc8246a5ef5ea84934de9d553313bdc5647ecb5
MecaCho <1099833328@qq.com>
2021-06-07 21:45:09 +0800

    Create leetcode-494-TargetSum.py
    Add 494. Target Sum

---
+++ algorithms/dynamic-programming/leetcode-494-TargetSum.py
@@ -0,0 +1,98 @@
+# encoding=utf8
+
+'''
+494. Target Sum
+
+You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+Example 2:
+
+Input: nums = [1], target = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+0 <= sum(nums[i]) <= 1000
+-1000 <= target <= 1000
+
+
+494. 目标和
+
+给你一个整数数组 nums 和一个整数 target 。
+
+向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+
+例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+
+ 
+
+示例 1：
+
+输入：nums = [1,1,1,1,1], target = 3
+输出：5
+解释：一共有 5 种方法让最终目标和为 3 。
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+示例 2：
+
+输入：nums = [1], target = 1
+输出：1
+ 
+
+提示：
+
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+0 <= sum(nums[i]) <= 1000
+-1000 <= target <= 100
+'''
+
+
+
+class Solution(object):
+    def findTargetSumWays(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # sum(P) - sum(N) = target
+        # sum(P) + sum(N) = sum(nums)
+        # sum(P)*2 = target + sum(nums)
+        sum_nums = 0
+        for num in nums:
+            sum_nums += num
+        if sum_nums < target or (sum_nums+target)%2 == 1:
+            return 0
+
+        sum_real = (sum_nums+target) / 2
+        dp = [0]*(sum_real+1)
+        dp[0] = 1
+        for num in nums:
+            for j in range(sum_real, num-1, -1):
+                dp[j] += dp[j-num]
+        return dp[sum_real]
