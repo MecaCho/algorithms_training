accae62a95120319f1d128eaa8e26cd7308976a5
MecaCho <1099833328@qq.com>
2021-06-09 22:13:43 +0800

    Create leetcode-879-ProfitableSchemes.py
    Add 879. Profitable Schemes

---
+++ algorithms/dynamic-programming/leetcode-879-ProfitableSchemes.py
@@ -0,0 +1,100 @@
+# encoding=utf8
+
+'''
+879. Profitable Schemes
+
+There is a group of n members, and a list of various crimes they could commit. The ith crime generates a profit[i] and requires group[i] members to participate in it. If a member participates in one crime, that member can't participate in another crime.
+
+Let's call a profitable scheme any subset of these crimes that generates at least minProfit profit, and the total number of members participating in that subset of crimes is at most n.
+
+Return the number of schemes that can be chosen. Since the answer may be very large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+Output: 2
+Explanation: To make a profit of at least 3, the group could either commit crimes 0 and 1, or just crime 1.
+In total, there are 2 schemes.
+Example 2:
+
+Input: n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+Output: 7
+Explanation: To make a profit of at least 5, the group could commit any crimes, as long as they commit one.
+There are 7 possible schemes: (0), (1), (2), (0,1), (0,2), (1,2), and (0,1,2).
+ 
+
+Constraints:
+
+1 <= n <= 100
+0 <= minProfit <= 100
+1 <= group.length <= 100
+1 <= group[i] <= 100
+profit.length == group.length
+0 <= profit[i] <= 100
+
+879. 盈利计划
+
+集团里有 n 名员工，他们可以完成各种各样的工作创造利润。
+
+第 i 种工作会产生 profit[i] 的利润，它要求 group[i] 名成员共同参与。如果成员参与了其中一项工作，就不能参与另一项工作。
+
+工作的任何至少产生 minProfit 利润的子集称为 盈利计划 。并且工作的成员总数最多为 n 。
+
+有多少种计划可以选择？因为答案很大，所以 返回结果模 10^9 + 7 的值。
+
+ 
+
+示例 1：
+
+输入：n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+输出：2
+解释：至少产生 3 的利润，该集团可以完成工作 0 和工作 1 ，或仅完成工作 1 。
+总的来说，有两种计划。
+示例 2：
+
+输入：n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+输出：7
+解释：至少产生 5 的利润，只要完成其中一种工作就行，所以该集团可以完成任何工作。
+有 7 种可能的计划：(0)，(1)，(2)，(0,1)，(0,2)，(1,2)，以及 (0,1,2) 。
+ 
+
+提示：
+
+1 <= n <= 100
+0 <= minProfit <= 100
+1 <= group.length <= 100
+1 <= group[i] <= 100
+profit.length == group.length
+0 <= profit[i] <= 100
+'''
+
+# https://leetcode-cn.com/problems/profitable-schemes/solution/ying-li-ji-hua-by-leetcode-solution-3t8o/
+
+class Solution(object):
+    def profitableSchemes(self, n, minProfit, group, profit):
+        """
+        :type n: int
+        :type minProfit: int
+        :type group: List[int]
+        :type profit: List[int]
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        
+        length = len(group)
+        dp = [[[0] * (minProfit + 1) for _ in range(n + 1)] for _ in range(length + 1)]
+        dp[0][0][0] = 1
+        for i in range(1, length + 1):
+            members, earn = group[i - 1], profit[i - 1]
+            for j in range(n + 1):
+                for k in range(minProfit + 1):
+                    if j < members:
+                        dp[i][j][k] = dp[i - 1][j][k]
+                    else:
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - members][max(0, k - earn)]) % MOD
+        
+        total = sum(dp[length][j][minProfit] for j in range(n + 1))
+        return total % MOD
+
