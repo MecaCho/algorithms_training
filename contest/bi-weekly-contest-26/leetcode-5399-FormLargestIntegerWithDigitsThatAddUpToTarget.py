402ab38bd4984128ca29a4e9f643af54fc3cf129
MecaCho <1099833328@qq.com>
2021-06-12 22:17:11 +0800

    Update leetcode-5399-FormLargestIntegerWithDigitsThatAddUpToTarget.py
    Add 1449. Form Largest Integer With Digits That Add up to Target solution

--- contest/bi-weekly-contest-26/leetcode-5399-FormLargestIntegerWithDigitsThatAddUpToTarget.py
+++ contest/bi-weekly-contest-26/leetcode-5399-FormLargestIntegerWithDigitsThatAddUpToTarget.py
@@ -1,4 +1,4 @@
-
+# encoding=utf8
 
 
 
@@ -148,4 +148,33 @@ class Solution(object):
 Use dynamic programming to find the maximum digits to paint given a total cost.
 Build the largest number possible using this DP table.
 '''
+
+class Solution1(object):
+    def largestNumber(self, cost, target):
+        """
+        :type cost: List[int]
+        :type target: int
+        :rtype: str
+        """
+
+        dp = [float("-inf")] * (target + 1)
+        dp[0] = 0
+
+        for c in cost:
+            for j in range(c, target + 1):
+                dp[j] = max(dp[j], dp[j - c] + 1)
+        
+        if dp[target] < 0:
+            return "0"
+        
+        ans = list()
+        j = target
+        for i in range(8, -1, -1):
+            c = cost[i]
+            while j >= c and dp[j] == dp[j - c] + 1:
+                ans.append(str(i + 1))
+                j -= c
+
+        return "".join(ans)
+
 
