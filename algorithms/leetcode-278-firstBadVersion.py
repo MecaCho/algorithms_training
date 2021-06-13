d2caa435b323f31bcc97e41472c7377b1055ec5e
MecaCho <1099833328@qq.com>
2021-06-13 22:02:05 +0800

    Update leetcode-278-firstBadVersion.py
    Add 278. First Bad Version golfing solution

--- algorithms/leetcode-278-firstBadVersion.py
+++ algorithms/leetcode-278-firstBadVersion.py
@@ -81,7 +81,24 @@ class Solution(object):
                 l = mid + 1
         return l
     
-    
+# golang solution
+
+'''
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+    	return sort.Search(n, func(i int) bool {
+		return isBadVersion(i)
+	})
+}
+'''
+
 # solutions
 
 '''
