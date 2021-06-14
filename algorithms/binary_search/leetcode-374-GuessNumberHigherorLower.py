8b758780cae6a711c2ded909d25c5d0177f7bdd7
MecaCho <1099833328@qq.com>
2021-06-14 17:43:03 +0800

    Update leetcode-374-GuessNumberHigherorLower.py
    Add 374. Guess Number Higher or Lower solution

--- algorithms/binary_search/leetcode-374-GuessNumberHigherorLower.py
+++ algorithms/binary_search/leetcode-374-GuessNumberHigherorLower.py
@@ -81,6 +81,31 @@ Constraints:
 1 <= pick <= n
 '''
 
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l < r:
+            mid = (l+r)/2
+            res = guess(mid)
+            if res == 0:
+                return mid
+            elif res > 0:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+
+
 # golang solution
 
 '''
