f573a53ffb513e599eee9dbf7390d8debd2b9264
MecaCho <1099833328@qq.com>
2021-06-29 22:07:58 +0800

    Create leetcode-168-ExcelSheetColumnTitle.py
    Add 168. Excel Sheet Column Title

---
+++ algorithms/string/leetcode-168-ExcelSheetColumnTitle.py
@@ -0,0 +1,96 @@
+# encoding=utf8
+
+
+'''
+168. Excel Sheet Column Title
+
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+Example 4:
+
+Input: columnNumber = 2147483647
+Output: "FXSHRXW"
+ 
+
+Constraints:
+
+1 <= columnNumber <= 231 - 1
+
+168. Excel表列名称
+
+给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+
+例如：
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+示例 1：
+
+输入：columnNumber = 1
+输出："A"
+示例 2：
+
+输入：columnNumber = 28
+输出："AB"
+示例 3：
+
+输入：columnNumber = 701
+输出："ZY"
+示例 4：
+
+输入：columnNumber = 2147483647
+输出："FXSHRXW"
+ 
+
+提示：
+
+1 <= columnNumber <= 231 - 1
+'''
+
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+
+        res = ""
+        while columnNumber:
+            columnNumber -= 1
+            res = chr(ord("A")+columnNumber % 26) + res
+            columnNumber /= 26
+        return res
+
