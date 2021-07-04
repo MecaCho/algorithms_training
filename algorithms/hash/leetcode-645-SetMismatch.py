daf139dc64bdf8ab305a8438e21cea33e9d0f7fe
MecaCho <1099833328@qq.com>
2021-07-04 20:53:21 +0800

    Update leetcode-645-SetMismatch.py
    Add 645. Set Mismatch solution

--- algorithms/hash/leetcode-645-SetMismatch.py
+++ algorithms/hash/leetcode-645-SetMismatch.py
@@ -1,7 +1,19 @@
-
+# encoding=utf8
 
 
 '''
+645. Set Mismatch
+The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+Given an array nums representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
+
+Example 1:
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Note:
+The given array size will in the range [2, 10000].
+The given array's numbers won't have any order.
+
 645. 错误的集合
 集合 S 包含从1到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
 
@@ -16,17 +28,6 @@
 给定数组的长度范围是 [2, 10000]。
 给定的数组是无序的。
 
-645. Set Mismatch
-The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
-
-Given an array nums representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
-
-Example 1:
-Input: nums = [1,2,2,4]
-Output: [2,3]
-Note:
-The given array size will in the range [2, 10000].
-The given array's numbers won't have any order.
 '''
 
 
@@ -47,6 +48,16 @@ class Solution(object):
         #         dul = i+1
         # return [dul, los]
         # return [k for k, v in hash_map.items() if v == 2]
+
+        
+        
+class Solution1(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1))-sum(set(nums))]
 
 
 # solutions
