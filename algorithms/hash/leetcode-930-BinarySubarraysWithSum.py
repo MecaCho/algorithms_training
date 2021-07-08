99f2bde8b037a0a9041660dbed97874c503abb58
MecaCho <1099833328@qq.com>
2021-07-08 23:10:01 +0800

    Create leetcode-930-BinarySubarraysWithSum.py
    Add 930. Binary Subarrays With Sum Golang solution

---
+++ algorithms/hash/leetcode-930-BinarySubarraysWithSum.py
@@ -0,0 +1,73 @@
+# encoding=utf8
+
+'''
+930. Binary Subarrays With Sum
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+Example 2:
+
+Input: nums = [0,0,0,0,0], goal = 0
+Output: 15
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+nums[i] is either 0 or 1.
+0 <= goal <= nums.length
+
+930. 和相同的二元子数组
+给你一个二元数组 nums ，和一个整数 goal ，请你统计并返回有多少个和为 goal 的 非空 子数组。
+
+子数组 是数组的一段连续部分。
+
+ 
+
+示例 1：
+
+输入：nums = [1,0,1,0,1], goal = 2
+输出：4
+解释：
+有 4 个满足题目要求的子数组：[1,0,1]、[1,0,1,0]、[0,1,0,1]、[1,0,1]
+示例 2：
+
+输入：nums = [0,0,0,0,0], goal = 0
+输出：15
+ 
+
+提示：
+
+1 <= nums.length <= 3 * 104
+nums[i] 不是 0 就是 1
+0 <= goal <= nums.length
+'''
+
+# golang solution
+
+'''
+func numSubarraysWithSum(nums []int, goal int) int {
+	cnt := make(map[int]int)
+	sum := 0
+	res := 0
+	for _, num := range nums {
+		cnt[sum]++
+		sum += num
+		res += cnt[sum-goal]
+	}
+	return res
+}
+'''
+
