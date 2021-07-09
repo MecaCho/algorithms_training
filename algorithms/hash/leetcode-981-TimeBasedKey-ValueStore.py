3cb3ef8d1b9cd2d0b59b4ac603437e3b8fa0e095
MecaCho <1099833328@qq.com>
2021-07-10 02:07:47 +0800

    Create leetcode-981-TimeBasedKey-ValueStore.py
    Add 981. Time Based Key-Value Store

---
+++ algorithms/hash/leetcode-981-TimeBasedKey-ValueStore.py
@@ -0,0 +1,125 @@
+# encoding=utf8
+
+'''
+981. Time Based Key-Value Store
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+
+Example 1:
+
+Input
+["TimeMap", "set", "get", "get", "set", "get", "get"]
+[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+Output
+[null, null, "bar", "bar", null, "bar2", "bar2"]
+
+Explanation
+TimeMap timeMap = new TimeMap();
+timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+timeMap.get("foo", 1);         // return "bar"
+timeMap.get("foo", 3);         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+timeMap.set("foo", "bar2", 4); // store the key "foo" and value "ba2r" along with timestamp = 4.
+timeMap.get("foo", 4);         // return "bar2"
+timeMap.get("foo", 5);         // return "bar2"
+ 
+
+Constraints:
+
+1 <= key.length, value.length <= 100
+key and value consist of lowercase English letters and digits.
+1 <= timestamp <= 107
+All the timestamps timestamp of set are strictly increasing.
+At most 2 * 105 calls will be made to set and get.
+
+981. 基于时间的键值存储
+创建一个基于时间的键值存储类 TimeMap，它支持下面两个操作：
+
+1. set(string key, string value, int timestamp)
+
+存储键 key、值 value，以及给定的时间戳 timestamp。
+2. get(string key, int timestamp)
+
+返回先前调用 set(key, value, timestamp_prev) 所存储的值，其中 timestamp_prev <= timestamp。
+如果有多个这样的值，则返回对应最大的  timestamp_prev 的那个值。
+如果没有值，则返回空字符串（""）。
+ 
+
+示例 1：
+
+输入：inputs = ["TimeMap","set","get","get","set","get","get"], inputs = [[],["foo","bar",1],["foo",1],["foo",3],["foo","bar2",4],["foo",4],["foo",5]]
+输出：[null,null,"bar","bar",null,"bar2","bar2"]
+解释：  
+TimeMap kv;   
+kv.set("foo", "bar", 1); // 存储键 "foo" 和值 "bar" 以及时间戳 timestamp = 1   
+kv.get("foo", 1);  // 输出 "bar"   
+kv.get("foo", 3); // 输出 "bar" 因为在时间戳 3 和时间戳 2 处没有对应 "foo" 的值，所以唯一的值位于时间戳 1 处（即 "bar"）   
+kv.set("foo", "bar2", 4);   
+kv.get("foo", 4); // 输出 "bar2"   
+kv.get("foo", 5); // 输出 "bar2"   
+
+示例 2：
+
+输入：inputs = ["TimeMap","set","set","get","get","get","get","get"], inputs = [[],["love","high",10],["love","low",20],["love",5],["love",10],["love",15],["love",20],["love",25]]
+输出：[null,null,null,"","high","high","low","low"]
+ 
+
+提示：
+
+所有的键/值字符串都是小写的。
+所有的键/值字符串长度都在 [1, 100] 范围内。
+所有 TimeMap.set 操作中的时间戳 timestamps 都是严格递增的。
+1 <= timestamp <= 10^7
+TimeMap.set 和 TimeMap.get 函数在每个测试用例中将（组合）调用总计 120000 次。
+'''
+
+# golang solution
+
+'''
+type item struct {
+	value string
+	timeStamp int
+}
+
+type TimeMap struct {
+	values map[string][]item
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() TimeMap {
+	return TimeMap{make(map[string][]item)}
+}
+
+
+func (this *TimeMap) Set(key string, value string, timestamp int)  {
+	this.values[key] = append(this.values[key], item{value: value, timeStamp: timestamp})
+}
+
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	items := this.values[key]
+	i := sort.Search(len(items), func(i int) bool {
+		return items[i].timeStamp > timestamp
+	})
+	if i > 0{
+		return items[i-1].value
+	}
+	return ""
+}
+
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
+'''
+
+
