811633d0d480cd6b64710c1b4d14b6bde96f57a9
MecaCho <1099833328@qq.com>
2021-07-11 11:59:06 +0800

    Create leetcode-274-H-Index.py
    Add 274. H-Index Golang solution

---
+++ algorithms/binary_search/leetcode-274-H-Index.py
@@ -0,0 +1,62 @@
+# encoding=utf8
+
+'''
+274. H-Index
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: A scientist has an index h if h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
+
+If there are several possible values for h, the maximum one is taken as the h-index.
+
+ 
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+ 
+
+Constraints:
+
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+
+274. H 指数
+给定一位研究者论文被引用次数的数组（被引用次数是非负整数）。编写一个方法，计算出研究者的 h 指数。
+
+h 指数的定义：h 代表“高引用次数”（high citations），一名科研人员的 h 指数是指他（她）的 （N 篇论文中）总共有 h 篇论文分别被引用了至少 h 次。且其余的 N - h 篇论文每篇被引用次数 不超过 h 次。
+
+例如：某人的 h 指数是 20，这表示他已发表的论文中，每篇被引用了至少 20 次的论文总共有 20 篇。
+
+ 
+
+示例：
+
+输入：citations = [3,0,6,1,5]
+输出：3 
+解释：给定数组表示研究者总共有 5 篇论文，每篇论文相应的被引用了 3, 0, 6, 1, 5 次。
+     由于研究者有 3 篇论文每篇 至少 被引用了 3 次，其余两篇论文每篇被引用 不多于 3 次，所以她的 h 指数是 3。
+ 
+
+提示：如果 h 有多种可能的值，h 指数是其中最大的那个。
+'''
+
+# golang solution
+
+'''
+func hIndex(citations []int) int {
+	sort.Ints(citations)
+	i := sort.Search(len(citations), func(i int) bool {
+		return citations[i] >= (len(citations)-i)
+	})
+	return len(citations)-i
+}
+'''
+
