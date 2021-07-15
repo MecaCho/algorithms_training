eeb03fc6d32c2852cd576fd8ad2372216d5629b6
MecaCho <1099833328@qq.com>
2021-07-15 22:15:39 +0800

    Create leetcode-1846-MaximumElementAfterDecreasingandRearranging.py
    Add 1846. Maximum Element After Decreasing and Rearranging Golang solution

---
+++ algorithms/sort/leetcode-1846-MaximumElementAfterDecreasingandRearranging.py
@@ -0,0 +1,110 @@
+# encoding=utf8
+
+'''
+1846. Maximum Element After Decreasing and Rearranging
+You are given an array of positive integers arr. Perform some operations (possibly none) on arr so that it satisfies these conditions:
+
+The value of the first element in arr must be 1.
+The absolute difference between any 2 adjacent elements must be less than or equal to 1. In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). abs(x) is the absolute value of x.
+There are 2 types of operations that you can perform any number of times:
+
+Decrease the value of any element of arr to a smaller positive integer.
+Rearrange the elements of arr to be in any order.
+Return the maximum possible value of an element in arr after performing the operations to satisfy the conditions.
+
+ 
+
+Example 1:
+
+Input: arr = [2,2,1,2,1]
+Output: 2
+Explanation: 
+We can satisfy the conditions by rearranging arr so it becomes [1,2,2,2,1].
+The largest element in arr is 2.
+Example 2:
+
+Input: arr = [100,1,1000]
+Output: 3
+Explanation: 
+One possible way to satisfy the conditions is by doing the following:
+1. Rearrange arr so it becomes [1,100,1000].
+2. Decrease the value of the second element to 2.
+3. Decrease the value of the third element to 3.
+Now arr = [1,2,3], which satisfies the conditions.
+The largest element in arr is 3.
+Example 3:
+
+Input: arr = [1,2,3,4,5]
+Output: 5
+Explanation: The array already satisfies the conditions, and the largest element is 5.
+ 
+
+Constraints:
+
+1 <= arr.length <= 105
+1 <= arr[i] <= 109
+
+
+1846. 减小和重新排列数组后的最大元素
+给你一个正整数数组 arr 。请你对 arr 执行一些操作（也可以不进行任何操作），使得数组满足以下条件：
+
+arr 中 第一个 元素必须为 1 。
+任意相邻两个元素的差的绝对值 小于等于 1 ，也就是说，对于任意的 1 <= i < arr.length （数组下标从 0 开始），都满足 abs(arr[i] - arr[i - 1]) <= 1 。abs(x) 为 x 的绝对值。
+你可以执行以下 2 种操作任意次：
+
+减小 arr 中任意元素的值，使其变为一个 更小的正整数 。
+重新排列 arr 中的元素，你可以以任意顺序重新排列。
+请你返回执行以上操作后，在满足前文所述的条件下，arr 中可能的 最大值 。
+
+ 
+
+示例 1：
+
+输入：arr = [2,2,1,2,1]
+输出：2
+解释：
+我们可以重新排列 arr 得到 [1,2,2,2,1] ，该数组满足所有条件。
+arr 中最大元素为 2 。
+示例 2：
+
+输入：arr = [100,1,1000]
+输出：3
+解释：
+一个可行的方案如下：
+1. 重新排列 arr 得到 [1,100,1000] 。
+2. 将第二个元素减小为 2 。
+3. 将第三个元素减小为 3 。
+现在 arr = [1,2,3] ，满足所有条件。
+arr 中最大元素为 3 。
+示例 3：
+
+输入：arr = [1,2,3,4,5]
+输出：5
+解释：数组已经满足所有条件，最大元素为 5 。
+ 
+
+提示：
+
+1 <= arr.length <= 105
+1 <= arr[i] <= 109
+
+'''
+
+# golang solutions
+
+'''
+func maximumElementAfterDecrementingAndRearranging(arr []int) int {
+	sort.Ints(arr)
+
+    arr[0] = 1
+	
+	for i := 1; i < len(arr); i++{
+		if arr[i] - arr[i-1] > 1{
+			arr[i] = arr[i-1] + 1
+		}
+	}
+	
+	return arr[len(arr)-1]
+}
+'''
+
