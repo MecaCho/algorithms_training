c82210315926c967d412a91669bfa7cc36579a7a
MecaCho <1099833328@qq.com>
2021-07-20 21:52:18 +0800

    Create leetcode-1877-MinimizeMaximumPairSuminArray.py
    Add 1877. Minimize Maximum Pair Sum in Array

---
+++ algorithms/sort/leetcode-1877-MinimizeMaximumPairSuminArray.py
@@ -0,0 +1,85 @@
+# encoding=utf8
+
+'''
+1877. Minimize Maximum Pair Sum in Array
+The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+Each element of nums is in exactly one pair, and
+The maximum pair sum is minimized.
+Return the minimized maximum pair sum after optimally pairing up the elements.
+
+ 
+
+Example 1:
+
+Input: nums = [3,5,2,3]
+Output: 7
+Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+Example 2:
+
+Input: nums = [3,5,4,2,4,6]
+Output: 8
+Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
+ 
+
+Constraints:
+
+n == nums.length
+2 <= n <= 105
+n is even.
+1 <= nums[i] <= 105
+
+1877. 数组中最大数对和的最小值
+一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+
+比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+
+nums 中每个元素 恰好 在 一个 数对中，且
+最大数对和 的值 最小 。
+请你在最优数对划分的方案下，返回最小的 最大数对和 。
+
+ 
+
+示例 1：
+
+输入：nums = [3,5,2,3]
+输出：7
+解释：数组中的元素可以分为数对 (3,3) 和 (5,2) 。
+最大数对和为 max(3+3, 5+2) = max(6, 7) = 7 。
+示例 2：
+
+输入：nums = [3,5,4,2,4,6]
+输出：8
+解释：数组中的元素可以分为数对 (3,5)，(4,4) 和 (6,2) 。
+最大数对和为 max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8 。
+ 
+
+提示：
+
+n == nums.length
+2 <= n <= 105
+n 是 偶数 。
+1 <= nums[i] <= 105
+'''
+
+class Solution(object):
+    def minPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        i, j = 0, len(nums)-1
+        res = 0
+        while i < j:
+            res = max(res, nums[i]+nums[j])
+            i += 1
+            j -= 1
+        return res
+
