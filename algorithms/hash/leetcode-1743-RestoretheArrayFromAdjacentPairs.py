33a4b58154d453be9a40ce3aed75e661aff5efea
MecaCho <1099833328@qq.com>
2021-07-25 05:25:28 +0800

    Create leetcode-1743-RestoretheArrayFromAdjacentPairs.py
    Add 1743. Restore the Array From Adjacent Pairs Golang solution

---
+++ algorithms/hash/leetcode-1743-RestoretheArrayFromAdjacentPairs.py
@@ -0,0 +1,116 @@
+# encoding=utf8
+
+'''
+1743. Restore the Array From Adjacent Pairs
+There is an integer array nums that consists of n unique elements, but you have forgotten it. However, you do remember every pair of adjacent elements in nums.
+
+You are given a 2D integer array adjacentPairs of size n - 1 where each adjacentPairs[i] = [ui, vi] indicates that the elements ui and vi are adjacent in nums.
+
+It is guaranteed that every adjacent pair of elements nums[i] and nums[i+1] will exist in adjacentPairs, either as [nums[i], nums[i+1]] or [nums[i+1], nums[i]]. The pairs can appear in any order.
+
+Return the original array nums. If there are multiple solutions, return any of them.
+
+ 
+
+Example 1:
+
+Input: adjacentPairs = [[2,1],[3,4],[3,2]]
+Output: [1,2,3,4]
+Explanation: This array has all its adjacent pairs in adjacentPairs.
+Notice that adjacentPairs[i] may not be in left-to-right order.
+Example 2:
+
+Input: adjacentPairs = [[4,-2],[1,4],[-3,1]]
+Output: [-2,4,1,-3]
+Explanation: There can be negative numbers.
+Another solution is [-3,1,4,-2], which would also be accepted.
+Example 3:
+
+Input: adjacentPairs = [[100000,-100000]]
+Output: [100000,-100000]
+ 
+
+Constraints:
+
+nums.length == n
+adjacentPairs.length == n - 1
+adjacentPairs[i].length == 2
+2 <= n <= 105
+-105 <= nums[i], ui, vi <= 105
+There exists some nums that has adjacentPairs as its pairs.
+
+1743. 从相邻元素对还原数组
+存在一个由 n 个不同元素组成的整数数组 nums ，但你已经记不清具体内容。好在你还记得 nums 中的每一对相邻元素。
+
+给你一个二维整数数组 adjacentPairs ，大小为 n - 1 ，其中每个 adjacentPairs[i] = [ui, vi] 表示元素 ui 和 vi 在 nums 中相邻。
+
+题目数据保证所有由元素 nums[i] 和 nums[i+1] 组成的相邻元素对都存在于 adjacentPairs 中，存在形式可能是 [nums[i], nums[i+1]] ，也可能是 [nums[i+1], nums[i]] 。这些相邻元素对可以 按任意顺序 出现。
+
+返回 原始数组 nums 。如果存在多种解答，返回 其中任意一个 即可。
+
+ 
+
+示例 1：
+
+输入：adjacentPairs = [[2,1],[3,4],[3,2]]
+输出：[1,2,3,4]
+解释：数组的所有相邻元素对都在 adjacentPairs 中。
+特别要注意的是，adjacentPairs[i] 只表示两个元素相邻，并不保证其 左-右 顺序。
+示例 2：
+
+输入：adjacentPairs = [[4,-2],[1,4],[-3,1]]
+输出：[-2,4,1,-3]
+解释：数组中可能存在负数。
+另一种解答是 [-3,1,4,-2] ，也会被视作正确答案。
+示例 3：
+
+输入：adjacentPairs = [[100000,-100000]]
+输出：[100000,-100000]
+ 
+
+提示：
+
+nums.length == n
+adjacentPairs.length == n - 1
+adjacentPairs[i].length == 2
+2 <= n <= 105
+-105 <= nums[i], ui, vi <= 105
+题目数据保证存在一些以 adjacentPairs 作为元素对的数组 nums
+'''
+
+# golang solution
+
+'''
+func restoreArray(adjacentPairs [][]int) []int {
+	res := make([]int, len(adjacentPairs)+1)
+	items := make(map[int][]int, len(adjacentPairs)+1)
+	for _, pair := range adjacentPairs{
+		items[pair[0]] = append(items[pair[0]], pair[1])
+		items[pair[1]] = append(items[pair[1]], pair[0])
+	}
+	
+	for k, v := range items{
+		if len(v) == 1{
+			res[0] = k
+			break
+		}
+	}
+	// 2:[1,3], 1:2, 4:3
+	// 3:[4,2]
+    // fmt.Println(items)
+	
+	res[1] = items[res[0]][0]
+	
+	for i := 2; i <= len(adjacentPairs);i++{
+		if res[i-2] != items[res[i-1]][0]{
+			res[i] = items[res[i-1]][0]
+		} else{
+			res[i] = items[res[i-1]][1]
+		}
+	}
+	
+	return res
+}
+'''
+
+
