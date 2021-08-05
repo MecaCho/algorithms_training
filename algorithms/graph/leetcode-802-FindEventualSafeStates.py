e5b14d791db379c713f40879e4bc1bcd37e98e5c
MecaCho <1099833328@qq.com>
2021-08-05 20:42:07 +0800

    Create leetcode-802-FindEventualSafeStates.py
    Add 802. Find Eventual Safe States

---
+++ algorithms/graph/leetcode-802-FindEventualSafeStates.py
@@ -0,0 +1,90 @@
+# encoding=utf8
+
+'''
+802. Find Eventual Safe States
+We start at some node in a directed graph, and every turn, we walk along a directed edge of the graph. If we reach a terminal node (that is, it has no outgoing directed edges), we stop.
+
+We define a starting node to be safe if we must eventually walk to a terminal node. More specifically, there is a natural number k, so that we must have stopped at a terminal node in less than k steps for any choice of where to walk.
+
+Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
+
+The directed graph has n nodes with labels from 0 to n - 1, where n is the length of graph. The graph is given in the following form: graph[i] is a list of labels j such that (i, j) is a directed edge of the graph, going from node i to node j.
+
+ 
+
+Example 1:
+
+Illustration of graph
+Input: graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+Output: [2,4,5,6]
+Explanation: The given graph is shown above.
+Example 2:
+
+Input: graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]
+Output: [4]
+ 
+
+Constraints:
+
+n == graph.length
+1 <= n <= 104
+0 <= graph[i].length <= n
+graph[i] is sorted in a strictly increasing order.
+The graph may contain self-loops.
+The number of edges in the graph will be in the range [1, 4 * 104].
+
+802. 找到最终的安全状态
+在有向图中，以某个节点为起始节点，从该点出发，每一步沿着图中的一条有向边行走。如果到达的节点是终点（即它没有连出的有向边），则停止。
+
+对于一个起始节点，如果从该节点出发，无论每一步选择沿哪条有向边行走，最后必然在有限步内到达终点，则将该起始节点称作是 安全 的。
+
+返回一个由图中所有安全的起始节点组成的数组作为答案。答案数组中的元素应当按 升序 排列。
+
+该有向图有 n 个节点，按 0 到 n - 1 编号，其中 n 是 graph 的节点数。图以下述形式给出：graph[i] 是编号 j 节点的一个列表，满足 (i, j) 是图的一条有向边。
+
+ 
+
+示例 1：
+
+Illustration of graph
+输入：graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+输出：[2,4,5,6]
+解释：示意图如上。
+示例 2：
+
+输入：graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]
+输出：[4]
+ 
+
+提示：
+
+n == graph.length
+1 <= n <= 104
+0 <= graph[i].length <= n
+graph[i] 按严格递增顺序排列。
+图中可能包含自环。
+图中边的数目在范围 [1, 4 * 104] 内。
+'''
+
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[int]
+        """
+        n = len(graph)
+        color = [0] * n
+
+        def safe(x):
+            if color[x] > 0:
+                return color[x] == 2
+            color[x] = 1
+            for y in graph[x]:
+                if not safe(y):
+                    return False
+            color[x] = 2
+            return True
+
+        return [i for i in range(n) if safe(i)]
+
+      
