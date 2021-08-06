5cad3d45c2e514622ce65db1aa0380663a2d227a
MecaCho <1099833328@qq.com>
2021-08-06 23:47:01 +0800

    Create leetcode-847. Shortest Path Visiting All Nodes.py
    Add 847. Shortest Path Visiting All Nodes

---
+++ algorithms/graph/leetcode-847. Shortest Path Visiting All Nodes.py
@@ -0,0 +1,67 @@
+# encoding=utf8
+
+'''
+847. Shortest Path Visiting All Nodes
+You have an undirected, connected graph of n nodes labeled from 0 to n - 1. You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
+
+Return the length of the shortest path that visits every node. You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+
+ 
+
+Example 1:
+
+
+Input: graph = [[1,2,3],[0],[0],[0]]
+Output: 4
+Explanation: One possible path is [1,0,2,0,3]
+Example 2:
+
+
+Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+Output: 4
+Explanation: One possible path is [0,1,4,2,3]
+ 
+
+Constraints:
+
+n == graph.length
+1 <= n <= 12
+0 <= graph[i].length < n
+graph[i] does not contain i.
+If graph[a] contains b, then graph[b] contains a.
+The input graph is always connected.
+
+
+847. 访问所有节点的最短路径
+存在一个由 n 个节点组成的无向连通图，图中的节点按从 0 到 n - 1 编号。
+
+给你一个数组 graph 表示这个图。其中，graph[i] 是一个列表，由所有与节点 i 直接相连的节点组成。
+
+返回能够访问所有节点的最短路径的长度。你可以在任一节点开始和停止，也可以多次重访节点，并且可以重用边。
+
+ 
+
+示例 1：
+
+
+输入：graph = [[1,2,3],[0],[0],[0]]
+输出：4
+解释：一种可能的路径为 [1,0,2,0,3]
+示例 2：
+
+
+
+输入：graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+输出：4
+解释：一种可能的路径为 [0,1,4,2,3]
+ 
+
+提示：
+
+n == graph.length
+1 <= n <= 12
+0 <= graph[i].length < n
+graph[i] 不包含 i
+如果 graph[a] 包含 b ，那么 graph[b] 也包含 a
+输入的图总是连通图
+'''
