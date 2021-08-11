d054435ca29c7933464454374931a7d27097cb87
MecaCho <1099833328@qq.com>
2021-08-11 22:13:34 +0800

    Create leetcode-446-ArithmeticSlicesII-Subsequence.py
    Add 446. Arithmetic Slices II - Subsequence

---
+++ algorithms/dynamic-programming/leetcode-446-ArithmeticSlicesII-Subsequence.py
@@ -0,0 +1,99 @@
+# encoding=utf8
+
+'''
+446. Arithmetic Slices II - Subsequence
+Given an integer array nums, return the number of all the arithmetic subsequences of nums.
+
+A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences.
+For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
+A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+
+For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+The answer is guaranteed to fit in 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,4,6,8,10]
+Output: 7
+Explanation: All arithmetic subsequence slices are:
+[2,4,6]
+[4,6,8]
+[6,8,10]
+[2,4,6,8]
+[4,6,8,10]
+[2,4,6,8,10]
+[2,6,10]
+Example 2:
+
+Input: nums = [7,7,7,7,7]
+Output: 16
+Explanation: Any subsequence of this array is arithmetic.
+ 
+
+Constraints:
+
+1  <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+
+446. 等差数列划分 II - 子序列
+给你一个整数数组 nums ，返回 nums 中所有 等差子序列 的数目。
+
+如果一个序列中 至少有三个元素 ，并且任意两个相邻元素之差相同，则称该序列为等差序列。
+
+例如，[1, 3, 5, 7, 9]、[7, 7, 7, 7] 和 [3, -1, -5, -9] 都是等差序列。
+再例如，[1, 1, 2, 5, 7] 不是等差序列。
+数组中的子序列是从数组中删除一些元素（也可能不删除）得到的一个序列。
+
+例如，[2,5,10] 是 [1,2,1,2,4,1,5,10] 的一个子序列。
+题目数据保证答案是一个 32-bit 整数。
+
+ 
+
+示例 1：
+
+输入：nums = [2,4,6,8,10]
+输出：7
+解释：所有的等差子序列为：
+[2,4,6]
+[4,6,8]
+[6,8,10]
+[2,4,6,8]
+[4,6,8,10]
+[2,4,6,8,10]
+[2,6,10]
+示例 2：
+
+输入：nums = [7,7,7,7,7]
+输出：16
+解释：数组中的任意子序列都是等差子序列。
+ 
+
+提示：
+
+1  <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+'''
+
+# golsng solution
+
+'''
+func numberOfArithmeticSlices(nums []int) int {
+    n := len(nums)
+    res := 0
+    f := make([]map[int]int, n)
+    for i := 0; i < n; i++{
+        f[i] = map[int]int{}
+        for j := 0; j < i; j++{
+            d := nums[i] - nums[j]
+            count := f[j][d]
+            res += count
+            f[i][d] += count + 1
+        }
+    }
+    return res
+}
+'''
