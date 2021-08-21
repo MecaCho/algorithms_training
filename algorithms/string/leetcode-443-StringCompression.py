b3e648a7ee8b89c0bfcf3a52b7669e28cf5bbc51
MecaCho <1099833328@qq.com>
2021-08-21 23:12:46 +0800

    Create leetcode-443-StringCompression.py
    Add 443. String Compression

---
+++ algorithms/string/leetcode-443-StringCompression.py
@@ -0,0 +1,108 @@
+# encoding=utf8
+
+'''
+443. String Compression
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+ 
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+Example 2:
+
+Input: chars = ["a"]
+Output: Return 1, and the first character of the input array should be: ["a"]
+Explanation: The only group is "a", which remains uncompressed since it's a single character.
+Example 3:
+
+Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+Example 4:
+
+Input: chars = ["a","a","a","b","b","a","a"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","3","b","2","a","2"].
+Explanation: The groups are "aaa", "bb", and "aa". This compresses to "a3b2a2". Note that each group is independent even if two groups have the same character.
+ 
+
+Constraints:
+
+1 <= chars.length <= 2000
+chars[i] is a lower-case English letter, upper-case English letter, digit, or symbol.
+
+
+443. 压缩字符串
+给你一个字符数组 chars ，请使用下述算法压缩：
+
+从一个空字符串 s 开始。对于 chars 中的每组 连续重复字符 ：
+
+如果这一组长度为 1 ，则将字符追加到 s 中。
+否则，需要向 s 追加字符，后跟这一组的长度。
+压缩后得到的字符串 s 不应该直接返回 ，需要转储到字符数组 chars 中。需要注意的是，如果组长度为 10 或 10 以上，则在 chars 数组中会被拆分为多个字符。
+
+请在 修改完输入数组后 ，返回该数组的新长度。
+
+你必须设计并实现一个只使用常量额外空间的算法来解决此问题。
+
+ 
+
+示例 1：
+
+输入：chars = ["a","a","b","b","c","c","c"]
+输出：返回 6 ，输入数组的前 6 个字符应该是：["a","2","b","2","c","3"]
+解释：
+"aa" 被 "a2" 替代。"bb" 被 "b2" 替代。"ccc" 被 "c3" 替代。
+示例 2：
+
+输入：chars = ["a"]
+输出：返回 1 ，输入数组的前 1 个字符应该是：["a"]
+解释：
+没有任何字符串被替代。
+示例 3：
+
+输入：chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+输出：返回 4 ，输入数组的前 4 个字符应该是：["a","b","1","2"]。
+解释：
+由于字符 "a" 不重复，所以不会被压缩。"bbbbbbbbbbbb" 被 “b12” 替代。
+注意每个数字在数组中都有它自己的位置。
+ 
+
+提示：
+
+1 <= chars.length <= 2000
+chars[i] 可以是小写英文字母、大写英文字母、数字或符号
+'''
+
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        res = 0
+        count = 1
+        for i in range(len(chars)):
+            if i + 1 == len(chars) or chars[i] != chars[i+1]:
+                chars[res] = chars[i]
+                res += 1
+                if count > 1:
+                    for ch in str(count):
+                        chars[res] = ch
+                        res += 1
+                count = 0
+            count += 1
+
+        return res
+
