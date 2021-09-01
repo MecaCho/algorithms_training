4e9343059c8f9bddc853212b24309aa127c9708f
MecaCho <1099833328@qq.com>
2021-09-02 00:25:22 +0800

    Update leetcode-I22-getKthFromEnd.py
    Add getKthFromEnd Golang solution

--- algorithms/linked_list/leetcode-I22-getKthFromEnd.py
+++ algorithms/linked_list/leetcode-I22-getKthFromEnd.py
@@ -38,3 +38,27 @@ class Solution(object):
             first = first.next
             second = second.next
         return second
+
+# golang solution
+
+'''
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	fast, slow := head, head
+	for i := 0; i < k; i++{
+		fast = fast.Next
+	}
+	
+	for fast != nil{
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return slow
+}
+'''
