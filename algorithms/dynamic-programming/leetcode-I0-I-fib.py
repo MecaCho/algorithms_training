55920bffcdbcbd522de300f17446b1c86b729352
MecaCho <1099833328@qq.com>
2021-09-04 16:26:15 +0800

    Update leetcode-I0-I-fib.py
    Add Fibonacci Golang solution

--- algorithms/dynamic-programming/leetcode-I0-I-fib.py
+++ algorithms/dynamic-programming/leetcode-I0-I-fib.py
@@ -105,3 +105,18 @@ class Solution:
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
+
+# golang solution
+
+'''
+func fib(n int) int {
+    a, b := 0, 1
+    for i := 0; i < n; i++{
+        tmp := (a + b) % 1000000007
+        b = a
+        a = tmp
+    }
+    return a
+
+}
+'''
