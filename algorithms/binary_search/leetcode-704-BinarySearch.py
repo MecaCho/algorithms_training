572e7f1be7eba9e82ab28db19bc2e2d77a5b7715
MecaCho <1099833328@qq.com>
2021-09-06 19:41:58 +0800

    Update leetcode-704-BinarySearch.py
    Add 704. Binary Search Golang solution

--- algorithms/binary_search/leetcode-704-BinarySearch.py
+++ algorithms/binary_search/leetcode-704-BinarySearch.py
@@ -1,5 +1,4 @@
-
-
+# encoding=utf8
 
 
 '''
@@ -70,5 +69,24 @@ func search(nums []int, target int) int {
 
     return -1
 
+}
+'''
+
+# golang solution
+
+'''
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
 }
 '''
