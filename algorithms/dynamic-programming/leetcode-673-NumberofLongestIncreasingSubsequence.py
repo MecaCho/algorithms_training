7bedaa090e7653f34a3cbba8cb05ef3511dd6623
MecaCho <1099833328@qq.com>
2021-09-20 22:16:14 +0800

    Create leetcode-673-NumberofLongestIncreasingSubsequence.py
    Add 673. Number of Longest Increasing Subsequence

---
+++ algorithms/dynamic-programming/leetcode-673-NumberofLongestIncreasingSubsequence.py
@@ -0,0 +1,70 @@
+# encoding=utf8
+
+'''
+673. Number of Longest Increasing Subsequence
+Given an integer array nums, return the number of longest increasing subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+Example 2:
+
+Input: nums = [2,2,2,2,2]
+Output: 5
+Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 2000
+-106 <= nums[i] <= 106
+
+673. 最长递增子序列的个数
+给定一个未排序的整数数组，找到最长递增子序列的个数。
+
+示例 1:
+
+输入: [1,3,5,4,7]
+输出: 2
+解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+示例 2:
+
+输入: [2,2,2,2,2]
+输出: 5
+解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
+'''
+
+class Solution(object):
+    def findNumberOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1] * len(nums)
+        cnt = [1] * len(nums)
+        max_len = 0
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        cnt[i] = cnt[j]
+                    elif dp[j] + 1 == dp[i]:
+                        cnt[i] += cnt[j]
+                        
+            if dp[i] > max_len:
+                max_len = dp[i]
+                res = cnt[i] 
+            elif dp[i] == max_len:
+                res += cnt[i]
+
+        return res
