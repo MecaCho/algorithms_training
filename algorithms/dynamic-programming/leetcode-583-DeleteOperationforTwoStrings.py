1c05d969af75b38b7cc2763b4fa01705385ce6b7
MecaCho <1099833328@qq.com>
2021-09-25 21:29:53 +0800

    Create leetcode-583-DeleteOperationforTwoStrings.py
    Add 583. Delete Operation for Two Strings

---
+++ algorithms/dynamic-programming/leetcode-583-DeleteOperationforTwoStrings.py
@@ -0,0 +1,63 @@
+# encoding=utf8
+
+'''
+583. Delete Operation for Two Strings
+Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+ 
+
+Example 1:
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Example 2:
+
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 500
+word1 and word2 consist of only lowercase English letters.
+
+583. 两个字符串的删除操作
+给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+
+ 
+
+示例：
+
+输入: "sea", "eat"
+输出: 2
+解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ 
+
+提示：
+
+给定单词的长度不超过500。
+给定单词中的字符只含有小写字母。
+'''
+
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        m = len(word1)
+        n = len(word2)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return n+m - dp[m][n]*2
+
