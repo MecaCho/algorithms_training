00aa956b7b47e34a8aaaedb852d1c57777a5574c
MecaCho <1099833328@qq.com>
2021-09-28 19:30:36 +0800

    Update leetcode-437-PathSumIII.py
    Add 437. Path Sum III solution

--- algorithms/tree/leetcode-437-PathSumIII.py
+++ algorithms/tree/leetcode-437-PathSumIII.py
@@ -1,4 +1,33 @@
+# encoding=utf8
+
 '''
+437. Path Sum III
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+
 437. 路径总和 III
 给定一个二叉树，它的每个结点都存放着一个整数值。
 
@@ -26,34 +55,34 @@ root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
 2.  5 -> 2 -> 1
 3.  -3 -> 11
 
-
-437. Path Sum III
-You are given a binary tree in which each node contains an integer value.
-
-Find the number of paths that sum to a given value.
-
-The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
-
-The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
-
-Example:
-
-root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
-
-      10
-     /  \
-    5   -3
-   / \    \
-  3   2   11
- / \   \
-3  -2   1
-
-Return 3. The paths that sum to 8 are:
-
-1.  5 -> 3
-2.  5 -> 2 -> 1
-3. -3 -> 11
 '''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rootSum(self, root, sum):
+            if not root:
+                return 0
+            res = 0
+            if sum == root.val:
+                res = 1
+            return res + self.rootSum(root.left, sum-root.val) + self.rootSum(root.right, sum-root.val)
+        
+    def pathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return self.pathSum(root.left, targetSum)+self.pathSum(root.right, targetSum)+self.rootSum(root, targetSum)
+
 
 
 # Definition for a binary tree node.
