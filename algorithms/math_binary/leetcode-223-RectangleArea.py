ec44934d56ebcfb7cdd242ef1377b12d51711cc3
MecaCho <1099833328@qq.com>
2021-09-30 17:08:28 +0800

    Create leetcode-223-RectangleArea.py
    Add 223. Rectangle Area

---
+++ algorithms/math_binary/leetcode-223-RectangleArea.py
@@ -0,0 +1,74 @@
+# encoding=utf8
+
+'''
+223. Rectangle Area
+Given the coordinates of two rectilinear rectangles in a 2D plane, return the total area covered by the two rectangles.
+
+The first rectangle is defined by its bottom-left corner (ax1, ay1) and its top-right corner (ax2, ay2).
+
+The second rectangle is defined by its bottom-left corner (bx1, by1) and its top-right corner (bx2, by2).
+
+ 
+
+Example 1:
+
+Rectangle Area
+Input: ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
+Output: 45
+Example 2:
+
+Input: ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2, by1 = -2, bx2 = 2, by2 = 2
+Output: 16
+ 
+
+Constraints:
+
+-104 <= ax1, ay1, ax2, ay2, bx1, by1, bx2, by2 <= 104
+
+
+223. 矩形面积
+给你 二维 平面上两个 由直线构成的 矩形，请你计算并返回两个矩形覆盖的总面积。
+
+每个矩形由其 左下 顶点和 右上 顶点坐标表示：
+
+第一个矩形由其左下顶点 (ax1, ay1) 和右上顶点 (ax2, ay2) 定义。
+第二个矩形由其左下顶点 (bx1, by1) 和右上顶点 (bx2, by2) 定义。
+ 
+
+示例 1：
+
+Rectangle Area
+输入：ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
+输出：45
+示例 2：
+
+输入：ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2, by1 = -2, bx2 = 2, by2 = 2
+输出：16
+ 
+
+提示：
+
+-104 <= ax1, ay1, ax2, ay2, bx1, by1, bx2, by2 <= 104
+'''
+
+class Solution(object):
+    def computeArea(self, ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+        """
+        :type ax1: int
+        :type ay1: int
+        :type ax2: int
+        :type ay2: int
+        :type bx1: int
+        :type by1: int
+        :type bx2: int
+        :type by2: int
+        :rtype: int
+        """
+        area1 = (ax2 - ax1) * (ay2 - ay1)
+        area2 = (bx2 - bx1) * (by2 - by1)
+        overlapWidth = min(ax2, bx2) - max(ax1, bx1)
+        overlapHeight = min(ay2, by2) - max(ay1, by1)
+        overlapArea = max(overlapWidth, 0) * max(overlapHeight, 0)
+        return area1 + area2 - overlapArea
+
+      
