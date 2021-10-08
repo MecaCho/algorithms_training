472c0190f85362ac62e21df699a4deebf6e72345
MecaCho <1099833328@qq.com>
2021-10-08 18:48:45 +0800

    Create leetcode-187-RepeatedDNASequences.py
    Add 187. Repeated DNA Sequences

---
+++ algorithms/string/leetcode-187-RepeatedDNASequences.py
@@ -0,0 +1,66 @@
+# encoding=utf8
+
+'''
+187. Repeated DNA Sequences
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+Example 2:
+
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either 'A', 'C', 'G', or 'T'.
+
+187. 重复的DNA序列
+所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，例如："ACGAATTCCG"。在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
+
+编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串 s 中出现次数超过一次。
+
+ 
+
+示例 1：
+
+输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+输出：["AAAAACCCCC","CCCCCAAAAA"]
+示例 2：
+
+输入：s = "AAAAAAAAAAAAA"
+输出：["AAAAAAAAAA"]
+ 
+
+提示：
+
+0 <= s.length <= 105
+s[i] 为 'A'、'C'、'G' 或 'T'
+'''
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        cnt = defaultdict(int)
+        res = []
+        for i in range(len(s)-10, -1, -1):
+            sub = s[i:i+10]
+            cnt[sub] += 1
+            if cnt[sub] == 2:
+                res.append(sub)
+        return res
+
