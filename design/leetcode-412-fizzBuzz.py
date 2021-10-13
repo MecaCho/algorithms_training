e9599acaa6c8a5c0a86532492d74f2f09d2bc510
MecaCho <1099833328@qq.com>
2021-10-13 21:22:57 +0800

    Update and rename leetcode-fizzBuzz.py to leetcode-412-fizzBuzz.py
    Add 412. Fizz Buzz

--- design/leetcode-412-fizzBuzz.py
+++ design/leetcode-412-fizzBuzz.py
@@ -1,6 +1,32 @@
 # encoding=utf8
 
 '''
+412. Fizz Buzz
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i if non of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 104
 
 Fizz Buzz
 Write a program that outputs the string representation of numbers from 1 to n.
@@ -85,3 +111,20 @@ if __name__ == '__main__':
     demo = Solution()
     print(demo.fizzBuzz(15))
 
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def get_num(i):
+            if i % 3 == 0 and i % 5 == 0:
+                return "FizzBuzz"
+            if i % 3 == 0:
+                return "Fizz"
+            if i % 5 == 0:
+                return "Buzz"
+            return str(i)
+        return [get_num(i+1) for i in range(n)]
+    
+    
