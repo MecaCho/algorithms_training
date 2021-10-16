5a3688adc4073e230cc4a31a356c7fad3c259d0e
MecaCho <1099833328@qq.com>
2021-10-16 13:35:35 +0800

    Create leetcode-282-ExpressionAddOperators.py
    Add 282. Expression Add Operators

---
+++ algorithms/backtracking/leetcode-282-ExpressionAddOperators.py
@@ -0,0 +1,80 @@
+# encoding=utf8
+
+'''
+282. Expression Add Operators
+Given a string num that contains only digits and an integer target, return all possibilities to insert the binary operators '+', '-', and/or '*' between the digits of num so that the resultant expression evaluates to the target value.
+
+Note that operands in the returned expressions should not contain leading zeros.
+
+ 
+
+Example 1:
+
+Input: num = "123", target = 6
+Output: ["1*2*3","1+2+3"]
+Explanation: Both "1*2*3" and "1+2+3" evaluate to 6.
+Example 2:
+
+Input: num = "232", target = 8
+Output: ["2*3+2","2+3*2"]
+Explanation: Both "2*3+2" and "2+3*2" evaluate to 8.
+Example 3:
+
+Input: num = "105", target = 5
+Output: ["1*0+5","10-5"]
+Explanation: Both "1*0+5" and "10-5" evaluate to 5.
+Note that "1-05" is not a valid expression because the 5 has a leading zero.
+Example 4:
+
+Input: num = "00", target = 0
+Output: ["0*0","0+0","0-0"]
+Explanation: "0*0", "0+0", and "0-0" all evaluate to 0.
+Note that "00" is not a valid expression because the 0 has a leading zero.
+Example 5:
+
+Input: num = "3456237490", target = 9191
+Output: []
+Explanation: There are no expressions that can be created from "3456237490" to evaluate to 9191.
+ 
+
+Constraints:
+
+1 <= num.length <= 10
+num consists of only digits.
+-231 <= target <= 231 - 1
+
+
+282. 给表达式添加运算符
+给定一个仅包含数字 0-9 的字符串 num 和一个目标值整数 target ，在 num 的数字之间添加 二元 运算符（不是一元）+、- 或 * ，返回所有能够得到目标值的表达式。
+
+ 
+
+示例 1:
+
+输入: num = "123", target = 6
+输出: ["1+2+3", "1*2*3"] 
+示例 2:
+
+输入: num = "232", target = 8
+输出: ["2*3+2", "2+3*2"]
+示例 3:
+
+输入: num = "105", target = 5
+输出: ["1*0+5","10-5"]
+示例 4:
+
+输入: num = "00", target = 0
+输出: ["0+0", "0-0", "0*0"]
+示例 5:
+
+输入: num = "3456237490", target = 9191
+输出: []
+ 
+
+提示：
+
+1 <= num.length <= 10
+num 仅含数字
+-231 <= target <= 231 - 1
+
+'''
