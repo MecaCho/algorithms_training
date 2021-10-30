166509feed7955086ccc906e24fdf94cf89623fc
MecaCho <1099833328@qq.com>
2021-10-30 15:03:32 +0800

    Update leetcode-260-SingleNumberIII.py
    Add 260. Single Number III Golang solution

--- algorithms/bitwise-operations/leetcode-260-SingleNumberIII.py
+++ algorithms/bitwise-operations/leetcode-260-SingleNumberIII.py
@@ -1,17 +1,6 @@
+# encoding=utf8
 
 '''
-260. 只出现一次的数字 III
-给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。
-
-示例 :
-
-输入: [1,2,1,3,2,5]
-输出: [3,5]
-注意：
-
-结果输出的顺序并不重要，对于上面的例子， [5, 3] 也是正确答案。
-你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？
-
 260. Single Number III
 Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
 
@@ -23,6 +12,18 @@ Note:
 
 The order of the result is not important. So in the above example, [5, 3] is also correct.
 Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+
+260. 只出现一次的数字 III
+给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。
+
+示例 :
+
+输入: [1,2,1,3,2,5]
+输出: [3,5]
+注意：
+
+结果输出的顺序并不重要，对于上面的例子， [5, 3] 也是正确答案。
+你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？
 '''
 
 class Solution(object):
@@ -44,6 +45,31 @@ class Solution(object):
             else:
                 b ^= nums[i]
         return [a, b]
+    
+# golang solution
+
+'''
+func singleNumber(nums []int) []int {
+	xors := 0
+	for i := 0; i < len(nums); i++ {
+		xors ^= nums[i]
+	}
+	div := 1
+	for div&xors == 0 {
+		div <<= 1
+
+	}
+	a, b := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i]&div != 0 {
+			a ^= nums[i]
+		} else {
+			b ^= nums[i]
+		}
+	}
+	return []int{a, b}
+}
+'''
 
 '''
 概述
