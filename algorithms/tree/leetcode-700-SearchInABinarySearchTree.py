9a79bb4afdbf7fb2b10c1b02d3b2438c3e2b8ce0
MecaCho <1099833328@qq.com>
2021-11-26 21:18:50 +0800

    Update leetcode-700-SearchInABinarySearchTree.py

--- algorithms/tree/leetcode-700-SearchInABinarySearchTree.py
+++ algorithms/tree/leetcode-700-SearchInABinarySearchTree.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 700. 二叉搜索树中的搜索
 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
@@ -45,7 +47,24 @@ Note that an empty tree is represented by NULL, therefore you would see the expe
 
 
 
-
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution1(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        while root:
+            if root.val == val:
+                return root
+            root = root.left if val < root.val else root.right
+        return root
 
 # Definition for a binary tree node.
 # class TreeNode(object):
