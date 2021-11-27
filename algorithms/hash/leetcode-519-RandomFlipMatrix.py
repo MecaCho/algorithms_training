35405f07805ebc9ad415892eab73356e6cd401c9
MecaCho <1099833328@qq.com>
2021-11-27 22:36:50 +0800

    Create leetcode-519-RandomFlipMatrix.py
    Add 519. Random Flip Matrix

---
+++ algorithms/hash/leetcode-519-RandomFlipMatrix.py
@@ -0,0 +1,108 @@
+# encoding=utf8
+
+'''
+519. Random Flip Matrix
+There is an m x n binary grid matrix with all the values set 0 initially. Design an algorithm to randomly pick an index (i, j) where matrix[i][j] == 0 and flips it to 1. All the indices (i, j) where matrix[i][j] == 0 should be equally likely to be returned.
+
+Optimize your algorithm to minimize the number of calls made to the built-in random function of your language and optimize the time and space complexity.
+
+Implement the Solution class:
+
+Solution(int m, int n) Initializes the object with the size of the binary matrix m and n.
+int[] flip() Returns a random index [i, j] of the matrix where matrix[i][j] == 0 and flips it to 1.
+void reset() Resets all the values of the matrix to be 0.
+ 
+
+Example 1:
+
+Input
+["Solution", "flip", "flip", "flip", "reset", "flip"]
+[[3, 1], [], [], [], [], []]
+Output
+[null, [1, 0], [2, 0], [0, 0], null, [2, 0]]
+
+Explanation
+Solution solution = new Solution(3, 1);
+solution.flip();  // return [1, 0], [0,0], [1,0], and [2,0] should be equally likely to be returned.
+solution.flip();  // return [2, 0], Since [1,0] was returned, [2,0] and [0,0]
+solution.flip();  // return [0, 0], Based on the previously returned indices, only [0,0] can be returned.
+solution.reset(); // All the values are reset to 0 and can be returned.
+solution.flip();  // return [2, 0], [0,0], [1,0], and [2,0] should be equally likely to be returned.
+ 
+
+Constraints:
+
+1 <= m, n <= 104
+There will be at least one free cell for each call to flip.
+At most 1000 calls will be made to flip and reset.
+
+519. 随机翻转矩阵
+给你一个 m x n 的二元矩阵 matrix ，且所有值被初始化为 0 。请你设计一个算法，随机选取一个满足 matrix[i][j] == 0 的下标 (i, j) ，并将它的值变为 1 。所有满足 matrix[i][j] == 0 的下标 (i, j) 被选取的概率应当均等。
+
+尽量最少调用内置的随机函数，并且优化时间和空间复杂度。
+
+实现 Solution 类：
+
+Solution(int m, int n) 使用二元矩阵的大小 m 和 n 初始化该对象
+int[] flip() 返回一个满足 matrix[i][j] == 0 的随机下标 [i, j] ，并将其对应格子中的值变为 1
+void reset() 将矩阵中所有的值重置为 0
+ 
+
+示例：
+
+输入
+["Solution", "flip", "flip", "flip", "reset", "flip"]
+[[3, 1], [], [], [], [], []]
+输出
+[null, [1, 0], [2, 0], [0, 0], null, [2, 0]]
+
+解释
+Solution solution = new Solution(3, 1);
+solution.flip();  // 返回 [1, 0]，此时返回 [0,0]、[1,0] 和 [2,0] 的概率应当相同
+solution.flip();  // 返回 [2, 0]，因为 [1,0] 已经返回过了，此时返回 [2,0] 和 [0,0] 的概率应当相同
+solution.flip();  // 返回 [0, 0]，根据前面已经返回过的下标，此时只能返回 [0,0]
+solution.reset(); // 所有值都重置为 0 ，并可以再次选择下标返回
+solution.flip();  // 返回 [2, 0]，此时返回 [0,0]、[1,0] 和 [2,0] 的概率应当相同
+ 
+
+提示：
+
+1 <= m, n <= 104
+每次调用flip 时，矩阵中至少存在一个值为 0 的格子。
+最多调用 1000 次 flip 和 reset 方法。
+'''
+
+class Solution(object):
+
+    def __init__(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        """
+        self.col = n
+        self.total = m * n
+        self.index = 0
+
+    def flip(self):
+        """
+        :rtype: List[int]
+        """
+        if self.index == self.total:
+            self.index = 0
+        x = self.index / self.col
+        y = self.index % self.col
+        self.index += 1
+        return [x, y]
+
+    def reset(self):
+        """
+        :rtype: None
+        """
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(m, n)
+# param_1 = obj.flip()
+# obj.reset()
+
