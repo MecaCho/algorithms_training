556b4ab20e58394f0982592ead5fba1e2b7ec70c
MecaCho <1099833328@qq.com>
2021-12-12 21:18:44 +0800

    Update leetcode_709_toLowerCase.py
    Update 709. To Lower Case

--- algorithms/old/leetcode_709_toLowerCase/leetcode_709_toLowerCase.py
+++ algorithms/old/leetcode_709_toLowerCase/leetcode_709_toLowerCase.py
@@ -38,3 +38,15 @@ class Solution(object):
 if __name__ == '__main__':
     demo = Solution()
     print demo.toLowerCase("Hello")
+
+# tips
+
+'''
+大写字母A - Z 的ASCII 码范围为 [65, 90]：
+
+小写字母a - z 的ASCII 码范围为 [97, 122]。
+
+因此，如果我们发现ch 的ASCII 码在 [65, 96]的范围内，那么我们将它的ASCII 码增加 32，即可得到对应的小写字母。
+
+可以对ch 的ASCII 码与 32做按位或运算，替代与 32的加法运算
+'''
