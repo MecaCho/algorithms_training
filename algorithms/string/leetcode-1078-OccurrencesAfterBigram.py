8d969ae8f65295481043681637c799ef62d1f23c
MecaCho <1099833328@qq.com>
2021-12-26 23:35:59 +0800

    Create leetcode-1078-OccurrencesAfterBigram.py
    Add 1078. Occurrences After Bigram

---
+++ algorithms/string/leetcode-1078-OccurrencesAfterBigram.py
@@ -0,0 +1,47 @@
+# encoding=utf8
+
+
+'''
+1078. Occurrences After Bigram
+Given two strings first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+
+Return an array of all the words third for each occurrence of "first second third".
+
+ 
+
+Example 1:
+
+Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
+Output: ["girl","student"]
+Example 2:
+
+Input: text = "we will we will rock you", first = "we", second = "will"
+Output: ["we","rock"]
+ 
+
+Constraints:
+
+1 <= text.length <= 1000
+text consists of lowercase English letters and spaces.
+All the words in text a separated by a single space.
+1 <= first.length, second.length <= 10
+first and second consist of lowercase English letters.
+'''
+
+
+class Solution(object):
+    def findOcurrences(self, text, first, second):
+        """
+        :type text: str
+        :type first: str
+        :type second: str
+        :rtype: List[str]
+        """
+        res = []
+        words = text.split()
+        for i in range(len(words)):
+            if words[i] == first and i+1 < len(words) and words[i+1] == second and i+2 < len(words):
+                res.append(words[i+2])
+        return res
+
+
