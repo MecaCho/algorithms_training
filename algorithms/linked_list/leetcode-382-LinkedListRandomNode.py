f63ae005da7b024b483432a604db737b6bdfe26c
MecaCho <1099833328@qq.com>
2022-01-16 03:05:40 +0800

    Create leetcode-382-LinkedListRandomNode.py
    Add 382. Linked List Random Node

---
+++ algorithms/linked_list/leetcode-382-LinkedListRandomNode.py
@@ -0,0 +1,74 @@
+# encoding=utf8
+
+'''
+382. Linked List Random Node
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Implement the Solution class:
+
+Solution(ListNode head) Initializes the object with the integer array nums.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be choosen.
+ 
+
+Example 1:
+
+
+Input
+["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 3, 2, 2, 3]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.getRandom(); // return 1
+solution.getRandom(); // return 3
+solution.getRandom(); // return 2
+solution.getRandom(); // return 2
+solution.getRandom(); // return 3
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ 
+
+Constraints:
+
+The number of nodes in the linked list will be in the range [1, 104].
+-104 <= Node.val <= 104
+At most 104 calls will be made to getRandom.
+ 
+
+Follow up:
+
+What if the linked list is extremely large and its length is unknown to you?
+Could you solve this efficiently without using extra space?
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import random
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        :type head: Optional[ListNode]
+        """
+        self.values = []
+        while head:
+            self.values.append(head.val)
+            head = head.next
+
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.choice(self.values)
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
