ab60d6f6c88c3b9c4ded78a0a9f5b2b3d475ebdf
MecaCho <1099833328@qq.com>
2022-01-19 19:59:45 +0800

    Create leetcode-219-ContainsDuplicateII.py
    Add 219. Contains Duplicate II

---
+++ algorithms/hash/leetcode-219-ContainsDuplicateII.py
@@ -0,0 +1,45 @@
+# encoding=utf8
+
+'''
+219. Contains Duplicate II
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+'''
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        index_dict = {}
+
+        for i in range(len(nums)):
+            if nums[i] in index_dict and i - index_dict[nums[i]] <= k:
+                return True
+            index_dict[nums[i]] = i
+
+        return False
+      
