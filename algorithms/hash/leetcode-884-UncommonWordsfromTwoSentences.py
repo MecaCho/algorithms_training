387b1f4b55f04a66a24c7b7ed87c431ee8738117
MecaCho <1099833328@qq.com>
2022-01-30 22:43:54 +0800

    Create leetcode-884-UncommonWordsfromTwoSentences.py
    Add 884. Uncommon Words from Two Sentences

---
+++ algorithms/hash/leetcode-884-UncommonWordsfromTwoSentences.py
@@ -0,0 +1,45 @@
+# encoding=utf8
+
+
+'''
+884. Uncommon Words from Two Sentences
+A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+
+A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+Output: ["sweet","sour"]
+Example 2:
+
+Input: s1 = "apple apple", s2 = "banana"
+Output: ["banana"]
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 200
+s1 and s2 consist of lowercase English letters and spaces.
+s1 and s2 do not have leading or trailing spaces.
+All the words in s1 and s2 are separated by a single space.
+'''
+
+class Solution(object):
+    def uncommonFromSentences(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: List[str]
+        """
+        # s = " ".join([s1, s2])
+        # cnt1 = Counter(s.split())
+        # return [word for word, c in cnt1.items() if c == 1]
+        words = s1.split()+s2.split()
+        return [word for word in words if words.count(word) == 1]
+
+
