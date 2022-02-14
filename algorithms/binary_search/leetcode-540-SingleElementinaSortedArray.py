8fc71fa453914c37ab63efd1ad71c563968dfb33
MecaCho <1099833328@qq.com>
2022-02-14 21:48:24 +0800

    Create leetcode-540-SingleElementinaSortedArray.py
    Add 540. Single Element in a Sorted Array

---
+++ algorithms/binary_search/leetcode-540-SingleElementinaSortedArray.py
@@ -0,0 +1,44 @@
+# encoding=utf8
+
+'''
+540. Single Element in a Sorted Array
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+'''
+
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) /2
+            if nums[mid] == nums[mid ^ 1]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
+
+
