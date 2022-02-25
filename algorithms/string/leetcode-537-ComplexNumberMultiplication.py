167bfeb19de94219525da56114776f9c8b73e5aa
MecaCho <1099833328@qq.com>
2022-02-25 22:28:43 +0800

    Create leetcode-537-ComplexNumberMultiplication.py
    Add 537. Complex Number Multiplication

---
+++ algorithms/string/leetcode-537-ComplexNumberMultiplication.py
@@ -0,0 +1,43 @@
+# encoding=utf8
+
+
+'''
+537. Complex Number Multiplication
+A complex number can be represented as a string on the form "real+imaginaryi" where:
+
+real is the real part and is an integer in the range [-100, 100].
+imaginary is the imaginary part and is an integer in the range [-100, 100].
+i2 == -1.
+Given two complex numbers num1 and num2 as strings, return a string of the complex number that represents their multiplications.
+
+ 
+
+Example 1:
+
+Input: num1 = "1+1i", num2 = "1+1i"
+Output: "0+2i"
+Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+Example 2:
+
+Input: num1 = "1+-1i", num2 = "1+-1i"
+Output: "0+-2i"
+Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+ 
+
+Constraints:
+
+num1 and num2 are valid complex numbers.
+'''
+
+class Solution(object):
+    def complexNumberMultiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        real1, imag1 = map(int, num1[:-1].split('+'))
+        real2, imag2 = map(int, num2[:-1].split('+'))
+        return str(real1 * real2 - imag1 * imag2)+"+"+str(real1 * imag2 + imag1 * real2) +'i'
+
+
