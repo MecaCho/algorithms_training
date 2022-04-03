2cf306a530ca9b34136083247e7bffa2c96bf0a0
MecaCho <1099833328@qq.com>
2022-04-03 20:59:19 +0800

    Create leetcode-744-FindSmallestLetterGreaterThanTarget.py
    Add 744. Find Smallest Letter Greater Than Target

---
+++ algorithms/binary_search/leetcode-744-FindSmallestLetterGreaterThanTarget.py
@@ -0,0 +1,49 @@
+# encoding=utf8
+
+'''
+744. Find Smallest Letter Greater Than Target
+Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+
+Note that the letters wrap around.
+
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+ 
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Example 3:
+
+Input: letters = ["c","f","j"], target = "d"
+Output: "f"
+ 
+
+Constraints:
+
+2 <= letters.length <= 104
+letters[i] is a lowercase English letter.
+letters is sorted in non-decreasing order.
+letters contains at least two different characters.
+target is a lowercase English letter.
+'''
+
+# golang 
+
+'''
+func nextGreatestLetter(letters []byte, target byte) byte {
+    for _, c := range letters {
+        if c > target{
+            return c
+        }
+    }
+    return letters[0]
+}
+'''
+
+
+
