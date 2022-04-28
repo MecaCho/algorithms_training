24f07bbc4e3776775b5908c1ae8720d54b3d54c4
MecaCho <1099833328@qq.com>
2022-04-28 22:20:14 +0800

    Update leetcode-905-SortArrayByParity.py
    Add 905. Sort Array By Parity solution

--- algorithms/arr/leetcode-905-SortArrayByParity.py
+++ algorithms/arr/leetcode-905-SortArrayByParity.py
@@ -1,23 +1,6 @@
+# encoding=utf8
+
 '''
-905. 按奇偶排序数组
-给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
-
-你可以返回满足此条件的任何数组作为答案。
-
- 
-
-示例：
-
-输入：[3,1,2,4]
-输出：[2,4,3,1]
-输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
- 
-
-提示：
-
-1 <= A.length <= 5000
-0 <= A[i] <= 5000
-
 905. Sort Array By Parity
 Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
 
@@ -33,6 +16,25 @@ The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
  
 
 Note:
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+
+905. 按奇偶排序数组
+给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
+
+你可以返回满足此条件的任何数组作为答案。
+
+ 
+
+示例：
+
+输入：[3,1,2,4]
+输出：[2,4,3,1]
+输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
+ 
+
+提示：
 
 1 <= A.length <= 5000
 0 <= A[i] <= 5000
@@ -55,3 +57,12 @@ class Solution(object):
             elif A[j] % 2 == 1:
                 j -= 1
         return A
+
+       
+class Solution(object):
+   def sortArrayByParity(self, nums):
+       """
+       :type nums: List[int]
+       :rtype: List[int]
+       """
+       return [num for num in nums if not num & 1 ] + [num  for num in nums if num & 1]
