81c68d42895fe4e054f495ca6f0f991dcdcab110
MecaCho <1099833328@qq.com>
2022-05-13 23:57:30 +0800

    Update leetcode-I0105-OneEditDistance.py
    Update OneEditDistance

--- crackingTheCodingI/leetcode-I0105-OneEditDistance.py
+++ crackingTheCodingI/leetcode-I0105-OneEditDistance.py
@@ -1,4 +1,26 @@
+# encoding=utf8
+
+
 '''
+OneEditDistance
+
+There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+
+ 
+
+Example 1:
+
+Input: 
+first = "pale"
+second = "ple"
+Output: True
+Example 2:
+
+Input: 
+first = "pales"
+second = "pal"
+Output: False
+
 面试题 01.05. 一次编辑
 字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。 给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。
 
