6984d587e3e855b5c54a0da7ef930a101d18402e
MecaCho <1099833328@qq.com>
2022-05-15 22:40:49 +0800

    Create leetcode-812-LargestTriangleArea.py
    Add 812. Largest Triangle Area

---
+++ algorithms/math_binary/leetcode-812-LargestTriangleArea.py
@@ -0,0 +1,40 @@
+# encoding=utf8
+
+'''
+812. Largest Triangle Area
+Given an array of points on the X-Y plane points where points[i] = [xi, yi], return the area of the largest triangle that can be formed by any three different points. Answers within 10-5 of the actual answer will be accepted.
+
+ 
+
+Example 1:
+
+
+Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+Output: 2.00000
+Explanation: The five points are shown in the above figure. The red triangle is the largest.
+Example 2:
+
+Input: points = [[1,0],[0,0],[0,1]]
+Output: 0.50000
+ 
+
+Constraints:
+
+3 <= points.length <= 50
+-50 <= xi, yi <= 50
+All the given points are unique.
+
+'''
+
+class Solution(object):
+    def largestTriangleArea(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: float
+        """
+        def triangleArea(x1, y1, x2, y2, x3, y3):
+            return abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2) / 2.0
+        return max(triangleArea(x1, y1, x2, y2, x3, y3) for (x1, y1), (x2, y2), (x3, y3) in combinations(points, 3))
+
+
+
