d429f0520d6ba9eca4ffd9d432200d277460be58
MecaCho <1099833328@qq.com>
2022-05-28 22:50:14 +0800

    Update leetcode-1021-RemoveOutermostParentheses.py
    Add 1021. Remove Outermost Parentheses solution

--- algorithms/stack/leetcode-1021-RemoveOutermostParentheses.py
+++ algorithms/stack/leetcode-1021-RemoveOutermostParentheses.py
@@ -1,4 +1,46 @@
+# encoding=utf8
+
 '''
+1021. Remove Outermost Parentheses
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation:
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation:
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation:
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+
+
+Note:
+
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
+
 1021. 删除最外层的括号
 有效括号字符串为空 ("")、"(" + A + ")" 或 A + B，其中 A 和 B 都是有效的括号字符串，+ 代表字符串的连接。例如，""，"()"，"(())()" 和 "(()(()))" 都是有效的括号字符串。
 
@@ -39,46 +81,6 @@ S.length <= 10000
 S[i] 为 "(" 或 ")"
 S 是一个有效括号字符串
 
-
-1021. Remove Outermost Parentheses
-A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
-
-A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
-
-Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
-
-Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
-
-
-
-Example 1:
-
-Input: "(()())(())"
-Output: "()()()"
-Explanation:
-The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
-After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
-Example 2:
-
-Input: "(()())(())(()(()))"
-Output: "()()()()(())"
-Explanation:
-The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
-After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
-Example 3:
-
-Input: "()()"
-Output: ""
-Explanation:
-The input string is "()()", with primitive decomposition "()" + "()".
-After removing outer parentheses of each part, this is "" + "" = "".
-
-
-Note:
-
-S.length <= 10000
-S[i] is "(" or ")"
-S is a valid parentheses string
 '''
 
 
@@ -105,4 +107,28 @@ class Solution(object):
                     res += stack[1:-1]
                     stack = ""
         return res
+
+    
+# solution1
+
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        left = 0
+        for i in range(len(s)):
+            if s[i] == "(":
+                if left > 0:
+                    res.append("(")
+                left += 1
+            if s[i] == ")":
+                left -= 1
+                if left > 0:
+                    res.append(")")
+
+        return "".join(res)
+
 
