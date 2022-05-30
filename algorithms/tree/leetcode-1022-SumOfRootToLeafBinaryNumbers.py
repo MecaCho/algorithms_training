9a81b92e46d5574d973a17f84f0f800ab23cbf59
MecaCho <1099833328@qq.com>
2022-05-30 23:08:18 +0800

    Create leetcode-1022-SumOfRootToLeafBinaryNumbers.py
    Add 1022. Sum of Root To Leaf Binary Numbers

---
+++ algorithms/tree/leetcode-1022-SumOfRootToLeafBinaryNumbers.py
@@ -0,0 +1,59 @@
+# encoding=utf8
+
+'''
+1022. Sum of Root To Leaf Binary Numbers
+You are given the root of a binary tree where each node has a value 0 or 1. Each root-to-leaf path represents a binary number starting with the most significant bit.
+
+For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf. Return the sum of these numbers.
+
+The test cases are generated so that the answer fits in a 32-bits integer.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+Example 2:
+
+Input: root = [0]
+Output: 0
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+Node.val is 0 or 1.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumRootToLeaf(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.sum = 0
+        def dfs(root, tmp):
+            if not root:
+                return
+            val = root.val
+            tmp *= 2
+            tmp += val
+            if not root.left and not root.right:
+                self.sum += tmp
+                return
+            dfs(root.left, tmp)
+            dfs(root.right, tmp)
+        dfs(root, 0)
+        return self.sum
+
+
