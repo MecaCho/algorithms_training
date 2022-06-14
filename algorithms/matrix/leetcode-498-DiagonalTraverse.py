8cdeb0dfe3cc4144dd836b21ff48e1d226ce3bb8
MecaCho <1099833328@qq.com>
2022-06-14 22:38:57 +0800

    Create leetcode-498-DiagonalTraverse.py
    Add 498. Diagonal Traverse

---
+++ algorithms/matrix/leetcode-498-DiagonalTraverse.py
@@ -0,0 +1,55 @@
+# encoding=utf8
+
+'''
+498. Diagonal Traverse
+Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+Example 2:
+
+Input: mat = [[1,2],[3,4]]
+Output: [1,2,3,4]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+-105 <= mat[i][j] <= 105
+
+'''
+
+class Solution(object):
+    def findDiagonalOrder(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[int]
+        """
+        ans = []
+        m, n = len(mat), len(mat[0])
+        for i in range(m + n - 1):
+            if i % 2:
+                x = 0 if i < n else i - n + 1
+                y = i if i < n else n - 1
+                while x < m and y >= 0:
+                    ans.append(mat[x][y])
+                    x += 1
+                    y -= 1
+            else:
+                x = i if i < m else m - 1
+                y = 0 if i < m else i - m + 1
+                while x >= 0 and y < n:
+                    ans.append(mat[x][y])
+                    x -= 1
+                    y += 1
+        return ans
+
+      
