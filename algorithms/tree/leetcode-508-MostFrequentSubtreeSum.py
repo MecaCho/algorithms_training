7cbfd4f354c65d150b126d15314520858f6ccb2d
MecaCho <1099833328@qq.com>
2022-06-19 22:40:52 +0800

    Update leetcode-508-MostFrequentSubtreeSum.py
    Update 508. Most Frequent Subtree Sum

--- algorithms/tree/leetcode-508-MostFrequentSubtreeSum.py
+++ algorithms/tree/leetcode-508-MostFrequentSubtreeSum.py
@@ -1,5 +1,25 @@
+# encoding=utf8
 
 '''
+508. Most Frequent Subtree Sum
+Given the root of a tree, you are asked to find the most frequent subtree sum. The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself). So what is the most frequent subtree sum value? If there is a tie, return all the values with the highest frequency in any order.
+
+Examples 1
+Input:
+
+  5
+ /  \
+2   -3
+return [2, -3, 4], since all the values happen only once, return all of them in any order.
+Examples 2
+Input:
+
+  5
+ /  \
+2   -5
+return [2], since 2 happens twice, however -5 only occur once.
+Note: You may assume the sum of values in any subtree is in the range of 32-bit signed integer.
+
 508. 出现次数最多的子树元素和
 给你一个二叉树的根结点，请你找出出现次数最多的子树元素和。一个结点的「子树元素和」定义为以该结点为根的二叉树上所有结点的元素之和（包括结点本身）。
 
@@ -26,25 +46,6 @@
 
 
 提示： 假设任意子树元素和均可以用 32 位有符号整数表示。
-
-508. Most Frequent Subtree Sum
-Given the root of a tree, you are asked to find the most frequent subtree sum. The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself). So what is the most frequent subtree sum value? If there is a tie, return all the values with the highest frequency in any order.
-
-Examples 1
-Input:
-
-  5
- /  \
-2   -3
-return [2, -3, 4], since all the values happen only once, return all of them in any order.
-Examples 2
-Input:
-
-  5
- /  \
-2   -5
-return [2], since 2 happens twice, however -5 only occur once.
-Note: You may assume the sum of values in any subtree is in the range of 32-bit signed integer.
 '''
 
 
@@ -84,3 +85,39 @@ class Solution(object):
         # print(self.vals)
         max_val = max(self.vals.values())
         return [k for k, v in self.vals.items() if v == max_val]
+      
+      
+      
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findFrequentTreeSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        self.vals = {}
+        def dfs(root):
+            if not root:
+                return 0
+
+            val = root.val
+            left_sum = dfs(root.left)
+            right_sum = dfs(root.right)
+
+            key = val+left_sum+right_sum
+            if key in self.vals:
+                self.vals[key] += 1
+            else:
+                self.vals[key] = 1
+            return key
+
+        dfs(root)
+        # print(self.vals)
+        max_val = max(self.vals.values())
+        return [k for k,v in self.vals.items() if v == max_val] 
+
