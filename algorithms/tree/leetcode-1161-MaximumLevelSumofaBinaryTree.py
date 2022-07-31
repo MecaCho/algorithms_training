095f098fcdd18140f961e08a2485648904eeeb41
MecaCho <1099833328@qq.com>
2022-07-31 22:40:02 +0800

    Update leetcode-1161-MaximumLevelSumofaBinaryTree.py

--- algorithms/tree/leetcode-1161-MaximumLevelSumofaBinaryTree.py
+++ algorithms/tree/leetcode-1161-MaximumLevelSumofaBinaryTree.py
@@ -58,4 +58,27 @@ class Solution:
         return res
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        vals = []
+        def dfs(root: TreeNode, depth: int) -> int:
+            if not root:
+                return
+            if depth >= len(vals):
+                vals.append(root.val)
+            else:
+                vals[depth] += root.val
+            dfs(root.left, depth+1)
+            dfs(root.right, depth+1)
+        dfs(root, 0)
+
+        return vals.index(max(vals)) + 1 
+
+
 
