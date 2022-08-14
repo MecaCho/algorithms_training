f8755a80ef538cbe07c64ac974a2b71b0de4f4a2
MecaCho <1099833328@qq.com>
2022-08-14 21:57:30 +0800

    Update leetcode-contest-186-1-5392-MaximumScoreAfterSplittingaString.py

--- contest/weekly-contest-186/leetcode-contest-186-1-5392-MaximumScoreAfterSplittingaString.py
+++ contest/weekly-contest-186/leetcode-contest-186-1-5392-MaximumScoreAfterSplittingaString.py
@@ -1,4 +1,41 @@
+# encoding=utf8
+
+
 '''
+5392. Maximum Score After Splitting a String
+Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+
+
+Example 1:
+
+Input: s = "011101"
+Output: 5
+Explanation:
+All possible ways of splitting s into two non-empty substrings are:
+left = "0" and right = "11101", score = 1 + 4 = 5
+left = "01" and right = "1101", score = 1 + 3 = 4
+left = "011" and right = "101", score = 1 + 2 = 3
+left = "0111" and right = "01", score = 1 + 1 = 2
+left = "01110" and right = "1", score = 2 + 1 = 3
+Example 2:
+
+Input: s = "00111"
+Output: 5
+Explanation: When left = "00" and right = "111", we get the maximum score = 2 + 3 = 5
+Example 3:
+
+Input: s = "1111"
+Output: 3
+
+
+Constraints:
+
+2 <= s.length <= 500
+The string s consists of characters '0' and '1' only.
+
 5392. 分割字符串的最大得分
 给你一个由若干 0 和 1 组成的字符串 s ，请你计算并返回将该字符串分割成两个 非空 子字符串（即 左 子字符串和 右 子字符串）所能获得的最大得分。
 
@@ -33,39 +70,7 @@
 2 <= s.length <= 500
 字符串 s 仅由字符 '0' 和 '1' 组成。
 
-5392. Maximum Score After Splitting a String
-Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
 
-The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
-
-
-
-Example 1:
-
-Input: s = "011101"
-Output: 5
-Explanation:
-All possible ways of splitting s into two non-empty substrings are:
-left = "0" and right = "11101", score = 1 + 4 = 5
-left = "01" and right = "1101", score = 1 + 3 = 4
-left = "011" and right = "101", score = 1 + 2 = 3
-left = "0111" and right = "01", score = 1 + 1 = 2
-left = "01110" and right = "1", score = 2 + 1 = 3
-Example 2:
-
-Input: s = "00111"
-Output: 5
-Explanation: When left = "00" and right = "111", we get the maximum score = 2 + 3 = 5
-Example 3:
-
-Input: s = "1111"
-Output: 3
-
-
-Constraints:
-
-2 <= s.length <= 500
-The string s consists of characters '0' and '1' only.
 '''
 
 
