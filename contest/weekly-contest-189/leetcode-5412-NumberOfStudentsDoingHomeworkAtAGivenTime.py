123961c5fa8cadcfb04a2581e325d5fb38fbfa31
MecaCho <1099833328@qq.com>
2022-08-19 19:43:30 +0800

    Update leetcode-5412-NumberOfStudentsDoingHomeworkAtAGivenTime.py
    Update 5412. Number of Students Doing Homework at a Given Time

--- contest/weekly-contest-189/leetcode-5412-NumberOfStudentsDoingHomeworkAtAGivenTime.py
+++ contest/weekly-contest-189/leetcode-5412-NumberOfStudentsDoingHomeworkAtAGivenTime.py
@@ -1,6 +1,37 @@
-
+# encoding=utf8
 
 '''
+1450. Number of Students Doing Homework at a Given Time
+Given two integer arrays startTime and endTime and given an integer queryTime.
+
+The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+
+Return the number of students doing their homework at time queryTime. More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
+ 
+
+Example 1:
+
+Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+Output: 1
+Explanation: We have 3 students where:
+The first student started doing homework at time 1 and finished at time 3 and wasn't doing anything at time 4.
+The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
+The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
+Example 2:
+
+Input: startTime = [4], endTime = [4], queryTime = 4
+Output: 1
+Explanation: The only student was doing their homework at the queryTime.
+ 
+
+Constraints:
+
+startTime.length == endTime.length
+1 <= startTime.length <= 100
+1 <= startTime[i] <= endTime[i] <= 1000
+1 <= queryTime <= 1000
+
 5412. 在既定时间做作业的学生人数
 给你两个整数数组 startTime（开始时间）和 endTime（结束时间），并指定一个整数 queryTime 作为查询时间。
 
@@ -88,6 +119,10 @@ startTime.length == endTime.length
 1 <= queryTime <= 1000
 '''
 
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
+
 
 class Solution(object):
     def busyStudent(self, startTime, endTime, queryTime):
@@ -97,10 +132,6 @@ class Solution(object):
         :type queryTime: int
         :rtype: int
         """
-        # times = []
-        # for i in range(len(startTime)):
-        #     times.append((startTime[i], endTime[i]))
-        # # print(times)
         count = 0
         for i in range(len(startTime)):
             if startTime[i] <= queryTime <= endTime[i]:
