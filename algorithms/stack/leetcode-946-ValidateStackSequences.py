b64e17de30852d20a49fbf706a2d317ded15cc2d
MecaCho <1099833328@qq.com>
2022-08-31 23:15:36 +0800

    Update leetcode-946-ValidateStackSequences.py
    Update 946. Validate Stack Sequences

--- algorithms/stack/leetcode-946-ValidateStackSequences.py
+++ algorithms/stack/leetcode-946-ValidateStackSequences.py
@@ -1,4 +1,35 @@
+# encoding=utf8
+
 '''
+946. Validate Stack Sequences
+Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+Output: true
+Explanation: We might do the following sequence:
+push(1), push(2), push(3), push(4),
+pop() -> 4,
+push(5),
+pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+Example 2:
+
+Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+Output: false
+Explanation: 1 cannot be popped before 2.
+ 
+
+Constraints:
+
+1 <= pushed.length <= 1000
+0 <= pushed[i] <= 1000
+All the elements of pushed are unique.
+popped.length == pushed.length
+popped is a permutation of pushed.
+
 946. 验证栈序列
 给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true；否则，返回 false 。
 
