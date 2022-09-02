6e7556a1b59b0beb145b898ec0fb561f20e5ca1c
MecaCho <1099833328@qq.com>
2022-09-02 22:43:26 +0800

    Update leetcode-687-LongestUnivaluePath.py
    Update 687. Longest Univalue Path

--- algorithms/tree/leetcode-687-LongestUnivaluePath.py
+++ algorithms/tree/leetcode-687-LongestUnivaluePath.py
@@ -1,4 +1,33 @@
+# encoding=utf8
+
 '''
+687. Longest Univalue Path
+Given the root of a binary tree, return the length of the longest path, where each node in the path has the same value. This path may or may not pass through the root.
+
+The length of the path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,5,1,1,null,5]
+Output: 2
+Explanation: The shown image shows that the longest path of the same value (i.e. 5).
+Example 2:
+
+
+Input: root = [1,4,5,4,4,null,5]
+Output: 2
+Explanation: The shown image shows that the longest path of the same value (i.e. 4).
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
+The depth of the tree will not exceed 1000.
+
 687. 最长同值路径
 给定一个二叉树，找到最长的路径，这个路径中的每个节点具有相同值。 这条路径可以经过也可以不经过根节点。
 
@@ -66,6 +95,30 @@ Output: 2
 
 Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
 '''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def dfs(root: Optional[TreeNode]):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            left1 = left + 1 if root.left and root.left.val == root.val else 0
+            right1 = right + 1 if root.right and root.right.val == root.val else 0
+            self.res = max(self.res, left1+right1)
+            return max(left1, right1)
+
+        dfs(root)
+        return self.res
+
+
 
 
 # Definition for a binary tree node.
