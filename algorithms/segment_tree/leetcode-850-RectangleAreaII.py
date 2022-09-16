4a190b9522032158481d41f850cd4a7e866a3345
MecaCho <1099833328@qq.com>
2022-09-16 23:49:35 +0800

    Create leetcode-850-RectangleAreaII.py
    Add 850. Rectangle Area II

---
+++ algorithms/segment_tree/leetcode-850-RectangleAreaII.py
@@ -0,0 +1,57 @@
+# encoding=utf8
+
+'''
+850. Rectangle Area II
+You are given a 2D array of axis-aligned rectangles. Each rectangle[i] = [xi1, yi1, xi2, yi2] denotes the ith rectangle where (xi1, yi1) are the coordinates of the bottom-left corner, and (xi2, yi2) are the coordinates of the top-right corner.
+
+Calculate the total area covered by all rectangles in the plane. Any area covered by two or more rectangles should only be counted once.
+
+Return the total area. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+
+Input: rectangles = [[0,0,2,2],[1,0,2,3],[1,0,3,1]]
+Output: 6
+Explanation: A total area of 6 is covered by all three rectangles, as illustrated in the picture.
+From (1,1) to (2,2), the green and red rectangles overlap.
+From (1,0) to (2,3), all three rectangles overlap.
+Example 2:
+
+Input: rectangles = [[0,0,1000000000,1000000000]]
+Output: 49
+Explanation: The answer is 1018 modulo (109 + 7), which is 49.
+ 
+
+Constraints:
+
+1 <= rectangles.length <= 200
+rectanges[i].length == 4
+0 <= xi1, yi1, xi2, yi2 <= 109
+
+'''
+
+class Solution:
+    def rectangleArea(self, rectangles: List[List[int]]) -> int:
+        def update(v, b, e):
+            l, r, m = v * 2, v * 2 + 1, (b + e) // 2
+            if y1 <= Y[b] and Y[e] <= y2: C[v] += c
+            else: y1 < Y[m] and update(l, b, m), Y[m] < y2 and update(r, m, e)
+            M[v] = C[v] and Y[e] - Y[b] or M[l] + M[r]
+
+        res, V, N = 0, 1, len(rectangles) * 2
+        C, M, L, Y = [0] * N * 8, [0] * N * 8, [], []
+        for x1, y1, x2, y2 in rectangles:
+            L += (x1, y1, y2, 1), (x2, y1, y2, -1)
+            Y += y1, y2
+        L.sort()
+        Y.sort()
+
+        for (x1, y1, y2, c), (x2, _, _, _) in itertools.pairwise(L):
+            update(V, 0, N - 1)
+            res += (x2 - x1) * M[V]
+        return res % (10 ** 9 + 7)
+
+
