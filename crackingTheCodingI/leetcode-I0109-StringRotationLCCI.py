d3fa4f0c90e73b4a74b6e04178a7697f6bc3986c
MecaCho <1099833328@qq.com>
2022-09-29 21:16:30 +0800

    Update leetcode-I0109-StringRotationLCCI.py
    update 01.09. String Rotation

--- crackingTheCodingI/leetcode-I0109-StringRotationLCCI.py
+++ crackingTheCodingI/leetcode-I0109-StringRotationLCCI.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 面试题 01.09. String Rotation LCCI
 Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 (e.g.,"waterbottle" is a rotation of"erbottlewat"). Can you use only one call to the method that checks if one word is a substring of another?
