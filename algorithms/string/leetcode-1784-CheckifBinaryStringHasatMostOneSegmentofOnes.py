103ecdf70f08d542815c5fd8f61df2aaf84fd20c
MecaCho <1099833328@qq.com>
2022-10-03 22:47:19 +0800

    Create leetcode-1784-CheckifBinaryStringHasatMostOneSegmentofOnes.py
    Add 1784. Check if Binary String Has at Most One Segment of Ones

---
+++ algorithms/string/leetcode-1784-CheckifBinaryStringHasatMostOneSegmentofOnes.py
@@ -0,0 +1,45 @@
+# encoding=utf8
+
+'''
+1784. Check if Binary String Has at Most One Segment of Ones
+Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+Example 2:
+
+Input: s = "110"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i]​​​​ is either '0' or '1'.
+s[0] is '1'.
+'''
+
+# golang solution
+
+'''
+func checkOnesSegment(s string) bool {
+	count := 0
+	pre := -1
+	for i := 0; i < len(s); i++ {
+		if s[i] == '1' {
+			if pre != i-1 {
+				return false
+			}
+			count++
+			pre = i
+		}
+	}
+	return true
+}
+'''
+
