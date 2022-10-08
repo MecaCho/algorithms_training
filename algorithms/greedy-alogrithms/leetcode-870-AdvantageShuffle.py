110502c781b261ee23b9fc3bb9c25e6da9811be0
MecaCho <1099833328@qq.com>
2022-10-08 23:07:29 +0800

    Create leetcode-870-AdvantageShuffle.py
    Add 870. Advantage Shuffle

---
+++ algorithms/greedy-alogrithms/leetcode-870-AdvantageShuffle.py
@@ -0,0 +1,46 @@
+# encoding=utf8
+
+'''
+870. Advantage Shuffle
+You are given two integer arrays nums1 and nums2 both of the same length. The advantage of nums1 with respect to nums2 is the number of indices i for which nums1[i] > nums2[i].
+
+Return any permutation of nums1 that maximizes its advantage with respect to nums2.
+
+ 
+
+Example 1:
+
+Input: nums1 = [2,7,11,15], nums2 = [1,10,4,11]
+Output: [2,11,7,15]
+Example 2:
+
+Input: nums1 = [12,24,8,32], nums2 = [13,25,32,11]
+Output: [24,32,8,12]
+ 
+
+Constraints:
+
+1 <= nums1.length <= 105
+nums2.length == nums1.length
+0 <= nums1[i], nums2[i] <= 109
+'''
+
+class Solution:
+    def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n = len(nums1)
+        idx1, idx2 = list(range(n)), list(range(n))
+        idx1.sort(key=lambda x: nums1[x])
+        idx2.sort(key=lambda x: nums2[x])
+
+        ans = [0] * n
+        left, right = 0, n - 1
+        for i in range(n):
+            if nums1[idx1[i]] > nums2[idx2[left]]:
+                ans[idx2[left]] = nums1[idx1[i]]
+                left += 1
+            else:
+                ans[idx2[right]] = nums1[idx1[i]]
+                right -= 1
+        
+        return ans
+
