2d6c0680c927257dbeb52042126e782f0ccf9082
MecaCho <1099833328@qq.com>
2022-10-09 14:44:49 +0800

    Create leetcode-856-ScoreofParentheses.py
    Add 856. Score of Parentheses

---
+++ algorithms/stack/leetcode-856-ScoreofParentheses.py
@@ -0,0 +1,47 @@
+# encoding=utf8
+
+'''
+856. Score of Parentheses
+Given a balanced parentheses string s, return the score of the string.
+
+The score of a balanced parentheses string is based on the following rule:
+
+"()" has score 1.
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: 1
+Example 2:
+
+Input: s = "(())"
+Output: 2
+Example 3:
+
+Input: s = "()()"
+Output: 2
+ 
+
+Constraints:
+
+2 <= s.length <= 50
+s consists of only '(' and ')'.
+s is a balanced parentheses string.
+'''
+
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        tmp = 0
+        res = 0
+        for i in range(len(s)):
+            if s[i] == "(":
+                tmp += 1
+            else:
+                tmp -= 1
+                if s[i-1] == "(":
+                    res += 2**tmp
+        return res
+
