0110f6f061e666b43f4bc83740b16d199f2f24bc
MecaCho <1099833328@qq.com>
2022-10-28 22:28:54 +0800

    Create leetcode-907-SumofSubarrayMinimums.py
    Add 907. Sum of Subarray Minimums

---
+++ algorithms/leetcode-907-SumofSubarrayMinimums.py
@@ -0,0 +1,44 @@
+# encoding=utf8
+
+'''
+907. Sum of Subarray Minimums
+Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: arr = [3,1,2,4]
+Output: 17
+Explanation: 
+Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
+Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+Sum is 17.
+Example 2:
+
+Input: arr = [11,81,94,43,3]
+Output: 444
+ 
+
+Constraints:
+
+1 <= arr.length <= 3 * 104
+1 <= arr[i] <= 3 * 104
+'''
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 1000000007
+        n = len(arr)
+        monoStack = []
+        dp = [0] * n
+        ans = 0
+        for i, x in enumerate(arr):
+            while monoStack and arr[monoStack[-1]] > x:
+                monoStack.pop()
+            k = i - monoStack[-1] if monoStack else i + 1
+            dp[i] = k * x + (dp[i - k] if monoStack else 0)
+            ans = (ans + dp[i]) % MOD
+            monoStack.append(i)
+        return ans
+
