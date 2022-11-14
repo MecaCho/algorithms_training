05311ddfc0280ecb4c1a6e02eae00fdd9b291e2c
MecaCho <1099833328@qq.com>
2022-11-14 23:22:14 +0800

    Update leetcode-805-SplitArrayWithSameAverage.py
    Update 805. Split Array With Same Average

--- algorithms/dynamic-programming/leetcode-805-SplitArrayWithSameAverage.py
+++ algorithms/dynamic-programming/leetcode-805-SplitArrayWithSameAverage.py
@@ -28,3 +28,42 @@ Constraints:
 1 <= nums.length <= 30
 0 <= nums[i] <= 104
 '''
+
+# tips
+
+'''
+动态规划 + 位运算优化。
+
+令 sum 和 n 分别为数组 nums 的和与元素个数，sumA 和 k 为数组 A 的和与元素个数。
+
+根据题目要求 sumA / k = (sum - sumA) / (n - k)，可推得：sumA = sum * k / n。
+
+那么我们可以将题目转化为：从 nums 中选 k 个数，使这 k 个数的和为 sum * k / n。
+
+我们可以用动态规划，令 dp[s][x] 表示是否能从 nums 中选 x 个数使它们的和为 s。
+
+那么状态转移关系为：如果能从 nums 中选 x 个数使它们的和为 s，那么可以再加上一个 nums 中的数 num，即从 nums 中选 x + 1 个数使它们的和为 s + num。
+
+if (dp[s][x] == true) 
+    dp[s + num][x + 1] = true;
+'''
+
+class Solution:
+    def splitArraySameAverage(self, nums: List[int]) -> bool:
+        n = len(nums)
+        m = n // 2
+        s = sum(nums)
+        if all(s * i % n for i in range(1, m + 1)):
+            return False
+
+        dp = [set() for _ in range(m + 1)]
+        dp[0].add(0)
+        for num in nums:
+            for i in range(m, 0, -1):
+                for x in dp[i - 1]:
+                    curr = x + num
+                    if curr * n == s * i:
+                        return True
+                    dp[i].add(curr)
+        return False
+
