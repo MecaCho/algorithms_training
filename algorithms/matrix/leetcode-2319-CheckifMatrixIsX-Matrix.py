9541c35219c64f498bcc090408f96edcb0eb150b
MecaCho <1099833328@qq.com>
2023-01-31 22:15:38 +0800

    Create leetcode-2319-CheckifMatrixIsX-Matrix.py
    Add 2319. Check if Matrix Is X-Matrix

---
+++ algorithms/matrix/leetcode-2319-CheckifMatrixIsX-Matrix.py
@@ -0,0 +1,51 @@
+# encoding=utf8
+
+'''
+2319. Check if Matrix Is X-Matrix
+A square matrix is said to be an X-Matrix if both of the following conditions hold:
+
+All the elements in the diagonals of the matrix are non-zero.
+All other elements are 0.
+Given a 2D integer array grid of size n x n representing a square matrix, return true if grid is an X-Matrix. Otherwise, return false.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[2,0,0,1],[0,3,1,0],[0,5,2,0],[4,0,0,2]]
+Output: true
+Explanation: Refer to the diagram above. 
+An X-Matrix should have the green elements (diagonals) be non-zero and the red elements be 0.
+Thus, grid is an X-Matrix.
+Example 2:
+
+
+Input: grid = [[5,7,0],[0,3,1],[0,5,0]]
+Output: false
+Explanation: Refer to the diagram above.
+An X-Matrix should have the green elements (diagonals) be non-zero and the red elements be 0.
+Thus, grid is not an X-Matrix.
+ 
+
+Constraints:
+
+n == grid.length == grid[i].length
+3 <= n <= 100
+0 <= grid[i][j] <= 105
+'''
+
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        row_len = len(grid)
+        col_len = len(grid[0])
+        corners = [(0, 0), (0, col_len-1), (row_len-1, 0), (row_len-1, col_len-1)]
+        for i in range(row_len):
+            for j in range(col_len):
+                if i == j or (i+j) == col_len-1:
+                    if grid[i][j] == 0:
+                        return False
+                else:
+                    if grid[i][j] != 0:
+                        return False
+        return True
