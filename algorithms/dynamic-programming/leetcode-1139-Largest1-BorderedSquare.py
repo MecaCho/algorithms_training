912cad6cbd0eb6d408e2a9ea6a26850436d4602a
MecaCho <1099833328@qq.com>
2023-02-18 08:33:14 +0800

    Update leetcode-1139-Largest1-BorderedSquare.py

--- algorithms/dynamic-programming/leetcode-1139-Largest1-BorderedSquare.py
+++ algorithms/dynamic-programming/leetcode-1139-Largest1-BorderedSquare.py
@@ -1,3 +1,5 @@
+#encoding=UTF8
+
 '''
 1139. 最大的以 1 为边界的正方形
 给你一个由若干 0 和 1 组成的二维网格 grid，请你找出边界全部由 1 组成的最大 正方形 子网格，并返回该子网格中的元素数量。如果不存在，则返回 0。
@@ -74,7 +76,24 @@ class Solution(object):
                         cur -= 1
         return max_val * max_val
 
-
+    
+class Solution:
+    def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        left = [[0] * (n + 1) for _ in range(m + 1)]
+        up = [[0] * (n + 1) for _ in range(m + 1)]
+        maxBorder = 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if grid[i - 1][j - 1]:
+                    left[i][j] = left[i][j - 1] + 1
+                    up[i][j] = up[i - 1][j] + 1
+                    border = min(left[i][j], up[i][j])
+                    while left[i - border + 1][j] < border or up[i][j - border + 1] < border:
+                        border -= 1
+                    maxBorder = max(maxBorder, border)
+        return maxBorder ** 2
+    
 # tips
 
 '''
