db85cd2d4a630845f3bdd8d2929285df763ec412
MecaCho <1099833328@qq.com>
2023-03-11 23:07:01 +0800

    Create leetcode-1705-FindLongestSubarrayLCCI.py
    Add 17.05. Find Longest Subarray LCCI

---
+++ algorithms/prefix_sum/leetcode-1705-FindLongestSubarrayLCCI.py
@@ -0,0 +1,22 @@
+# encoding=utf8
+
+'''
+面试题 17.05. Find Longest Subarray LCCI
+Given an array filled with letters and numbers, find the longest subarray with an equal number of letters and numbers.
+
+Return the subarray. If there are more than one answer, return the one which has the smallest index of its left endpoint. If there is no answer, return an empty arrary.
+
+Example 1:
+
+Input: ["A","1","B","C","D","2","3","4","E","5","F","G","6","7","H","I","J","K","L","M"]
+
+Output: ["A","1","B","C","D","2","3","4","E","5","F","G","6","7"]
+Example 2:
+
+Input: ["A","A"]
+
+Output: []
+Note:
+
+array.length <= 100000
+'''
