54858634fe37b92130d8e8dd1df26c5db77cb0fc
MecaCho <1099833328@qq.com>
2023-04-05 22:25:51 +0800

    Create leetcode-2427-NumberofCommonFactors.py
    Add 2427. Number of Common Factors

---
+++ algorithms/math_binary/leetcode-2427-NumberofCommonFactors.py
@@ -0,0 +1,41 @@
+# encoding=utf8
+
+'''
+leetcode-2427-NumberofCommonFactors.py
+2427. Number of Common Factors
+Given two positive integers a and b, return the number of common factors of a and b.
+
+An integer x is a common factor of a and b if x divides both a and b.
+
+ 
+
+Example 1:
+
+Input: a = 12, b = 6
+Output: 4
+Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
+Example 2:
+
+Input: a = 25, b = 30
+Output: 2
+Explanation: The common factors of 25 and 30 are 1, 5.
+ 
+
+Constraints:
+
+1 <= a, b <= 1000
+'''
+
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        c = gcd(a, b)
+        res = 0
+        x = 1
+        while x*x <= c:
+            if c % x == 0:
+                res += 1
+                if x*x != c:
+                    res += 1
+            x += 1
+        return res
+
