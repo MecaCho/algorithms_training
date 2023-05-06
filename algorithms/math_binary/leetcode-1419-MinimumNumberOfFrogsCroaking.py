6476717678b46f78fdc318d4cf0d6201e5e30552
MecaCho <1099833328@qq.com>
2023-05-06 22:27:27 +0800

    Update leetcode-1419-MinimumNumberOfFrogsCroaking.py
    Update 1419. Minimum Number of Frogs Croaking

--- algorithms/math_binary/leetcode-1419-MinimumNumberOfFrogsCroaking.py
+++ algorithms/math_binary/leetcode-1419-MinimumNumberOfFrogsCroaking.py
@@ -1,5 +1,40 @@
+# encoding=utf8
 
 '''
+1419. Minimum Number of Frogs Croaking
+Given the string croakOfFrogs, which represents a combination of the string "croak" from different frogs, that is, multiple frogs can croak at the same time, so multiple “croak” are mixed. Return the minimum number of different frogs to finish all the croak in the given string.
+
+A valid "croak" means a frog is printing 5 letters ‘c’, ’r’, ’o’, ’a’, ’k’ sequentially. The frogs have to print all five letters to finish a croak. If the given string is not a combination of valid "croak" return -1.
+
+
+
+Example 1:
+
+Input: croakOfFrogs = "croakcroak"
+Output: 1
+Explanation: One frog yelling "croak" twice.
+Example 2:
+
+Input: croakOfFrogs = "crcoakroak"
+Output: 2
+Explanation: The minimum number of frogs is two.
+The first frog could yell "crcoakroak".
+The second frog could yell later "crcoakroak".
+Example 3:
+
+Input: croakOfFrogs = "croakcrook"
+Output: -1
+Explanation: The given string is an invalid combination of "croak" from different frogs.
+Example 4:
+
+Input: croakOfFrogs = "croakcroa"
+Output: -1
+
+
+Constraints:
+
+1 <= croakOfFrogs.length <= 10^5
+All characters in the string are: 'c', 'r', 'o', 'a' or 'k'.
 
 1419. 数青蛙
 给你一个字符串 croakOfFrogs，它表示不同青蛙发出的蛙鸣声（字符串 "croak" ）的组合。由于同一时间可以有多只青蛙呱呱作响，所以 croakOfFrogs 中会混合多个 “croak” 。请你返回模拟字符串中所有蛙鸣所需不同青蛙的最少数目。
@@ -38,41 +73,31 @@
 1 <= croakOfFrogs.length <= 10^5
 字符串中的字符只有 'c', 'r', 'o', 'a' 或者 'k'
 
-1419. Minimum Number of Frogs Croaking
-Given the string croakOfFrogs, which represents a combination of the string "croak" from different frogs, that is, multiple frogs can croak at the same time, so multiple “croak” are mixed. Return the minimum number of different frogs to finish all the croak in the given string.
-
-A valid "croak" means a frog is printing 5 letters ‘c’, ’r’, ’o’, ’a’, ’k’ sequentially. The frogs have to print all five letters to finish a croak. If the given string is not a combination of valid "croak" return -1.
-
-
-
-Example 1:
-
-Input: croakOfFrogs = "croakcroak"
-Output: 1
-Explanation: One frog yelling "croak" twice.
-Example 2:
-
-Input: croakOfFrogs = "crcoakroak"
-Output: 2
-Explanation: The minimum number of frogs is two.
-The first frog could yell "crcoakroak".
-The second frog could yell later "crcoakroak".
-Example 3:
-
-Input: croakOfFrogs = "croakcrook"
-Output: -1
-Explanation: The given string is an invalid combination of "croak" from different frogs.
-Example 4:
-
-Input: croakOfFrogs = "croakcroa"
-Output: -1
-
-
-Constraints:
-
-1 <= croakOfFrogs.length <= 10^5
-All characters in the string are: 'c', 'r', 'o', 'a' or 'k'.
 '''
+
+class Solution:
+    def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
+        c, r, o, a, k = 0,0,0,0,0
+        res = 0
+        for ch in croakOfFrogs:
+            if ch == "c":
+                c += 1
+            elif ch == "r":
+                r += 1
+            elif ch == "o":
+                o += 1
+            elif ch == "a":
+                a += 1
+            elif ch == "k":
+                k += 1
+            # print(c,r,o,a,k)
+            res = max(res, c-k)
+            if c < r or r < o or o < a or a < k:
+                return -1
+        if c == r and r == o and o == a and a == k:
+            return res
+        return -1
+
 
 
 
