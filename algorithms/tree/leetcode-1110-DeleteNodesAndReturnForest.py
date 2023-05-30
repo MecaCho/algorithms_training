dd2567caa17738c5a78e27ded3769f2d630658eb
MecaCho <1099833328@qq.com>
2023-05-30 22:40:49 +0800

    Update leetcode-1110-DeleteNodesAndReturnForest.py
    Update 1110. Delete Nodes And Return Forest

--- algorithms/tree/leetcode-1110-DeleteNodesAndReturnForest.py
+++ algorithms/tree/leetcode-1110-DeleteNodesAndReturnForest.py
@@ -1,34 +1,7 @@
 
-
-
-
-
+# encoding=utf8
 
 '''
-1110. 删点成林
-给出二叉树的根节点 root，树上每个节点都有一个不同的值。
-
-如果节点值在 to_delete 中出现，我们就把该节点从树上删去，最后得到一个森林（一些不相交的树构成的集合）。
-
-返回森林中的每棵树。你可以按任意顺序组织答案。
-
-
-
-示例：
-
-
-
-输入：root = [1,2,3,4,5,6,7], to_delete = [3,5]
-输出：[[1,2,null,4],[6],[7]]
-
-
-提示：
-
-树中的节点数最大为 1000。
-每个节点都有一个介于 1 到 1000 之间的值，且各不相同。
-to_delete.length <= 1000
-to_delete 包含一些从 1 到 1000、各不相同的值。
-
 
 1110. Delete Nodes And Return Forest
 Given the root of a binary tree, each node in the tree has a distinct value.
@@ -53,6 +26,30 @@ The number of nodes in the given tree is at most 1000.
 Each node has a distinct value between 1 and 1000.
 to_delete.length <= 1000
 to_delete contains distinct values between 1 and 1000.
+
+1110. 删点成林
+给出二叉树的根节点 root，树上每个节点都有一个不同的值。
+
+如果节点值在 to_delete 中出现，我们就把该节点从树上删去，最后得到一个森林（一些不相交的树构成的集合）。
+
+返回森林中的每棵树。你可以按任意顺序组织答案。
+
+
+
+示例：
+
+
+
+输入：root = [1,2,3,4,5,6,7], to_delete = [3,5]
+输出：[[1,2,null,4],[6],[7]]
+
+
+提示：
+
+树中的节点数最大为 1000。
+每个节点都有一个介于 1 到 1000 之间的值，且各不相同。
+to_delete.length <= 1000
+to_delete 包含一些从 1 到 1000、各不相同的值。
 '''
 
 
@@ -87,7 +84,3 @@ class Solution(object):
 
         dfs(root, 1)
         return self.vals
-
-
-'''
-'''
