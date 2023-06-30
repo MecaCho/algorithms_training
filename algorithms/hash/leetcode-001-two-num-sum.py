d099b65cc77fcf3244e070145115c21ad8536f57
MecaCho <1099833328@qq.com>
2023-07-01 00:45:56 +0800

    Update leetcode-001-two-num-sum.py

--- algorithms/hash/leetcode-001-two-num-sum.py
+++ algorithms/hash/leetcode-001-two-num-sum.py
@@ -1,18 +1,6 @@
 # encoding=utf8
 
 '''
-1. 两数之和
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-
-示例:
-
-给定 nums = [2, 7, 11, 15], target = 9
-
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
-
 1. Two Sum
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
@@ -24,6 +12,18 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+1. 两数之和
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
 '''
 
 
