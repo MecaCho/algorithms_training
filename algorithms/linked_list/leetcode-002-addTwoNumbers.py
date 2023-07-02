2f6bcf19dd9cae31fd040e6913c7104e8cc05a17
MecaCho <1099833328@qq.com>
2023-07-02 22:32:35 +0800

    Update leetcode-002-addTwoNumbers.py

--- algorithms/linked_list/leetcode-002-addTwoNumbers.py
+++ algorithms/linked_list/leetcode-002-addTwoNumbers.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 2. Add Two Numbers
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
@@ -24,6 +26,28 @@ Explanation: 342 + 465 = 807.
 输出：7 -> 0 -> 8
 原因：342 + 465 = 807
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = ListNode(0)
+        pre = 0
+        res = cur
+        while l1 or l2 or pre:
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
+            sum_ = pre + num1 + num2
+            pre = sum_ // 10
+            cur.next = ListNode(sum_%10)
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return  res.next
+
 
 
 
