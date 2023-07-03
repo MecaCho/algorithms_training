965c9f772683e63ad235eebbd706939273d48c09
MecaCho <1099833328@qq.com>
2023-07-03 21:25:21 +0800

    Update leetcode-jz-445-addTwoSumII.py
    Update 445. Add Two Numbers II

--- jianzhi/leetcode-jz-445-addTwoSumII.py
+++ jianzhi/leetcode-jz-445-addTwoSumII.py
@@ -1,4 +1,37 @@
+# encoding=utf8
+
 '''
+445. Add Two Numbers II
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
+Example 2:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+Example 3:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ 
+
+Follow up: Could you solve it without reversing the input lists?
+
 445. 两数相加 II
 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
 
@@ -17,6 +50,33 @@
 输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 输出：7 -> 8 -> 0 -> 7
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        vals1, vals2 = [], []
+        while l1:
+            vals1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            vals2.append(l2.val)
+            l2 = l2.next
+        pre = 0
+        cur = None
+        while vals1 or vals2 or pre:
+            val1 = vals1.pop() if vals1 else 0
+            val2 = vals2.pop() if vals2 else 0
+            sum_ = val1 + val2 + pre
+            pre = sum_ // 10
+            node = ListNode(sum_%10)
+            node.next = cur
+            cur = node
+        return cur
+
 
 
 
