07a24f3d2892918100be72f748ab72649dd5953b
MecaCho <1099833328@qq.com>
2023-09-04 22:07:22 +0800

    Update leetcode-449-SerializeAndDeserializeBST.py

--- algorithms/tree/leetcode-449-SerializeAndDeserializeBST.py
+++ algorithms/tree/leetcode-449-SerializeAndDeserializeBST.py
@@ -26,3 +26,47 @@ The number of nodes in the tree is in the range [0, 104].
 0 <= Node.val <= 104
 The input tree is guaranteed to be a binary search tree.
 '''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        arr = []
+        def postOrder(root: TreeNode) -> None:
+            if root is None:
+                return
+            postOrder(root.left)
+            postOrder(root.right)
+            arr.append(root.val)
+        postOrder(root)
+        return ' '.join(map(str, arr))
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        arr = list(map(int, data.split()))
+        def construct(lower: int, upper: int) -> TreeNode:
+            if arr == [] or arr[-1] < lower or arr[-1] > upper:
+                return None
+            val = arr.pop()
+            root = TreeNode(val)
+            root.right = construct(val, upper)
+            root.left = construct(lower, val)
+            return root
+        return construct(-inf, inf)
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
+
