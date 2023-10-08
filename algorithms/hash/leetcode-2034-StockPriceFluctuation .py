d1b6daf4819f25a02d24e4d48f71dfe5943b780c
MecaCho <1099833328@qq.com>
2023-10-08 22:45:39 +0800

    Update leetcode-2034-StockPriceFluctuation .py
    Update 2034. Stock Price Fluctuation

--- algorithms/hash/leetcode-2034-StockPriceFluctuation .py
+++ algorithms/hash/leetcode-2034-StockPriceFluctuation .py
@@ -104,3 +104,40 @@ class StockPrice(object):
 # param_4 = obj.minimum()
 
 
+
+
+class StockPrice:
+
+    def __init__(self):
+        import sortedcontainers
+        self.prices = sortedcontainers.SortedList()
+        self.prices_map = {}
+        self.max_time = 0
+
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp in self.prices_map:
+            self.prices.discard(self.prices_map[timestamp])
+        self.prices.add(price)
+
+        self.prices_map[timestamp] = price
+        self.max_time = max(self.max_time, timestamp)
+
+
+    def current(self) -> int:
+        return self.prices_map[self.max_time]
+
+    def maximum(self) -> int:
+        return self.prices[-1]
+
+    def minimum(self) -> int:
+        return self.prices[0]
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
+
+
