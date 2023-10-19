41a3aaeb527c75d661b89b5c91cc5364bda49286
MecaCho <1099833328@qq.com>
2023-10-19 13:30:52 +0800

    Update leetcode-1726-TuplewithSameProduct.py

--- algorithms/math_binary/leetcode-1726-TuplewithSameProduct.py
+++ algorithms/math_binary/leetcode-1726-TuplewithSameProduct.py
@@ -32,3 +32,14 @@ Constraints:
 1 <= nums[i] <= 104
 All elements in nums are distinct.
 '''
+
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        cnt = Counter([nums[i] * nums[j] for i in range(n) for j in range(i+1, n)])
+        res = 0
+        for v in cnt.values():
+            res += v * (v-1) * 4
+        return res
+
+
