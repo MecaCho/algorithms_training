e5a96c2572dd7fde2a0fa0f5337ba3132182a002
MecaCho <1099833328@qq.com>
2023-11-26 22:07:30 +0800

    Update leetcode-828-CountUniqueCharactersofAllSubstringsofaGivenString.py

--- algorithms/hash/leetcode-828-CountUniqueCharactersofAllSubstringsofaGivenString.py
+++ algorithms/hash/leetcode-828-CountUniqueCharactersofAllSubstringsofaGivenString.py
@@ -35,3 +35,19 @@ Constraints:
 1 <= s.length <= 105
 s consists of uppercase English letters only.
 '''
+
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+        index = collections.defaultdict(list)
+        for i, c in enumerate(s):
+            index[c].append(i)
+
+        res = 0
+        for arr in index.values():
+            arr = [-1] + arr + [len(s)]
+            for i in range(1, len(arr) - 1):
+                res += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i])
+        return res
+
+
+
