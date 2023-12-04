ffe1636b205bc3e4fdebf6d3db6734de3144c49f
MecaCho <1099833328@qq.com>
2023-12-04 21:28:25 +0800

    Update leetcode-1038-BinarySearchTreetoGreaterSumTree.py

--- algorithms/tree/leetcode-1038-BinarySearchTreetoGreaterSumTree.py
+++ algorithms/tree/leetcode-1038-BinarySearchTreetoGreaterSumTree.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 1038. Binary Search Tree to Greater Sum Tree
 Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
