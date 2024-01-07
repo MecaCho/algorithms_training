e32229b49cb1cfc3d7fc73e8d956f0f277191d8f
wenqi <1099833328@qq.com>
2024-01-07 22:27:39 +0800

    Update
    Signed-off-by: wenqi <1099833328@qq.com>

--- algorithms/hash/leetcode-383-RansomNote.py
+++ algorithms/hash/leetcode-383-RansomNote.py
@@ -42,3 +42,11 @@ class Solution(object):
                 return False
         return True
 
+
+
+
+
+
+class Solution:
+        def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+                    return Counter(ransomNote) <= Counter(magazine)
