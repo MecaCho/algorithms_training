0c2c9559c091c12fa0076f84f500327a05b6c54c
wenqi <1099833328@qq.com>
2024-01-14 16:12:59 +0800

    Add linked list duplated items deleted method
    Signed-off-by: wenqi <1099833328@qq.com>

--- algorithms/linked_list/leetcode-83-deleteDuplicates.py
+++ algorithms/linked_list/leetcode-83-deleteDuplicates.py
@@ -105,6 +105,21 @@ class Solution20210326(object):
 
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+        def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+                res = ListNode(0, head)
+                   
+                           while head and head.next:
+                                       if head.val == head.next.val:
+                                                       head.next = head.next.next
+                                                                   else: 
+                                                                                   head = head.next
+                                                                                           return res.next
 # solutions
 
 '''
