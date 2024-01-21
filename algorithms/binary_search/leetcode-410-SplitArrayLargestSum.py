e4a7d290725c1bc8285b2c07ec90428c5e363fd0
MecaCho <1099833328@qq.com>
2024-01-21 21:38:42 +0800

    Update leetcode-410-SplitArrayLargestSum.py

--- algorithms/binary_search/leetcode-410-SplitArrayLargestSum.py
+++ algorithms/binary_search/leetcode-410-SplitArrayLargestSum.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 410. Split Array Largest Sum
 Given an array which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays. Write an algorithm to minimize the largest sum among these m subarrays.
