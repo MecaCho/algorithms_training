d8ce976b6797d48ce61c92babcaead71596e1ac5
MecaCho <1099833328@qq.com>
2024-02-09 22:41:49 +0800

    Update leetcode-236-lowestCommonAncestor.py

--- algorithms/tree/leetcode-236-lowestCommonAncestor.py
+++ algorithms/tree/leetcode-236-lowestCommonAncestor.py
@@ -1,4 +1,4 @@
-
+# encoding=utf8
 
 '''
 236. Lowest Common Ancestor of a Binary Tree
@@ -116,7 +116,26 @@ class Solution(object):
         tarvalsal_tree(root, p, q)
         return self.ans
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left:
+            return right
+        if not right:
+            return left
+        return root
+
+        
 '''
 方法一：递归
 思路和算法
