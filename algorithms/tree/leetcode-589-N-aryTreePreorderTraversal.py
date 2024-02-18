eafcd55af452852020490ab97676f58185d6579e
MecaCho <1099833328@qq.com>
2024-02-18 23:54:46 +0800

    Update leetcode-589-N-aryTreePreorderTraversal.py
    Update 589. N-ary Tree Preorder Traversal

--- algorithms/tree/leetcode-589-N-aryTreePreorderTraversal.py
+++ algorithms/tree/leetcode-589-N-aryTreePreorderTraversal.py
@@ -57,3 +57,23 @@ class Solution(object):
         return res
             
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        def dfs(node: 'Node'):
+            if node is None:
+                return
+            res.append(node.val)
+            for ch in node.children:
+                dfs(ch)
+        dfs(root)
+        return res
+        
