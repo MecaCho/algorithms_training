d1b11cd946449cf3537a39e3ace1650479ebfe58
MecaCho <1099833328@qq.com>
2024-03-04 23:13:56 +0800

    Update leetcode-232-ImplementQueueUsingStacks.py

--- algorithms/stack/leetcode-232-ImplementQueueUsingStacks.py
+++ algorithms/stack/leetcode-232-ImplementQueueUsingStacks.py
@@ -166,6 +166,41 @@ class MyQueue20210305(object):
 # param_3 = obj.peek()
 # param_4 = obj.empty()
 
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if self.stack2:
+            return self.stack2.pop()
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if self.stack2:
+            return self.stack2[-1]
+        else:
+            return self.stack1[0] if self.stack1 else None
+
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
 # solutions
 
 '''
