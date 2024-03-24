2f67aa7684b4e564e7c762fa67957099007ed7ec
MecaCho <1099833328@qq.com>
2024-03-24 14:18:51 +0800

    Update leetcode-322-coinChange.py

--- algorithms/leetcode-322-coinChange.py
+++ algorithms/leetcode-322-coinChange.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 322. Coin Change
 You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
@@ -44,4 +46,19 @@ class Solution(object):
             for coin in coins:
                 if i-coin >= 0:
                     dp[i] = min(dp[i-coin] + 1, dp[i])
+        return dp[amount] if dp[amount] != float("inf") else -1
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        n = len(coins)
+        dp = [float("inf")]*(amount+1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount+1):
+                # print(coin, amount)
+                dp[i] = min(dp[i], dp[i-coin] + 1)
+        print(dp)
         return dp[amount] if dp[amount] != float("inf") else -1
