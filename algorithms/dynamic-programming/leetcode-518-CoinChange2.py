7ebc47271e75a2b0ba8fbdd7cdee49c57ba87472
MecaCho <1099833328@qq.com>
2024-03-25 22:55:43 +0800

    Update leetcode-518-CoinChange2.py
    Update 518. Coin Change 2

--- algorithms/dynamic-programming/leetcode-518-CoinChange2.py
+++ algorithms/dynamic-programming/leetcode-518-CoinChange2.py
@@ -1,3 +1,4 @@
+# encoding=utf8
 
 '''
 
@@ -86,6 +87,20 @@ class Solution(object):
                     dp[i] = dp[i-coin] + dp[i]
         # print(dp)
         return dp[-1]
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount+1)
+        # for coin in coins:
+        #     dp[coin] = 1
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] += dp[i-coin]
+        # print(dp)
+        return dp[amount]
+
 
 '''
 
