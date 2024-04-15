a526e6df728972f538a7a717a4bf9d5c2e34029a
MecaCho <1099833328@qq.com>
2024-04-15 22:15:40 +0800

    Update leetcode-706-DesignHashMap.py
    Update 706. Design HashMap

--- algorithms/hash/leetcode-706-DesignHashMap.py
+++ algorithms/hash/leetcode-706-DesignHashMap.py
@@ -129,6 +129,27 @@ class MyHashMap(object):
 
 
 
+class MyHashMap:
+
+    def __init__(self):
+        self.values = [-1] * 1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.values[key] = value
+
+    def get(self, key: int) -> int:
+        return self.values[key]
+
+    def remove(self, key: int) -> None:
+        self.values[key] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+
 # solutions
 
 '''
