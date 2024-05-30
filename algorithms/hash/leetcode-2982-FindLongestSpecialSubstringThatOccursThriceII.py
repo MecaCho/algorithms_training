96ee75b658dc8742e28db1994af0ac40b75ed114
MecaCho <1099833328@qq.com>
2024-05-30 20:15:33 +0800

    Create leetcode-2982-FindLongestSpecialSubstringThatOccursThriceII.py
    Add 2982. Find Longest Special Substring That Occurs Thrice II

---
+++ algorithms/hash/leetcode-2982-FindLongestSpecialSubstringThatOccursThriceII.py
@@ -0,0 +1,55 @@
+# encoding=utf8
+
+'''
+2982. Find Longest Special Substring That Occurs Thrice II
+
+You are given a string s that consists of lowercase English letters.
+
+A string is called special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
+
+Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring occurs at least thrice.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: s = "aaaa"
+Output: 2
+Explanation: The longest special substring which occurs thrice is "aa": substrings "aaaa", "aaaa", and "aaaa".
+It can be shown that the maximum length achievable is 2.
+Example 2:
+
+Input: s = "abcdef"
+Output: -1
+Explanation: There exists no special substring which occurs at least thrice. Hence return -1.
+Example 3:
+
+Input: s = "abcaba"
+Output: 1
+Explanation: The longest special substring which occurs thrice is "a": substrings "abcaba", "abcaba", and "abcaba".
+It can be shown that the maximum length achievable is 1.
+ 
+
+Constraints:
+
+3 <= s.length <= 5 * 105
+s consists of only lowercase English letters.
+'''
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        cnts = defaultdict(list)
+        cnt = 0
+        for i, ch in enumerate(s):
+            cnt += 1
+            if (i == len(s) - 1) or ch != s[i+1]:
+                cnts[ch].append(cnt)
+                cnt = 0
+        res = 0
+        for cnt in cnts.values():
+            cnt.sort(reverse=True)
+            cnt.extend([0, 0])
+            res = max(res, cnt[0]-2, min(cnt[0]-1, cnt[1]), cnt[2])
+        return res if res else -1
