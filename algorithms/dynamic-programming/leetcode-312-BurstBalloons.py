6fa2a1b736272bf67875d68f7637247ee01014ab
wenqi <1099833328@qq.com>
2024-06-09 22:33:35 +0800

    Update

--- algorithms/dynamic-programming/leetcode-312-BurstBalloons.py
+++ algorithms/dynamic-programming/leetcode-312-BurstBalloons.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 312. Burst Balloons
 Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
@@ -52,3 +54,20 @@ class Solution(object):
                     dp[left][right] = max(dp[left][right], nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right])
         return dp[0][n - 1]
 
+
+
+
+class Solution:
+        def maxCoins(self, nums: List[int]) -> int:
+
+                    nums = [1] + [i for i in nums if i > 0] + [1]
+                            n = len(nums)
+                                    dp = [[0]*n for _ in range(n)]
+
+                                            for k in range(2, n):
+                                                            for left in range(0, n - k):
+                                                                                right = left + k
+                                                                                                for i in range(left + 1,right):
+                                                                                                                        dp[left][right] = max(dp[left][right],
+                                                                                                                                                        nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right])
+                                                                                                                                return dp[0][n - 1]
