573c2fef71d101bc1169f707ca78ef43d27e8e05
wenqi <1099833328@qq.com>
2024-06-30 20:09:37 +0800

    Update

--- algorithms/dynamic-programming/leetcode-494-TargetSum.py
+++ algorithms/dynamic-programming/leetcode-494-TargetSum.py
@@ -96,3 +96,27 @@ class Solution(object):
             for j in range(sum_real, num-1, -1):
                 dp[j] += dp[j-num]
         return dp[sum_real]
+
+    '''
+    func findTargetSumWays(nums []int, target int) int {
+        sum := 0
+            for _, v := range nums {
+                    sum += v
+                        }
+                            diff := sum - target
+                                if diff < 0 || diff%2 == 1 {
+                                        return 0
+                                            }
+                                                neg := diff / 2
+                                                    dp := make([]int, neg+1)
+                                                        dp[0] = 1
+                                                            for _, num := range nums {
+                                                                    for j := neg; j >= num; j-- {
+                                                                                dp[j] += dp[j-num]
+                                                                                        }
+                                                                                            }
+                                                                                                return dp[neg]
+
+
+                                                                                                }
+                                                                                                '''
