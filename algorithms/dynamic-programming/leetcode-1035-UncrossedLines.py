5908878085495bd9ca004585a0910c9518c7f039
wenqi <1099833328@qq.com>
2024-08-11 23:40:06 +0800

    Update

--- algorithms/dynamic-programming/leetcode-1035-UncrossedLines.py
+++ algorithms/dynamic-programming/leetcode-1035-UncrossedLines.py
@@ -202,4 +202,22 @@ func max(a, b int) int {
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
 
+class Solution(object):
+        def maxUncrossedLines(self, nums1, nums2):
+                    """
+                            :type nums1: List[int]
+                                    :type nums2: List[int]
+                                            :rtype: int
+                                                    """
+                                                            m, n = len(nums1), len(nums2)
+                                                                    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
 
+                                                                            for i in range(1, m+1):
+                                                                                            for j in range(1, n+1):
+                                                                                                                if nums1[i-1] == nums2[j-1]:
+                                                                                                                                        dp[i][j] = dp[i-1][j-1] + 1
+                                                                                                                                                        else:
+                                                                                                                                                                                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                                                                                                                                                                                            
+                                                                                                                                                                                                    # print(dp)
+                                                                                                                                                                                                            return dp[-1][-1]
