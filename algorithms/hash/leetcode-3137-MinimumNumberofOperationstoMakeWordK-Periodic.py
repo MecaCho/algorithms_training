1df9e443dfaf3662255237e1a9020e0b643e036b
MecaCho <1099833328@qq.com>
2024-08-17 21:29:58 +0800

    Update leetcode-3137-MinimumNumberofOperationstoMakeWordK-Periodic.py

--- algorithms/hash/leetcode-3137-MinimumNumberofOperationstoMakeWordK-Periodic.py
+++ algorithms/hash/leetcode-3137-MinimumNumberofOperationstoMakeWordK-Periodic.py
@@ -49,3 +49,10 @@ Constraints:
 k divides word.length.
 word consists only of lowercase English letters.
 '''
+
+class Solution:
+    def minimumOperationsToMakeKPeriodic(self, word: str, k: int) -> int:
+        n = len(word)
+        cnt = Counter(word[i:i+k] for i in range(0, n, k))
+        return n//k - max(cnt.values())
+
