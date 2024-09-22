ddb7da73dc1e189eaa24ddefc97ca9f571009243
MecaCho <1099833328@qq.com>
2024-09-23 01:06:09 +0800

    Update leetcode-1014-BestSightseeingPair.py

--- algorithms/arr/leetcode-1014-BestSightseeingPair.py
+++ algorithms/arr/leetcode-1014-BestSightseeingPair.py
@@ -1,25 +1,6 @@
+# encoding=utf8
+
 '''
-1014. 最佳观光组合
-给定正整数数组 A，A[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的距离为 j - i。
-
-一对景点（i < j）组成的观光组合的得分为（A[i] + A[j] + i - j）：景点的评分之和减去它们两者之间的距离。
-
-返回一对观光景点能取得的最高分。
-
-
-
-示例：
-
-输入：[8,1,5,2,6]
-输出：11
-解释：i = 0, j = 2, A[i] + A[j] + i - j = 8 + 5 + 0 - 2 = 11
-
-
-提示：
-
-2 <= A.length <= 50000
-1 <= A[i] <= 1000
-
 1014. Best Sightseeing Pair
 Given an array A of positive integers, A[i] represents the value of the i-th sightseeing spot, and two sightseeing spots i and j have distance j - i between them.
 
@@ -40,6 +21,27 @@ Note:
 
 2 <= A.length <= 50000
 1 <= A[i] <= 1000
+
+1014. 最佳观光组合
+给定正整数数组 A，A[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的距离为 j - i。
+
+一对景点（i < j）组成的观光组合的得分为（A[i] + A[j] + i - j）：景点的评分之和减去它们两者之间的距离。
+
+返回一对观光景点能取得的最高分。
+
+
+
+示例：
+
+输入：[8,1,5,2,6]
+输出：11
+解释：i = 0, j = 2, A[i] + A[j] + i - j = 8 + 5 + 0 - 2 = 11
+
+
+提示：
+
+2 <= A.length <= 50000
+1 <= A[i] <= 1000
 '''
 
 class Solution(object):
@@ -55,6 +57,15 @@ class Solution(object):
             max_val = max(max_val, pre_max+A[i]-i)
             pre_max = max(pre_max, A[i]+i)
         return max_val
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        res = 0
+        pre_max = values[0] 
+        for i in range(1, len(values)):
+            res = max(res, pre_max+values[i]-i)
+            pre_max = max(pre_max, values[i]+i)
+        return res
 
 
 # tips
