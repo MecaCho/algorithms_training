067cb286316a775f838fa2dc764c2c586ca44e79
MecaCho <1099833328@qq.com>
2025-01-27 22:52:49 +0800

    Update leetcode-45-JumpGameII.py

--- algorithms/greedy-alogrithms/leetcode-45-JumpGameII.py
+++ algorithms/greedy-alogrithms/leetcode-45-JumpGameII.py
@@ -1,22 +1,6 @@
+# encoding=utf8
 
 '''
-45. 跳跃游戏 II
-给定一个非负整数数组，你最初位于数组的第一个位置。
-
-数组中的每个元素代表你在该位置可以跳跃的最大长度。
-
-你的目标是使用最少的跳跃次数到达数组的最后一个位置。
-
-示例:
-
-输入: [2,3,1,1,4]
-输出: 2
-解释: 跳到最后一个位置的最小跳跃数是 2。
-     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
-说明:
-
-假设你总是可以到达数组的最后一个位置。
-
 45. Jump Game II
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
@@ -33,8 +17,24 @@ Explanation: The minimum number of jumps to reach the last index is 2.
 Note:
 
 You can assume that you can always reach the last index.
-'''
 
+45. 跳跃游戏 II
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+示例:
+
+输入: [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+说明:
+
+假设你总是可以到达数组的最后一个位置。
+'''
 
 
 class Solution(object):
@@ -68,7 +68,6 @@ class Solution(object):
         #     # max_step = max(max_step, dp[i])
         # # print(dp)
         # return dp[len(nums)-1]
-
 
 '''
 解题思路
