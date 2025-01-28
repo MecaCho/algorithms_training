c1a42d75398abe8db75b561fe037fc13a15d788e
MecaCho <1099833328@qq.com>
2025-01-28 22:03:28 +0800

    Update leetcode-119-PascalTriangleII.py

--- algorithms/arr/leetcode-119-PascalTriangleII.py
+++ algorithms/arr/leetcode-119-PascalTriangleII.py
@@ -1,23 +1,6 @@
 # encoding=utf8
 
 '''
-
-119. 杨辉三角 II
-给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
-
-
-
-在杨辉三角中，每个数是它左上方和右上方的数的和。
-
-示例:
-
-输入: 3
-输出: [1,3,3,1]
-进阶：
-
-你可以优化你的算法到 O(k) 空间复杂度吗？
-
-
 119. Pascal's Triangle II
 Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
 
@@ -33,6 +16,21 @@ Output: [1,3,3,1]
 Follow up:
 
 Could you optimize your algorithm to use only O(k) extra space?
+
+119. 杨辉三角 II
+给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 3
+输出: [1,3,3,1]
+进阶：
+
+你可以优化你的算法到 O(k) 空间复杂度吗？
 '''
 
 
