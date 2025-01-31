25748b89cf7946c366a728249af30e44a23300c9
MecaCho <1099833328@qq.com>
2025-01-31 23:21:39 +0800

    Update leetcode_541_reverseStr.py

--- algorithms/old/leetcode_541_reverseStr/leetcode_541_reverseStr.py
+++ algorithms/old/leetcode_541_reverseStr/leetcode_541_reverseStr.py
@@ -2,9 +2,10 @@
 
 '''
 541. Reverse String II
+
 Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
 
-If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
 
  
 
@@ -85,3 +86,5 @@ class Solution(object):
 if __name__ == '__main__':
     demo = Solution()
     print demo.reverseStr("abcdefgh", 3)
+
+
