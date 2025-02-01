bc4f7c9b58308028220cf22c5dc1523ee24a852e
MecaCho <1099833328@qq.com>
2025-02-01 23:33:02 +0800

    Update leetcode-81-SearchinRotatedSortedArrayII.py

--- algorithms/binary_search/leetcode-81-SearchinRotatedSortedArrayII.py
+++ algorithms/binary_search/leetcode-81-SearchinRotatedSortedArrayII.py
@@ -8,7 +8,9 @@ Before being passed to your function, nums is rotated at an unknown pivot index 
 
 Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
 
+You must decrease the overall operation steps as much as possible.
 
+ 
 
 Example 1:
 
@@ -18,7 +20,7 @@ Example 2:
 
 Input: nums = [2,5,6,0,0,1,2], target = 3
 Output: false
-
+ 
 
 Constraints:
 
@@ -26,10 +28,9 @@ Constraints:
 -104 <= nums[i] <= 104
 nums is guaranteed to be rotated at some pivot.
 -104 <= target <= 104
+ 
 
-
-Follow up: This problem is the same as Search in Rotated Sorted Array, where nums may contain duplicates. Would this affect the runtime complexity? How and why?
-
+Follow up: This problem is similar to Search in Rotated Sorted Array, but nums may contain duplicates. Would this affect the runtime complexity? How and why?
 
 
 81. 搜索旋转排序数组 II
@@ -67,12 +68,9 @@ Follow up: This problem is the same as Search in Rotated Sorted Array, where num
 
 
 
-# 对于数组中有重复元素的情况，二分查找时可能会有 a[l]=a[\textit{mid}]=a[r]a[l]=a[mid]=a[r]，此时无法判断区间 [l,\textit{mid}][l,mid] 和区间 [\textit{
-# mid}+1,r][mid+1,r] 哪个是有序的。
-#
-# 例如 \textit{nums}=[3,1,2,3,3,3,3]nums=[3,1,2,3,3,3,3]，\textit{target}=2target=2，首次二分时无法判断区间 [0,3][0,3] 和区间 [4,6][4,
-# 6] 哪个是有序的。
-#
+# 对于数组中有重复元素的情况，二分查找时可能会有 a[l]=a[mid]=a[r]，此时无法判断区间 [l,mid] 和区间 [mid+1,r] 哪个是有序的。
+
+# 例如nums=[3,1,2,3,3,3,3]，target=2，首次二分时无法判断区间 [0,3][0,3] 和区间 [4,6][4,6] 哪个是有序的。
 # 对于这种情况，我们只能将当前二分区间的左边界加一，右边界减一，然后在新区间上继续二分查找。
 
 
