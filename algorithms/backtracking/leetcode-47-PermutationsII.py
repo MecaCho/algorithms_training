6d670db1705329fb78f593f186c75840893fc000
MecaCho <1099833328@qq.com>
2025-02-06 23:50:01 +0800

    Update leetcode-47-PermutationsII.py

--- algorithms/backtracking/leetcode-47-PermutationsII.py
+++ algorithms/backtracking/leetcode-47-PermutationsII.py
@@ -1,4 +1,30 @@
+# encoding=utf8
+
 '''
+47. Permutations II
+
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 8
+-10 <= nums[i] <= 10
+
 47. 全排列 II
 给定一个可包含重复数字的序列，返回所有不重复的全排列。
 
@@ -12,18 +38,6 @@
   [2,1,1]
 ]
 
-47. Permutations II
-Given a collection of numbers that might contain duplicates, return all possible unique permutations.
-
-Example:
-
-Input: [1,1,2]
-Output:
-[
-  [1,1,2],
-  [1,2,1],
-  [2,1,1]
-]
 '''
 
 
