392da1475dcdfe16fad3ccdf7b5657930af82e59
wenqi <1099833328@qq.com>
2025-02-13 23:07:52 +0800

    update

--- algorithms/hash/leetcode-1742-MaximumNumberofBallsinaBox.py
+++ algorithms/hash/leetcode-1742-MaximumNumberofBallsinaBox.py
@@ -57,4 +57,8 @@ class Solution:
             result = max(result, r)
         return result
 
+class Solution:
+     def countBalls(self, lowLimit: int, highLimit: int) -> int:                
+        count = Counter(sum(map(int, str(i))) for i in range(lowLimit, highLimit + 1))
+        return max(count.values())
 
