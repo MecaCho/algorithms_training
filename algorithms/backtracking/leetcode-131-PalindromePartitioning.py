b7f75cd41f2f5e71c1c3adbc029cc67e4cc98811
MecaCho <1099833328@qq.com>
2025-03-01 23:25:42 +0800

    Update leetcode-131-PalindromePartitioning.py

--- algorithms/backtracking/leetcode-131-PalindromePartitioning.py
+++ algorithms/backtracking/leetcode-131-PalindromePartitioning.py
@@ -38,20 +38,6 @@ s contains only lowercase English letters.
   ["aa","b"],
   ["a","a","b"]
 ]
-
- 131. Palindrome Partitioning
-Given a string s, partition s such that every substring of the partition is a palindrome.
-
-Return all possible palindrome partitioning of s.
-
-Example:
-
-Input: "aab"
-Output:
-[
-  ["aa","b"],
-  ["a","a","b"]
-]
 '''
 
 
