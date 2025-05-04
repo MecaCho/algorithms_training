4c6b8dee3312ef53407e90c5c65ebca0b8c6cd4f
MecaCho <1099833328@qq.com>
2025-05-04 23:40:08 +0800

    Update leetcode-838.py
    Add 1007. Minimum Domino Rotations For Equal Row

--- algorithms/dynamic-programming/leetcode-838.py
+++ algorithms/dynamic-programming/leetcode-838.py
@@ -1,37 +1,38 @@
-There are n dominoes in a line, and we place each domino vertically upright. In the beginning, we simultaneously push some of the dominoes either to the left or to the right.
+# encoding=utf8
 
-After each second, each domino that is falling to the left pushes the adjacent domino on the left. Similarly, the dominoes falling to the right push their adjacent dominoes standing on the right.
+'''
+1007. Minimum Domino Rotations For Equal Row
 
-When a vertical domino has dominoes falling on it from both sides, it stays still due to the balance of the forces.
+In a row of dominoes, tops[i] and bottoms[i] represent the top and bottom halves of the ith domino. (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
 
-For the purposes of this question, we will consider that a falling domino expends no additional force to a falling or already fallen domino.
+We may rotate the ith domino, so that tops[i] and bottoms[i] swap values.
 
-You are given a string dominoes representing the initial state where:
+Return the minimum number of rotations so that all the values in tops are the same, or all the values in bottoms are the same.
 
-    dominoes[i] = 'L', if the ith domino has been pushed to the left,
-    dominoes[i] = 'R', if the ith domino has been pushed to the right, and
-    dominoes[i] = '.', if the ith domino has not been pushed.
-    Return a string representing the final state.
+If it cannot be done, return -1.
 
-     
+ 
 
-     Example 1:
-
-         Input: dominoes = "RR.L"
-         Output: "RR.L"
-         Explanation: The first domino expends no additional force on the second domino.
-         Example 2:
+Example 1:
 
 
-             Input: dominoes = ".L.R...LR..L.."
-             Output: "LL.RR.LLRRLL.."
-              
+Input: tops = [2,1,2,4,2,2], bottoms = [5,2,6,2,3,2]
+Output: 2
+Explanation: 
+The first figure represents the dominoes as given by tops and bottoms: before we do any rotations.
+If we rotate the second and fourth dominoes, we can make every value in the top row equal to 2, as indicated by the second figure.
+Example 2:
 
-              Constraints:
+Input: tops = [3,5,1,2,3], bottoms = [3,6,3,3,4]
+Output: -1
+Explanation: 
+In this case, it is not possible to rotate the dominoes to make one row of values equal.
+ 
 
-                  n == dominoes.length
-                  1 <= n <= 105
-                  dominoes[i] is either 'L', 'R', or '.'.
+Constraints:
 
-
+2 <= tops.length <= 2 * 104
+bottoms.length == tops.length
+1 <= tops[i], bottoms[i] <= 6
+'''
 
