0cdc818bfbd3e675cd77d4c9d581ab64de199103
MecaCho <1099833328@qq.com>
2025-05-17 22:14:58 +0800

    Update leetcode-75-SortColors.py

--- algorithms/sort/leetcode-75-SortColors.py
+++ algorithms/sort/leetcode-75-SortColors.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 75. Sort Colors
 Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
