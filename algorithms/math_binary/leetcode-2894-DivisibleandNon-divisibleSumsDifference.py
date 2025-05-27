b8645ee8ca5c3728d67f99e9c512afa743097a7f
MecaCho <1099833328@qq.com>
2025-05-27 22:10:43 +0800

    Update leetcode-2894-DivisibleandNon-divisibleSumsDifference.py

--- algorithms/math_binary/leetcode-2894-DivisibleandNon-divisibleSumsDifference.py
+++ algorithms/math_binary/leetcode-2894-DivisibleandNon-divisibleSumsDifference.py
@@ -43,3 +43,8 @@ Constraints:
 
 1 <= n, m <= 1000
 '''
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        return n * (n + 1) // 2 - n // m * (n // m + 1) * m
+        
