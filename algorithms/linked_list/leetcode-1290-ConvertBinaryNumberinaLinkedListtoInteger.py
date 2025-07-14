082507a3445dc2ff80004be6aae863441916fc9e
MecaCho <1099833328@qq.com>
2025-07-14 21:24:05 +0800

    Update leetcode-1290-ConvertBinaryNumberinaLinkedListtoInteger.py
    Update 1290. Convert Binary Number in a Linked List to Integer

--- algorithms/linked_list/leetcode-1290-ConvertBinaryNumberinaLinkedListtoInteger.py
+++ algorithms/linked_list/leetcode-1290-ConvertBinaryNumberinaLinkedListtoInteger.py
@@ -28,4 +28,48 @@ Constraints:
 The Linked List is not empty.
 Number of nodes will not exceed 30.
 Each node's value is either 0 or 1.
+
+1290. 二进制链表转整数
+
+给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+
+请你返回该链表所表示数字的 十进制值 。
+
+最高位 在链表的头部。
+
+ 
+
+示例 1：
+
+
+
+输入：head = [1,0,1]
+输出：5
+解释：二进制数 (101) 转化为十进制数 (5)
+示例 2：
+
+输入：head = [0]
+输出：0
+ 
+
+提示：
+
+链表不为空。
+链表的结点总数不超过 30。
+每个结点的值不是 0 就是 1。
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        res = 0
+        while head:
+            res = 2*res + head.val
+            head = head.next
+        return res
+
+        
