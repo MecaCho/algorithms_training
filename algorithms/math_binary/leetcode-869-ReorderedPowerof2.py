9bce501ea0c826812f228ef6b88d843a974b2704
MecaCho <1099833328@qq.com>
2025-08-10 21:06:32 +0800

    Update leetcode-869-ReorderedPowerof2.py

--- algorithms/math_binary/leetcode-869-ReorderedPowerof2.py
+++ algorithms/math_binary/leetcode-869-ReorderedPowerof2.py
@@ -87,4 +87,9 @@ class Solution(object):
 
         return countDigits(n) in powerOf2Digits
 
+pow_two_sorted_str_set = {''.join(sorted(str(1 << i))) for i in range(30)}
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        s = ''.join(sorted(str(n)))
+        return s in pow_two_sorted_str_set
 
