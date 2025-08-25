6ba7dacaca60dd0857e834057cfe74ba05c60a06
MecaCho <1099833328@qq.com>
2025-08-25 23:26:35 +0800

    Update leetcode-498-DiagonalTraverse.py
    update 498. Diagonal Traverse

--- algorithms/matrix/leetcode-498-DiagonalTraverse.py
+++ algorithms/matrix/leetcode-498-DiagonalTraverse.py
@@ -2,6 +2,7 @@
 
 '''
 498. Diagonal Traverse
+
 Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
 
  
@@ -25,6 +26,30 @@ n == mat[i].length
 1 <= m * n <= 104
 -105 <= mat[i][j] <= 105
 
+498. 对角线遍历
+
+给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
+
+ 
+
+示例 1：
+
+
+输入：mat = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[1,2,4,7,5,3,6,8,9]
+示例 2：
+
+输入：mat = [[1,2],[3,4]]
+输出：[1,2,3,4]
+ 
+
+提示：
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+-105 <= mat[i][j] <= 105
 '''
 
 class Solution(object):
