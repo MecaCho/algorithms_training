2f9a144d93ba323ec900dfab3b10c7f85f5acb63
MecaCho <1099833328@qq.com>
2025-09-25 00:30:03 +0800

    Update leetcode-120-Triangle.py

--- algorithms/dynamic-programming/leetcode-120-Triangle.py
+++ algorithms/dynamic-programming/leetcode-120-Triangle.py
@@ -1,6 +1,8 @@
-'''
+# encoding=utf8
 
+'''
 120. Triangle
+
 Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
 
 For example, given the following triangle
@@ -18,6 +20,7 @@ Note:
 Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
 
 120. 三角形最小路径和
+
 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
 
 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
@@ -41,8 +44,13 @@ Bonus point if you are able to do this using only O(n) extra space, where n is t
 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
 '''
 
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(i + 1):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
 
-# 动态规划
 
 class Solution(object):
     def minimumTotal(self, triangle):
