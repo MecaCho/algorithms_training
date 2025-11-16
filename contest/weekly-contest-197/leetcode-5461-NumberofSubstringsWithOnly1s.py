daa64e24b4047b3d06081b0d7624fbd78d338229
MecaCho <1099833328@qq.com>
2025-11-16 19:32:05 +0800

    Update leetcode-5461-NumberofSubstringsWithOnly1s.py

--- contest/weekly-contest-197/leetcode-5461-NumberofSubstringsWithOnly1s.py
+++ contest/weekly-contest-197/leetcode-5461-NumberofSubstringsWithOnly1s.py
@@ -38,14 +38,15 @@ s[i] == '0' or s[i] == '1'
 1 <= s.length <= 10^5
 
 
-5461. 仅含 1 的子串数
+1513. 仅含 1 的子串数
+
 给你一个二进制字符串 s（仅由 '0' 和 '1' 组成的字符串）。
 
 返回所有字符都为 1 的子字符串的数目。
 
 由于答案可能很大，请你将它对 10^9 + 7 取模后返回。
 
-
+ 
 
 示例 1：
 
@@ -55,48 +56,45 @@ s[i] == '0' or s[i] == '1'
 "1" -> 5 次
 "11" -> 3 次
 "111" -> 1 次
+
 示例 2：
 
 输入：s = "101"
 输出：2
 解释：子字符串 "1" 在 s 中共出现 2 次
+
 示例 3：
 
 输入：s = "111111"
 输出：21
 解释：每个子字符串都仅由 '1' 组成
+
 示例 4：
 
 输入：s = "000"
 输出：0
 
+ 
 
 提示：
 
-s[i] == '0' 或 s[i] == '1'
-1 <= s.length <= 10^5
+    s[i] == '0' 或 s[i] == '1'
+    1 <= s.length <= 10^5
 '''
 
 
-
-class Solution(object):
-    def numSub(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # def get_num(n):
-        #     return n * (n+1) / 2
-        one_list = [i for i in re.split("0*", s) if i]
-
-        res = 0
-        # print(one_list)
-        for l in one_list:
-            num = len(l)
-            res += num * (num+1)/2
-            res %= 10**9 + 7
-        return res
-
+class Solution:
+    def numSub(self, s: str) -> int:
+        MOD = 1_000_000_007
+        ans = 0
+        last0 = -1
+        for i, ch in enumerate(s):
+            if ch == '0':
+                last0 = i  
+            else:
+                ans += i - last0  
+        return ans % MOD
+        
 # tips
 
 '''
