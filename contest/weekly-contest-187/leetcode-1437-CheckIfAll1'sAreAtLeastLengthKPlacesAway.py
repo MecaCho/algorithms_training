584a8f2e69e17d0f9ae6a6f4e9b02e37bb5ff787
MecaCho <1099833328@qq.com>
2025-11-17 14:05:03 +0800

    Update leetcode-1437-CheckIfAll1'sAreAtLeastLengthKPlacesAway.py

--- contest/weekly-contest-187/leetcode-1437-CheckIfAll1'sAreAtLeastLengthKPlacesAway.py
+++ contest/weekly-contest-187/leetcode-1437-CheckIfAll1'sAreAtLeastLengthKPlacesAway.py
@@ -1,3 +1,5 @@
+# encoding=utf8
+
 '''
 1437. 是否所有 1 都至少相隔 k 个元素
 给你一个由若干 0 和 1 组成的数组 nums 以及整数 k。如果所有 1 都至少相隔 k 个元素，则返回 True ；否则，返回 False 。
@@ -70,25 +72,16 @@ Constraints:
 nums[i] is 0 or 1
 '''
 
-
-
-
-class Solution(object):
-    def kLengthApart(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
         start = None
-        min_k = float("inf")
         for i in range(len(nums)):
             if nums[i] == 1:
                 if start is not None:
-                    min_k = min(min_k, i - start - 1)
+                    if i - start <= k:
+                        return False
                 start = i
-        # print(min_k)
-        return min_k >= k
+        return True
 
 # tips
 
