d785bab9bffb64bdcf9b95f0c2fc7c5fa1c2760c
wenqi <1099833328@qq.com>
2026-01-25 23:37:35 +0800

    update

--- algorithms/sort/leetcode-1984-MinimumDifferenceBetweenHighestandLowestofKScores.py
+++ algorithms/sort/leetcode-1984-MinimumDifferenceBetweenHighestandLowestofKScores.py
@@ -37,14 +37,5 @@ Constraints:
 0 <= nums[i] <= 105
 '''
 
-class Solution(object):
-    def minimumDifference(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        nums.sort()
-        return min([nums[i+k-1] - nums[i] for i in range(len(nums)-k+1)])
 
       
