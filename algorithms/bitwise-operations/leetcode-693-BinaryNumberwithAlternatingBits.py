211865cefdd05da91cbbcc997ff8a898cf6a3f5e
MecaCho <1099833328@qq.com>
2026-02-18 23:34:27 +0800

    Update leetcode-693-BinaryNumberwithAlternatingBits.py

--- algorithms/bitwise-operations/leetcode-693-BinaryNumberwithAlternatingBits.py
+++ algorithms/bitwise-operations/leetcode-693-BinaryNumberwithAlternatingBits.py
@@ -2,6 +2,7 @@
 
 '''
 693. Binary Number with Alternating Bits
+
 Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
 
  
@@ -26,6 +27,38 @@ Explanation: The binary representation of 11 is: 1011.
 Constraints:
 
 1 <= n <= 231 - 1
+
+693. 交替位二进制数
+
+给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+
+ 
+
+示例 1：
+
+输入：n = 5
+输出：true
+解释：5 的二进制表示是：101
+
+示例 2：
+
+输入：n = 7
+输出：false
+解释：7 的二进制表示是：111.
+
+示例 3：
+
+输入：n = 11
+输出：false
+解释：11 的二进制表示是：1011.
+
+ 
+
+提示：
+
+    1 <= n <= 231 - 1
+
+
 '''
 
 class Solution(object):
