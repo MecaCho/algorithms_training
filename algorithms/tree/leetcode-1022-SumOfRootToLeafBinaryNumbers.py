0dc30dfc5e554ec63d596d2ee03769caa7884bde
MecaCho <1099833328@qq.com>
2026-02-24 21:17:11 +0800

    Update leetcode-1022-SumOfRootToLeafBinaryNumbers.py

--- algorithms/tree/leetcode-1022-SumOfRootToLeafBinaryNumbers.py
+++ algorithms/tree/leetcode-1022-SumOfRootToLeafBinaryNumbers.py
@@ -2,6 +2,7 @@
 
 '''
 1022. Sum of Root To Leaf Binary Numbers
+
 You are given the root of a binary tree where each node has a value 0 or 1. Each root-to-leaf path represents a binary number starting with the most significant bit.
 
 For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
@@ -27,6 +28,36 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 1000].
 Node.val is 0 or 1.
+
+1022. 从根到叶的二进制数之和
+
+给出一棵二叉树，其上每个结点的值都是 0 或 1 。每一条从根到叶的路径都代表一个从最高有效位开始的二进制数。
+
+    例如，如果路径为 0 -> 1 -> 1 -> 0 -> 1，那么它表示二进制数 01101，也就是 13 。
+
+对树上的每一片叶子，我们都要找出从根到该叶子的路径所表示的数字。
+
+返回这些数字之和。题目数据保证答案是一个 32 位 整数。
+
+ 
+
+示例 1：
+
+输入：root = [1,0,1,0,1,0,1]
+输出：22
+解释：(100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+示例 2：
+
+输入：root = [0]
+输出：0
+
+ 
+
+提示：
+
+    树中的节点数在 [1, 1000] 范围内
+    Node.val 仅为 0 或 1 
 '''
 
 # Definition for a binary tree node.
