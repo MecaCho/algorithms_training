461e35bb406ba1e94ab939c75022d6be32425240
wenqi <1099833328@qq.com>
2026-06-04 23:30:05 +0800

    update

--- algorithms/old/leetcode-294-flip-game-can-win.py
+++ algorithms/old/leetcode-294-flip-game-can-win.py
@@ -11,7 +11,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        for i in range(0, len(s) - 1):
+        for i in xrange(0, len(s) - 1):
             if (s[i] == "+" and s[i + 1] == "+") and not self.canWin("".join([s[:i], "--", s[i + 2:]])):
                 return True
         return False
