ec9e0bc4b5409f1182b7a32d884cdbfe15a63119
MecaCho <1099833328@qq.com>
2026-06-28 23:39:35 +0800

    Update leetcode-1846-MaximumElementAfterDecreasingandRearranging.py

--- algorithms/sort/leetcode-1846-MaximumElementAfterDecreasingandRearranging.py
+++ algorithms/sort/leetcode-1846-MaximumElementAfterDecreasingandRearranging.py
@@ -90,6 +90,20 @@ arr 中最大元素为 3 。
 
 '''
 
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        n = len(arr)
+        cnt = [0] * (n + 1)
+        for x in arr:
+            cnt[min(x, n)] += 1
+
+        ans = 0
+        for x in range(1, n + 1):
+            ans = min(ans + cnt[x], x)
+        return ans
+        
+        
+
 # golang solutions
 
 '''
