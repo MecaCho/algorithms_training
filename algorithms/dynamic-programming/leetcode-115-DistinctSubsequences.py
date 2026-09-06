2528eab5db58746efca10ce27ac218d2da1ef2de
wenqi <1099833328@qq.com>
2026-09-06 22:08:26 +0800

    update

--- algorithms/dynamic-programming/leetcode-115-DistinctSubsequences.py
+++ algorithms/dynamic-programming/leetcode-115-DistinctSubsequences.py
@@ -175,92 +175,40 @@ class Solution {
 
 
 '''
-方法一：动态规划
-假设字符串 ss 和 tt 的长度分别为 mm 和 nn。如果 tt 是 ss 的子序列，则 ss 的长度一定大于或等于 tt 的长度，即只有当 m \ge nm≥n 时，tt 才可能是 ss 的子序列。如果 m<nm<n，则 tt 一定不是 ss 的子序列，因此直接返回 00。
 
-当 m \ge nm≥n 时，可以通过动态规划的方法计算在 ss 的子序列中 tt 出现的个数。
+---
 
-创建二维数组 \textit{dp}dp，其中 \textit{dp}[i][j]dp[i][j] 表示在 s[i:]s[i:] 的子序列中 t[j:]t[j:] 出现的个数。
+### 动态规划 (Dynamic Programming)
 
-上述表示中，s[i:]s[i:] 表示 ss 从下标 ii 到末尾的子字符串，t[j:]t[j:] 表示 tt 从下标 jj 到末尾的子字符串。
+设字符串 `s` 的长度为 $m$，字符串 `t` 的长度为 $n$。
 
-考虑动态规划的边界情况：
+#### 1. 状态定义
+设 `dp[i][j]` 表示：字符串 `s` 的前 `i` 个字符 `s[0..i-1]` 中，能构成字符串 `t` 的前 `j` 个字符 `t[0..j-1]` 的不同子序列个数。
 
-当 j=nj=n 时，t[j:]t[j:] 为空字符串，由于空字符串是任何字符串的子序列，因此对任意 0 \le i \le m0≤i≤m，有 \textit{dp}[i][n]=1dp[i][n]=1；
+#### 2. 边界条件
+- `dp[i][0] = 1`（对于任意 $0 \le i \le m$）：空字符串 `t` 是任意字符串前缀的子序列，且只有 1 种选取方式（什么都不选）。
+- `dp[0][j] = 0`（对于任意 $1 \le j \le n$）：空字符串 `s` 无法构成非空字符串 `t` 的子序列。
 
-当 i=mi=m 且 j<nj<n 时，s[i:]s[i:] 为空字符串，t[j:]t[j:] 为非空字符串，由于非空字符串不是空字符串的子序列，因此对任意 0 \le j<n0≤j<n，有 \textit{dp}[m][j]=0dp[m][j]=0。
+#### 3. 状态转移方程
+对于第 $i$ 个字符 $s[i-1]$ 和第 $j$ 个字符 $t[j-1]$：
 
-当 i<mi<m 且 j<nj<n 时，考虑 \textit{dp}[i][j]dp[i][j] 的计算：
+1. **若 $s[i-1] == t[j-1]$**：
+   当前字符匹配，此时有 2 种选择：
+   - 使用 $s[i-1]$ 匹配 $t[j-1]$：方案数加 $dp[i-1][j-1]$。
+   - 不使用 $s[i-1]$（丢弃该字符），继续用 $s[0..i-2]$ 匹配 $t[0..j-1]$：方案数加 $dp[i-1][j]$。
+   $$\text{dp}[i][j] = \text{dp}[i-1][j-1] + \text{dp}[i-1][j]$$
 
-当 s[i]=t[j]s[i]=t[j] 时，\textit{dp}[i][j]dp[i][j] 由两部分组成：
+2. **若 $s[i-1] \neq t[j-1]$**：
+   无法用 $s[i-1]$ 匹配 $t[j-1]$，只能舍弃 $s[i-1]$：
+   $$\text{dp}[i][j] = \text{dp}[i-1][j]$$
 
-如果 s[i]s[i] 和 t[j]t[j] 匹配，则考虑 t[j+1:]t[j+1:] 作为 s[i+1:]s[i+1:] 的子序列，子序列数为 \textit{dp}[i+1][j+1]dp[i+1][j+1]；
+---
 
-如果 s[i]s[i] 不和 t[j]t[j] 匹配，则考虑 t[j:]t[j:] 作为 s[i+1:]s[i+1:] 的子序列，子序列数为 \textit{dp}[i+1][j]dp[i+1][j]。
+### 复杂度分析
 
-因此当 s[i]=t[j]s[i]=t[j] 时，有 \textit{dp}[i][j]=\textit{dp}[i+1][j+1]+\textit{dp}[i+1][j]dp[i][j]=dp[i+1][j+1]+dp[i+1][j]。
+- **时间复杂度**：$\mathcal{O}(m \times n)$，其中 $m$ 和 $n$ 分别为 `s` 和 `t` 的长度。双重循环遍历状态矩阵。
+- **空间复杂度**：$\mathcal{O}(m \times n)$，需要维度的二维 DP 数组（可通过一维滚动数组进一步优化至 $\mathcal{O}(n)$）。
 
-当 s[i] \ne t[j]s[i] 
-
-​	
- =t[j] 时，s[i]s[i] 不能和 t[j]t[j] 匹配，因此只考虑 t[j:]t[j:] 作为 s[i+1:]s[i+1:] 的子序列，子序列数为 \textit{dp}[i+1][j]dp[i+1][j]。
-
-因此当 s[i] \ne t[j]s[i] 
-
-​	
- =t[j] 时，有 \textit{dp}[i][j]=\textit{dp}[i+1][j]dp[i][j]=dp[i+1][j]。
-
-由此可以得到如下状态转移方程：
-
-\textit{dp}[i][j] = \begin{cases} \textit{dp}[i+1][j+1]+\textit{dp}[i+1][j], & s[i]=t[j]\\ \textit{dp}[i+1][j], & s[i] \ne t[j] \end{cases}
-dp[i][j]={ 
-dp[i+1][j+1]+dp[i+1][j],
-dp[i+1][j],
-​	
-  
-s[i]=t[j]
-s[i] 
-
-​	
- =t[j]
-​	
- 
-
-最终计算得到 \textit{dp}[0][0]dp[0][0] 即为在 ss 的子序列中 tt 出现的个数。
-
-
-1 / 24
-
-JavaJavaScriptGolangPython3C++C
-
-class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        m, n = len(s), len(t)
-        if m < n:
-            return 0
-        
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m + 1):
-            dp[i][n] = 1
-        
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if s[i] == t[j]:
-                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
-                else:
-                    dp[i][j] = dp[i + 1][j]
-        
-        return dp[0][0]
-复杂度分析
-
-时间复杂度：O(mn)O(mn)，其中 mm 和 nn 分别是字符串 ss 和 tt 的长度。二维数组 \textit{dp}dp 有 m+1m+1 行和 n+1n+1 列，需要对 \textit{dp}dp 中的每个元素进行计算。
-
-空间复杂度：O(mn)O(mn)，其中 mm 和 nn 分别是字符串 ss 和 tt 的长度。创建了 m+1m+1 行 n+1n+1 列的二维数组 \textit{dp}dp。
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/distinct-subsequences/solution/bu-tong-de-zi-xu-lie-by-leetcode-solutio-urw3/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+---
 '''
-
 
