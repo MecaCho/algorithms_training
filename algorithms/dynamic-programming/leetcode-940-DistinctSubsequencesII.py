55dc13e67dfe9e2676a53d4ed46fe05f664c8a34
wenqi <1099833328@qq.com>
2026-09-07 23:42:54 +0800

    update

--- algorithms/dynamic-programming/leetcode-940-DistinctSubsequencesII.py
+++ algorithms/dynamic-programming/leetcode-940-DistinctSubsequencesII.py
@@ -1,10 +1,12 @@
 # encoding=utf8
 
+
 '''
 940. Distinct Subsequences II
 Given a string s, return the number of distinct non-empty subsequences of s. Since the answer may be very large, return it modulo 109 + 7.
 
 A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not.
+
  
 
 Example 1:
@@ -12,22 +14,33 @@ Example 1:
 Input: s = "abc"
 Output: 7
 Explanation: The 7 distinct subsequences are "a", "b", "c", "ab", "ac", "bc", and "abc".
+
 Example 2:
 
 Input: s = "aba"
 Output: 6
 Explanation: The 6 distinct subsequences are "a", "b", "ab", "aa", "ba", and "aba".
+
 Example 3:
 
 Input: s = "aaa"
 Output: 3
 Explanation: The 3 distinct subsequences are "a", "aa" and "aaa".
+
  
 
 Constraints:
 
-1 <= s.length <= 2000
-s consists of lowercase English letters.
+    1 <= s.length <= 2000
+    s consists of lowercase English letters.
 '''
 
 
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 10**9 + 7
+        last = [0] * 26
+        for ch in s:
+            idx = ord(ch) - ord('a')
+            last[idx] = (sum(last) + 1) % MOD
+        return sum(last) % MOD
